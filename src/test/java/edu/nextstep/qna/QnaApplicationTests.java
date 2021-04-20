74074b8403a82912a40ad5dbf8a0b3c0460b4375
포비 <pobi@woowahan.com>
2021-04-20 10:48:25 +0900

    initial commit

---
+++ src/test/java/edu/nextstep/qna/QnaApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.nextstep.qna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QnaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
