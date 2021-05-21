b4501794e46ddbd6ec291b24e938ac22b9c7d332
ted.kang <ted.kang@kakaocorp.com>
2021-05-22 04:33:02 +0900

    feature: BaseEntity 추가
    생성일, 수정일을 담는 abstract class 추가

---
+++ src/main/java/qna/domain/BaseEntity.java
@@ -0,0 +1,21 @@
+package qna.domain;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+  @Column(nullable = false)
+  @CreatedDate
+  LocalDateTime createdAt;
+
+  @LastModifiedDate
+  LocalDateTime updatedAt;
+
+}
