b62874c84e88a7b7b90734493e3fbb77a76a85b2
dhmin5693 <dhmin5693@naver.com>
2021-05-22 02:11:28 +0900

    Question entity id에 누락된 annotation 추가

--- src/main/java/qna/domain/Question.java
+++ src/main/java/qna/domain/Question.java
@@ -2,13 +2,16 @@ package qna.domain;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class Question extends BaseEntity{
+public class Question extends BaseEntity {
 
     @Id
-    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
     private Long id;
 
     @Column(length = 100)
