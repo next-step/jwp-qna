fcdf3e8315a31e3d3cafd425b28a7d3544645078
ted.kang <ted.kang@kakaocorp.com>
2021-05-24 23:00:08 +0900

    test: DeleteHistory 클래스 연관관계 매핑
    엔티티 변경에 따른 테스트 코드 변경 반영

--- src/test/java/qna/domain/DeleteHistoryRepositoryTest.java
+++ src/test/java/qna/domain/DeleteHistoryRepositoryTest.java
@@ -4,31 +4,41 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.time.LocalDateTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static qna.domain.ContentType.QUESTION;
+import static qna.domain.UserTest.JAVAJIGI;
 
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @DataJpaTest
 class DeleteHistoryRepositoryTest {
 
-  private final DeleteHistory givenHistory = new DeleteHistory(QUESTION, 1L, 1L, LocalDateTime.now());
-
   @Autowired
   private DeleteHistoryRepository deleteHistoryRepository;
+  @Autowired
+  private UserRepository userRepository;
 
-  private DeleteHistory saved;
+  private User savedUser;
 
   @BeforeEach
   void setUp() {
-    saved = deleteHistoryRepository.save(givenHistory);
+    User toSaveUser = new User("ted", "password", "name", "enemfk777@gmail.com");
+    savedUser = userRepository.save(toSaveUser);
   }
 
   @DisplayName("저장 후 반환 값은 원본과 같다.")
   @Test
   void saveTest() {
-    assertThat(saved).isEqualTo(givenHistory);
+    //given
+    DeleteHistory toSaveDeleteHistory = new DeleteHistory(QUESTION, 1L, savedUser, LocalDateTime.now());
+
+    //when & then
+    DeleteHistory saved = deleteHistoryRepository.save(toSaveDeleteHistory);
+    assertThat(saved).isEqualTo(toSaveDeleteHistory);
   }
 }
