7276efd1ca530d321eea2a09f133e64cf3d836d3
itdar <thesockerrr@gmail.com>
2021-05-25 01:58:03 +0900

    test: AnswerTest 추가
    - Answer 생성 후, save 테스트
    - 어플리케이션 실행하여 각 entity 클래스가 일단 동작하게끔 구현되었나 확인 테스트를 겸함

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,6 +1,32 @@
 package qna.domain;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest
 public class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @Autowired
+    AnswerRepository answerRepository;
+
+    @AfterEach
+    public void clear() {
+        answerRepository.deleteAll();
+    }
+
+    @Test
+    void save() {
+        Answer a1 = answerRepository.save(A1);
+
+        answerRepository.findById(a1.getId())
+                .orElseThrow(IllegalArgumentException::new);
+
+        assertThat(answerRepository.findById(a1.getId()).get()).isEqualTo(a1);
+    }
 }
