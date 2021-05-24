44366e7dc20e1f3189fd25db6fb5d4eac58d3ec0
etff <wish2rich@gmail.com>
2021-05-24 18:25:02 +0900

    refactor: 사용자 엔티티의 맵핑 정보를 추가한다

--- src/main/java/qna/domain/User.java
+++ src/main/java/qna/domain/User.java
@@ -2,18 +2,45 @@ package qna.domain;
 
 import qna.UnAuthorizedException;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Objects;
 
+/**
+ * 사용자.
+ */
+@Entity
 public class User {
     public static final GuestUser GUEST_USER = new GuestUser();
 
+    /**
+     * 사용자 식별자.
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    /**
+     * 사용자 식별자.
+     */
     private String userId;
+    /**
+     * 비밀 번호.
+     */
     private String password;
+    /**
+     * 사용자 이름.
+     */
     private String name;
+    /**
+     * 이메일.
+     */
     private String email;
 
-    private User() {
+    protected User() {
+        this(null, null, null, null);
     }
 
     public User(String userId, String password, String name, String email) {
@@ -28,6 +55,12 @@ public class User {
         this.email = email;
     }
 
+    /**
+     * 사용자 정보를 갱신한다.
+     *
+     * @param loginUser 로그인한 사용자.
+     * @param target    갱신할 사용자 정보.
+     */
     public void update(User loginUser, User target) {
         if (!matchUserId(loginUser.userId)) {
             throw new UnAuthorizedException();
@@ -45,10 +78,22 @@ public class User {
         return this.userId.equals(userId);
     }
 
+    /**
+     * 주어진 비밀번호와 입력받은 비밀번호를 비교하여 일치하면 true, 일치하지 않으면 false를 리턴한다.
+     *
+     * @param targetPassword 입력받은 비밃번호
+     * @return 비밀번호 일치 여부
+     */
     public boolean matchPassword(String targetPassword) {
         return this.password.equals(targetPassword);
     }
 
+    /**
+     * 이름과 이메일을 비교하여 일치하면 true, 아니라면 false를 리턴한다.
+     *
+     * @param target 입력받은 사용자 정보
+     * @return 이름과 이메일 일치여부
+     */
     public boolean equalsNameAndEmail(User target) {
         if (Objects.isNull(target)) {
             return false;
@@ -58,6 +103,9 @@ public class User {
                 email.equals(target.email);
     }
 
+    /**
+     * 게스트 사용자라면 true, 아니라면 false를 리턴한다.
+     */
     public boolean isGuestUser() {
         return false;
     }
