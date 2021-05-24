6640bfc97948b389f3dd4a01f067ad727ead9dc8
dhmin5693 <dhmin5693@naver.com>
2021-05-24 23:42:50 +0900

    `Answer` 의 `Question`을 ``@ManyToOne` 관계로 설정

--- src/main/java/qna/domain/Answer.java
+++ src/main/java/qna/domain/Answer.java
@@ -8,6 +8,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import qna.NotFoundException;
 import qna.UnAuthorizedException;
@@ -26,8 +27,9 @@ public class Answer extends BaseEntity implements Serializable {
     @JoinColumn(name = "writer_id")
     private User writer;
 
-    @Column
-    private Long questionId;
+    @ManyToOne
+    @JoinColumn(name = "question_id")
+    private Question question;
 
     @Column(columnDefinition = "LONGTEXT")
     private String contents;
@@ -53,7 +55,7 @@ public class Answer extends BaseEntity implements Serializable {
         }
 
         this.writer = writer;
-        this.questionId = question.getId();
+        this.question = question;
         this.contents = contents;
     }
 
@@ -62,7 +64,7 @@ public class Answer extends BaseEntity implements Serializable {
     }
 
     public void toQuestion(Question question) {
-        this.questionId = question.getId();
+        this.question = question;
     }
 
     public Long getId() {
@@ -81,12 +83,12 @@ public class Answer extends BaseEntity implements Serializable {
         this.writer = writer;
     }
 
-    public Long getQuestionId() {
-        return questionId;
+    public Question getQuestion() {
+        return question;
     }
 
-    public void setQuestionId(Long questionId) {
-        this.questionId = questionId;
+    public void setQuestion(Question question) {
+        this.question = question;
     }
 
     public String getContents() {
@@ -110,7 +112,7 @@ public class Answer extends BaseEntity implements Serializable {
         return "Answer{" +
             "id=" + id +
             ", writer=" + writer +
-            ", questionId=" + questionId +
+            ", question=" + question +
             ", contents='" + contents + '\'' +
             ", deleted=" + deleted +
             '}';
--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -15,13 +15,14 @@ import qna.config.TestDataSourceConfig;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static qna.domain.QuestionTest.Q1;
 import static qna.domain.UserTest.JAVAJIGI;
 import static qna.domain.UserTest.SANJIGI;
 
 @TestDataSourceConfig
 public class AnswerTest {
-    public static final Answer A1 = new Answer(JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
-    public static final Answer A2 = new Answer(SANJIGI, QuestionTest.Q1, "Answers Contents2");
+    public static final Answer A1 = new Answer(JAVAJIGI, Q1, "Answers Contents1");
+    public static final Answer A2 = new Answer(SANJIGI, Q1, "Answers Contents2");
 
     @Autowired
     private AnswerRepository answerRepository;
@@ -29,30 +30,40 @@ public class AnswerTest {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private QuestionRepository questionRepository;
+
     @BeforeEach
     void setUp() {
-        A1.setWriter(userRepository.save(JAVAJIGI));
-        A2.setWriter(userRepository.save(SANJIGI));
+
+        User javajigi = userRepository.save(JAVAJIGI);
+        User sanjigi = userRepository.save(SANJIGI);
+
+        Q1.setWriter(javajigi);
+        Question question = questionRepository.save(Q1);
+
+        A1.setWriter(javajigi);
+        A2.setWriter(sanjigi);
+
+        A1.toQuestion(question);
+        A2.toQuestion(question);
     }
 
     @DisplayName("Answer entity 저장 검증")
     @MethodSource("testcase")
     @ParameterizedTest
     void saveTest(Answer answer) {
-        userRepository.save(answer.getWriter());
-
         Answer saved = answerRepository.save(answer);
         assertNotNull(saved.getId());
 
         assertEquals(answer.getWriter(), saved.getWriter());
-        assertEquals(answer.getQuestionId(), saved.getQuestionId());
+        assertEquals(answer.getQuestion(), saved.getQuestion());
     }
 
     @DisplayName("findById 검증")
     @MethodSource("testcase")
     @ParameterizedTest
     void findByIdAndDeletedFalseTest(Answer answer) {
-        userRepository.save(answer.getWriter());
         Answer expected = answerRepository.save(answer);
         Answer actual = answerRepository.findByIdAndDeletedFalse(expected.getId())
                                         .orElseThrow(EntityNotFoundException::new);
@@ -69,11 +80,13 @@ public class AnswerTest {
     @Test
     void findByQuestionIdAndDeletedFalseTest() {
 
+        Question question = A1.getQuestion();
+
         List<Answer> expected = new ArrayList<>();
         expected.add(answerRepository.save(A1));
         expected.add(answerRepository.save(A2));
 
-        List<Answer> actual = answerRepository.findByQuestionIdAndDeletedFalse(QuestionTest.Q1.getId());
+        List<Answer> actual = answerRepository.findByQuestionIdAndDeletedFalse(question.getId());
 
         for (int i = 0; i < expected.size(); i++) {
             equals(expected.get(i), actual.get(i));
@@ -83,7 +96,7 @@ public class AnswerTest {
     private void equals(Answer expected, Answer actual) {
         assertEquals(expected.getId(), actual.getId());
         assertEquals(expected.getWriter(), actual.getWriter());
-        assertEquals(expected.getQuestionId(), actual.getQuestionId());
+        assertEquals(expected.getQuestion(), actual.getQuestion());
         assertEquals(expected.getContents(), actual.getContents());
     }
 }
