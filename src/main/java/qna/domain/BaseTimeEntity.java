4aef5d2c50595cd9ceef8823f85c1ec1b376e859
itdar <thesockerrr@gmail.com>
2021-05-25 01:53:33 +0900

    feat: BaseTimeEntity 클래스 구현 및 어플에 적용
    - 다른 entity에서 상속받아 사용될 Auditing entity class
    - 어플리케이션 시작부에서 enable auditing 함

---
+++ src/main/java/qna/domain/BaseTimeEntity.java
@@ -0,0 +1,21 @@
+package qna.domain;
+
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+}
