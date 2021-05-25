274a2529c9c793fd98e23724944405da6d085105
ohgillwhan <soora_genius@naver.com>
2021-05-26 00:55:34 +0900

    refact : QuestionRepositoryTest 깔끔하게 리팩터링

--- src/test/java/qna/domain/QuestionRepositoryTest.java
+++ src/test/java/qna/domain/QuestionRepositoryTest.java
@@ -1,5 +1,6 @@
 package qna.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,46 +18,40 @@ public class QuestionRepositoryTest {
     @Autowired
     private QuestionRepository questionRepository;
 
+    private Question question;
+    private Question deletedQuestion;
+
+    @BeforeEach
+    public void setUp() {
+        question = new Question("Hello", "Hello");
+        deletedQuestion = new Question("Bye", "Bye");
+
+        deletedQuestion.setDeleted(true);
+
+        questionRepository.saveAll(Arrays.asList(question, deletedQuestion));
+    }
+
     @Test
     @DisplayName("저장을 하고, 다시 가져왔을 때 원본 객체와 같아야 한다")
     public void 저장을_하고_다시_가져왔을_때_원본_객체와_같아야_한다() {
-        Question savedQuestion = questionRepository.save(QuestionTest.Q1);
-        Question foundQuestion = questionRepository.findById(savedQuestion.getId()).orElseThrow(EntityNotFoundException::new);
+        Question foundQuestion = questionRepository.findById(question.getId()).orElseThrow(EntityNotFoundException::new);
 
-        assertSame(savedQuestion, foundQuestion);
+        assertSame(question, foundQuestion);
     }
 
     @Test
     @DisplayName("삭제가 되어있으면, findByIdAndDeletedFalse는 찾지 못한다")
     public void 삭제를_하지_않으면_findByIdAndDeletedFalse는_찾지_못한다() {
-        Question deleteTestQuestion = new Question("Hello", "Hello");
-
-        Question savedQuestion = questionRepository.save(deleteTestQuestion);
-
-        assertThat(questionRepository.findByIdAndDeletedFalse(savedQuestion.getId()).isPresent())
-                .isTrue();
-
-        // 삭제처리
-        savedQuestion.setDeleted(true);
-        assertThat(questionRepository.findByIdAndDeletedFalse(savedQuestion.getId()).isPresent())
-                .isFalse();
+        assertThat(questionRepository.findByIdAndDeletedFalse(question.getId()))
+                .isPresent();
+        assertThat(questionRepository.findByIdAndDeletedFalse(deletedQuestion.getId()))
+                .isNotPresent();
     }
 
     @Test
     @DisplayName("삭제가 되어있으면, findByDeletedFalse에는 포함이 되면 안된다")
     public void 삭제가_되어있으면_findByDeletedFalse에는_포함이_되면_안된다() {
-        Question deletedQuestion = new Question("A1", "A1");
-        deletedQuestion.setDeleted(true);
-
-        questionRepository.saveAll(
-                Arrays.asList(
-                        QuestionTest.Q1,
-                        QuestionTest.Q2,
-                        deletedQuestion
-                )
-        );
-
         assertThat(questionRepository.findByDeletedFalse())
-                .containsExactlyInAnyOrder(QuestionTest.Q1, QuestionTest.Q2);
+                .containsExactlyInAnyOrder(question);
     }
 }
