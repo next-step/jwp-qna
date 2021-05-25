0902a3fee2f59c2c74271244a846e6022f3d6d0e
etff <wish2rich@gmail.com>
2021-05-25 23:53:26 +0900

    refactor: application.yml 스프링 설정 파일 변경

--- src/test/java/qna/domain/utils/JpaTest.java
+++ src/test/java/qna/domain/utils/JpaTest.java
@@ -2,6 +2,7 @@ package qna.domain.utils;
 
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import qna.domain.AnswerRepository;
@@ -11,6 +12,7 @@ import qna.domain.UserRepository;
 
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public
 class JpaTest {
     @Autowired
