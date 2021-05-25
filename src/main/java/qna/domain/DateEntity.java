0a326ce61cea0190b75d349655e678e796faed82
cw-seungkwon <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>
2021-05-26 00:09:25 +0900

    refactor: 날짜 생성방식 변경
    1. annotation 을 사용하는 것이 더 깔끔하다 판단하여 수정
    2. updated_at 은 setter 가 존재하면 안되다고 판단하여 제거

--- src/main/java/qna/domain/DateEntity.java
+++ src/main/java/qna/domain/DateEntity.java
@@ -6,15 +6,19 @@ import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public class DateEntity {
 
+	@CreatedDate
 	@Column(name = "created_at", nullable = false, updatable = false)
-	private LocalDateTime createdAt = LocalDateTime.now();
+	private LocalDateTime createdAt;
 
+	@LastModifiedDate
 	@Column(name = "updated_at")
 	private LocalDateTime updatedAt;
 
@@ -22,15 +26,11 @@ public class DateEntity {
 		return createdAt;
 	}
 
-	public void setCreatedAt(LocalDateTime createdAt) {
-		this.createdAt = createdAt;
-	}
-
 	public LocalDateTime getUpdatedAt() {
 		return updatedAt;
 	}
 
-	public void setUpdatedAt(LocalDateTime updatedAt) {
-		this.updatedAt = updatedAt;
+	public void update() {
+		this.updatedAt = LocalDateTime.now();
 	}
 }
