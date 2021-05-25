618ca3873592b1748c0e6468e75025f0765bdfa8
itdar <thesockerrr@gmail.com>
2021-05-25 22:57:04 +0900

    chore: BaseTimeEntity (Auditing class) 속성에 nullable 변경

--- src/main/java/qna/domain/BaseTimeEntity.java
+++ src/main/java/qna/domain/BaseTimeEntity.java
@@ -4,6 +4,7 @@ import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
@@ -12,6 +13,7 @@ import java.time.LocalDateTime;
 @EntityListeners(AuditingEntityListener.class)
 public class BaseTimeEntity {
 
+    @Column(nullable = false)
     @CreatedDate
     private LocalDateTime createdAt;
 
