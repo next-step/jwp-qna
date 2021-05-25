3317b35f3db8d001a3484ad35d1896c48b04a8fc
ted.kang <ted.kang@kakaocorp.com>
2021-05-25 23:01:21 +0900

    feat: DeleteHistories equals & hascode 재정의

--- src/main/java/qna/domain/DeleteHistories.java
+++ src/main/java/qna/domain/DeleteHistories.java
@@ -1,6 +1,7 @@
 package qna.domain;
 
 import java.util.List;
+import java.util.Objects;
 
 public class DeleteHistories {
 
@@ -17,5 +18,18 @@ public class DeleteHistories {
   public DeleteHistories addHistory(DeleteHistory toAddHistory) {
     histories.add(toAddHistory);
     return this;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    DeleteHistories that = (DeleteHistories) o;
+    return histories.equals(that.histories);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(histories);
   }
 }
