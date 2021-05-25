3ca0c62613a91086953521289137315780ee26be
cw-seungkwon <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>
2021-05-26 00:03:25 +0900

    test: select 가 되는지 검증한다

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -3,6 +3,7 @@ package qna.domain;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,14 +17,25 @@ public class AnswerTest {
 	@Autowired
 	private AnswerRepository answerRepository;
 
+	@BeforeEach
+	void init() {
+		answerRepository.save(A2);
+	}
+
 	@Test
 	@DisplayName(value = "answer entity 가 DB에 저장되면 id 값, 저장 날짜가 할당된다")
-	void dbConnect() {
+	void save() {
 		Answer persistedA1 = answerRepository.save(A1);
 		assertAll(
 			() -> assertThat(persistedA1.getId()).isNotNull(),
 			() -> assertThat(persistedA1.getCreatedAt()).isNotNull()
 		);
+	}
+
+	@Test
+	@DisplayName(value = "저장된 entity 를 select 하여 반환한다")
+	void select() {
+		assertThat(answerRepository.findByIdAndDeletedFalse(A2.getId()).get()).isEqualTo(A2);
 	}
 
 }
