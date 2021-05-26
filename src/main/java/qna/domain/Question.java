905762b74c61695ae08ed66a3a4cb18465c81adc
ohgillwhan <soora_genius@naver.com>
2021-05-26 22:04:07 +0900

    refact : Question에 필요없는 함수 제거

--- src/main/java/qna/domain/Question.java
+++ src/main/java/qna/domain/Question.java
@@ -64,31 +64,12 @@ public class Question extends BaseEntity {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContents() {
-        return contents;
-    }
-
-    public void setContents(String contents) {
-        this.contents = contents;
-    }
-
     public List<Answer> getAnswers() { return Collections.unmodifiableList(answers); }
 
     public User getWriter() {
         return writer;
     }
+
     public boolean isDeleted() {
         return deleted;
     }
