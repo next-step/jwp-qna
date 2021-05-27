77a8aa53e53f675e95b505b02ecbb4712fc7fa3a
limdingdong <38725277+limdingdong@users.noreply.github.com>
2021-05-27 17:03:16 +0900

    refactor : 미사용 메소드 제거

--- src/main/java/qna/domain/Answer.java
+++ src/main/java/qna/domain/Answer.java
@@ -54,32 +54,8 @@ public class Answer {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public Long getWriterId() {
         return writerId;
-    }
-
-    public void setWriterId(Long writerId) {
-        this.writerId = writerId;
-    }
-
-    public Long getQuestionId() {
-        return questionId;
-    }
-
-    public void setQuestionId(Long questionId) {
-        this.questionId = questionId;
-    }
-
-    public String getContents() {
-        return contents;
-    }
-
-    public void setContents(String contents) {
-        this.contents = contents;
     }
 
     public boolean isDeleted() {
