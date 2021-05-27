ea34392559f87c9f471c3af692be1274760d8cfe
ohgillwhan <soora_genius@naver.com>
2021-05-27 22:59:56 +0900

    refact : Safe하도록 객체를 생성하여 처리

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,5 +1,6 @@
 package qna.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import qna.CannotDeleteException;
@@ -14,23 +15,32 @@ public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1", UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2", UserTest.SANJIGI);
 
+    private Question question;
+
+    @BeforeEach
+    void setUp() {
+        question = new Question("title1", "contents1", UserTest.JAVAJIGI);
+    }
+
     @Test
     @DisplayName("본인이 쓴 글이 아니면 CannotDeleteException가 발생하여 삭제가 불가능하다")
     void 본인이_쓴_글이_아니면_CannotDeleteException가_발생하여_삭제가_불가능하다() {
         assertThatExceptionOfType(CannotDeleteException.class)
-                .isThrownBy(() -> Q1.delete(UserTest.SANJIGI));
-        assertThat(Q1.isDeleted())
+                .isThrownBy(() -> question.delete(UserTest.SANJIGI));
+        assertThat(question.isDeleted())
                 .isFalse();
     }
 
     @Test
     @DisplayName("본인이 쓴 글이면 삭제가 가능하다")
     void 본인이_쓴_글이면_삭제가_가능하다() {
-        List<DeleteHistory> deleteHistories = assertDoesNotThrow(() -> Q2.delete(UserTest.SANJIGI));
+        Question question = new Question("title1", "contents1", UserTest.JAVAJIGI);
+
+        List<DeleteHistory> deleteHistories = assertDoesNotThrow(() -> question.delete(UserTest.JAVAJIGI));
 
         assertThat(deleteHistories)
                 .hasSize(1);
-        assertThat(Q2.isDeleted())
+        assertThat(question.isDeleted())
                 .isTrue();
     }
 }
