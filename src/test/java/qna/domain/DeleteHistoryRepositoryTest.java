820f654deb7cd62a10a7b18bed0a3aa0018821c0
ohgillwhan <soora_genius@naver.com>
2021-05-27 21:28:53 +0900

    refact : DeleteHistory 에 대한 테스트코드 리팩터링

--- src/test/java/qna/domain/DeleteHistoryRepositoryTest.java
+++ src/test/java/qna/domain/DeleteHistoryRepositoryTest.java
@@ -28,35 +28,45 @@ class DeleteHistoryRepositoryTest {
 
     private EntityManagerHelper entityManagerHelper;
 
+    private User savedUser;
+
     private DeleteHistory deleteHistory;
-    private User user;
 
     @BeforeEach
     void setUp() {
         entityManagerHelper = new EntityManagerHelper(entityManager);
 
-        user = userRepository.save(new User("USER", "PASSWORD", "NAME", "EMAIL"));
-        deleteHistory = deleteHistoryRepository.save(
-                new DeleteHistory(ContentType.ANSWER, 1L, user, LocalDateTime.now())
-        );
+        savedUser = userRepository.save(new User("USER", "PASSWORD", "NAME", "EMAIL"));
+
+        deleteHistory = new DeleteHistory(ContentType.ANSWER, 1L, savedUser, LocalDateTime.now());
     }
 
     @Test
     @DisplayName("저장을 하고, 다시 가져왔을 때 원본 객체와 같아야 한다")
     void 저장을_하고_다시_가져왔을_때_원본_객체와_같아야_한다() {
-        assertThat(deleteHistory)
-                .isEqualTo(deleteHistoryRepository.findById(deleteHistory.getId()).orElseThrow(EntityNotFoundException::new));
+        DeleteHistory savedDeleteHistory = deleteHistoryRepository.save(deleteHistory);
+
+        DeleteHistory foundDeleteHistory = deleteHistoryRepository.findById(deleteHistory.getId())
+                .orElseThrow(EntityNotFoundException::new);
+
+        assertThat(savedDeleteHistory)
+                .isEqualTo(foundDeleteHistory);
     }
 
     @Test
     @DisplayName("User와 연관관계를 맺었을 때, 다시 가져올 때도 맺혀있어야 한다")
     void User와_연관관계를_맺었을_때_다시_가져올_때도_맺혀있어야_한다() {
+        DeleteHistory savedDeleteHistory = deleteHistoryRepository.save(deleteHistory);
+
         entityManagerHelper.flushAndClear();
 
-        DeleteHistory foundDeleteHistory = deleteHistoryRepository.findById(deleteHistory.getId())
+        DeleteHistory foundDeleteHistory = deleteHistoryRepository.findById(savedDeleteHistory.getId())
                 .orElseThrow(EntityNotFoundException::new);
 
-        assertThat(foundDeleteHistory.isDeleteBy(userRepository.findById(user.getId()).orElseThrow(EntityNotFoundException::new)))
+        User foundUser = userRepository.findById(savedUser.getId())
+                .orElseThrow(EntityNotFoundException::new);
+
+        assertThat(foundDeleteHistory.isDeleteBy(foundUser))
                 .isTrue();
     }
 }
