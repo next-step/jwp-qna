6ce1fc012c097cf857b9691e892579994705858d
tmkwak <tmkwak@hmm21.com>
2021-05-28 16:21:46 +0900

    refactor : AnswerRepositoryTest의 A1,A2 다시 AnswerTest로 이동 - 통일성을 위해 수정

--- src/test/java/qna/domain/AnswerRepositoryTest.java
+++ src/test/java/qna/domain/AnswerRepositoryTest.java
@@ -13,16 +13,13 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 @DataJpaTest
 public class AnswerRepositoryTest {
 
-	public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
-	public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
-
 	@Autowired
 	private AnswerRepository answers;
 
 	@Test
 	@DisplayName("Answer 저장 테스트")
 	void save() {
-		Answer answer = A1;
+		Answer answer = AnswerTest.A1;
 		Answer actual = answers.save(answer);
 		assertThat(answers.findByIdAndDeletedFalse(actual.getId()).isPresent()).isTrue();
 	}
@@ -30,7 +27,7 @@ public class AnswerRepositoryTest {
 	@Test
 	@DisplayName("Answer id로 Answer 조회 테스트")
 	void findById() {
-		Answer answer = A1;
+		Answer answer = AnswerTest.A1;
 		Answer saved = answers.save(answer);
 		Answer actual = answers.findByIdAndDeletedFalse(saved.getId()).get();
 		assertAll(
@@ -45,7 +42,7 @@ public class AnswerRepositoryTest {
 	@Test
 	@DisplayName("Answer 수정 테스트")
 	void update() {
-		Answer answer = A1;
+		Answer answer = AnswerTest.A1;
 		Answer saved = answers.save(answer);
 		Answer actual = answers.findByIdAndDeletedFalse(saved.getId()).get();
 		Date updateAt = new Date();
@@ -60,7 +57,7 @@ public class AnswerRepositoryTest {
 	@Test
 	@DisplayName("Answer 삭제 테스트")
 	void delete() {
-		Answer answer = A1;
+		Answer answer = AnswerTest.A1;
 		Answer saved = answers.save(answer);
 		long savedId = saved.getId();
 		answers.delete(saved);
--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -10,6 +10,9 @@ import qna.UnAuthorizedException;
 
 public class AnswerTest {
 
+	public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
+	public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
 	@Test
 	@DisplayName("Answer 생성 테스트")
 	void create() {
