da11861abc5d5b7cf39603ece5492e735d568017
limdingdong <38725277+limdingdong@users.noreply.github.com>
2021-05-28 20:28:01 +0900

    refactor : 미사용 getter, setter 제거

--- src/main/java/qna/domain/Question.java
+++ src/main/java/qna/domain/Question.java
@@ -49,32 +49,8 @@ public class Question {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContents() {
-        return contents;
-    }
-
-    public void setContents(String contents) {
-        this.contents = contents;
-    }
-
     public Long getWriterId() {
         return writerId;
-    }
-
-    public void setWriterId(Long writerId) {
-        this.writerId = writerId;
     }
 
     public boolean isDeleted() {
