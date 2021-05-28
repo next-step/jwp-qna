58ca6c562a90fb195d107138ce04135e79dd8acc
tmkwak <tmkwak@hmm21.com>
2021-05-28 15:57:13 +0900

    style : DeleteHistoryTest @DisplayName 추가

--- src/test/java/qna/domain/DeleteHistoryTest.java
+++ src/test/java/qna/domain/DeleteHistoryTest.java
@@ -4,11 +4,13 @@ import static org.assertj.core.api.Assertions.*;
 
 import java.time.LocalDateTime;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class DeleteHistoryTest {
 
 	@Test
+	@DisplayName("DeleteHistory 생성 테스트")
 	void create() {
 		DeleteHistory deleteHistory = new DeleteHistory(ContentType.QUESTION, 1L, 1L, LocalDateTime.now());
 		assertThat(deleteHistory).isNotNull();
