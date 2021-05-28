aaaea312de19d1002c3b6f52c523eaf8548d89da
tmkwak <tmkwak@hmm21.com>
2021-05-28 14:59:31 +0900

    test : AnswerRepository CRUD 테스트

--- src/main/java/qna/domain/AnswerRepository.java
+++ src/main/java/qna/domain/AnswerRepository.java
@@ -1,14 +1,13 @@
 package qna.domain;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 @Repository
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
-    List<Answer> findByQuestionIdAndDeletedFalse(Long questionId);
-
-    Optional<Answer> findByIdAndDeletedFalse(Long id);
+	List<Answer> findByQuestionIdAndDeletedFalse(Long questionId);
+	Optional<Answer> findByIdAndDeletedFalse(Long id);
 }
---
+++ src/test/java/qna/domain/AnswerRepositoryTest.java
@@ -0,0 +1,67 @@
+package qna.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import java.util.Date;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+public class AnswerRepositoryTest {
+
+	@Autowired
+	private AnswerRepository answers;
+
+	@Test
+	@DisplayName("Answer 저장 테스트")
+	void saveAnswar() {
+		Answer expected = AnswerTest.A1;
+		Answer actual = answers.save(expected);
+		assertThat(answers.findByIdAndDeletedFalse(actual.getId()).isPresent()).isTrue();
+	}
+
+	@Test
+	@DisplayName("Answer id로 Answer 조회 테스트")
+	void findById() {
+		Answer expected = AnswerTest.A1;
+		Answer saved = answers.save(expected);
+		Answer actual = answers.findByIdAndDeletedFalse(saved.getId()).get();
+		assertAll(
+			() -> assertThat(actual).isNotNull(),
+			() -> assertThat(actual.getWriterId()).isEqualTo(expected.getId()),
+			() -> assertThat(actual.getContents()).isEqualTo(expected.getContents()),
+			() -> assertThat(actual.isDeleted()).isEqualTo(false),
+			() -> assertThat(actual.getCreateAt()).isNotNull()
+		);
+	}
+
+	@Test
+	@DisplayName("Answer 수정 테스트")
+	void update() {
+		Answer expected = AnswerTest.A1;
+		Answer saved = answers.save(expected);
+		Answer actual = answers.findByIdAndDeletedFalse(saved.getId()).get();
+		Date updateAt = new Date();
+
+		actual.setContents("수정했어요");
+		actual.setUpdateAt(updateAt);
+		answers.flush();
+		assertThat(saved.getContents()).isEqualTo("수정했어요");
+		assertThat(saved.getUpdateAt()).isEqualTo(updateAt);
+	}
+
+	@Test
+	@DisplayName("Answer 삭제 테스트")
+	void delete() {
+		Answer expected = AnswerTest.A1;
+		Answer saved = answers.save(expected);
+		answers.delete(saved);
+		answers.flush();
+		assertThat(answers.findByIdAndDeletedFalse(expected.getId()).isPresent()).isFalse();
+	}
+
+}
