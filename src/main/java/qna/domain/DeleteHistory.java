b7e7b7208870788336bf7aae166c9a02d84689ee
jimin.joo <wnwlals22@naver.com>
2021-05-29 01:38:29 +0900

    feat(DeleteHistory Entity)

--- src/main/java/qna/domain/DeleteHistory.java
+++ src/main/java/qna/domain/DeleteHistory.java
@@ -1,14 +1,34 @@
 package qna.domain;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
+@Table(name = "delete_history")
+@Entity
 public class DeleteHistory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "content_type")
     private ContentType contentType;
+
+    @Column(name = "content_id")
     private Long contentId;
+
+    @Column(name = "deleted_by_id")
     private Long deletedById;
+
+    @Column(name = "create_date")
     private LocalDateTime createDate = LocalDateTime.now();
+
+    protected DeleteHistory() {
+        // empty
+    }
+
 
     public DeleteHistory(ContentType contentType, Long contentId, Long deletedById, LocalDateTime createDate) {
         this.contentType = contentType;
