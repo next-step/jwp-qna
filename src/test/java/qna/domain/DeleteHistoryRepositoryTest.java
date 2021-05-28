356675cdf8855936db73cdb03963195aba2b2b26
limdingdong <38725277+limdingdong@users.noreply.github.com>
2021-05-28 20:24:00 +0900

    test : 삭제이력 저장 테스트

---
+++ src/test/java/qna/domain/DeleteHistoryRepositoryTest.java
@@ -0,0 +1,30 @@
+package qna.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+class DeleteHistoryRepositoryTest {
+
+    @Autowired
+    private DeleteHistoryRepository deleteHistories;
+    
+    @Test
+    @DisplayName("삭제이력 저장")
+    void save() {
+        // given
+        DeleteHistory deleteHistory = new DeleteHistory(ContentType.QUESTION, QuestionTest.Q1.getId(), UserTest.JAVAJIGI.getId(), LocalDateTime.now());
+
+        // when
+        DeleteHistory actual = deleteHistories.save(deleteHistory);
+
+        // then
+        assertThat(actual).isSameAs(deleteHistory);
+    }
+}
