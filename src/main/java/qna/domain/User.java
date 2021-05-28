3a11090313ab9b580d3df563ad2132850150a6ac
jimin.joo <wnwlals22@naver.com>
2021-05-29 01:51:11 +0900

    feat(User Entity)

--- src/main/java/qna/domain/User.java
+++ src/main/java/qna/domain/User.java
@@ -2,18 +2,33 @@ package qna.domain;
 
 import qna.UnAuthorizedException;
 
+import javax.persistence.*;
 import java.util.Objects;
 
-public class User {
-    public static final GuestUser GUEST_USER = new GuestUser();
+@Table(name = "user")
+@Entity
+public class User extends CreateAndUpdateTimeEntity{
 
+    private static final GuestUser GUEST_USER = new GuestUser();
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "user_id", nullable = false, unique = true, length = 20)
     private String userId;
+
+    @Column(name = "password", nullable = false, length = 20)
     private String password;
+
+    @Column(name = "name", nullable = false, length = 20)
     private String name;
+
+    @Column(name = "email", length = 50)
     private String email;
 
-    private User() {
+    protected User() {
+        // empty
     }
 
     public User(String userId, String password, String name, String email) {
