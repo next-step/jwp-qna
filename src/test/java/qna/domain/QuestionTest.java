506bf095cffb93faa9d90f689b5e8d5648da9b21
tmkwak <tmkwak@hmm21.com>
2021-05-28 16:25:58 +0900

    test : Question 테스트 - 객체 생성 테스트

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,21 @@
 package qna.domain;
 
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class QuestionTest {
-    public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
-    public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+	public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
+	public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+	@Test
+	@DisplayName("Question 생성")
+	void create() {
+		Question question = new Question("title", "content").writeBy(UserTest.JAVAJIGI);
+		assertThat(question).isNotNull();
+		assertThat(question.getTitle()).isEqualTo("title");
+		assertThat(question.getContents()).isEqualTo("content");
+	}
 }
