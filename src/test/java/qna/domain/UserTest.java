dd95cf673eedfbca4615d274f7a9f2baecba8ca5
HoonHee Jeong <jhh992000@gmail.com>
2021-05-30 05:36:24 +0900

    test: 테스트용 사용자 추가

--- src/test/java/qna/domain/UserTest.java
+++ src/test/java/qna/domain/UserTest.java
@@ -3,4 +3,5 @@ package qna.domain;
 public class UserTest {
     public static final User JAVAJIGI = new User(1L, "javajigi", "password", "name", "javajigi@slipp.net");
     public static final User SANJIGI = new User(2L, "sanjigi", "password", "name", "sanjigi@slipp.net");
+    public static final User HOONHEE = new User(3L, "jhh992000", "password", "정훈희", "jhh992000@gmail.com");
 }
