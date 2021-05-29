06d9a8c324d7fceaf1acc484a440b3fbd21b8898
yjs2952 <yjs2952@naver.com>
2021-05-29 22:01:53 +0900

    refactor: mock 테스트를 위한 equals, hashCode 메소드 재정의

--- src/main/java/qna/domain/DeleteHistory.java
+++ src/main/java/qna/domain/DeleteHistory.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Entity
 @EntityListeners(AuditingEntityListener.class)
@@ -42,6 +43,22 @@ public class DeleteHistory {
                 ", contentType=" + contentType +
                 ", contentId=" + contentId +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DeleteHistory that = (DeleteHistory) o;
+        return Objects.equals(id, that.id) &&
+                contentType == that.contentType &&
+                Objects.equals(contentId, that.contentId) &&
+                Objects.equals(deletedBy, that.deletedBy);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, contentType, contentId, deletedBy);
     }
 
     public Long getId() {
