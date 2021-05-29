4919f97bbae21b3230da8999d70ffff142e905a6
ttuop <plintcsz@gmail.com>
2021-05-29 14:15:33 +0900

    test: Answer 테스트 코드 구현

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,6 +1,43 @@
 package qna.domain;
 
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
 public class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @Autowired
+    private AnswerRepository answerRepository;
+
+    @Test
+    void save() {
+        // given
+        final Answer answer = answerRepository.save(A1);
+
+        // when
+        final Long actual = answer.getId();
+
+        // then
+        assertThat(actual).isEqualTo(A1.getId());
+    }
+
+    @Test
+    void findById() {
+        // given
+        final Answer expected = answerRepository.save(A1);
+
+        // when
+        final Optional<Answer> optAnswer = answerRepository.findById(UserTest.JAVAJIGI.getId());
+        final Answer actual = optAnswer.orElseThrow(IllegalArgumentException::new);
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
 }
