1b0934ba0a64f5af9555ecfa6626c3ba14bbf6ed
JoyceLee9310 <leejoyce9310@gmail.com>
2021-05-29 16:24:17 +0900

    feat : User 기능 추가 및 UserRepositoryTest 작성

--- src/main/java/qna/domain/User.java
+++ src/main/java/qna/domain/User.java
@@ -3,6 +3,7 @@ package qna.domain;
 import qna.UnAuthorizedException;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.Objects;
 
 @Entity
@@ -13,17 +14,23 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(length = 20, nullable = false)
+    @Column(name = "user_id", length = 20, nullable = false, unique = true)
     private String userId;
 
-    @Column(length = 20, nullable = false)
+    @Column(name = "password", length = 20, nullable = false)
     private String password;
 
-    @Column(length = 20, nullable = false)
+    @Column(name = "name", length = 20, nullable = false)
     private String name;
 
-    @Column(length = 50)
+    @Column(name = "email", length = 50)
     private String email;
+
+    @Column(name = "created_at", nullable = false, columnDefinition = "TIMESTAMP")
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column(name = "updated_at", columnDefinition = "TIMESTAMP")
+    private LocalDateTime updatedAt;
 
     protected User() {
     }
@@ -86,24 +93,12 @@ public class User {
         return userId;
     }
 
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
     public String getPassword() {
         return password;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public String getEmail() {
@@ -111,7 +106,16 @@ public class User {
     }
 
     public void setEmail(String email) {
+        this.updatedAt = LocalDateTime.now();
         this.email = email;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return this.createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return this.updatedAt;
     }
 
     @Override
---
+++ src/test/java/qna/domain/UserRepositoryTest.java
@@ -0,0 +1,76 @@
+package qna.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DataIntegrityViolationException;
+
+import javax.persistence.EntityManager;
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@DataJpaTest
+public class UserRepositoryTest {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    EntityManager em;
+
+    private User expected;
+
+    @BeforeEach
+    void setUp() {
+        //Given
+        expected = userRepository.save(UserTest.JAVAJIGI);
+    }
+
+    @DisplayName("저장 후에 조회해서 가져온 객체가 동일한 객체인지 확인한다")
+    @Test
+    void check_same_instance() {
+        //Then
+        User actual = userRepository.findById(expected.getId())
+                .orElseThrow(() -> new IllegalArgumentException());
+        assertThat(actual).isSameAs(expected);
+    }
+
+    @DisplayName("save()실행 시, id/createdAt에 데이터가 들어가는지 확인한다")
+    @Test
+    void check_save_result() {
+        //Then
+        assertThat(expected.getId()).isNotNull();
+        assertThat(expected.getCreatedAt()).isNotNull()
+                .isBefore(LocalDateTime.now());
+    }
+
+    @DisplayName("update()실행 시, updatedAt에 데이터가 들어가는지 확인한다")
+    @Test
+    void check_updatedAt() {
+        //When
+        expected.setEmail("pythonjigi@slipp.net");
+
+        //Then
+        assertThat(expected.getUpdatedAt()).isNotNull()
+                .isAfter(expected.getCreatedAt())
+                .isBefore(LocalDateTime.now());
+    }
+
+    @DisplayName("동일한 user_id로 생성하려는 경우, exception이 발생한다 -> 발생 안함")
+    @Test
+    void check_unique_constraints() {
+        //Given
+        //em.flush();
+        userRepository.flush();
+
+        User newUser = new User(3L, "javajigi", "password", "name", "pythonjigi@slipp.net");
+        //User newUser = new User("javajigi", "password", "name", "pythonjigi@slipp.net");
+
+        //When + Then
+        assertThatThrownBy(() -> userRepository.save(newUser)).isInstanceOf(DataIntegrityViolationException.class);
+    }
+}
