76aa926d4fc0735376ac22b7456fb9f1c86f00a3
ttuop <plintcsz@gmail.com>
2021-05-29 14:20:56 +0900

    test: Question 테스트 코드 구현

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,31 @@
 package qna.domain;
 
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
 public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    @Test
+    void save() {
+        // given
+        final Question expected = questionRepository.save(Q1);
+
+        // when
+        final Optional<Question> optActual = questionRepository.findById(Q1.getId());
+        final Question actual = optActual.orElseThrow(IllegalArgumentException::new);
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
 }
