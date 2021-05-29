7fef1d3b5a0ccb43617d658b642707bebb6a1d0e
ttuop <plintcsz@gmail.com>
2021-05-29 14:24:14 +0900

    test: User 테스트 코드 구현

--- src/test/java/qna/domain/UserTest.java
+++ src/test/java/qna/domain/UserTest.java
@@ -1,6 +1,31 @@
 package qna.domain;
 
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
 public class UserTest {
     public static final User JAVAJIGI = new User(1L, "javajigi", "password", "name", "javajigi@slipp.net");
     public static final User SANJIGI = new User(2L, "sanjigi", "password", "name", "sanjigi@slipp.net");
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void save() {
+        // given
+        final User expected = userRepository.save(JAVAJIGI);
+
+        // when
+        final Optional<User> optActual = userRepository.findByUserId(JAVAJIGI.getUserId());
+        final User actual = optActual.orElseThrow(IllegalArgumentException::new);
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
 }
