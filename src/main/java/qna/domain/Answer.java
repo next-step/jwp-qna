0c3f96f1a5554df2199bacfe50ec721885707f86
ttuop <plintcsz@gmail.com>
2021-05-29 11:09:03 +0900

    feat: Answer, DeleteHistory, Question, User 엔티티 매핑

--- src/main/java/qna/domain/Answer.java
+++ src/main/java/qna/domain/Answer.java
@@ -3,7 +3,6 @@ package qna.domain;
 import java.util.Objects;
 
 import javax.persistence.Column;
-import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -12,7 +11,6 @@ import javax.persistence.Lob;
 import qna.NotFoundException;
 import qna.UnAuthorizedException;
 
-@Entity
 public class Answer extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
