7a41d95cb8fdbfd1ea3c6b6c753729ec1749e6cc
sanggu <tkdrn8578@naver.com>
2021-05-30 18:20:04 +0900

    refactor: id 로 question을 조회하다록 수정

--- src/main/java/qna/service/QnaService.java
+++ src/main/java/qna/service/QnaService.java
@@ -39,7 +39,7 @@ public class QnaService {
             throw new CannotDeleteException("질문을 삭제할 권한이 없습니다.");
         }
 
-        List<Answer> answers = answerRepository.findByQuestionIdAndDeletedFalse(questionId);
+        List<Answer> answers = answerRepository.findByQuestionIdAndDeletedFalse(question.getId());
         for (Answer answer : answers) {
             if (!answer.isOwner(loginUser)) {
                 throw new CannotDeleteException("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
