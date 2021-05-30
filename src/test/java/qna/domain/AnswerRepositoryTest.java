57df77e038ace400aa958a21ed09784bc75c4a02
sanggu <tkdrn8578@naver.com>
2021-05-30 17:39:56 +0900

    test(answer): findByIdAndDeletedFalse에 대한 테스트 코드 추가

--- src/test/java/qna/domain/AnswerRepositoryTest.java
+++ src/test/java/qna/domain/AnswerRepositoryTest.java
@@ -1,11 +1,13 @@
 package qna.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static qna.domain.AnswerTest.A1;
@@ -26,20 +28,26 @@ class AnswerRepositoryTest {
     @Autowired
     private AnswerRepository answerRepository;
 
-    @Test
-    @DisplayName("질문 글에 저장된 모든 답변을 조회한다.")
-    void findByQuestionIdAndDeletedFalse() {
-        //given
+    private Question question;
+    private Answer firstAnswer;
+    private Answer secondAnswer;
+
+    @BeforeEach
+    void setUp() {
         userRepository.save(JAVAJIGI);
         userRepository.save(SANJIGI);
 
-        Question question = questionRepository.save(Q1);
+        question = questionRepository.save(Q1);
 
-        Answer firstAnswer = answerRepository.save(A1);
+        firstAnswer = answerRepository.save(A1);
         firstAnswer.toQuestion(question);
-        Answer secondAnswer = answerRepository.save(A2);
+        secondAnswer = answerRepository.save(A2);
         secondAnswer.toQuestion(question);
+    }
 
+    @Test
+    @DisplayName("질문 글에 저장된 모든 답변을 조회한다.")
+    void findByQuestionIdAndDeletedFalse() {
         //when
         List<Answer> answers = answerRepository.findByQuestionIdAndDeletedFalse(question.getId());
 
@@ -48,5 +56,15 @@ class AnswerRepositoryTest {
                 firstAnswer,
                 secondAnswer
         );
+    }
+
+    @Test
+    @DisplayName("ID에 해당하는 답변을 조회한다.")
+    void findByIdAndDeletedFalse() {
+        //when
+        Optional<Answer> findAnswer = answerRepository.findByIdAndDeletedFalse(firstAnswer.getId());
+
+        //then
+        assertThat(findAnswer.get()).isEqualTo(firstAnswer);
     }
 }
