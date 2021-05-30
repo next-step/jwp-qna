a3b4ba2c8e0413a39c026015058a6dbe15feb961
JoyceLee9310 <leejoyce9310@gmail.com>
2021-05-30 14:58:59 +0900

    feat : User의 equals 기능 추가 및 테스트 구현

--- src/main/java/qna/domain/User.java
+++ src/main/java/qna/domain/User.java
@@ -104,6 +104,23 @@ public class User extends BaseTimeEntity {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return id.equals(user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, userId, password, name, email);
+    }
+
+    @Override
     public String toString() {
         return "User{" +
                 "id=" + id +
--- src/test/java/qna/domain/UserTest.java
+++ src/test/java/qna/domain/UserTest.java
@@ -1,6 +1,21 @@
 package qna.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class UserTest {
     public static final User JAVAJIGI = new User(1L, "javajigi", "password", "name", "javajigi@slipp.net");
     public static final User SANJIGI = new User(2L, "sanjigi", "password", "name", "sanjigi@slipp.net");
+
+
+    @DisplayName("id 값만으로 equals() 여부를 점검하는지 확인한다")
+    @Test
+    void check_equals_method(){
+        User onlyIdDifferentUser = new User(99L, "javajigi", "password", "name", "javajigi@slipp.net");
+        User onlyIdSameUser = new User(1L, "pythonjigi", "pythonPW","pythonName","python@slipp.net");
+        assertThat(JAVAJIGI.equals(onlyIdDifferentUser)).isFalse();
+        assertThat(JAVAJIGI.equals(onlyIdSameUser)).isTrue();
+    }
 }
