1188866df490c2f3ea017ad8303cd7bbf906c20c
limdingdong <38725277+limdingdong@users.noreply.github.com>
2021-05-30 15:48:28 +0900

    test : 연관관계 매핑 테스트 - 삭제한 유저

--- src/main/java/qna/domain/DeleteHistory.java
+++ src/main/java/qna/domain/DeleteHistory.java
@@ -32,6 +32,10 @@ public class DeleteHistory {
         this.createDate = createDate;
     }
 
+    public User deleteUser() {
+        return deleteUser;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
--- src/test/java/qna/domain/DeleteHistoryRepositoryTest.java
+++ src/test/java/qna/domain/DeleteHistoryRepositoryTest.java
@@ -13,12 +13,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 class DeleteHistoryRepositoryTest {
 
     @Autowired
+    private UserRepository users;
+
+    @Autowired
     private DeleteHistoryRepository deleteHistories;
     
     @Test
     @DisplayName("삭제이력 저장")
     void save() {
         // given
+        users.save(UserTest.JAVAJIGI);
         DeleteHistory deleteHistory = new DeleteHistory(ContentType.QUESTION, QuestionTest.Q1.getId(), UserTest.JAVAJIGI, LocalDateTime.now());
 
         // when
@@ -26,5 +30,19 @@ class DeleteHistoryRepositoryTest {
 
         // then
         assertThat(actual).isSameAs(deleteHistory);
+    }
+
+    @Test
+    @DisplayName("연관관계 매핑 테스트 - 삭제한 유저")
+    void deleteUser() {
+        // given
+        User deleteUser = users.save(UserTest.JAVAJIGI);
+
+        // when
+        DeleteHistory deleteHistory = new DeleteHistory(ContentType.QUESTION, QuestionTest.Q1.getId(), deleteUser, LocalDateTime.now());
+        DeleteHistory actual = deleteHistories.save(deleteHistory);
+
+        // then
+        assertThat(actual.deleteUser()).isSameAs(deleteUser);
     }
 }
