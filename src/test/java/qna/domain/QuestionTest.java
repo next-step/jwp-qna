1b373982afd24b6bc785dc09e8a6bd3abc808ca7
sanggu <tkdrn8578@naver.com>
2021-05-30 17:52:41 +0900

    refactor: Q2에 대한 상수 값 추출

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -4,6 +4,9 @@ public class QuestionTest {
     public static final String Q1_TITLE = "title1";
     public static final String Q1_CONTENT = "contents1";
 
+    public static final String Q2_TITLE = "title2";
+    public static final String Q2_CONTENT = "contents2";
+
     public static final Question Q1 = new Question(Q1_TITLE, Q1_CONTENT).writeBy(UserTest.JAVAJIGI);
-    public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+    public static final Question Q2 = new Question(Q2_TITLE, Q2_CONTENT).writeBy(UserTest.SANJIGI);
 }
