cba6910f5d711a2887cdcdb29b7c93d5bfacc74d
jaewanLee <ianlee9180@gmail.com>
2021-05-30 16:01:46 +0900

    feat : BaseEntity 추상화 클래스 생성 - createdAt, updatedAt과 같이 중복되는 필드를 한번에 관리할 수 있도록 한다

---
+++ src/main/java/qna/domain/BaseEntity.java
@@ -0,0 +1,16 @@
+package qna.domain;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.util.Date;
+
+@MappedSuperclass
+public abstract class BaseEntity {
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt = new Date();
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updatedAt;
+}
