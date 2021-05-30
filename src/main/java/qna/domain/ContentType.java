5760a15ce2641aa98b39be5ba1514cae46f37180
ascii92der <ascii92der@gmail.com>
2021-05-30 19:08:59 +0900

    refactor:  - DeleteHistory entity mapping  - add DeleteHistoryTest with DataJpaTest  - add ContentTypeConverter for entity mapping

--- src/main/java/qna/domain/ContentType.java
+++ src/main/java/qna/domain/ContentType.java
@@ -1,5 +1,15 @@
 package qna.domain;
 
 public enum ContentType {
-    QUESTION, ANSWER
+    QUESTION("question"), ANSWER("answer");
+
+    private final String type;
+
+    ContentType(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
 }
