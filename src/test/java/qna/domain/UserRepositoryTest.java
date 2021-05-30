595abb563ef088d7628326bfaf2434b1536b17b9
sanggu <tkdrn8578@naver.com>
2021-05-30 17:58:15 +0900

    test(user): userId로 사용자를 조회하는 findByUserId에 대한 테스트코드 추가

---
+++ src/test/java/qna/domain/UserRepositoryTest.java
@@ -0,0 +1,31 @@
+package qna.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static qna.domain.UserTest.JAVAJIGI;
+
+@DataJpaTest
+class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    @DisplayName("ID에 해당하는 User를 조회한다.")
+    void findByUserId() {
+        //given
+        User javajigi = userRepository.save(JAVAJIGI);
+
+        //when
+        Optional<User> findUser = userRepository.findByUserId(javajigi.getUserId());
+
+        //then
+        assertThat(findUser.get()).isEqualTo(javajigi);
+    }
+}
