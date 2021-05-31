9c2eca064b3431d4b0444d8caa387eec280e54e7
백승열 <beck33333@naver.com>
2021-05-31 23:42:58 +0900

    test : UserRepositoryTest save, findById 테스트 추가

---
+++ src/test/java/qna/domain/UserRepositoryTest.java
@@ -0,0 +1,40 @@
+package qna.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+
+@DataJpaTest
+class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository users;
+
+    @Test
+    void save() {
+        User expected = UserTest.JAVAJIGI;
+        User actual = users.save(expected);
+        assertAll(
+                () -> assertThat(actual.getId()).isNotNull(),
+                () -> assertThat(actual.getUserId()).isNotNull(),
+                () -> assertThat(actual.getPassword()).isNotNull(),
+                () -> assertThat(actual.getName()).isNotNull(),
+                () -> assertThat(actual.getUserId()).isEqualTo(expected.getUserId()),
+                () -> assertThat(actual.getPassword()).isEqualTo(expected.getPassword()),
+                () -> assertThat(actual.getName()).isEqualTo(expected.getName()),
+                () -> assertThat(actual.getEmail()).isEqualTo(expected.getEmail())
+        );
+    }
+
+    @Test
+    void findByName() {
+        User expected = UserTest.JAVAJIGI;
+        users.save(expected);
+        User actual = users.findByUserId(expected.getUserId()).get();
+        assertThat(actual).isEqualTo(expected);
+    }
+}
