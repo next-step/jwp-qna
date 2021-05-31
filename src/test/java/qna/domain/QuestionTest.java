d028a7f88952c1080a67b02beea116a11a45cdb8
yjs2952 <yjs2952@naver.com>
2021-05-31 15:44:30 +0900

    test: Question 삭제 시 다른 사람이 작성한 Answer 가 있는 경우 예외가 발생한다

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,39 +1,61 @@
 package qna.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import qna.CannotDeleteException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static qna.domain.AnswerTest.A1;
 
 public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
 
+    Question question;
+    Answer answer1;
+    Answer answer2;
+
+    @BeforeEach
+    void setUp() {
+        question = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
+        answer1 = new Answer(UserTest.JAVAJIGI, question, "Answers Contents1");
+        answer2 = new Answer(UserTest.SANJIGI, question, "Answers Contents2");
+    }
+
     @Test
     @DisplayName("작성자와 다른 사용자가 삭제시 예외가 발생한다")
     void deleteNotOwnerExceptionTest() {
-        assertThatThrownBy(() -> Q1.deleteByOwner(UserTest.SANJIGI))
+        assertThatThrownBy(() -> question.deleteByOwner(UserTest.SANJIGI))
                 .isInstanceOf(CannotDeleteException.class);
     }
 
     @Test
     @DisplayName("삭제 시 deleted : true 변경, contentType : Question 인 DeleteHistory 를 반환한다")
     void deleteQuestionTest() throws CannotDeleteException {
-        DeleteHistory deleteHistory = Q1.deleteByOwner(UserTest.JAVAJIGI);
+        DeleteHistory deleteHistory = question.deleteByOwner(question.getWriter());
 
-        assertThat(Q1.isDeleted()).isEqualTo(true);
+        assertThat(question.isDeleted()).isEqualTo(true);
         assertThat(deleteHistory.getContentType()).isEqualTo(ContentType.QUESTION);
     }
 
     @Test
     @DisplayName("삭제 시 매핑된 answers 도 삭제된다")
     void deleteAnswersTest() throws CannotDeleteException {
-        Q1.addAnswer(A1);
-        Q1.deleteByOwner(UserTest.JAVAJIGI);
+        question.addAnswer(answer1);
+        question.deleteByOwner(question.getWriter());
 
-        assertThat(A1.isDeleted()).isEqualTo(true);
+        assertThat(answer1.isDeleted()).isEqualTo(true);
+    }
+
+    @Test
+    @DisplayName("삭제 시 다른 사람이 작성한 Answer 가 있는 경우 예외가 발생한다")
+    void deleteAnswersContainsNotOwnerExceptionTest() {
+        question.addAnswer(answer1);
+        question.addAnswer(answer2);
+
+        assertThat(answer1.getWriter()).isNotSameAs(answer2.getWriter());
+        assertThatThrownBy(() -> question.deleteByOwner(question.getWriter()))
+                .isInstanceOf(CannotDeleteException.class);
     }
 }
