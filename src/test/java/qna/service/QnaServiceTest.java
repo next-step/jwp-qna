0dcaa6196828f5c505bb42fcd6a6f1b2b18587b9
shkwon <oper912@naver.com>
2021-06-01 12:46:30 +0900

    refactor: Question Answer 양방향 매핑 및 Answer Embedded 타입으로 선언

--- src/test/java/qna/service/QnaServiceTest.java
+++ src/test/java/qna/service/QnaServiceTest.java
@@ -38,7 +38,7 @@ class QnAServiceTest {
 
     @BeforeEach
     public void setUp() throws Exception {
-        question = new Question(1L, "title1", "contents1").writeBy(UserTest.JAVAJIGI);
+        question = new Question(1L, "title1", "contents1", UserTest.JAVAJIGI);
         answer = new Answer(1L, UserTest.JAVAJIGI, question, "Answers Contents1");
         question.addAnswer(answer);
     }
