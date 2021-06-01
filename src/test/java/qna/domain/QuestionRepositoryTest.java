3901378287610545013d5ef4606e80ca8606e947
박소정 <psj6414@naver.com>
2021-06-02 00:30:04 +0900

    test: 서비스 layer의 deleteQuetion 메소드 개선 테스트

--- src/test/java/qna/domain/QuestionRepositoryTest.java
+++ src/test/java/qna/domain/QuestionRepositoryTest.java
@@ -5,8 +5,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import qna.CannotDeleteException;
+import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 @DataJpaTest
@@ -81,11 +85,56 @@ public class QuestionRepositoryTest {
         question.addAnswers(answer1);
         question.addAnswers(answer2);
 
-        List<Answer> result = question.getAnswers();
+        Answers result = question.getAnswers();
 
         assertAll (
-                () -> assertThat(result).hasSize(2),
-                () -> assertThat(result).contains(answer1, answer2)
+                () -> assertThat(result.getAnswers()).hasSize(2),
+                () -> assertThat(result.getAnswers()).contains(answer1, answer2)
         );
+    }
+
+    @Test
+    @DisplayName("질문 등록자가 아닌 사용자가 delete 시도시 예외 발생 테스트")
+    void validOwner() {
+        Question question = new Question(1L, "title1", "contents1").writeBy(user1);
+        Answer answer1 = new Answer(user1, question, "Answers Contents1");
+        question.addAnswers(answer1);
+
+        assertThatThrownBy(() -> {
+            question.validOwner(user2);
+        }).isInstanceOf(CannotDeleteException.class)
+                .hasMessageContaining("질문을 삭제할 권한이 없습니다.");
+    }
+
+    @Test
+    @DisplayName("Question에 다른 사람이 작성한 Answer 삭제시 예외 확인")
+    void delete_another_user() throws CannotDeleteException {
+        Question question = new Question(1L, "title1", "contents1").writeBy(user1);
+        Answer answer1 = new Answer(user1, question, "Answers Contents1");
+        Answer answer2 = new Answer(user2, question, "Answers Contents2");
+        question.addAnswers(answer1);
+        question.addAnswers(answer2);
+
+        assertThatThrownBy(() -> {
+            List<DeleteHistory> deleteHistories = question.delete(user1);
+        }).isInstanceOf(CannotDeleteException.class)
+                .hasMessageContaining("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("Question과 Answer 삭제 테스트")
+    void delete() throws CannotDeleteException {
+        Question question = new Question(1L, "title1", "contents1").writeBy(user1);
+        Answer answer1 = new Answer(user1, question, "Answers Contents1");
+        Answer answer2 = new Answer(user1, question, "Answers Contents2");
+        question.addAnswers(answer1);
+        question.addAnswers(answer2);
+
+        List<DeleteHistory> resultHistories = Arrays.asList(
+                new DeleteHistory(ContentType.QUESTION, question.getId(), question.getWriter(), LocalDateTime.now()),
+                new DeleteHistory(ContentType.ANSWER, answer1.getId(), answer1.getWriter(), LocalDateTime.now()),
+                new DeleteHistory(ContentType.ANSWER, answer2.getId(), answer2.getWriter(), LocalDateTime.now())
+        );
+        assertThat(question.delete(user1)).containsAll(resultHistories);
     }
 }
