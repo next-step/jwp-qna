2871644ca5ac65d67f46913e3b62ca475e88121d
sanggu <tkdrn8578@naver.com>
2021-06-02 23:56:50 +0900

    refactor: List<Answer>를 가지는 Answers 추가

---
+++ src/main/java/qna/domain/Answers.java
@@ -0,0 +1,46 @@
+package qna.domain;
+
+import qna.CannotDeleteException;
+
+import javax.persistence.Embeddable;
+import javax.persistence.OneToMany;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Embeddable
+public class Answers {
+
+    @OneToMany(mappedBy = "question")
+    private List<Answer> answers = new ArrayList<>();
+
+    public Answers() {
+    }
+
+    public Answers(List<Answer> answers) {
+        this.answers = answers;
+    }
+
+    public void validateOwners(User loginUser) throws CannotDeleteException {
+        for (Answer answer : answers) {
+            validateOwner(loginUser, answer);
+        }
+    }
+
+    private void validateOwner(User loginUser, Answer answer) throws CannotDeleteException {
+        if (!answer.isOwner(loginUser)) {
+            throw new CannotDeleteException("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
+        }
+    }
+
+    public void deleteAllAndAddHistories(List<DeleteHistory> deleteHistories) {
+        for (Answer answer : answers) {
+            answer.setDeleted(true);
+            deleteHistories.add(new DeleteHistory(ContentType.ANSWER, answer.getId(), answer.getWriter(), LocalDateTime.now()));
+        }
+    }
+
+    public void add(Answer answer) {
+        answers.add(answer);
+    }
+}
