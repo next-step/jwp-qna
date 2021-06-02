bc34829a874d9c51f46e08e989297bd54e67c104
kht2199 <kht2199@gmail.com>
2021-06-02 23:21:46 +0900

    fix: UserRepository repository annotation 제거

--- src/main/java/qna/repository/UserRepository.java
+++ src/main/java/qna/repository/UserRepository.java
@@ -1,13 +1,11 @@
 package qna.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import qna.domain.User;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUserId(String userId);
 }
