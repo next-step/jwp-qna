19fc6ce4620011d838862e44425c147e01793364
younghoon <bblnotbb@yahho.com>
2021-06-02 17:42:52 +0900

    feat : add question test that uses wriiten methods

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,39 @@
 package qna.domain;
 
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
 public class QuestionTest {
-    public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
-    public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+	@Autowired
+	QuestionRepository questionRepository;
+
+	public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
+	public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+	@Test
+	@DisplayName("jpa 작성 메소드 사용(findByDeletedFalse)")
+	void use_written_method_findByDeletedFalse() {
+		Q1.setDeleted(true);
+		questionRepository.save(Q1);
+		assertThat(questionRepository.findByDeletedFalse().size()).isEqualTo(0);
+
+		Q1.setDeleted(false);
+		assertThat(questionRepository.findByDeletedFalse().size()).isEqualTo(1);
+	}
+
+	@Test
+	@DisplayName("jpa 작성 메소드 사용(findByIdAndDeletedFalse)")
+	void use_written_method_findByIdAndDeletedFalse() {
+		Question saveQ1 = questionRepository.save(Q1);
+		assertThat(questionRepository.findByIdAndDeletedFalse(saveQ1.getId())).isEqualTo(
+			questionRepository.findById(saveQ1.getId()));
+
+		saveQ1.setDeleted(true);
+		assertThat(questionRepository.findByIdAndDeletedFalse(saveQ1.getId()).isPresent()).isFalse();
+	}
 }
