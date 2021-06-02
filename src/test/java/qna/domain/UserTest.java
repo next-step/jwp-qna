a8ac20ef484cda9f763559e046a832c59ac52294
younghoon <bblnotbb@yahho.com>
2021-06-02 17:49:22 +0900

    feat : add user test code that uses written method and relative user class

--- src/test/java/qna/domain/UserTest.java
+++ src/test/java/qna/domain/UserTest.java
@@ -1,6 +1,29 @@
 package qna.domain;
 
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
 public class UserTest {
-    public static final User JAVAJIGI = new User(1L, "javajigi", "password", "name", "javajigi@slipp.net");
-    public static final User SANJIGI = new User(2L, "sanjigi", "password", "name", "sanjigi@slipp.net");
+	@Autowired
+	private UserRepository userRepository;
+
+	public static final User JAVAJIGI = new User(1L, "javajigi", "password", "name", "javajigi@slipp.net");
+	public static final User SANJIGI = new User(2L, "sanjigi", "password", "name", "sanjigi@slipp.net");
+
+	@Test
+	@DisplayName("jpa 작성 메소드 사용(findByUserId)")
+	void use_written_method_findByUserId() {
+		User saveA1 = userRepository.save(JAVAJIGI);
+		User saveA2 = userRepository.save(SANJIGI);
+
+		assertThat(userRepository.findByUserId(saveA1.getUserId()).get()).isEqualTo(
+			userRepository.findById(saveA1.getId()).get());
+		assertThat(userRepository.findByUserId(saveA2.getUserId()).get()).isEqualTo(
+			userRepository.findById(saveA2.getId()).get());
+	}
 }
