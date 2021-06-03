8f0b31e794d5ba0f3c53589578c2c1eb6758d281
fDevJc <fDevJc@gmail.com>
2021-06-04 00:50:35 +0900

    test: 미사용코드 삭제

--- src/test/java/qna/domain/DeleteHistoryRepositoryTest.java
+++ src/test/java/qna/domain/DeleteHistoryRepositoryTest.java
@@ -1,7 +1,5 @@
 package qna.domain;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 public class DeleteHistoryRepositoryTest {
 
 }
--- src/test/java/qna/domain/UserRepositoryTest.java
+++ src/test/java/qna/domain/UserRepositoryTest.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 @DataJpaTest
 public class UserRepositoryTest {
