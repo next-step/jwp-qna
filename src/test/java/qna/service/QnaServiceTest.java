91181b15b0760328da3d0f19da7500525de0b8c5
accidentlywoo <accidentlywoo@gmail.com>
2021-06-03 18:46:22 +0900

    test : 호출없는 repository 조회 시나리오 날리기

--- src/test/java/qna/service/QnaServiceTest.java
+++ src/test/java/qna/service/QnaServiceTest.java
@@ -46,7 +46,7 @@ class QnaServiceTest {
 	@Test
 	public void delete_성공 () throws Exception {
 		when(questionRepository.findByIdAndDeletedFalse(question.id())).thenReturn(Optional.of(question));
-		when(answerRepository.findByQuestionAndDeletedFalse(question)).thenReturn(Arrays.asList(answer));
+//		when(answerRepository.findByQuestionAndDeletedFalse(question)).thenReturn(Arrays.asList(answer));
 
 		assertThat(question.deleted()).isFalse();
 		qnaService.deleteQuestion(UserTest.JAVAJIGI, question.id());
@@ -66,7 +66,7 @@ class QnaServiceTest {
 	@Test
 	public void delete_성공_질문자_답변자_같음 () throws Exception {
 		when(questionRepository.findByIdAndDeletedFalse(question.id())).thenReturn(Optional.of(question));
-		when(answerRepository.findByQuestionAndDeletedFalse(question)).thenReturn(Arrays.asList(answer));
+//		when(answerRepository.findByQuestionAndDeletedFalse(question)).thenReturn(Arrays.asList(answer));
 
 		qnaService.deleteQuestion(UserTest.JAVAJIGI, question.id());
 
@@ -81,7 +81,7 @@ class QnaServiceTest {
 		question.addAnswer(answer2);
 
 		when(questionRepository.findByIdAndDeletedFalse(question.id())).thenReturn(Optional.of(question));
-		when(answerRepository.findByQuestionAndDeletedFalse(question)).thenReturn(Arrays.asList(answer, answer2));
+//		when(answerRepository.findByQuestionAndDeletedFalse(question)).thenReturn(Arrays.asList(answer, answer2));
 
 		assertThatThrownBy(() -> qnaService.deleteQuestion(UserTest.JAVAJIGI, question.id()))
 				.isInstanceOf(CannotDeleteException.class);
