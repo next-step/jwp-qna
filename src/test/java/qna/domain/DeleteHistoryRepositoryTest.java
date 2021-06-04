a92b77801c59d5712f4adbfd1443a584e9b91984
mskangg <funnycoding91@gmail.com>
2021-06-04 14:04:45 +0900

    test: DeleteHistoryRepository 테스트 given 수정
    - 깨지는 테스트 수정

--- src/test/java/qna/domain/DeleteHistoryRepositoryTest.java
+++ src/test/java/qna/domain/DeleteHistoryRepositoryTest.java
@@ -1,8 +1,8 @@
 package qna.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.persistence.EntityNotFoundException;
 import java.time.LocalDateTime;
@@ -11,19 +11,23 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class DeleteHistoryRepositoryTest extends JpaTest {
+    private User user1;
 
-    @Autowired
-    private DeleteHistoryRepository deleteHistories;
+    @BeforeEach
+    void setUp() {
+        user1 = new User("userId1", "1234", "userName1", "userEmail1");
+        getUsers().save(user1);
+    }
 
     @DisplayName("삭제 이력을 저장한다.")
     @Test
     void save() {
         //given
         DeleteHistory deleteHistory = new DeleteHistory(
-                ContentType.ANSWER, 1L, 1L, LocalDateTime.now());
+                ContentType.ANSWER, 1L, user1, LocalDateTime.now());
 
         //when
-        DeleteHistory actual = deleteHistories.save(deleteHistory);
+        DeleteHistory actual = getDeleteHistories().save(deleteHistory);
 
         //then
         assertThat(actual).isSameAs(deleteHistory);
@@ -33,11 +37,11 @@ class DeleteHistoryRepositoryTest extends JpaTest {
     @Test
     void findById() {
         //given
-        DeleteHistory deleteHistory = deleteHistories.save(new DeleteHistory(
-                ContentType.QUESTION, 1L, 1L, LocalDateTime.now()));
+        DeleteHistory deleteHistory = getDeleteHistories().save(new DeleteHistory(
+                ContentType.QUESTION, 1L, user1, LocalDateTime.now()));
 
         //when
-        DeleteHistory actual = deleteHistories.findById(deleteHistory.getId())
+        DeleteHistory actual = getDeleteHistories().findById(deleteHistory.getId())
                 .orElseThrow(EntityNotFoundException::new);
 
         //then
@@ -49,7 +53,7 @@ class DeleteHistoryRepositoryTest extends JpaTest {
     void findByIdException() {
         //given
         //when
-        assertThatThrownBy(() -> deleteHistories.findById(-1L)
+        assertThatThrownBy(() -> getDeleteHistories().findById(-1L)
                 .orElseThrow(EntityNotFoundException::new))
                 .isInstanceOf(EntityNotFoundException.class); //then
     }
