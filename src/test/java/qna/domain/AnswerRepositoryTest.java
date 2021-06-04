029a678e21e0c2231eeb20667038024bf394d1a0
mskangg <funnycoding91@gmail.com>
2021-06-04 09:58:18 +0900

    test: AnswerRepository 테스트 추가
    - 깨지는 테스트 수정

--- src/test/java/qna/domain/AnswerRepositoryTest.java
+++ src/test/java/qna/domain/AnswerRepositoryTest.java
@@ -1,63 +1,72 @@
 package qna.domain;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.jupiter.api.*;
 
 import javax.persistence.EntityNotFoundException;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static qna.domain.AnswerTest.A1;
-import static qna.domain.AnswerTest.A2;
-import static qna.domain.QuestionTest.Q1;
 
 class AnswerRepositoryTest extends JpaTest {
+    private User user1;
+    private User user2;
+    private Question question1;
+    private Answer answer1;
+    private Answer answer2;
 
-    @Autowired
-    private AnswerRepository answers;
+    @BeforeEach
+    void setUp() {
+        user1 = new User("userId1", "1234", "userName1", "userEmail1");
+        user2 = new User("userId2", "1234", "userName2", "userEmail2");
+        getUsers().saveAll(Arrays.asList(user1, user2));
+
+        question1 = new Question("title1", "contents1").writeBy(user1);
+        getQuestions().save(question1);
+
+        answer1 = new Answer(user1, question1, "contents1");
+        answer2 = new Answer(user2, question1, "contents2");
+        getAnswers().saveAll(Arrays.asList(answer1, answer2));
+    }
 
     @DisplayName("질문 아이디에 해당하는 답변 목록 중 삭제 상태가 아닌 것들만 리턴한다")
     @Test
     void findByQuestionIdAndDeletedFalse() {
         //given
-        answers.save(A1);
-        answers.save(A2);
-        A1.setDeleted(false);
-        A2.setDeleted(true);
+        answer1.setDeleted(false);
+        answer2.setDeleted(true);
 
         //when
-        List<Answer> actual = answers.findByQuestionIdAndDeletedFalse(Q1.getId());
+        List<Answer> actual = getAnswers().findByQuestionIdAndDeletedFalse(question1.getId());
 
         //then
         assertThat(actual.size()).isEqualTo(1);
+        assertThat(actual.get(0)).isSameAs(answer1);
     }
 
     @DisplayName("답변 아이디에 해당하는 답변이 삭제 상태가 아니라면 리턴한다.")
     @Test
     void findByIdAndDeletedFalse() {
         //given
-        answers.save(A1);
-        A1.setDeleted(false);
+        answer1.setDeleted(false);
 
         //when
-        Answer actual = answers.findByIdAndDeletedFalse(A1.getId())
+        Answer actual = getAnswers().findByIdAndDeletedFalse(answer1.getId())
                 .orElseThrow(EntityNotFoundException::new);
 
         //then
-        assertThat(actual).isSameAs(A1);
+        assertThat(actual).isSameAs(answer1);
     }
 
     @DisplayName("답변 아이디에 해당하는 답변이 삭제 상태라면 EntityNotFoundException 을 발생시킨다.")
     @Test
     void findByIdAndDeletedFalseIfPresent() {
         //given
-        answers.save(A1);
-        A1.setDeleted(true);
+        answer1.setDeleted(true);
 
         //when
-        assertThatThrownBy(() -> answers.findByIdAndDeletedFalse(A1.getId())
+        assertThatThrownBy(() -> getAnswers().findByIdAndDeletedFalse(answer1.getId())
                 .orElseThrow(EntityNotFoundException::new))
                 .isInstanceOf(EntityNotFoundException.class); //then
     }
