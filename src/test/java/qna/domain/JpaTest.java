95c4d5406a3e5c63060e31a4ff6501d75fec179d
mskangg <funnycoding91@gmail.com>
2021-06-04 14:11:27 +0900

    refactor: 테스트 용이하게 Repository 정의 후 재사용

--- src/test/java/qna/domain/JpaTest.java
+++ src/test/java/qna/domain/JpaTest.java
@@ -1,5 +1,6 @@
 package qna.domain;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
@@ -7,4 +8,31 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public abstract class JpaTest {
 
+    @Autowired
+    private AnswerRepository answers;
+
+    @Autowired
+    private QuestionRepository questions;
+
+    @Autowired
+    private DeleteHistoryRepository deleteHistories;
+
+    @Autowired
+    private UserRepository users;
+
+    public AnswerRepository getAnswers() {
+        return answers;
+    }
+
+    public QuestionRepository getQuestions() {
+        return questions;
+    }
+
+    public DeleteHistoryRepository getDeleteHistories() {
+        return deleteHistories;
+    }
+
+    public UserRepository getUsers() {
+        return users;
+    }
 }
