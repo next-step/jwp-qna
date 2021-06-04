fd464d73d07427bbba01a33ceb549a1550b2fedf
Junhee Ko <jko@rainist.com>
2021-06-04 15:30:35 +0900

    feat: entity setting to Answer class

--- src/main/java/qna/domain/Answer.java
+++ src/main/java/qna/domain/Answer.java
@@ -1,16 +1,44 @@
 package qna.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
 import qna.NotFoundException;
 import qna.UnAuthorizedException;
 
+import java.time.LocalDateTime;
 import java.util.Objects;
 
+@Entity
 public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column
     private Long writerId;
+
+    @Column
     private Long questionId;
+
+    @Column
+    @Lob
     private String contents;
+
+    @Column(nullable= false)
     private boolean deleted = false;
+
+    @Column(nullable= false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable= false)
+    private LocalDateTime updatedAt;
+
+    public Answer() {
+    }
 
     public Answer(User writer, Question question, String contents) {
         this(null, writer, question, contents);
