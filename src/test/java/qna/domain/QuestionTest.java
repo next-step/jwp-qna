f891127d3fac913154f62ecad9c4d68b4510308d
cw-seungkwon <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>
2021-06-05 02:23:12 +0900

    test: question 에 대한 쿼리 검증
    repository 에 정의된 쿼리들을 검증한다.
    그 외에 insert, select, update 를 검증한다

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,66 @@
 package qna.domain;
 
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
 public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    private long QUESTION_ID;
+
+    @BeforeEach
+    void init() {
+        questionRepository.save(Q1);
+        List<Question> list = questionRepository.findAll();
+        this.QUESTION_ID = list.get(0).getId();
+    }
+
+    @Test
+    @DisplayName(value = "저장된 질문을 select 하여 저장한 데이터 맞는지 검증한다")
+    void select() {
+        List<Question> list = questionRepository.findAll();
+        long id = list.get(0).getId();
+        Question question = questionRepository.findByIdAndDeletedFalse(id).get();
+        assertThat(question.getTitle()).isEqualTo("title1");
+        assertThat(question.getContents()).isEqualTo("contents1");
+    }
+
+    @Test
+    @DisplayName(value = "새로운 질문을 저장한다")
+    void insert() {
+        Question question = questionRepository.save(Q2);
+        assertThat(question).isEqualTo(Q2);
+    }
+
+    @Test
+    @DisplayName(value = "질문의 제목을 수정하고 DB에 반영이 되었는지 검증힌다")
+    void update() {
+        final String newTitle = "질문 있습니다~";
+        Question question = questionRepository.getOne(QUESTION_ID);
+        question.setTitle(newTitle);
+        questionRepository.saveAndFlush(question);
+
+        Question afterUpdate = questionRepository.findByIdAndDeletedFalse(QUESTION_ID).get();
+        assertThat(afterUpdate.getTitle()).isEqualTo(newTitle);
+    }
+
+    @Test
+    @DisplayName(value = "삭제되지 않은 모든 질문을 select 한다")
+    void selectWhereNotDelete() {
+        List<Question> notDeletedQuestions = questionRepository.findByDeletedFalse();
+        assertThat(notDeletedQuestions.size()).isEqualTo(1);
+    }
+
 }
