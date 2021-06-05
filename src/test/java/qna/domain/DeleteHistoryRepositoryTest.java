985c788e46aba254a47dcac46bc979b669e5bf11
백승열 <beck33333@naver.com>
2021-06-06 00:15:55 +0900

    test : DeleteHistoryRepository 연관 관계 테스트

---
+++ src/test/java/qna/domain/DeleteHistoryRepositoryTest.java
@@ -0,0 +1,55 @@
+package qna.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static java.time.LocalDateTime.now;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+
+@DataJpaTest
+public class DeleteHistoryRepositoryTest {
+
+    @Autowired
+    private UserRepository users;
+
+    @Autowired
+    private DeleteHistoryRepository deleteHistories;
+
+    private User u1;
+
+    @BeforeEach
+    void setUp() {
+        u1 = new User("seungyeol", "password", "name", "beck33333@naver.com");
+        users.save(u1);
+    }
+
+    @Test
+    void save() {
+
+        DeleteHistory expected = new DeleteHistory(ContentType.QUESTION,1L,u1,now());
+        DeleteHistory actual = deleteHistories.save(expected);
+
+        assertThat(expected).isEqualTo(actual);
+        assertThat(actual.getUser()).isEqualTo(u1);
+    }
+
+    @Test
+    void update() {
+
+        DeleteHistory expected = new DeleteHistory(ContentType.QUESTION,1L,u1,now());
+        DeleteHistory actual = deleteHistories.save(expected);
+
+
+        User u2 = new User("baek", "password", "name", "beck33@naver.com");
+        actual.setUser(users.save(u2));
+
+        assertThat(actual.getUser()).isNotEqualTo(u1);
+        assertThat(actual.getUser()).isEqualTo(u2);
+    }
+
+}
