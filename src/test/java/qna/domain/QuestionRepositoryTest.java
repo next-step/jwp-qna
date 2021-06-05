27e92bbe425e7ed1aab743b69bd592b85f518d03
백승열 <beck33333@naver.com>
2021-06-06 00:16:43 +0900

    test : QuestionRepositoryTest 연관 관계 테스트

--- src/test/java/qna/domain/QuestionRepositoryTest.java
+++ src/test/java/qna/domain/QuestionRepositoryTest.java
@@ -1,5 +1,8 @@
 package qna.domain;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -13,10 +16,26 @@ class QuestionRepositoryTest {
 
     @Autowired
     private QuestionRepository questions;
+    @Autowired
+    private UserRepository users;
+
+    private Question q1;
+    private User u1;
+    @BeforeEach
+    void setUp() {
+        q1 = new Question("제목 이에요", "본문 입니다.");
+        u1 = new User("seungyeol", "password", "name", "beck33333@naver.com");
+    }
+
+    @AfterEach
+    void deleteAll() {
+        questions.deleteAll();
+        users.deleteAll();
+    }
 
     @Test
     void save() {
-        Question expected = QuestionTest.Q1;
+        Question expected = q1;
         Question actual = questions.save(expected);
         assertAll(
                 () -> assertThat(actual.getId()).isNotNull(),
@@ -24,13 +43,33 @@ class QuestionRepositoryTest {
                 () -> assertThat(actual.isDeleted()).isNotNull(),
                 () -> assertThat(actual.getContents()).isEqualTo(expected.getContents()),
                 () -> assertThat(actual.getTitle()).isEqualTo(expected.getTitle()),
-                () -> assertThat(actual.getWriterId()).isEqualTo(expected.getWriterId())
+                () -> assertThat(actual.getWriter()).isEqualTo(expected.getWriter())
         );
     }
 
     @Test
+    void saveWithWriter() {
+        Question expected = q1;
+        q1.setWriter(users.save(u1));
+
+        Question actual = questions.save(expected);
+        assertThat(actual.getWriter()).isEqualTo(expected.getWriter());
+    }
+
+    @Test
+    void updateWithWriter() {
+        Question given = q1;
+        questions.save(given);
+
+        given.setWriter(users.save(u1));
+        questions.flush();
+        assertThat(given.getWriter().getUserId()).isEqualTo("seungyeol");
+    }
+
+
+    @Test
     void findByName() {
-        Question expected = QuestionTest.Q2;
+        Question expected = q1;
         questions.save(expected);
         Question actual = questions.findById(expected.getId()).get();
         assertThat(actual).isEqualTo(expected);
@@ -39,7 +78,7 @@ class QuestionRepositoryTest {
 
     @Test
     void update() {
-        Question expected = QuestionTest.Q1;
+        Question expected = q1;
         Question saved = questions.save(expected);
 
         saved.setContents("Question Contents Changed");
@@ -48,7 +87,7 @@ class QuestionRepositoryTest {
 
     @Test
     void delete() {
-        Question expected = QuestionTest.Q1;
+        Question expected = q1;
         Question saved = questions.save(expected);
 
         questions.delete(saved);
