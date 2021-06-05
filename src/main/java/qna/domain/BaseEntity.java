09710fe3110b3c239bb788d7c51a6f33e1556626
jordy-torvalds <jordy-torvalds@kakao.com>
2021-06-05 12:31:36 +0900

    feat: Answer 도메인 구현 및 생성일자, 수정일자를 담는 감사 클래스 구현 및 적용

---
+++ src/main/java/qna/domain/BaseEntity.java
@@ -0,0 +1,30 @@
+package qna.domain;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners({AuditingEntityListener.class})
+public class BaseEntity {
+
+    @CreatedBy
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createAt;
+
+    @LastModifiedBy
+    private LocalDateTime updateAt;
+
+    public LocalDateTime getCreateAt() {
+        return createAt;
+    }
+
+    public LocalDateTime getUpdateAt() {
+        return updateAt;
+    }
+}
