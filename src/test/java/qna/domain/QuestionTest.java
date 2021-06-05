56bfac18be63a15172c5a14ffb2766ab85bd6f7c
younghoon <bblnotbb@yahho.com>
2021-06-05 18:37:54 +0900

    refactor : rename question repository to questions

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -11,7 +11,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 @DataJpaTest
 public class QuestionTest {
 	@Autowired
-	QuestionRepository questionRepository;
+	QuestionRepository questions;
 
 	Question Q1;
 	Question Q2;
@@ -25,32 +25,32 @@ public class QuestionTest {
 	@Test
 	@DisplayName("jpa 작성 메소드 사용(findByTitleContainingOrderByIdDesc)")
 	void select_start_with_order_by_id_desc() {
-		questionRepository.save(Q1);
-		questionRepository.save(Q2);
+		questions.save(Q1);
+		questions.save(Q2);
 
-		assertThat(questionRepository.findByTitleContainingOrderByIdDesc("title").get(0)).isEqualTo(
-			questionRepository.findById(2L).get());
+		assertThat(questions.findByTitleContainingOrderByIdDesc("title").get(0)).isEqualTo(
+			questions.findById(2L).get());
 	}
 
 	@Test
 	@DisplayName("jpa 작성 메소드 사용(findByDeletedFalse)")
 	void use_written_method_findByDeletedFalse() {
 		Q1.setDeleted(true);
-		questionRepository.save(Q1);
-		assertThat(questionRepository.findByDeletedFalse().size()).isEqualTo(0);
+		questions.save(Q1);
+		assertThat(questions.findByDeletedFalse().size()).isEqualTo(0);
 
 		Q1.setDeleted(false);
-		assertThat(questionRepository.findByDeletedFalse().size()).isEqualTo(1);
+		assertThat(questions.findByDeletedFalse().size()).isEqualTo(1);
 	}
 
 	@Test
 	@DisplayName("jpa 작성 메소드 사용(findByIdAndDeletedFalse)")
 	void use_written_method_findByIdAndDeletedFalse() {
-		Question saveQ1 = questionRepository.save(Q1);
-		assertThat(questionRepository.findByIdAndDeletedFalse(saveQ1.getId())).isEqualTo(
-			questionRepository.findById(saveQ1.getId()));
+		Question saveQ1 = questions.save(Q1);
+		assertThat(questions.findByIdAndDeletedFalse(saveQ1.getId())).isEqualTo(
+			questions.findById(saveQ1.getId()));
 
 		saveQ1.setDeleted(true);
-		assertThat(questionRepository.findByIdAndDeletedFalse(saveQ1.getId()).isPresent()).isFalse();
+		assertThat(questions.findByIdAndDeletedFalse(saveQ1.getId()).isPresent()).isFalse();
 	}
 }
