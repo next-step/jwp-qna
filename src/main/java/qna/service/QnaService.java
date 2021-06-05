b739d12f91239989c390c18ec1e1356f52bde2f4
HoonHee Jeong <jhh992000@gmail.com>
2021-06-05 23:38:02 +0900

    refactor: 쓰지않는 answerRepository, log상수 제거

--- src/main/java/qna/service/QnaService.java
+++ src/main/java/qna/service/QnaService.java
@@ -1,28 +1,21 @@
 package qna.service;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import qna.domain.DeleteHistories;
 import qna.domain.Question;
 import qna.domain.User;
 import qna.exception.NotFoundException;
-import qna.repository.AnswerRepository;
 import qna.repository.QuestionRepository;
 
 @Service
 public class QnaService {
 
-    private static final Logger log = LoggerFactory.getLogger(QnaService.class);
-
     private QuestionRepository questionRepository;
-    private AnswerRepository answerRepository;
     private DeleteHistoryService deleteHistoryService;
 
-    public QnaService(QuestionRepository questionRepository, AnswerRepository answerRepository, DeleteHistoryService deleteHistoryService) {
+    public QnaService(QuestionRepository questionRepository, DeleteHistoryService deleteHistoryService) {
         this.questionRepository = questionRepository;
-        this.answerRepository = answerRepository;
         this.deleteHistoryService = deleteHistoryService;
     }
 
