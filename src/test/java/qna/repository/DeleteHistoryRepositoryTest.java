45dcf4a6841bbed4a05a3cbae0faf897522a6af2
dohchoi91 <dohchoi91@gmail.com>
2021-06-05 11:03:12 +0900

    feat: 특정 유저 삭제 히스토리 조회 기능 추가

--- src/test/java/qna/repository/DeleteHistoryRepositoryTest.java
+++ src/test/java/qna/repository/DeleteHistoryRepositoryTest.java
@@ -3,26 +3,51 @@ package qna.repository;
 import static org.assertj.core.api.Assertions.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import qna.domain.AnswerTest;
 import qna.domain.ContentType;
 import qna.domain.DeleteHistory;
+import qna.domain.QuestionTest;
 import qna.domain.UserTest;
 
 @DataJpaTest
 public class DeleteHistoryRepositoryTest {
 	@Autowired
-	DeleteHistoryRepository deleteHistories;
+	private final DeleteHistoryRepository deleteHistories;
+	private final UserRepository users;
+
+	@Autowired
+	public DeleteHistoryRepositoryTest(DeleteHistoryRepository deleteHistories, UserRepository users) {
+		this.deleteHistories = deleteHistories;
+		this.users = users;
+		users.save(UserTest.JAVAJIGI);
+		users.save(UserTest.SANJIGI);
+	}
+
 
 	@Test
 	@DisplayName("저장")
 	void save() {
-		DeleteHistory expected = new DeleteHistory(ContentType.ANSWER, 1L, UserTest.JAVAJIGI, LocalDateTime.now());
+		DeleteHistory expected = new DeleteHistory(ContentType.ANSWER, AnswerTest.A1.getId(), UserTest.JAVAJIGI, LocalDateTime.now());
 		DeleteHistory actual = deleteHistories.save(expected);
 		assertThat(actual).isEqualTo(expected);
+	}
+
+	@Test
+	@DisplayName("특정 유저 삭제 내역 조회")
+	void findByDeletedUser() {
+		DeleteHistory history1 = new DeleteHistory(ContentType.ANSWER, AnswerTest.A1.getId(), UserTest.SANJIGI, LocalDateTime.now());
+		DeleteHistory history2= new DeleteHistory(ContentType.QUESTION, QuestionTest.Q1.getId(), UserTest.SANJIGI, LocalDateTime.now());
+		deleteHistories.save(history1);
+		deleteHistories.save(history2);
+
+		List<DeleteHistory> histories = deleteHistories.findByDeletedUser(UserTest.SANJIGI);
+		assertThat(histories.size()).isEqualTo(2);
 	}
 }
