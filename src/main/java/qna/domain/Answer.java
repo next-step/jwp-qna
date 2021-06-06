e9a26f74288522c5f65da697bb8866dbe9d976b4
jordy-torvalds <jordy-torvalds@kakao.com>
2021-06-06 23:29:50 +0900

    refactor: 사용하지 않는 setter/getter 모두 제거

--- src/main/java/qna/domain/Answer.java
+++ src/main/java/qna/domain/Answer.java
@@ -59,32 +59,11 @@ public class Answer extends BaseEntity {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public User getWriter() {
         return this.writer;
     }
-
-    public void setWriterId(User writer) {
-        this.writer = writer;
-    }
-
-    public Question getQuestion() {
-        return question;
-    }
-
     public void setQuestion(Question question) {
         this.question = question;
-    }
-
-    public String getContents() {
-        return contents;
-    }
-
-    public void setContents(String contents) {
-        this.contents = contents;
     }
 
     public boolean isDeleted() {
