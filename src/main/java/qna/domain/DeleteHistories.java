2e9eac030b7edc642d0a5056f3ceeda33b190da4
fDevJc <fDevJc@gmail.com>
2021-06-07 02:16:04 +0900

    feat: 일급컬렉션 DeleteHistories 추가

---
+++ src/main/java/qna/domain/DeleteHistories.java
@@ -0,0 +1,30 @@
+package qna.domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class DeleteHistories {
+    private final List<DeleteHistory> deleteHistoryList;
+
+    public DeleteHistories(List<DeleteHistory> deleteHistoryList) {
+        this.deleteHistoryList = deleteHistoryList;
+    }
+
+    public DeleteHistories() {
+        this(new ArrayList<>());
+    }
+
+    public void add(DeleteHistory deleteHistory) {
+        deleteHistoryList.add(deleteHistory);
+    }
+
+    public void addAll(DeleteHistories deleteHistories) {
+        deleteHistoryList.addAll(deleteHistories.getDeleteHistoryList());
+    }
+
+    public List<DeleteHistory> getDeleteHistoryList() {
+        return Collections.unmodifiableList(deleteHistoryList);
+    }
+
+}
---
+++ src/test/java/qna/domain/DeleteHistoriesTest.java
@@ -0,0 +1,9 @@
+package qna.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeleteHistoriesTest {
+
+}
