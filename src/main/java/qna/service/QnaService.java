8ea9270d483dced5d8ce11ded0119be0143170b7
fDevJc <fDevJc@gmail.com>
2021-06-08 00:39:29 +0900

    refactor: deleteQuestion 리팩토링

--- src/main/java/qna/service/QnaService.java
+++ src/main/java/qna/service/QnaService.java
@@ -35,32 +35,7 @@ public class QnaService {
     @Transactional
     public void deleteQuestion(User loginUser, Long questionId) {
         Question question = findQuestionById(questionId);
-        if (!question.isOwner(loginUser)) {
-            throw new CannotDeleteException("질문을 삭제할 권한이 없습니다.");
-        }
-
-        List<Answer> answers = answerRepository.findByQuestionAndDeletedFalse(question);
-        for (Answer answer : answers) {
-            if (!answer.isOwner(loginUser)) {
-                throw new CannotDeleteException("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
-            }
-        }
-
-        List<DeleteHistory> deleteHistories = new ArrayList<>();
-        question.setDeleted(true);
-        deleteHistories.add(new DeleteHistory(ContentType.QUESTION, questionId, question.getWriter(), LocalDateTime.now()));
-        for (Answer answer : answers) {
-            answer.setDeleted(true);
-            deleteHistories.add(new DeleteHistory(ContentType.ANSWER, answer.getId(), answer.getWriter(), LocalDateTime.now()));
-        }
-        deleteHistoryService.saveAll(deleteHistories);
-    }
-
-    @Transactional
-    public void deleteQuestion_refactor(User loginUser, Long questionId) {
-        Question question = findQuestionById(questionId);
         DeleteHistories deleteHistories = question.deleteWithAnswers(loginUser);
         deleteHistoryService.saveAll(deleteHistories.getDeleteHistoryList());
-//        deleteHistoryService.saveAll_refactor(deleteHistories);
     }
 }
