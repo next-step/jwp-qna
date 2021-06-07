05d193f9784dc3bb6fc370d2c9cff77f0afbdaef
younghoon <bblnotbb@yahho.com>
2021-06-07 22:41:24 +0900

    refactor : refactor user test class(tidy up duplicated variables)

--- src/test/java/qna/domain/UserTest.java
+++ src/test/java/qna/domain/UserTest.java
@@ -15,11 +15,8 @@ public class UserTest {
 	@Test
 	@DisplayName("jpql 사용)")
 	void select_name_by_email_using_jpql() {
-		User JAVAJIGI = new User(1L, "javajigi", "password", "name", "javajigi@slipp.net");
-		User SANJIGI = new User(2L, "sanjigi", "password", "name", "sanjigi@slipp.net");
-
-		User saveA1 = users.save(JAVAJIGI);
-		User saveA2 = users.save(SANJIGI);
+		User saveA1 = saveJavajigi();
+		User saveA2 = saveSanjigi();
 
 		users.findByEmail("javajigi");
 
@@ -29,15 +26,22 @@ public class UserTest {
 	@Test
 	@DisplayName("jpa 작성 메소드 사용(findByUserId)")
 	void use_written_method_findByUserId() {
-		User JAVAJIGI = new User(1L, "javajigi", "password", "name", "javajigi@slipp.net");
-		User SANJIGI = new User(2L, "sanjigi", "password", "name", "sanjigi@slipp.net");
-
-		User saveA1 = users.save(JAVAJIGI);
-		User saveA2 = users.save(SANJIGI);
+		User saveA1 = saveJavajigi();
+		User saveA2 = saveSanjigi();
 
 		assertThat(users.findByUserId(saveA1.getUserId()).get()).isEqualTo(
 			users.findById(saveA1.getId()).get());
 		assertThat(users.findByUserId(saveA2.getUserId()).get()).isEqualTo(
 			users.findById(saveA2.getId()).get());
+	}
+
+	private User saveJavajigi() {
+		User JAVAJIGI = new User(1L, "javajigi", "password", "name", "javajigi@slipp.net");
+		return users.save(JAVAJIGI);
+	}
+
+	private User saveSanjigi() {
+		User SANJIGI = new User(2L, "sanjigi", "password", "name", "sanjigi@slipp.net");
+		return users.save(SANJIGI);
 	}
 }
