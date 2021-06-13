64e2f8b6c2210e6d3fbb5726a4bd9e5acdec4f1c
cw-seungkwon <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>
2021-06-14 00:40:09 +0900

    feat: CannotDeleteException 이 기본 message를 갖도록 한다
    사용하는 측에서 별도의 message 를 입력하지 않으면
    기본 message 를 출력하도록 개선한다

--- src/main/java/qna/CannotDeleteException.java
+++ src/main/java/qna/CannotDeleteException.java
@@ -1,9 +1,21 @@
 package qna;
 
+import org.springframework.util.StringUtils;
+
 public class CannotDeleteException extends RuntimeException {
     private static final long serialVersionUID = 1L;
 
+    private static final String DEFAULT_MESSAGE = "다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.";
+
     public CannotDeleteException(String message) {
         super(message);
+    }
+
+    @Override
+    public String getMessage() {
+        if (StringUtils.hasText(super.getMessage())) {
+            return super.getMessage();
+        }
+        return DEFAULT_MESSAGE;
     }
 }
