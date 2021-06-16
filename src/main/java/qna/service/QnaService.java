8f1f00bfbb3b21a6c44c517d7eef9976a45a104e
younghoon <bblnotbb@yahho.com>
2021-06-16 09:53:43 +0900

    refactor : delete un-used repository

--- src/main/java/qna/service/QnaService.java
+++ src/main/java/qna/service/QnaService.java
@@ -7,7 +7,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import qna.CannotDeleteException;
 import qna.NotFoundException;
-import qna.domain.AnswerRepository;
 import qna.domain.Question;
 import qna.domain.QuestionRepository;
 import qna.domain.User;
@@ -17,13 +16,10 @@ public class QnaService {
 	private static final Logger log = LoggerFactory.getLogger(QnaService.class);
 
 	private QuestionRepository questionRepository;
-	private AnswerRepository answerRepository;
 	private DeleteHistoryService deleteHistoryService;
 
-	public QnaService(QuestionRepository questionRepository, AnswerRepository answerRepository,
-		DeleteHistoryService deleteHistoryService) {
+	public QnaService(QuestionRepository questionRepository, DeleteHistoryService deleteHistoryService) {
 		this.questionRepository = questionRepository;
-		this.answerRepository = answerRepository;
 		this.deleteHistoryService = deleteHistoryService;
 	}
 
