38203b231c6acb26423533ac36290b2a7c4a0520
KyungSik9870 <rudtlr9870@gmail.com>
2021-06-18 02:45:19 +0900

    feat: Question 에 연관관계 매핑

--- src/main/java/qna/domain/Question.java
+++ src/main/java/qna/domain/Question.java
@@ -2,10 +2,14 @@ package qna.domain;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -21,7 +25,9 @@ public class Question extends BaseTimeEntity {
 	@Lob
 	private String contents;
 
-	private Long writerId;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "writer_id", foreignKey = @ForeignKey(name = "fk_question_writer"))
+	private User writer;
 
 	@Column(nullable = false)
 	private boolean deleted = false;
@@ -40,12 +46,12 @@ public class Question extends BaseTimeEntity {
 	}
 
 	public Question writeBy(User writer) {
-		this.writerId = writer.getId();
+		this.writer = writer;
 		return this;
 	}
 
 	public boolean isOwner(User writer) {
-		return this.writerId.equals(writer.getId());
+		return this.writer.equals(writer);
 	}
 
 	public void addAnswer(Answer answer) {
@@ -76,12 +82,12 @@ public class Question extends BaseTimeEntity {
 		this.contents = contents;
 	}
 
-	public Long getWriterId() {
-		return writerId;
+	public User getWriter() {
+		return writer;
 	}
 
-	public void setWriterId(Long writerId) {
-		this.writerId = writerId;
+	public void setWriter(User writer) {
+		this.writer = writer;
 	}
 
 	public boolean isDeleted() {
@@ -98,7 +104,7 @@ public class Question extends BaseTimeEntity {
 			"id=" + id +
 			", title='" + title + '\'' +
 			", contents='" + contents + '\'' +
-			", writerId=" + writerId +
+			", writer=" + writer +
 			", deleted=" + deleted +
 			'}';
 	}
