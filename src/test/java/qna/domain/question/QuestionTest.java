0e5019f3355b79599c1ac4ef730e5fab66f5a44b
kht2199 <kht2199@gmail.com>
2021-06-19 13:57:02 +0900

    test: 질문 삭제 케이스 분리, 공통코드 Each로 이동.

--- src/test/java/qna/domain/question/QuestionTest.java
+++ src/test/java/qna/domain/question/QuestionTest.java
@@ -2,13 +2,9 @@ package qna.domain.question;
 
 import static org.assertj.core.api.Assertions.*;
 
-import java.util.stream.Stream;
-
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 
 import qna.domain.User;
 import qna.domain.UserTest;
@@ -19,42 +15,47 @@ public class QuestionTest {
     public static final Question Q1 = new Question(UserTest.JAVAJIGI, "title1", "contents1");
     public static final Question Q2 = new Question(UserTest.SANJIGI, "title2", "contents2");
 
+    Question question;
+
+    User questionWriter;
+
+    @BeforeEach
+    void beforeAll() {
+        questionWriter = UserTest.SANJIGI;
+        question = new Question(questionWriter, "title", "content");
+    }
+
     @Test
     @DisplayName("질문 데이터를 완전히 삭제하는 것이 아니라 데이터의 상태를 삭제 상태(deleted - boolean type)로 변경한다.")
     void when_deleteQuestion_then_deletedTrue() throws QuestionOwnerNotMatchedException, AnswerOwnerNotMatchedException {
-        Question question = new Question(UserTest.SANJIGI, "title", "content");
         assertThat(question).hasFieldOrPropertyWithValue("deleted", false);
         question.deleteBy(UserTest.SANJIGI);
         assertThat(question).hasFieldOrPropertyWithValue("deleted", true);
     }
 
-    @ParameterizedTest
+    @Test
     @DisplayName("로그인 사용자와 질문한 사람이 같은 경우 삭제할 수 있다")
-    @MethodSource("loginUserAttemptToDeleteDataSource")
-    void when_loginUserAttemptToDelete_then_succeed(User loginUser, User writer, boolean succeed) throws
+    void when_loginUserAttemptToDelete_then_succeed() throws
             AnswerOwnerNotMatchedException, QuestionOwnerNotMatchedException {
-        Question question = new Question(writer, "title", "content");
+        User loginUser = UserTest.SANJIGI;
         assertThat(question).hasFieldOrPropertyWithValue("deleted", false);
-        if (succeed) {
-            question.deleteBy(loginUser);
-            assertThat(question).hasFieldOrPropertyWithValue("deleted", true);
-            return;
-        }
-        assertThatThrownBy(() -> question.deleteBy(loginUser))
-            .isInstanceOf(QuestionOwnerNotMatchedException.class);
+        question.deleteBy(loginUser);
+        assertThat(question).hasFieldOrPropertyWithValue("deleted", true);
     }
 
-    static Stream<Arguments> loginUserAttemptToDeleteDataSource() {
-        return Stream.of(
-            Arguments.of(UserTest.SANJIGI, UserTest.SANJIGI, true),
-            Arguments.of(UserTest.SANJIGI, UserTest.JAVAJIGI, false)
-        );
+    @Test
+    @DisplayName("로그인 사용자와 질문한 사람이 다른 경우 삭제할 수 없다")
+    void when_loginUserAttemptToDelete_then_failed() {
+        User loginUser = UserTest.JAVAJIGI;
+        assertThat(loginUser).isNotEqualTo(questionWriter);
+        assertThat(question).hasFieldOrPropertyWithValue("deleted", false);
+        assertThatThrownBy(() -> question.deleteBy(loginUser))
+            .isInstanceOf(QuestionOwnerNotMatchedException.class);
     }
 
     @Test
     @DisplayName("답변이 없는 경우 삭제할 수 있다.")
     void when_noAnswers_then_succeed() throws QuestionOwnerNotMatchedException, AnswerOwnerNotMatchedException {
-        Question question = new Question(UserTest.SANJIGI, "title", "content");
         assertThat(question).hasFieldOrPropertyWithValue("deleted", false);
         question.deleteBy(UserTest.SANJIGI);
         assertThat(question).hasFieldOrPropertyWithValue("deleted", true);
@@ -63,8 +64,6 @@ public class QuestionTest {
     @Test
     @DisplayName("답변자가 자신만 있는 경우 있는 경우 삭제할 수 있다.")
     void when_answersOnlyWriter_then_succeed() throws QuestionOwnerNotMatchedException, AnswerOwnerNotMatchedException {
-        User questionWriter = UserTest.SANJIGI;
-        Question question = new Question(questionWriter, "title", "content");
         assertThat(question).hasFieldOrPropertyWithValue("deleted", false);
 
         User answerUser = UserTest.SANJIGI;
