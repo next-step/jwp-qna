2126b15760ca0535e12f29b162b2839f5a1d4a3f
KyungSik9870 <rudtlr9870@gmail.com>
2021-06-20 17:56:04 +0900

    feat: DeleteHistory 연관관계 설정, User equals 메소드 추가

--- src/main/java/qna/domain/DeleteHistory.java
+++ src/main/java/qna/domain/DeleteHistory.java
@@ -6,62 +6,70 @@ import java.util.Objects;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "delete_history")
 public class DeleteHistory {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @Enumerated(value = EnumType.STRING)
-    private ContentType contentType;
+	@Enumerated(value = EnumType.STRING)
+	private ContentType contentType;
 
-    private Long contentId;
+	private Long contentId;
 
-    private User deletedBy;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "deleted_by_id", foreignKey = @ForeignKey(name = "fk_delete_history_to_user"))
+	private User deletedBy;
 
-    private LocalDateTime createDate = LocalDateTime.now();
+	private LocalDateTime createDate = LocalDateTime.now();
 
-    public DeleteHistory() {
+	public DeleteHistory() {
 
-    }
+	}
 
-    public DeleteHistory(ContentType contentType, Long contentId, User deletedBy, LocalDateTime createDate) {
-        this.contentType = contentType;
-        this.contentId = contentId;
-        this.deletedBy = deletedBy;
-        this.createDate = createDate;
-    }
+	public DeleteHistory(ContentType contentType, Long contentId, User deletedBy, LocalDateTime createDate) {
+		this.contentType = contentType;
+		this.contentId = contentId;
+		this.deletedBy = deletedBy;
+		this.createDate = createDate;
+	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        DeleteHistory that = (DeleteHistory) o;
-        return Objects.equals(id, that.id) &&
-                contentType == that.contentType &&
-                Objects.equals(contentId, that.contentId) &&
-                Objects.equals(deletedBy, that.deletedBy);
-    }
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		DeleteHistory that = (DeleteHistory)o;
+		return Objects.equals(id, that.id) &&
+			contentType == that.contentType &&
+			Objects.equals(contentId, that.contentId) &&
+			Objects.equals(deletedBy, that.deletedBy);
+	}
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, contentType, contentId, deletedBy);
-    }
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, contentType, contentId, deletedBy);
+	}
 
-    @Override
-    public String toString() {
-        return "DeleteHistory{" +
-                "id=" + id +
-                ", contentType=" + contentType +
-                ", contentId=" + contentId +
-                ", deletedBy=" + deletedBy +
-                ", createDate=" + createDate +
-                '}';
-    }
+	@Override
+	public String toString() {
+		return "DeleteHistory{" +
+			"id=" + id +
+			", contentType=" + contentType +
+			", contentId=" + contentId +
+			", deletedBy=" + deletedBy +
+			", createDate=" + createDate +
+			'}';
+	}
 }
--- src/main/java/qna/domain/User.java
+++ src/main/java/qna/domain/User.java
@@ -122,6 +122,23 @@ public class User extends BaseTimeEntity {
 	}
 
 	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		User user = (User)o;
+		return Objects.equals(id, user.id) && Objects.equals(userId, user.userId)
+			&& Objects.equals(password, user.password) && Objects.equals(name, user.name)
+			&& Objects.equals(email, user.email);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, userId, password, name, email);
+	}
+
+	@Override
 	public String toString() {
 		return "User{" +
 			"id=" + id +
