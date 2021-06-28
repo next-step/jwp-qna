263a40a29a025ec3186a31ee9b5b2c5ac62d9072
rockjoon <49752338+rockjoon@users.noreply.github.com>
2021-06-28 23:45:22 +0900

    test : question, user save 테스트

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,25 @@
 package qna.domain;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
 public class QuestionTest {
+
+    @Autowired
+    QuestionRepository questions;
+
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+    @Test
+    void save() {
+        Question actual = questions.save(Q1);
+        assertThat(actual.getId()).isNotNull();
+        assertThat(actual.getTitle()).isEqualTo("title1");
+        assertThat(actual.getContents()).isEqualTo("contents1");
+    }
 }
