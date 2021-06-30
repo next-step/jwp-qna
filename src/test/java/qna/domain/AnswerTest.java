35efbc208943f30c49a77e5d2bacca389f2def88
rockjoon <49752338+rockjoon@users.noreply.github.com>
2021-06-30 23:32:15 +0900

    fix : step 1 피드백 반영

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -3,9 +3,11 @@ package qna.domain;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@EnableJpaAuditing
 @DataJpaTest
 public class AnswerTest {
 
