397e18d2db0344dc362ff6a888fc4388eddd9ff6
rockjoon <49752338+rockjoon@users.noreply.github.com>
2021-07-03 19:57:33 +0900

    test : 답변 삭제 테스트

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,32 +1,31 @@
 package qna.domain;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import qna.CannotDeleteException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-@EnableJpaAuditing
-@DataJpaTest
 public class AnswerTest {
-
-    @Autowired
-    AnswerRepository answers;
-    @Autowired
-    QuestionRepository questions;
-    @Autowired
-    UserRepository users;
 
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
 
+    @DisplayName("답변 개별 삭제 : 로그인 사용자와 답변 작성자가 같을 경우 삭제")
     @Test
-    void save() {
-        users.save(UserTest.JAVAJIGI);
-        questions.save(QuestionTest.Q1);
-        Answer actual = answers.save(A1);
-        assertThat(actual.getId()).isNotNull();
-        assertThat(actual.getContents()).isEqualTo("Answers Contents1");
+    void deleteBySameUser() throws CannotDeleteException {
+        User loginUser = UserTest.JAVAJIGI;
+        A1.delete(loginUser);
+        assertThat(A1.isDeleted()).isTrue();
     }
+
+    @DisplayName("답변 개별 삭제 : 로그인 사용자와 답변 작성자가 다를 경우 예외 발생")
+    @Test
+    void deleteByWrongUser() {
+        User loginUser = UserTest.SANJIGI;
+        assertThatThrownBy(() -> A1.delete(loginUser))
+                .isInstanceOf(CannotDeleteException.class);
+    }
+
 }
