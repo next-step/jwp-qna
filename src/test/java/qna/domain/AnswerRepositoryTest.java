753836af70236d1a7ad623bcc6b176e5f65ae58c
peter-alethio <peter@alethio.io>
2021-11-05 10:08:31 +0900

    test(answer): 정답 저장 테스트 추가

---
+++ src/test/java/qna/domain/AnswerRepositoryTest.java
@@ -0,0 +1,26 @@
+package qna.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class AnswerRepositoryTest {
+
+    @Autowired
+    private AnswerRepository answerRepository;
+
+    @Test
+    void 정답을_저장한다() {
+        Answer answer = answerRepository.save(AnswerTest.A1);
+        assertAll(
+                () -> assertThat(answer.getContents()).isEqualTo("Answers Contents1"),
+                () -> assertThat(answer.getId()).isEqualTo(1L),
+                () -> assertThat(answer.getQuestionId()).isEqualTo(null),
+                () -> assertThat(answer.getWriterId()).isEqualTo(1L)
+        );
+    }
+}
