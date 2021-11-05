ee168ff7fcf93795a46d4050f56693c1826a03f5
peter-alethio <peter@alethio.io>
2021-11-06 00:52:29 +0900

    feat(user): 사용자 컬럼 설정

--- src/main/java/qna/domain/User.java
+++ src/main/java/qna/domain/User.java
@@ -2,22 +2,27 @@ package qna.domain;
 
 import qna.UnAuthorizedException;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.Objects;
 
 @Entity
-public class User {
+public class User extends BaseEntity{
     public static final GuestUser GUEST_USER = new GuestUser();
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(length = 20, nullable = false)
     private String userId;
+
+    @Column(length = 20, nullable = false)
     private String password;
+
+    @Column(length = 20, nullable = false)
     private String name;
+
+    @Column(length = 50)
     private String email;
 
     protected User() {
