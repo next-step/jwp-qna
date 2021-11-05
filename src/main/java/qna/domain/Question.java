31931e6f36aab7cc1be1a95ed59773215e1a454b
peter-alethio <peter@alethio.io>
2021-11-06 00:49:39 +0900

    feat(question): 질문 컬럼 설정

--- src/main/java/qna/domain/Question.java
+++ src/main/java/qna/domain/Question.java
@@ -1,19 +1,23 @@
 package qna.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
-public class Question {
+public class Question extends BaseEntity{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(length = 100, nullable = false)
     private String title;
+
+    @Lob
     private String contents;
+
     private Long writerId;
+
+    @Column(nullable = false)
     private boolean deleted = false;
 
     protected Question() {
