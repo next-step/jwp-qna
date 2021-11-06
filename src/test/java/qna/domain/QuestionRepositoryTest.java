69fecfced83da90a456ade5be9904b281af644aa
IW.MOON <mooniw02@gmail.com>
2021-11-06 16:29:01 +0900

    test: 연관관계 설정에 따른 테스트 코드 수정

--- src/test/java/qna/domain/QuestionRepositoryTest.java
+++ src/test/java/qna/domain/QuestionRepositoryTest.java
@@ -1,6 +1,5 @@
 package qna.domain;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,7 +17,13 @@ public class QuestionRepositoryTest {
     @Autowired
     private QuestionRepository questionRepository;
 
-    private Question saveQuestion(Question question){
+    @Autowired
+    private UserRepository userRepository;
+
+    private Question saveQuestion(User user, Question question){
+
+        User saveUser = userRepository.save(user);
+        question.writeBy(saveUser);
         return questionRepository.save(question);
     }
 
@@ -26,8 +31,8 @@ public class QuestionRepositoryTest {
     @Test
     void saveQuestionTest() {
 
-        Question saveQuestion1 = saveQuestion(QuestionTest.Q1);
-        Question saveQuestion2 = saveQuestion(QuestionTest.Q2);
+        Question saveQuestion1 = saveQuestion(UserTest.JAVAJIGI, QuestionTest.Q1);
+        Question saveQuestion2 = saveQuestion(UserTest.SANJIGI, QuestionTest.Q2);
         assertAll(
                 () -> assertThat(saveQuestion1.getId()).isNotNull(),
                 () -> assertThat(saveQuestion1.getTitle()).isEqualTo(QuestionTest.Q1.getTitle()),
@@ -40,17 +45,17 @@ public class QuestionRepositoryTest {
     @Test
     void findByDeletedFalseTest() {
 
-        Question saveQuestion1 = saveQuestion(QuestionTest.Q1);
-        Question saveQuestion2 = saveQuestion(QuestionTest.Q2);
+        Question saveQuestion1 = saveQuestion(UserTest.JAVAJIGI, QuestionTest.Q1);
+        Question saveQuestion2 = saveQuestion(UserTest.SANJIGI, QuestionTest.Q2);
 
         assertEquals(2, questionRepository.findByDeletedFalse().size());
     }
+
     @DisplayName("QUESTION ID로 삭제되지 않은 질문을 확인한다.")
     @Test
     void findByIdAndDeletedFalseTest() {
 
-        Question saveQuestion2 = saveQuestion(QuestionTest.Q2);
-
+        Question saveQuestion2 = saveQuestion(UserTest.SANJIGI, QuestionTest.Q2);
         assertSame(saveQuestion2, questionRepository.findByIdAndDeletedFalse(saveQuestion2.getId()).get());
     }
 }
