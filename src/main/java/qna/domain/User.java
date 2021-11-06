89b4031ff784caef9eb6a0c736d3250253bff5ad
tyanotyrano <tyrano09@gmail.com>
2021-11-06 18:55:37 +0900

    feat(User.java): User 엔티티 클래스 작성
    User 엔티티 클래스를 작성한다.
    - User : User 엔티티 클래스 작성
    - README.md : 완료된 기능 요구사항 체크

--- src/main/java/qna/domain/User.java
+++ src/main/java/qna/domain/User.java
@@ -1,20 +1,36 @@
 package qna.domain;
 
-import qna.UnAuthorizedException;
-
 import java.util.Objects;
 
-public class User {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import qna.UnAuthorizedException;
+
+@Entity
+public class User extends BaseTimeEntity {
     public static final GuestUser GUEST_USER = new GuestUser();
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(length = 20, nullable = false)
     private String userId;
+
+    @Column(length = 20, nullable = false)
     private String password;
+
+    @Column(length = 20, nullable = false)
     private String name;
+
+    @Column(length = 50)
     private String email;
 
-    private User() {
-    }
+    protected User() {}
 
     public User(String userId, String password, String name, String email) {
         this(null, userId, password, name, email);
