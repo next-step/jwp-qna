8b40fcd0fff44c7b622b5630b21584de32722d4d
IW.MOON <mooniw02@gmail.com>
2021-11-07 00:45:43 +0900

    refactor: get, set 메서드 수정

--- src/main/java/qna/domain/User.java
+++ src/main/java/qna/domain/User.java
@@ -79,42 +79,17 @@ public class User extends DateTimeBaseEntity {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getUserId() {
         return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getEmail() {
         return email;
     }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
 
     @Override
     public boolean equals(Object o) {
