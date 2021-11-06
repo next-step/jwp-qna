1df636a87e65d21cfdfe0a691c666291ec08db2b
tyanotyrano <tyrano09@gmail.com>
2021-11-06 20:22:22 +0900

    refactor(BaseTimeEntity.java): BaseTimeEntity 에 getter 메소드 추가
    BaseTimeEntity 에 getter 메소드를 추가한다.
    - BaseTimeEntity : BaseTimeEntity 에 getter 메소드 추가

--- src/main/java/qna/domain/BaseTimeEntity.java
+++ src/main/java/qna/domain/BaseTimeEntity.java
@@ -21,4 +21,11 @@ public abstract class BaseTimeEntity {
 	@LastModifiedDate
 	private LocalDateTime updatedAt;
 
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
 }
