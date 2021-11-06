3f4e2a2b2bfebaf3164bafe3f09305eca3826fc7
tyanotyrano <tyrano09@gmail.com>
2021-11-06 18:39:32 +0900

    feat(DeleteHistory.java): DeleteHistory 엔티티 클래스 작성
    DeleteHistory 엔티티 클래스를 작성한다.
    - DeleteHistory : DeleteHistory 엔티티 클래스 작성
    - README.md : 완료된 기능 요구사항 체크

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 
 ### 1. DDL(Data Definition Language)을 보고 유추하여 엔티티 클래스와 리포지토리 클래스를 작성한다.
 - [X] Answer 엔티티 클래스 작성
-- [ ] DeleteHistory 엔티티 클래스 작성
+- [X] DeleteHistory 엔티티 클래스 작성
 - [ ] Question 엔티티 클래스 작성
 - [ ] User 엔티티 클래스 작성
 - [ ] 각 엔티티 별 리포지토리 클래스 확인
--- src/main/java/qna/domain/DeleteHistory.java
+++ src/main/java/qna/domain/DeleteHistory.java
@@ -3,12 +3,31 @@ package qna.domain;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class DeleteHistory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Enumerated(EnumType.STRING)
     private ContentType contentType;
+
     private Long contentId;
+
     private Long deletedById;
+
     private LocalDateTime createDate = LocalDateTime.now();
+
+    protected DeleteHistory() {}
 
     public DeleteHistory(ContentType contentType, Long contentId, Long deletedById, LocalDateTime createDate) {
         this.contentType = contentType;
