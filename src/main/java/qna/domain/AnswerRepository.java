cf10b8716d829da1d2f7762b967da7b9bdefc82f
devsigner <devsigner9920@icloud.com>
2021-11-07 23:31:31 +0900

    feat: 도메인별 저장소 JPA 학습 테스트를 위한 메소드 추가

--- src/main/java/qna/domain/AnswerRepository.java
+++ src/main/java/qna/domain/AnswerRepository.java
@@ -1,6 +1,8 @@
 package qna.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -9,4 +11,14 @@ public interface AnswerRepository extends JpaRepository<Answer, Long> {
   List<Answer> findByQuestionIdAndDeletedFalse(Long questionId);
 
   Optional<Answer> findByIdAndDeletedFalse(Long id);
+
+  List<Answer> findByWriterId(Long writerId);
+
+  @Modifying(clearAutomatically = true, flushAutomatically = true)
+  @Query("update Answer a set a.contents = ?2 where a.id = ?1")
+  int updateContentsById(Long id, String contents);
+
+  @Modifying(clearAutomatically = true, flushAutomatically = true)
+  @Query("update Answer a set a.deleted = ?2 where a.id = ?1")
+  int updateDeletedById(Long id, boolean deleted);
 }
--- src/main/java/qna/domain/UserRepository.java
+++ src/main/java/qna/domain/UserRepository.java
@@ -1,9 +1,15 @@
 package qna.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
   Optional<User> findByUserId(String userId);
+
+  @Modifying(clearAutomatically = true, flushAutomatically = true)
+  @Query("update User u set u.name = ?2 where u.id = ?1")
+  void updateNameById(long id, String name);
 }
