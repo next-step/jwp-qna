9446bcb0d52f7abd4213c8e3c00eadda3c031e22
Chloe <page.prologue@gmail.com>
2021-11-08 02:53:37 +0900

    feat : DeleteHistory 엔티티 매핑

--- src/main/java/qna/domain/DeleteHistory.java
+++ src/main/java/qna/domain/DeleteHistory.java
@@ -1,14 +1,30 @@
 package qna.domain;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Entity
 public class DeleteHistory {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
     private Long id;
+
+    @Enumerated(EnumType.STRING)
     private ContentType contentType;
+
+    @Column(updatable = false)
     private Long contentId;
+
+    @Column(updatable = false)
     private Long deletedById;
+
     private LocalDateTime createDate = LocalDateTime.now();
+
+    protected DeleteHistory() {
+    }
 
     public DeleteHistory(ContentType contentType, Long contentId, Long deletedById, LocalDateTime createDate) {
         this.contentType = contentType;
