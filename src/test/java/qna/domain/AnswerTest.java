ef9906ad8b6f9e71a446b3626c547441ee5b902d
keepbang <rlqud1125@naver.com>
2021-11-09 00:41:20 +0900

    feat: add answer update test

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,14 +1,17 @@
 package qna.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
@@ -19,15 +22,21 @@ public class AnswerTest {
     @Autowired
     private AnswerRepository answerRepository;
 
+    private Answer savedAnswer;
+
+    @BeforeEach
+    private void beforeAll(){
+        savedAnswer = answerRepository.save(A1);
+    }
+
     @Test
     @DisplayName("answer 등록")
     public void saveTest(){
-        Answer answer = answerRepository.save(A1);
         assertAll(
                 () -> assertThat(A1.getId()).isNotNull(),
-                () -> assertThat(A1.getWriterId()).isEqualTo(answer.getWriterId()),
-                () -> assertThat(A1.getQuestionId()).isEqualTo(answer.getQuestionId()),
-                () -> assertThat(A1.getContents()).isEqualTo(answer.getContents()),
+                () -> assertThat(A1.getWriterId()).isEqualTo(savedAnswer.getWriterId()),
+                () -> assertThat(A1.getQuestionId()).isEqualTo(savedAnswer.getQuestionId()),
+                () -> assertThat(A1.getContents()).isEqualTo(savedAnswer.getContents()),
                 () -> assertFalse(A1.isDeleted())
         );
     }
@@ -35,8 +44,6 @@ public class AnswerTest {
     @Test
     @DisplayName("question id로 deleted가 false인 answer 검색")
     public void findByQuestionIdAndDeletedFalseTest(){
-        Answer savedAnswer = answerRepository.save(A1);
-
         List<Answer> answers = answerRepository.findByQuestionIdAndDeletedFalse(QuestionTest.Q1.getId());
 
         assertAll(
@@ -48,15 +55,27 @@ public class AnswerTest {
     @Test
     @DisplayName("answer id로 deleted가 false인 answer 단일검색")
     public void findByIdAndDeletedFalseTest(){
-        Answer savedAnswer = answerRepository.save(A1);
-
         Optional<Answer> oAnswer = answerRepository.findByIdAndDeletedFalse(savedAnswer.getId());
 
+        assertAnswerDeleted(oAnswer, false);
+
+    }
+
+    @Test
+    @DisplayName("answer에 delete를 true로 수정")
+    public void updateAnswerDeletedTrue(){
+        savedAnswer.setDeleted(true);
+
+        Optional<Answer> oAnswer = answerRepository.findById(savedAnswer.getId());
+
+        assertAnswerDeleted(oAnswer, true);
+    }
+
+    private void assertAnswerDeleted(Optional<Answer> oAnswer, boolean isDeleted) {
         assertAll(
                 () -> assertEquals(oAnswer.get(), savedAnswer),
-                () -> assertFalse(oAnswer.get().isDeleted())
+                () -> assertThat(oAnswer.get().isDeleted()).isEqualTo(isDeleted)
         );
-
     }
 
 
