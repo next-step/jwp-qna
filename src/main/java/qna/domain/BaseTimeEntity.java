596e91bd505ea49298c59da0ef8696934f0655bf
devsigner <devsigner9920@icloud.com>
2021-11-09 02:07:17 +0900

    refactor: 요구사항 미반영 수정 완료

--- src/main/java/qna/domain/BaseTimeEntity.java
+++ src/main/java/qna/domain/BaseTimeEntity.java
@@ -4,17 +4,18 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.EntityListeners;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseTimeEntity {
   @CreatedDate
+  @Column(columnDefinition = "datetime(6)", nullable = false)
   private LocalDateTime createdAt;
 
   @LastModifiedDate
+  @Column(columnDefinition = "datetime(6)")
   private LocalDateTime updatedAt;
 
   public LocalDateTime getCreatedAt() {
