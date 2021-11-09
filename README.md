64e56ce29bee0e600ce397f75d991246dd07041e
김도영 <95doyoung@gmail.com>
2021-11-10 01:49:41 +0900

    feat :  user 테이블 구현 및 테스트

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 - [x] answer 테이블 구현 및 `@DataJpaTest` 사용한 학습 테스트
 - [x] delete_history 테이블 구현 및 `@DataJpaTest` 사용한 학습 테스트
 - [x] question 테이블 구현 및 `@DataJpaTest` 사용한 학습 테스트
-- [ ] user 테이블 구현 및 `@DataJpaTest` 사용한 학습 테스트
+- [x] user 테이블 구현 및 `@DataJpaTest` 사용한 학습 테스트
---
+++ src/main/java/qna/domain/user/User.java
@@ -0,0 +1,142 @@
+package qna.domain.user;
+
+import qna.UnAuthorizedException;
+import qna.domain.BaseTimeEntity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import java.util.Objects;
+
+@Entity
+@Table(uniqueConstraints = {
+        @UniqueConstraint(name = "UK_a3imlf41l37utmxiquukk8ajc", columnNames = {"userId"})
+})
+public class User extends BaseTimeEntity {
+    public static final GuestUser GUEST_USER = new GuestUser();
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, length = 20)
+    private String userId;
+    @Column(nullable = false, length = 20)
+    private String password;
+    @Column(nullable = false, length = 20)
+    private String name;
+    @Column(length = 50)
+    private String email;
+
+    public User() {
+    }
+
+    public User(String userId, String password, String name, String email) {
+        this(null, userId, password, name, email);
+    }
+
+    public User(Long id, String userId, String password, String name, String email) {
+        this.id = id;
+        this.userId = userId;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+    }
+
+    public void update(User loginUser, User target) {
+        if (!matchUserId(loginUser.userId)) {
+            throw new UnAuthorizedException();
+        }
+
+        if (!matchPassword(target.password)) {
+            throw new UnAuthorizedException();
+        }
+
+        this.name = target.name;
+        this.email = target.email;
+    }
+
+    private boolean matchUserId(String userId) {
+        return this.userId.equals(userId);
+    }
+
+    public boolean matchPassword(String targetPassword) {
+        return this.password.equals(targetPassword);
+    }
+
+    public boolean equalsNameAndEmail(User target) {
+        if (Objects.isNull(target)) {
+            return false;
+        }
+
+        return name.equals(target.name) &&
+                email.equals(target.email);
+    }
+
+    public boolean isGuestUser() {
+        return false;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (id != null ? !id.equals(user.id) : user.id != null) return false;
+        return userId != null ? userId.equals(user.userId) : user.userId == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (userId != null ? userId.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", userId='" + userId + '\'' +
+                ", password='" + password + '\'' +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    private static class GuestUser extends User {
+        @Override
+        public boolean isGuestUser() {
+            return true;
+        }
+    }
+}
