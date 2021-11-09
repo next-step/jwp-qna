8f27490ca0a494541df30c37117eab4b0152309f
송성빈 <ssb1225@midasit.com>
2021-11-09 13:52:07 +0900

    fix(DeleteHistory, Question, user): 테스트 실행을 위한 임시 수정

--- src/main/java/qna/domain/Question.java
+++ src/main/java/qna/domain/Question.java
@@ -1,11 +1,20 @@
 package qna.domain;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "question")
 public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String contents;
     private Long writerId;
     private boolean deleted = false;
+
+    protected Question() {
+    }
 
     public Question(String title, String contents) {
         this(null, title, contents);
