61f78db795853e0cc869d8513f2a64ac6a8e65e3
jerry92k <jens92.k@gmail.com>
2021-11-09 10:41:59 +0900

    feat(DeleteHistoryRepository): 학습테스트

--- src/main/java/qna/domain/DeleteHistoryRepository.java
+++ src/main/java/qna/domain/DeleteHistoryRepository.java
@@ -1,6 +1,14 @@
 package qna.domain;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DeleteHistoryRepository extends JpaRepository<DeleteHistory, Long> {
+
+	Optional<DeleteHistory> findById(Long id);
+	List<DeleteHistory> findAll();
+	List<DeleteHistory> findByDeletedById(Long id);
+
 }
---
+++ src/test/java/qna/domain/DeleteHistoryRepositoryTest.java
@@ -0,0 +1,51 @@
+package qna.domain;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+class DeleteHistoryRepositoryTest {
+
+	@Autowired
+	private DeleteHistoryRepository deleteHistoryRepository;
+
+	@Test
+	void save() {
+		DeleteHistory deleteHistory = new DeleteHistory(ContentType.QUESTION, QuestionTest.Q1.getId(), UserTest.SANJIGI.getId(),LocalDateTime.now());
+		DeleteHistory actual = deleteHistoryRepository.save(deleteHistory);
+		assertThat(deleteHistory).isEqualTo(actual);
+	}
+
+	@Test
+	public void findById(){
+		DeleteHistory savedHistory = deleteHistoryRepository.save(new DeleteHistory(ContentType.QUESTION, QuestionTest.Q1.getId(), UserTest.SANJIGI.getId(),LocalDateTime.now()));
+		Optional<DeleteHistory> findDeletedHistory = deleteHistoryRepository.findById(savedHistory.getId());
+		assertThat(findDeletedHistory.get()).isEqualTo(savedHistory);
+	}
+
+	@Test
+	public void findByAll(){
+		DeleteHistory savedHistory1 = deleteHistoryRepository.save(new DeleteHistory(ContentType.QUESTION, QuestionTest.Q1.getId(), UserTest.SANJIGI.getId(),LocalDateTime.now()));
+		DeleteHistory savedHistory2 = deleteHistoryRepository.save(new DeleteHistory(ContentType.ANSWER, AnswerTest.A1.getId(), UserTest.JAVAJIGI.getId(),LocalDateTime.now()));
+		List<DeleteHistory> findDeletedHistories = deleteHistoryRepository.findAll();
+		assertThat(findDeletedHistories.size()).isEqualTo(2);
+		assertThat(findDeletedHistories.contains(savedHistory1)).isTrue();
+		assertThat(findDeletedHistories.contains(savedHistory2)).isTrue();
+	}
+
+	@Test
+	public void findByDeletedById(){
+		Long deletedById= UserTest.SANJIGI.getId();
+		DeleteHistory savedHistory = deleteHistoryRepository.save(new DeleteHistory(ContentType.QUESTION, QuestionTest.Q1.getId(), deletedById,LocalDateTime.now()));
+		List<DeleteHistory> findDeletedHistories = deleteHistoryRepository.findByDeletedById(deletedById);
+		assertThat(findDeletedHistories.contains(savedHistory)).isTrue();
+	}
+}
