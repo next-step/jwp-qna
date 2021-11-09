2257fc9b7f89e925df02734e4670aa96b06794c0
cus4703 <cus4703@gmail.com>
2021-11-10 00:55:09 +0900

    refactor: 연관관계 테스트 케이스 추가

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,23 +1,32 @@
 package qna.domain;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.Optional;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static qna.domain.AnswerTest.*;
 
 @DataJpaTest
 public class QuestionTest {
-    public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
-    public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+    public static final Question Q1 = new Question(null, "title1", "contents1").writeBy(UserTest.JAVAJIGI);
+    public static final Question Q2 = new Question(null, "title2", "contents2").writeBy(UserTest.SANJIGI);
 
     @Autowired
     private QuestionRepository questionRepository;
+    @Autowired
+    private AnswerRepository answerRepository;
+    @Autowired
+    private UserRepository userRepository;
 
     private static Stream<Arguments> providerQuestions() {
         return Stream.of(
@@ -28,14 +37,37 @@ public class QuestionTest {
 
     @ParameterizedTest
     @MethodSource("providerQuestions")
+    @DisplayName("저장을 시도하는 객체와 저장후 반환되는 객체가 동일한지 체크한다.")
     public void 질문_생성(Question excepted) {
         Question actual = questionRepository.save(excepted);
 
         assertAll(
                 () -> assertThat(actual.getId()).isEqualTo(excepted.getId()),
-                () -> assertThat(actual.getWriterId()).isEqualTo(excepted.getWriterId()),
+                () -> assertThat(actual.getWriter()).isEqualTo(excepted.getWriter()),
                 () -> assertThat(actual.getContents()).isEqualTo(excepted.getContents()),
                 () -> assertThat(actual.getTitle()).isEqualTo(excepted.getTitle())
+        );
+    }
+
+    @BeforeEach
+    void setup() {
+        userRepository.save(US);
+        userRepository.save(JH);
+        questionRepository.save(QUESTION);
+        answerRepository.save(ANSWER);
+    }
+
+    @Test
+    @DisplayName("Question Entity 연관관계 정상 확인")
+    public void 연관관계_확인() {
+        Optional<Question> findOneQuestion = questionRepository.findById(QUESTION.getId());
+
+        Question excepted = findOneQuestion.orElse(null);
+
+        assertAll(
+                () -> assertThat(excepted.getWriter()).isEqualTo(US),
+                () -> assertThat(excepted.getContents()).isEqualTo(QUESTION.getContents()),
+                () -> assertThat(excepted.getTitle()).isEqualTo(QUESTION.getTitle())
         );
     }
 }
