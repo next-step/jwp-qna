117e874bf4c2a3de571d2fbbab3639e42ee4e9af
keepbang <rlqud1125@naver.com>
2021-11-09 21:36:35 +0900

    test: User 테스트 작성

--- src/main/java/qna/domain/User.java
+++ src/main/java/qna/domain/User.java
@@ -28,10 +28,10 @@ public class User {
     private String email;
 
     @Column(name = "created_at", nullable = false)
-    private LocalDateTime createdAt;
+    private LocalDateTime createdAt = LocalDateTime.now();
 
     @Column(name = "updated_at")
-    private LocalDateTime updatedAt;
+    private LocalDateTime updatedAt = LocalDateTime.now();
 
     private User() {
     }
@@ -86,40 +86,23 @@ public class User {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id)
+                && Objects.equals(userId, user.userId)
+                && Objects.equals(password, user.password)
+                && Objects.equals(name, user.name)
+                && Objects.equals(email, user.email)
+                && Objects.equals(createdAt, user.createdAt)
+                && Objects.equals(updatedAt, user.updatedAt);
     }
 
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, userId, password, name, email, createdAt, updatedAt);
     }
 
     @Override
--- src/test/java/qna/domain/UserTest.java
+++ src/test/java/qna/domain/UserTest.java
@@ -1,6 +1,52 @@
 package qna.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
 public class UserTest {
     public static final User JAVAJIGI = new User(1L, "javajigi", "password", "name", "javajigi@slipp.net");
     public static final User SANJIGI = new User(2L, "sanjigi", "password", "name", "sanjigi@slipp.net");
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User saveUser(){
+        return userRepository.save(JAVAJIGI);
+    }
+
+    @Test
+    @DisplayName("user 등록")
+    public void saveUserTest(){
+        User savedUser = saveUser();
+
+        assertEquals(savedUser, JAVAJIGI);
+    }
+
+    @Test
+    @DisplayName("user id로 검색")
+    public void findByUserIdTest(){
+        User savedUser = saveUser();
+
+        Optional<User> oUser = userRepository.findByUserId("javajigi");
+
+        assertTrue(oUser.isPresent());
+        assertEquals(oUser.get(), savedUser);
+    }
+
+    @Test
+    @DisplayName("찾는 user id가 없을 경우")
+    public void findByUserIdNotFoundTest(){
+        saveUser();
+
+        Optional<User> oUser = userRepository.findByUserId("sanjigi");
+
+        assertFalse(oUser.isPresent());
+    }
 }
