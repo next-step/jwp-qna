f42ba7e4a970d31ad890711149ce997e2832429e
jerry92k <jens92.k@gmail.com>
2021-11-09 11:01:57 +0900

    chore: 미사용 import 삭제

--- src/test/java/qna/repository/QuestionRepositoryTest.java
+++ src/test/java/qna/repository/QuestionRepositoryTest.java
@@ -9,9 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import qna.domain.Answer;
 import qna.domain.Question;
-import qna.domain.QuestionTest;
 import qna.domain.UserTest;
 
 @DataJpaTest
