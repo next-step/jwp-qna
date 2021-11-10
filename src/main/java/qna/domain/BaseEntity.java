5f561dac90d1ca9ccd36e222923623054791fdba
haedoang <42138469+haedoang@users.noreply.github.com>
2021-11-10 12:09:10 +0900

    1단계 - 엔티티 매핑 (#220)
    * docs: update ReadMe.md
    
    - add step1 mission
    
    * feat: 엔티티 작성
    
    - Answer, DeleteHistory, Question, User Entity 작성
    - 도메인 테스트 클래스 생성
    - 테스트용 StringUtil 클래스 생성

---
+++ src/main/java/qna/domain/BaseEntity.java
@@ -0,0 +1,31 @@
+package qna.domain;
+
+import jdk.nashorn.internal.objects.annotations.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+/**
+ * packageName : qna.domain
+ * fileName : BaseEntity
+ * author : haedoang
+ * date : 2021-11-09
+ * description : 공통 엔티티 클래스
+ */
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+}
