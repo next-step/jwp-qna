805167c8cec52781e14cdb070720eead8f924780
vicjed <vicjed@icloud.com>
2021-11-10 14:51:55 +0900

    test(UserRepository) : 유저Id로 유저 정보 조회 추가

--- src/main/java/qna/domain/UserRepository.java
+++ src/main/java/qna/domain/UserRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+import javax.swing.text.html.Option;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUserId(String userId);
+
+	Optional<User> findByName(String userId);
 }
