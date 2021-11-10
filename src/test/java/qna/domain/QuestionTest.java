8a2e01c286f35685e1a68fbc5ee47e0ab1798653
vicjed <vicjed@icloud.com>
2021-11-10 14:45:48 +0900

    test(QuestionTest) : Question 테스트 추가

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,49 +1,51 @@
 package qna.domain;
 
-
-
-
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Arrays;
 import java.util.List;
 
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 @DataJpaTest
 public class QuestionTest {
-    public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
-    public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+	public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
+	public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
 
-    @Autowired
-    private QuestionRepository questionRepository;
+	@Autowired
+	private QuestionRepository questionRepository;
 
-    @Test
-    void save() {
+	@Test
+	@DisplayName("두건의 데이터가 저장되었는지 검증 ")
+	void save() {
 
-        // when
-        List<Question> questions = questionRepository.saveAll(Arrays.asList(Q1, Q2));
+		// when
+		List<Question> questions = questionRepository.saveAll(Arrays.asList(Q1, Q2));
 
-        // then
-        assertAll(
-            ()-> assertEquals(questions.get(0),Q1),
-            ()-> assertEquals(questions.get(1),Q2)
-        );
-    }
+		List<Question> questionAll = questionRepository.findAll();
 
-    @Test
-    void findByDeletedFalse() {
-        List<Question> questions = questionRepository.saveAll(Arrays.asList(Q1, Q2));
-        Question question = questions.get(0);
-        question.setDeleted(true);
+		// then
+		assertAll(
+			() -> assertEquals(questions.get(0), Q1),
+			() -> assertEquals(questions.get(1), Q2),
+			() -> assertEquals(questionAll.size(), 2)
+		);
+	}
 
-        // when
-        List<Question> byDeletedFalse = questionRepository.findByDeletedFalse();
+	@Test
+	@DisplayName("전체 데이터수 - (삭제여부 true) = 전체 건수 검증")
+	void findByDeletedFalse() {
+		List<Question> questions = questionRepository.saveAll(Arrays.asList(Q1, Q2));
+		Question question = questions.get(0);
+		question.setDeleted(true);
 
-        // then
-        assertEquals(byDeletedFalse.size(),1);
-    }
+		// when
+		List<Question> byDeletedFalse = questionRepository.findByDeletedFalse();
+
+		// then
+		assertEquals(byDeletedFalse.size(), 1);
+	}
 }
