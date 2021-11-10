b8d0fb2ceab7d1eaf7267515bdb7e0de47a76ba3
cus4703 <cus4703@gmail.com>
2021-11-11 02:23:20 +0900

    refactor: DeleteHistory 검증 및 insertable 옵션값 삭제

--- src/main/java/qna/domain/DeleteHistory.java
+++ src/main/java/qna/domain/DeleteHistory.java
@@ -1,8 +1,13 @@
 package qna.domain;
 
+import org.springframework.data.annotation.CreatedDate;
+import qna.UnAuthorizedException;
+
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.Objects;
+
+import static qna.utils.ValidationUtils.isEmpty;
 
 @Entity
 @Table(name = "delete_history")
@@ -21,43 +26,45 @@ public class DeleteHistory {
     private Long contentId;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "deleted_by_id", insertable = false, updatable = false, foreignKey = @ForeignKey(name="fk_delete_history_to_user"))
+    @JoinColumn(name = "deleted_by_id", updatable = false, foreignKey = @ForeignKey(name = "fk_delete_history_to_user"))
     private User deletedByUser;
 
-    private LocalDateTime createDate = LocalDateTime.now();
+    @CreatedDate
+    private LocalDateTime createDate;
 
-    public DeleteHistory(ContentType contentType, Long contentId, User deletedByUser, LocalDateTime createDate) {
+    public DeleteHistory(ContentType contentType, Long contentId, User deletedByUser) {
+        validation(deletedByUser);
         this.contentType = contentType;
         this.contentId = contentId;
         this.deletedByUser = deletedByUser;
-        this.createDate = createDate;
     }
 
-    protected DeleteHistory() {}
+    protected DeleteHistory() {
+    }
+
+    private void validation(User deletedByUser) {
+        if (isEmpty(deletedByUser)) {
+            throw new UnAuthorizedException("삭제한 사용자가 없습니다.");
+        }
+    }
+
+    public User getDeletedByUser() {
+        return deletedByUser;
+    }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         DeleteHistory that = (DeleteHistory) o;
-        return Objects.equals(id, that.id) && contentType == that.contentType
+        return Objects.equals(id, that.id)
+                && contentType == that.contentType
                 && Objects.equals(contentId, that.contentId)
                 && Objects.equals(deletedByUser, that.deletedByUser);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, contentType, contentId, deletedByUser, createDate);
-    }
-
-    @Override
-    public String toString() {
-        return "DeleteHistory{" +
-                "id=" + id +
-                ", contentType=" + contentType +
-                ", contentId=" + contentId +
-                ", deletedByUser=" + deletedByUser +
-                ", createDate=" + createDate +
-                '}';
+        return Objects.hash(id, contentType, contentId, deletedByUser);
     }
 }
