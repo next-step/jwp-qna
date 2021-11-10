7b6a8372f20447f49b5deb77c6442083adc4de1d
cus4703 <cus4703@gmail.com>
2021-11-11 02:24:20 +0900

    refactor: User 검증 부분 수정

--- src/main/java/qna/domain/User.java
+++ src/main/java/qna/domain/User.java
@@ -1,10 +1,12 @@
 package qna.domain;
 
-import qna.UnAuthorizedException;
+import qna.NotFoundException;
 
 import javax.persistence.*;
 import java.util.List;
 import java.util.Objects;
+
+import static qna.utils.ValidationUtils.isEmpty;
 
 @Entity
 @Table(name = "user")
@@ -39,102 +41,48 @@ public class User extends BaseEntity {
     private List<Question> questions;
 
     public User(String userId, String password, String name, String email) {
-        this(null, userId, password, name, email);
-    }
-
-    public User(Long id, String userId, String password, String name, String email) {
-        this.id = id;
+        validation(userId, password);
         this.userId = userId;
         this.password = password;
         this.name = name;
         this.email = email;
     }
 
-    protected User() {}
+    protected User() {
+    }
 
-    public void update(User loginUser, User target) {
-        if (!matchUserId(loginUser.userId)) {
-            throw new UnAuthorizedException();
+    private void validation(String userId, String password) {
+        if (isEmpty(userId)) {
+            throw new NotFoundException("아이디 입력은 필수입니다.");
         }
-
-        if (!matchPassword(target.password)) {
-            throw new UnAuthorizedException();
+        if (isEmpty(password)) {
+            throw new NotFoundException("패스워드 입력은 필수입니다.");
         }
-
-        this.name = target.name;
-        this.email = target.email;
-    }
-
-    private boolean matchUserId(String userId) {
-        return this.userId.equals(userId);
-    }
-
-    public boolean matchPassword(String targetPassword) {
-        return this.password.equals(targetPassword);
-    }
-
-    public boolean equalsNameAndEmail(User target) {
-        if (Objects.isNull(target)) {
-            return false;
-        }
-
-        return name.equals(target.name) &&
-                email.equals(target.email);
-    }
-
-    public boolean isGuestUser() {
-        return false;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public String getUserId() {
         return userId;
     }
 
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
+    public boolean isGuestUser() {
+        return false;
     }
 
     @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", userId='" + userId + '\'' +
-                ", password='" + password + '\'' +
-                ", name='" + name + '\'' +
-                ", email='" + email + '\'' +
-                '}';
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id)
+                && Objects.equals(userId, user.userId)
+                && Objects.equals(password, user.password)
+                && Objects.equals(name, user.name)
+                && Objects.equals(email, user.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, userId, password, name, email);
     }
 
     private static class GuestUser extends User {
