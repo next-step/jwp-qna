72b8a595a36911881f924ce581f81845387d7130
ch3224bin <ahrocksil@gmail.com>
2021-11-11 12:23:21 +0900

    test : DeleteHistoryTest, DeleteHistoryRepositoryTest 분리

--- src/test/java/qna/domain/DeleteHistoryTest.java
+++ src/test/java/qna/domain/DeleteHistoryTest.java
@@ -1,33 +1,26 @@
 package qna.domain;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.within;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
-@DataJpaTest
 public class DeleteHistoryTest {
 
-    private DeleteHistoryRepository deleteHistoryRepository;
-
-    @Autowired
-    public DeleteHistoryTest(DeleteHistoryRepository deleteHistoryRepository) {
-        this.deleteHistoryRepository = deleteHistoryRepository;
-    }
-
+    @DisplayName("DeleteHistory를 생성한다")
     @Test
-    void testEquals() {
+    void testCrete() {
         DeleteHistory deleteHistory = new DeleteHistory(ContentType.ANSWER, 1L, 1L, LocalDateTime.now());
-        DeleteHistory savedDeleteHistory = deleteHistoryRepository.save(deleteHistory);
-        assertThat(savedDeleteHistory.getId()).isNotNull();
-        assertThat(savedDeleteHistory.getContentType()).isEqualTo(deleteHistory.getContentType());
-        assertThat(savedDeleteHistory.getContentId()).isEqualTo(deleteHistory.getContentId());
-        assertThat(savedDeleteHistory.getDeletedById()).isEqualTo(deleteHistory.getDeletedById());
-        assertThat(savedDeleteHistory.getCreateDate()).isCloseTo(LocalDateTime.now(), within(1, ChronoUnit.SECONDS));
+        assertAll(
+            () -> assertThat(deleteHistory.getContentType()).isEqualTo(deleteHistory.getContentType()),
+            () -> assertThat(deleteHistory.getContentId()).isEqualTo(deleteHistory.getContentId()),
+            () -> assertThat(deleteHistory.getDeletedById()).isEqualTo(deleteHistory.getDeletedById()),
+            () -> assertThat(deleteHistory.getCreateDate()).isCloseTo(LocalDateTime.now(), within(1, ChronoUnit.SECONDS))
+        );
     }
 }
