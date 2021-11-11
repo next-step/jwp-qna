fedc917ce032b1eefff6ef12e72967cddd4b866b
Jeongsu Yang <mycat83@gmail.com>
2021-11-11 23:14:10 +0900

    Revert "feat(subway): JPA 학습 테스트"
    This reverts commit de4e3da835a3d55a293daa05e599c23df8c4ffa5.

--- src/main/java/subway/domain/StationRepository.java
+++
@@ -1,8 +0,0 @@
-package subway.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StationRepository extends JpaRepository<Station, Long> {
-
-    Station findByName(String name);
-}
