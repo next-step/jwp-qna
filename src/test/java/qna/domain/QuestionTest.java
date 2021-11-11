26fa79ce5a27336280aae4f60edd483ffc04a450
Jeongsu Yang <mycat83@gmail.com>
2021-11-11 23:08:15 +0900

    test(qna): 답변이 있는 질문을 삭제하면 답변 삭제 테스트

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -14,18 +14,8 @@ public class QuestionTest {
     public static final Question NOT_ANSWERED_QUESTION = new Question("title3", "contents3").writeBy(UserTest.JAVAJIGI);
 
     @Test
-    @DisplayName("답변이 있는 질문을 삭제한다.")
-    void delete1() {
-        // when
-        ONE_ANSWERED_QUESTION.delete(UserTest.JAVAJIGI);
-
-        // then
-        assertThat(ONE_ANSWERED_QUESTION.isDeleted()).isTrue();
-    }
-
-    @Test
     @DisplayName("답변이 없는 질문을 삭제한다.")
-    void delete2() {
+    void delete1() {
         // when
         NOT_ANSWERED_QUESTION.delete(UserTest.JAVAJIGI);
 
@@ -34,8 +24,22 @@ public class QuestionTest {
     }
 
     @Test
+    @DisplayName("답변이 있는 질문을 삭제하면 답변도 삭제한다.")
+    void delete2() {
+        // given
+        ONE_ANSWERED_QUESTION.addAnswer(AnswerTest.A3);
+
+        // when
+        ONE_ANSWERED_QUESTION.delete(UserTest.JAVAJIGI);
+
+        // then
+        assertThat(ONE_ANSWERED_QUESTION.isDeleted()).isTrue();
+        assertThat(AnswerTest.A3.isDeleted()).isTrue();
+    }
+
+    @Test
     @DisplayName("질문자가 아닌 사용자가 질문을 삭제하면 예외가 발생한다.")
-    void deleteThrowException() {
+    void deleteThrowException1() {
         // when & then
         assertThatExceptionOfType(CannotDeleteException.class)
                 .isThrownBy(() -> TWO_ANSWERED_QUESTION.delete(UserTest.SANJIGI))
