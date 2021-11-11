c422c544db41af50c6e2cfbecbb57b7bc00348b4
moon <hoopster@airi.kr>
2021-11-11 16:03:26 +0900

    refac (Test) : 엔티티 연관관계 적용으로 인한 테스트 리팩토링

--- src/test/java/qna/domain/UserTest.java
+++ src/test/java/qna/domain/UserTest.java
@@ -19,7 +19,7 @@ public class UserTest {
 	UserRepository userRepository;
 
 	@Test
-	void inputNullUserId() {
+	void NOTNULL_값에_NULL이_들어가는트_경우_예외처리_테스트() {
 		// given
 		User user = new User(null, "test", "password", "name");
 
@@ -31,19 +31,18 @@ public class UserTest {
 	}
 
 	@Test
-	void inputEmailOverLengthRange() {
+	void 이메일_최대길이_제한_예외처리_테스트() {
 		// given
-		String email = "12345678910123456789qweqw4e56qw4e56wq45e4wq56e45wq64e65wq45e6wq121233";
-		SANJIGI.setEmail(email);
-
+		User user = new User(null, "test", "password",
+			"12345678910123456789qweqw4e56qw4e56wq45e4wq56e45wq64e65wq45e6wq121233");
 		// when // then
 		assertThatThrownBy(() -> {
-			userRepository.save(SANJIGI);
+			userRepository.save(user);
 		}).isInstanceOf(DataIntegrityViolationException.class);
 	}
 
 	@Test
-	void findById() {
+	void 유저_조회_테스트() {
 		// given
 		User user = new User("id", "test", "password", "name");
 		User expectUser = userRepository.save(user);
