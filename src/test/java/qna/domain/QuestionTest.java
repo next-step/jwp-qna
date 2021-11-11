5075a50effc9a8b92b32c162518a46c054dc483b
wooobo <irooori89@gmail.com>
2021-11-11 10:13:06 +0900

    feat : QuestionTest 테스트 케이스 추가

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,7 +1,9 @@
 package qna.domain;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -22,6 +24,47 @@ public class QuestionTest {
         Question actual = questions.findByTitle(Q1.getTitle()).get();
 
         // then
-        assertThat(actual).isEqualTo(expected);
+        assertAll(
+            () -> assertThat(actual).isEqualTo(expected),
+            () -> assertThat(actual.getTitle()).isEqualTo(expected.getTitle()),
+            () -> assertThat(actual.getContents()).isEqualTo(expected.getContents())
+        );
+    }
+
+    @Test
+    void writerId_작성자_일치() {
+
+        // when
+        Question question = questions.save(Q2);
+
+        // then
+        assertThat(question.getWriterId()).isEqualTo(UserTest.SANJIGI.getId());
+    }
+
+    @Test
+    @DisplayName("Question 을 통해 Answer 에 question.getId() 등록")
+    void addAnswer() {
+        // given
+        Question actual = questions.save(Q2);
+        Answer expect = AnswerTest.A1;
+
+        // when
+        actual.addAnswer(expect);
+
+        // then
+        assertThat(expect.getQuestionId()).isEqualTo(actual.getId());
+    }
+
+    @Test
+    @DisplayName("Question 의 작성 User 확인")
+    void isOwner() {
+        // when
+        Question actual = questions.save(Q2);
+
+        // then
+        assertAll(
+            () -> assertThat(actual.isOwner(UserTest.SANJIGI)).isTrue(),
+            () -> assertThat(actual.isOwner(UserTest.JAVAJIGI)).isFalse()
+        );
     }
 }
