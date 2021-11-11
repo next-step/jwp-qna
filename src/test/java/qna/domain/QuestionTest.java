da877c5fb670bfa63d0f18a92a09befe084cf497
jerry92k <jens92.k@gmail.com>
2021-11-11 18:07:23 +0900

    chore: 미사용 import 삭제

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -8,9 +8,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import qna.NotFoundException;
-import qna.UnAuthorizedException;
-
 @DataJpaTest
 public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
