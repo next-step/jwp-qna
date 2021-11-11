edc76d1d122a790e00475d223b06a7397e202545
jerry92k <jens92.k@gmail.com>
2021-11-11 09:53:42 +0900

    feat(Question): 연관관계 맵핑과 Equals 수정
    - Long writerId -> User Entity와 연관관계 맵핑
    - Answer에 대하여 일대다 양방향 연관관계 맵핑
    - Equals에 비즈니스 키만 포함되도록 변경

--- src/test/java/qna/repository/QuestionRepositoryTest.java
+++ src/test/java/qna/repository/QuestionRepositoryTest.java
@@ -5,11 +5,16 @@ import static org.assertj.core.api.Assertions.*;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import qna.domain.Question;
+import qna.domain.User;
 import qna.domain.UserTest;
 
 @DataJpaTest
@@ -18,16 +23,26 @@ class QuestionRepositoryTest {
 	@Autowired
 	QuestionRepository questionRepository;
 
+	@PersistenceContext
+	public EntityManager em;
+
+	private User user1 = new User( "tom1234", "password", "tom", "tom1232@slipp.net");
+
+	@BeforeEach
+	public void setup(){
+		em.persist(user1);
+	}
+
 	@Test
 	public void save(){
-		Question question = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
+		Question question = new Question("title1", "contents1").writeBy(user1);
 		Question actual = questionRepository.save(question);
 		assertThat(question).isEqualTo(actual);
 	}
 
 	@Test
 	public void findByDeletedFalse(){
-		Question savedQuestion = questionRepository.save(new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI));
+		Question savedQuestion = questionRepository.save(new Question("title1", "contents1").writeBy(user1));
 		List<Question> findQuestions = questionRepository.findByDeletedFalse();
 		assertThat(findQuestions.contains(savedQuestion)).isTrue();
 
@@ -39,7 +54,7 @@ class QuestionRepositoryTest {
 
 	@Test
 	public void findByIdAndDeletedFalse(){
-		Question savedQuestion = questionRepository.save(new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI));
+		Question savedQuestion = questionRepository.save(new Question("title1", "contents1").writeBy(user1));
 		Optional<Question> findQuestion = questionRepository.findByIdAndDeletedFalse(savedQuestion.getId());
 		assertThat(findQuestion.isPresent()).isTrue();
 
