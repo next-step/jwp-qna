5ca44301d817ab1e536351151fb5712d1dec8b54
ch3224bin <ahrocksil@gmail.com>
2021-11-11 12:17:02 +0900

    test : AnswerTest, AnswerRepositoryTest 분리

---
+++ src/test/java/qna/domain/AnswerRepositoryTest.java
@@ -0,0 +1,32 @@
+package qna.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+@DataJpaTest
+public class AnswerRepositoryTest {
+    private AnswerRepository answerRepository;
+
+    @Autowired
+    public AnswerRepositoryTest(AnswerRepository answerRepository) {
+        this.answerRepository = answerRepository;
+    }
+
+    @DisplayName("Answer가 저장된다")
+    @Test
+    void testSave() {
+        Answer answer = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
+        Answer savedAnswer = answerRepository.save(answer);
+        assertAll(
+                () -> assertThat(savedAnswer.getId()).isNotNull(),
+                () -> assertThat(savedAnswer.getWriterId()).isEqualTo(answer.getWriterId()),
+                () -> assertThat(savedAnswer.getQuestionId()).isEqualTo(answer.getQuestionId()),
+                () -> assertThat(savedAnswer.getContents()).isEqualTo(answer.getContents())
+        );
+    }
+}
--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,43 +1,41 @@
 package qna.domain;
 
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import qna.NotFoundException;
+import qna.UnAuthorizedException;
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
-import java.util.Arrays;
-import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.within;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
-@DataJpaTest
 public class AnswerTest {
-    public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
-    public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
-
-    private AnswerRepository answerRepository;
-
-    @Autowired
-    public AnswerTest(AnswerRepository answerRepository) {
-        this.answerRepository = answerRepository;
+    @DisplayName("Answer를 객체 생성한다")
+    @Test
+    void testCreate() {
+        Answer answer = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
+        assertAll(
+                () -> assertThat(answer.getWriterId()).isEqualTo(answer.getWriterId()),
+                () -> assertThat(answer.getQuestionId()).isEqualTo(answer.getQuestionId()),
+                () -> assertThat(answer.getContents()).isEqualTo(answer.getContents()),
+                () -> assertThat(answer.getCreatedAt()).isCloseTo(LocalDateTime.now(), within(1, ChronoUnit.SECONDS)),
+                () -> assertThat(answer.getUpdatedAt()).isNull()
+        );
     }
 
-    @ParameterizedTest
-    @MethodSource("testAnswerList")
-    void testEquals(Answer answer) {
-        Answer savedAnswer = answerRepository.save(answer);
-        assertThat(savedAnswer.getId()).isNotNull();
-        assertThat(savedAnswer.getWriterId()).isEqualTo(answer.getWriterId());
-        assertThat(savedAnswer.getQuestionId()).isEqualTo(answer.getQuestionId());
-        assertThat(savedAnswer.getContents()).isEqualTo(answer.getContents());
-        assertThat(savedAnswer.getCreatedAt()).isCloseTo(LocalDateTime.now(), within(1, ChronoUnit.SECONDS));
-        assertThat(savedAnswer.getUpdatedAt()).isNull();
+    @DisplayName("Write가 없으면 UnAuthorizedException을 던진다")
+    @Test
+    void testGivenNoWriteThenThrowException() {
+        assertThatThrownBy(() -> new Answer(null, QuestionTest.Q1, "Answers Contents1"))
+                .isInstanceOf(UnAuthorizedException.class);
     }
 
-    private static List<Answer> testAnswerList() {
-        return Arrays.asList(A1, A2);
+    @DisplayName("Question이 없으면 UnAuthorizedException을 던진다")
+    @Test
+    void testGivenNoQuestionThenThrowException() {
+        assertThatThrownBy(() -> new Answer(UserTest.JAVAJIGI, null, "Answers Contents1"))
+                .isInstanceOf(NotFoundException.class);
     }
 }
