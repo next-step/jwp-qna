6a292e2a0dcf4eea080e0b879ff4f8f54dc2870d
ch3224bin <ahrocksil@gmail.com>
2021-11-11 13:20:11 +0900

    test : UserTest, UserRepositoryTest 분리

--- src/test/java/qna/domain/UserTest.java
+++ src/test/java/qna/domain/UserTest.java
@@ -3,41 +3,31 @@ package qna.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
-import java.util.Arrays;
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
-@DataJpaTest
 public class UserTest {
     public static final User JAVAJIGI = new User(1L, "javajigi", "password", "name", "javajigi@slipp.net");
     public static final User SANJIGI = new User(2L, "sanjigi", "password", "name", "sanjigi@slipp.net");
 
-    private UserRepository userRepository;
-
-    @Autowired
-    public UserTest(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
     @ParameterizedTest
-    @MethodSource("testUserList")
-    void testEquals(User user) {
-        User savedUser = userRepository.save(user);
-        assertThat(savedUser.getId()).isEqualTo(user.getId());
-        assertThat(savedUser.getUserId()).isEqualTo(user.getUserId());
-        assertThat(savedUser.getPassword()).isEqualTo(user.getPassword());
-        assertThat(savedUser.getName()).isEqualTo(user.getName());
-        assertThat(savedUser.getEmail()).isEqualTo(user.getEmail());
-        assertThat(savedUser.getName()).isEqualTo(user.getName());
-        assertThat(savedUser.getCreatedAt()).isCloseTo(LocalDateTime.now(), within(1, ChronoUnit.SECONDS));
-        assertThat(savedUser.getUpdatedAt()).isNull();
+    @CsvSource(value = {"1:javajigi:password:name:javajigi@slipp.net"}, delimiter = ':')
+    void testEquals(Long id, String userId, String password, String name, String email) {
+        User user = new User(id, userId, password, name, email);
+        assertAll(
+            () -> assertThat(user.getId()).isEqualTo(id),
+            () -> assertThat(user.getUserId()).isEqualTo(userId),
+            () -> assertThat(user.getPassword()).isEqualTo(password),
+            () -> assertThat(user.getName()).isEqualTo(name),
+            () -> assertThat(user.getEmail()).isEqualTo(email),
+            () -> assertThat(user.getCreatedAt()).isCloseTo(LocalDateTime.now(), within(1, ChronoUnit.SECONDS)),
+            () -> assertThat(user.getUpdatedAt()).isNull()
+        );
     }
 
     @DisplayName("User 생성시 필수 값을 확인한다")
@@ -49,9 +39,5 @@ public class UserTest {
                 .isInstanceOf(IllegalArgumentException.class);
         assertThatThrownBy(() ->  new User(1L, "javajigi", "password", null, "javajigi@slipp.net"))
                 .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    private static List<User> testUserList() {
-        return Arrays.asList(JAVAJIGI, SANJIGI);
     }
 }
