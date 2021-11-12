844777a944991a0fdded58f4cbd8f35ac7b65226
wooobo <irooori89@gmail.com>
2021-11-13 00:20:38 +0900

    refactor : question.remove 변경으로 리팩토링

--- src/main/java/qna/service/QnaService.java
+++ src/main/java/qna/service/QnaService.java
@@ -50,7 +50,7 @@ public class QnaService {
         }
 
         List<DeleteHistory> deleteHistories = new ArrayList<>();
-        question.setDeleted(true);
+        question.remove();
         deleteHistories.add(
             new DeleteHistory(ContentType.QUESTION, questionId, question.getWriter(), LocalDateTime.now()));
         for (Answer answer : answers) {
