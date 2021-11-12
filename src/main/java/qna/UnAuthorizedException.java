50c4718f806fd07dcb8aa6371cab99dcad13a05f
wooobo <irooori89@gmail.com>
2021-11-12 17:57:48 +0900

    feat : UnAuthorizedException 예외 메시지 추가

--- src/main/java/qna/UnAuthorizedException.java
+++ src/main/java/qna/UnAuthorizedException.java
@@ -3,6 +3,7 @@ package qna;
 public class UnAuthorizedException extends RuntimeException {
     public static final String UNAUTHORIZED_EXCEPTION_USER_ID_NULL_MESSAGE = "인증되지 않은 사용자입니다.";
     public static final String UNAUTHORIZED_EXCEPTION_MISS_MATCH_PASSWORD_MESSAGE = "비밀번호가 일치하지 않습니다.";
+    public static final String GUEST_USER_NOT_QUESTION = "게스트는 질문을 작성 할 수 없습니다.";
     private static final long serialVersionUID = 1L;
 
     public UnAuthorizedException() {
