eceee6061ac6f92bcd6d7289439163f7cc277d4d
IW.MOON <mooniw02@gmail.com>
2021-11-12 10:52:26 +0900

    refactor: equals 및 hashcode 정의

--- src/main/java/qna/domain/Deleted.java
+++ src/main/java/qna/domain/Deleted.java
@@ -2,12 +2,13 @@ package qna.domain;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
+import java.util.Objects;
 
 @Embeddable
 public class Deleted {
 
     @Column(nullable = false)
-    private boolean deleted = false;
+    boolean deleted = false;
 
     public Deleted(boolean deleted) {
         this.deleted = deleted;
@@ -19,5 +20,18 @@ public class Deleted {
 
     public boolean isDeleted() {
         return deleted;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Deleted deleted1 = (Deleted) o;
+        return deleted == deleted1.deleted;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(deleted);
     }
 }
