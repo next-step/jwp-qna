79948172e9ec7dcd71eb0115699838411f45bf88
vicjed <vicjed@icloud.com>
2021-11-12 16:55:07 +0900

    test(QuestionRepository) : 테스트추가

--- src/test/java/qna/domain/QuestionRepositoryTest.java
+++ src/test/java/qna/domain/QuestionRepositoryTest.java
@@ -1,16 +1,19 @@
 package qna.domain;
 
-import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static qna.domain.QuestionTest.*;
 
 import java.util.Arrays;
 import java.util.List;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import qna.ErrorMessage;
+import qna.UnAuthorizedException;
 
 @DataJpaTest
 public class QuestionRepositoryTest {
@@ -37,27 +40,15 @@ public class QuestionRepositoryTest {
 
 	@Test
 	@DisplayName("전체 데이터수 - (삭제여부 true) = 전체 건수 검증")
-	void findByDeletedFalse() {
+	void given_questions_when_changeDeleteToTrue_then_excludeDeleteIsTrue () {
 		List<Question> questions = questionRepository.saveAll(Arrays.asList(Q1, Q2));
-		Question question = questions.get(0);
-		question.setDeleted(true);
 
 		// when
+		Question question = questions.get(0);
+		question.setDeleted(true);
 		List<Question> byDeletedFalse = questionRepository.findByDeletedFalse();
 
 		// then
 		assertEquals(byDeletedFalse.size(), 1);
-	}
-
-	@Test
-	void findByIdAndDeletedFalse() {
-		// given
-		questionRepository.saveAll(Arrays.asList(Q1, Q2));
-
-		// when
-		Question byIdAndDeletedFalse = questionRepository.findByIdAndDeletedFalse(Q1.getId()).get();
-
-		// then
-		assertThat(byIdAndDeletedFalse).isEqualTo(Q1);
 	}
 }
