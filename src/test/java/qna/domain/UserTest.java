9060d3f1cee7bf51c33d94d7351cb1b73c913ce4
vicjed <vicjed@icloud.com>
2021-11-12 16:30:19 +0900

    test(UserTest) : 유저테스트 추가

--- src/test/java/qna/domain/UserTest.java
+++ src/test/java/qna/domain/UserTest.java
@@ -1,19 +1,30 @@
 package qna.domain;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
+import static org.assertj.core.api.Assertions.*;
 
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-@DataJpaTest
 public class UserTest {
-    public static final User JAVAJIGI = new User(1L, "javajigi", "password", "javajigi", "javajigi@slipp.net");
-    public static final User SANJIGI = new User(2L, "sanjigi", "password", "sanjigi", "sanjigi@slipp.net");
+	public static final User JAVAJIGI = new User(1L, "javajigi", "password", "javajigi", "javajigi@slipp.net");
+	public static final User SANJIGI = new User(2L, "sanjigi", "password", "sanjigi", "sanjigi@slipp.net");
+
+	@Test
+	@DisplayName("JAVAJIGI 에서 SANJIGI로 변경했을 때 email,name SANJIGI 동일")
+	void given_JAVAJIGI_when_update_SANJIGI_then_equalsEmailAndName_SINJIGI_isTrue () {
+
+		//when
+		JAVAJIGI.update(JAVAJIGI,SANJIGI);
+
+		assertThat(JAVAJIGI.equalsNameAndEmail(SANJIGI)).isTrue();
+	}
+
+	@Test
+	@DisplayName("JAVAJIGI 에서 SANJIGI로 변경했을 때 email,name SANJIGI 동일")
+	void given_JAVAGIGI_then_matchPassword () {
+		// then
+		Assertions.assertThat(JAVAJIGI.matchPassword("password")).isTrue();
+		Assertions.assertThat(JAVAJIGI.matchPassword("notMatchPassword")).isFalse();
+	}
 }
