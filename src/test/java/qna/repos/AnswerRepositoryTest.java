215f739a81802c8a0cfa592a22cf77560c2ac7ab
youngrock.choi <tbvjdudfhr@naver.com>
2021-11-13 00:37:38 +0900

    refactor: 리뷰 반영
    createdAt not null
    Test 이름 변경

--- src/test/java/qna/repos/AnswerRepositoryTest.java
+++ src/test/java/qna/repos/AnswerRepositoryTest.java
@@ -1,10 +1,10 @@
-package qna.domain;
+package qna.repos;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import qna.repos.AnswerRepository;
+import qna.domain.Answer;
 
 import java.util.Optional;
 
@@ -12,9 +12,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 @DataJpaTest
-public class AnswerTest {
-    public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
-    public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+public class AnswerRepositoryTest {
+    public static final Answer A1 = new Answer(UserRepositoryTest.JAVAJIGI, QuestionRepositoryTest.Q1, "Answers Contents1");
+    public static final Answer A2 = new Answer(UserRepositoryTest.SANJIGI, QuestionRepositoryTest.Q1, "Answers Contents2");
 
     @Autowired
     private AnswerRepository repository;
--- src/test/java/qna/repos/QuestionRepositoryTest.java
+++ src/test/java/qna/repos/QuestionRepositoryTest.java
@@ -1,10 +1,10 @@
-package qna.domain;
+package qna.repos;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import qna.repos.QuestionRepository;
+import qna.domain.Question;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,9 +13,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 @DataJpaTest
-public class QuestionTest {
-    public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
-    public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+public class QuestionRepositoryTest {
+    public static final Question Q1 = new Question("title1", "contents1").writeBy(UserRepositoryTest.JAVAJIGI);
+    public static final Question Q2 = new Question("title2", "contents2").writeBy(UserRepositoryTest.SANJIGI);
 
     @Autowired
     private QuestionRepository repository;
