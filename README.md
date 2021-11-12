86f29fd5b3a1b10bd8f0d1bc9b2ba25b58c56720
donkey <rlaehdzlsla@gmail.com>
2021-11-13 00:10:06 +0900

    feat: delete_history - user 연관 관게 매핑

--- README.md
+++ README.md
@@ -1,35 +1,39 @@
 # JPA
--  QnA 서비스를 만들어가면서 JPA로 실제 도메인 모델을 어떻게 구성하고 객체와 테이블을 어떻게 매핑해야 하는지 알아본다.
+
+- QnA 서비스를 만들어가면서 JPA로 실제 도메인 모델을 어떻게 구성하고 객체와 테이블을 어떻게 매핑해야 하는지 알아본다.
 
 # 1단계 - 엔티티 매핑
 
 ### 요구사항
+
 - [X] 아래의 DDL(Data Definition Language)을 보고 유추하여 엔티티 클래스와 리포지토리 클래스를 작성해 본다.
-  - [X] Entity 클래스 작성
-    - [X] `Answer`
-    - [X] `DeleteHistory`
-    - [X] `Question`
-    - [X] `User`
-  - [X] repository 클래스 작성
-    - [X] `Answer`
-    - [X] `DeleteHistory`
-    - [X] `Question`
-    - [X] `User`
+    - [X] Entity 클래스 작성
+        - [X] `Answer`
+        - [X] `DeleteHistory`
+        - [X] `Question`
+        - [X] `User`
+    - [X] repository 클래스 작성
+        - [X] `Answer`
+        - [X] `DeleteHistory`
+        - [X] `Question`
+        - [X] `User`
 - [X] @DataJpaTest를 사용하여 학습 테스트를 해 본다.
-  - [X] `Answer`
-  - [X] `DeleteHistory`
-  - [X] `Question`
-  - [X] `User`
+    - [X] `Answer`
+    - [X] `DeleteHistory`
+    - [X] `Question`
+    - [X] `User`
 
 # 2단계 - 연관 관계 매핑
+
 - QnA 서비스를 만들어가면서 JPA로 실제 도메인 모델을 어떻게 구성하고 객체와 테이블을 어떻게 매핑해야 하는지 알아본다.
 
 ### 요구사항
-- [ ] 연관 관계 매핑
-  - [X] answer
-  - [ ] delete_history
-  - [X] question
-  
+
+- [X] 연관 관계 매핑
+    - [X] answer
+    - [X] delete_history
+    - [X] question
+
 ``` h2
 alter table answer
     add constraint fk_answer_to_question
@@ -50,5 +54,4 @@ alter table question
     add constraint fk_question_writer
         foreign key (writer_id)
             references user
-
 ```
--- src/main/java/qna/domain/DeleteHistory.java
+++ src/main/java/qna/domain/DeleteHistory.java
@@ -6,9 +6,12 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
@@ -27,12 +30,14 @@ public class DeleteHistory {
 
     private LocalDateTime createDate = LocalDateTime.now();
 
-    private Long deletedById;
+    @ManyToOne
+    @JoinColumn(foreignKey = @ForeignKey(name = "fk_delete_history_to_user"))
+    private User deletedBy;
 
-    public DeleteHistory(ContentType contentType, Long contentId, Long deletedById, LocalDateTime createDate) {
+    public DeleteHistory(ContentType contentType, Long contentId, User deletedBy, LocalDateTime createDate) {
         this.contentType = contentType;
         this.contentId = contentId;
-        this.deletedById = deletedById;
+        this.deletedBy = deletedBy;
         this.createDate = createDate;
     }
 
@@ -48,12 +53,12 @@ public class DeleteHistory {
         return Objects.equals(id, that.id) &&
                 contentType == that.contentType &&
                 Objects.equals(contentId, that.contentId) &&
-                Objects.equals(deletedById, that.deletedById);
+                Objects.equals(deletedBy, that.deletedBy);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, contentType, contentId, deletedById);
+        return Objects.hash(id, contentType, contentId, deletedBy);
     }
 
     @Override
@@ -62,7 +67,7 @@ public class DeleteHistory {
                 "id=" + id +
                 ", contentType=" + contentType +
                 ", contentId=" + contentId +
-                ", deletedById=" + deletedById +
+                ", deletedBy=" + deletedBy +
                 ", createDate=" + createDate +
                 '}';
     }
