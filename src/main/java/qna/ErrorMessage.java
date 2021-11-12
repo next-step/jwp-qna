cebf65cef65ada3fcf3e6f4af2f99497a3e34ecb
vicjed <vicjed@icloud.com>
2021-11-12 16:17:17 +0900

    feat(ErrorMessage) : 공통 예외 메시지 정의

---
+++ src/main/java/qna/ErrorMessage.java
@@ -0,0 +1,6 @@
+package qna;
+
+public class ErrorMessage {
+	public static final String USER_IS_NOT_NULL = "유저 정보는 NULL일 수 없습니다.";
+	public static final String QUESTION_NOT_FOUND = "게시글이 존재하지 않습니다.";
+}
