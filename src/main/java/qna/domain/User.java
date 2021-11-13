3ea588fbcbcc7b99ca61fffebb2d5770d89d1b33
Chaeyun Lee <chaeyunn@iochord.com>
2021-11-13 20:54:17 +0900

    refactor: 사용하지 않는 get, set 제거

--- src/main/java/qna/domain/User.java
+++ src/main/java/qna/domain/User.java
@@ -35,10 +35,10 @@ public class User extends AuditEntity {
 	private String userId;
 
 	@OneToMany(mappedBy = "writer")
-	private List<Answer> answers = new ArrayList<>();
+	private final List<Answer> answers = new ArrayList<>();
 
 	@OneToMany(mappedBy = "writer")
-	private List<Question> questions = new ArrayList<>();
+	private final List<Question> questions = new ArrayList<>();
 
 	protected User() {
 	}
@@ -93,40 +93,12 @@ public class User extends AuditEntity {
 		return id;
 	}
 
-	public void setId(Long id) {
-		this.id = id;
-	}
-
 	public String getUserId() {
 		return userId;
 	}
 
 	public void setUserId(String userId) {
 		this.userId = userId;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
 	}
 
 	public void addAnswer(Answer answer) {
