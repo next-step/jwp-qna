a1cda1f1f2c6cc1fb7be54c069dddfb04d2f4bb1
Chaeyun Lee <chaeyunn@iochord.com>
2021-11-13 20:49:57 +0900

    test: given을 setup으로 정리

--- src/test/java/qna/domain/AnswerRepositoryTest.java
+++ src/test/java/qna/domain/AnswerRepositoryTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.util.List;
 import java.util.Optional;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,15 +24,30 @@ class AnswerRepositoryTest {
 	@Autowired
 	UserRepository users;
 
+	User user1;
+	Question question1;
+	Answer answer1;
+	Answer answer2;
+
+	@BeforeEach
+	public void setup() {
+		user1 = new User("javajigi", "password", "name", "javajigi@slipp.net");
+		user1 = users.save(user1);
+
+		question1 = new Question("title1", "contents1").writeBy(user1);
+		question1 = questions.save(question1);
+
+		answer1 = new Answer(user1, question1, "Answers Contents1");
+		answer2 = new Answer(user1, question1, "Answers Contents2");
+		answer1 = answers.save(answer1);
+		answer2 = answers.save(answer2);
+	}
+
 	@Test
 	@DisplayName("답변을 생성하면, ID가 존재해야 한다")
 	void saveTest() {
 		// given
-		User writer1 = new User(1L, "javajigi", "password", "name", "javajigi@slipp.net");
-		writer1 = users.save(writer1);
-		Question q1 = new Question("title1", "contents1").writeBy(writer1);
-		q1 = questions.save(q1);
-		Answer answer = new Answer(writer1, q1, "Answers Contents1");
+		Answer answer = new Answer(user1, question1, "This is a new answer");
 
 		// when
 		Answer result = answers.save(answer);
@@ -48,36 +64,24 @@ class AnswerRepositoryTest {
 	@DisplayName("답변을 업데이트하면, 업데이트한 내용이 확인되어야 한다")
 	void updateTest() {
 		// given
-		User writer1 = new User(1L, "javajigi", "password", "name", "javajigi@slipp.net");
-		writer1 = users.save(writer1);
-		Question q1 = new Question("title1", "contents1").writeBy(writer1);
-		q1 = questions.save(q1);
-		Answer answer = new Answer(writer1, q1, "Answers Contents1");
 		String updateContents = "업데이트 된 내용입니다";
-		answers.save(answer);
-		answer.setContents(updateContents);
+		answer1.setContents(updateContents);
 
 		// when
-		answers.save(answer);
+		answer1 = answers.save(answer1);
 
 		// then
-		assertThat(answer.getContents()).isEqualTo(updateContents);
+		assertThat(answer1.getContents()).isEqualTo(updateContents);
 	}
 
 	@Test
 	@DisplayName("답변을 삭제하면, 답변을 찾을 수 없어야 한다")
 	void deleteTest() {
 		// given
-		User writer1 = new User(1L, "javajigi", "password", "name", "javajigi@slipp.net");
-		writer1 = users.save(writer1);
-		Question q1 = new Question("title1", "contents1").writeBy(writer1);
-		q1 = questions.save(q1);
-		Answer answer = new Answer(writer1, q1, "Answers Contents1");
-		answers.save(answer);
-		answers.delete(answer);
+		answers.delete(answer1);
 
 		// when
-		Optional<Answer> answerOptional = answers.findById(answer.getId());
+		Optional<Answer> answerOptional = answers.findById(answer1.getId());
 
 		// then
 		assertThat(answerOptional).isEmpty();
@@ -86,42 +90,26 @@ class AnswerRepositoryTest {
 	@Test
 	@DisplayName("답변을 아이디로 찾을 수 있어야 한다")
 	void findByQuestionIdAndDeletedFalseTest() {
-		// given
-		User writer1 = new User(1L, "javajigi", "password", "name", "javajigi@slipp.net");
-		writer1 = users.save(writer1);
-		Question q1 = new Question("title1", "contents1").writeBy(writer1);
-		q1 = questions.save(q1);
-		Answer answer1 = new Answer(writer1, q1, "Answers Contents1");
-		Answer answer2 = new Answer(writer1, q1, "Answers Contents2");
-		answer1 = answers.save(answer1);
-		answer2 = answers.save(answer2);
-
 		// when
 		Optional<Answer> founded = answers.findByIdAndDeletedFalse(answer1.getId());
 
 		// then
-		assertThat(founded).isPresent();
+		assertAll(
+			() -> assertThat(founded).isPresent(),
+			() -> assertThat(founded).containsSame(answer1)
+		);
 	}
 
 	@Test
 	@DisplayName("답변을 질문으로 찾을 수 있어야 한다")
 	void findByQuestionIdAndDeletedFalse() {
-
-		// given
-		User writer1 = new User(1L, "javajigi", "password", "name", "javajigi@slipp.net");
-		writer1 = users.save(writer1);
-		Question q1 = new Question("title1", "contents1").writeBy(writer1);
-		q1 = questions.save(q1);
-		Answer answer1 = new Answer(writer1, q1, "Answers Contents1");
-		Answer answer2 = new Answer(writer1, q1, "Answers Contents2");
-
-		answers.save(answer1);
-		answers.save(answer2);
-
 		// when
-		List<Answer> founded = answers.findByQuestionAndDeletedFalse(q1);
+		List<Answer> founded = answers.findByQuestionAndDeletedFalse(question1);
 
 		// then
-		assertThat(founded.size()).isEqualTo(2);
+		assertAll(
+			() -> assertThat(founded.size()).isEqualTo(2),
+			() -> assertThat(founded).contains(answer1, answer2)
+		);
 	}
 }
--- src/test/java/qna/domain/QuestionRepositoryTest.java
+++ src/test/java/qna/domain/QuestionRepositoryTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.util.List;
 import java.util.Optional;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,17 +17,37 @@ class QuestionRepositoryTest {
 
 	@Autowired
 	QuestionRepository questions;
-	
+
 	@Autowired
 	UserRepository users;
+
+	User user1;
+	User user2;
+	Question question1;
+	Question question2;
+	Question deletedQuestion;
+
+	@BeforeEach
+	public void setup() {
+		user1 = new User("javajigi", "password", "name", "javajigi@slipp.net");
+		user2 = new User("sanjigi", "password", "name", "sanjigi@slipp.net");
+		user1 = users.save(user1);
+		user2 = users.save(user2);
+
+		question1 = new Question("title1", "contents1").writeBy(user1);
+		question2 = new Question("title2", "contents2").writeBy(user2);
+		deletedQuestion = new Question("title3", "contents3").writeBy(user1);
+		deletedQuestion.setDeleted(true);
+		question1 = questions.save(question1);
+		question2 = questions.save(question2);
+		deletedQuestion = questions.save(deletedQuestion);
+	}
 
 	@Test
 	@DisplayName("질문을 저장하면 ID가 존재해야 한다")
 	void saveTest() {
 		// given
-		User writer1 = new User(1L, "javajigi", "password", "name", "javajigi@slipp.net");
-		writer1 = users.save(writer1);
-		Question question1 = new Question("title1", "contents1").writeBy(writer1);
+		Question question1 = new Question("title1", "contents1").writeBy(user1);
 
 		// when
 		questions.save(question1);
@@ -39,24 +60,13 @@ class QuestionRepositoryTest {
 	@DisplayName("삭제되지 않은 질문들을 조회할 수 있어야 한다")
 	void findByDeletedFalseTest() {
 
-		// given
-		User writer1 = new User(1L, "javajigi", "password", "name", "javajigi@slipp.net");
-		User writer2 = new User(2L, "sanjigi", "password", "name", "sanjigi@slipp.net");
-		writer1 = users.save(writer1);
-		writer2 = users.save(writer2);
-		Question question1 = new Question("title1", "contents1").writeBy(writer1);
-		question1.setDeleted(true);
-		Question question2 = new Question("title2", "contents2").writeBy(writer2);
-		questions.save(question1);
-		questions.save(question2);
-
 		// when
 		List<Question> result = questions.findByDeletedFalse();
 
 		// then
 		assertAll(
-			() -> assertThat(result.size()).isEqualTo(1),
-			() -> assertThat(result.get(0)).isEqualTo(question2)
+			() -> assertThat(result.size()).isEqualTo(2),
+			() -> assertThat(result).contains(question1, question2)
 		);
 
 	}
@@ -65,26 +75,13 @@ class QuestionRepositoryTest {
 	@DisplayName("삭제하지 않은 것 중에 아이디로 조회할 수 있어야 한다")
 	void findByIdAndDeletedFalse() {
 
-		// given
-		User writer1 = new User(1L, "javajigi", "password", "name", "javajigi@slipp.net");
-		User writer2 = new User(2L, "sanjigi", "password", "name", "sanjigi@slipp.net");
-		writer1 = users.save(writer1);
-		writer2 = users.save(writer2);
-		Question question1 = new Question("title1", "contents1").writeBy(writer1);
-		Question deletedQuestion = new Question("title2", "contents2").writeBy(writer2);
-		deletedQuestion.setDeleted(true);
-		questions.save(question1);
-		questions.save(deletedQuestion);
-
 		// when
 		Optional<Question> result = questions.findByIdAndDeletedFalse(question1.getId());
-		Optional<Question> deletedResult = questions.findByIdAndDeletedFalse(deletedQuestion.getId());
 
 		// then
 		assertAll(
 			() -> assertThat(result).isPresent(),
-			() -> assertThat(result).containsSame(question1),
-			() -> assertThat(deletedResult).isEmpty()
+			() -> assertThat(result).containsSame(question1)
 		);
 	}
 
