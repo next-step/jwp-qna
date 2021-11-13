76c63f14287b1260212ca1a5517e7871f2613ee6
Junhwan Kim <junhwan.tech@gmail.com>
2021-11-13 21:29:51 +0900

    style: Repository code formatting

--- src/main/java/qna/domain/DeleteHistoryRepository.java
+++ src/main/java/qna/domain/DeleteHistoryRepository.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DeleteHistoryRepository extends JpaRepository<DeleteHistory, Long> {
+
     Optional<DeleteHistory> findByContentId(Long contentId);
 
     Optional<DeleteHistory> findByDeletedById(Long deletedById);
