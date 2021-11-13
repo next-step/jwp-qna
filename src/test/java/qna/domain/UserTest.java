155b8230711bba17ec51a93eda8d4ab8d3784686
ChangSubKwak <paradise1732@gmail.com>
2021-11-14 01:08:27 +0900

    fix: QnaService 테스트를 위해, JAVAJIGI와 SANJIGI변수에 대하여 원본 코드로 원복

--- src/test/java/qna/domain/UserTest.java
+++ src/test/java/qna/domain/UserTest.java
@@ -1,38 +1,37 @@
 package qna.domain;
 
-import org.junit.jupiter.api.*;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.*;
-import java.util.stream.Stream;
+import java.util.stream.*;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertAll;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.*;
+import org.junit.jupiter.params.provider.*;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.boot.test.autoconfigure.orm.jpa.*;
 
 @DataJpaTest
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class UserTest {
-    public static final User JAVAJIGI = new User("javajigi", "password", "name", "javajigi@slipp.net");
-    public static final User SANJIGI = new User("sanjigi", "password", "name", "sanjigi@slipp.net");
+    public static final User JAVAJIGI = new User(1L, "javajigi", "password", "name", "javajigi@slipp.net");
+    public static final User SANJIGI = new User(2L, "sanjigi", "password", "name", "sanjigi@slipp.net");
 
     @Autowired
     private UserRepository userRepository;
-
-    @BeforeAll
-    void setUp() {
-        userRepository.save(JAVAJIGI);
-        userRepository.save(SANJIGI);
-    }
 
     private static Stream<Arguments> provideUsers() {
         return Stream.of(
             Arguments.of(JAVAJIGI),
             Arguments.of(SANJIGI)
         );
+    }
+
+    @BeforeAll
+    void setUp() {
+        userRepository.save(JAVAJIGI);
+        userRepository.save(SANJIGI);
     }
 
     @DisplayName("User객체룰 입력으로 받는 save통하여 저장한 후 조회하면, 결과의 속성과 입력객체의 속성은 동일하다.")
