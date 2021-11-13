61628736ebded1a464c4148a32672cb9a56c2564
Junhwan Kim <junhwan.tech@gmail.com>
2021-11-13 17:13:24 +0900

    test(DeleteHistoryTest): DeleteHistory Entity 클래스 학습 테스트

---
+++ src/test/java/qna/domain/DeleteHistoryTest.java
@@ -0,0 +1,92 @@
+package qna.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import java.time.LocalDateTime;
+import javax.persistence.EntityManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+class DeleteHistoryTest {
+
+    @Autowired
+    private DeleteHistoryRepository deleteHistoryRepository;
+
+    @Autowired
+    private EntityManager entityManager;
+
+    private DeleteHistory history1;
+    private DeleteHistory history2;
+
+    @BeforeEach
+    void setUp() {
+        history1 = new DeleteHistory(ContentType.QUESTION, 1L, 1L,
+            LocalDateTime.now());
+        history2 = new DeleteHistory(ContentType.ANSWER, 2L, 2L,
+            LocalDateTime.now());
+
+        deleteHistoryRepository.save(history1);
+        deleteHistoryRepository.save(history2);
+    }
+
+    @AfterEach
+    void tearDown() {
+        deleteHistoryRepository.deleteAll();
+    }
+
+    @Test
+    void test_삭제내역_저장() {
+        deleteHistoryRepository.deleteAll();
+        entityManager.clear();
+        DeleteHistory actual = deleteHistoryRepository.save(history1);
+
+        assertAll(
+            () -> assertThat(actual).isNotNull(),
+            () -> assertThat(actual.getId()).isEqualTo(history1.getId()),
+            () -> assertThat(actual.getContentType()).isEqualTo(history1.getContentType()),
+            () -> assertThat(actual.getContentId()).isEqualTo(history1.getContentId()),
+            () -> assertThat(actual.getDeletedById()).isEqualTo(history1.getDeletedById())
+        );
+    }
+
+    @Test
+    void test_content_id로_조회() {
+        DeleteHistory actual = deleteHistoryRepository.findByContentId(history1.getContentId())
+            .orElse(null);
+
+        assertAll(
+            () -> assertThat(actual).isNotNull(),
+            () -> assertThat(actual.getContentId()).isEqualTo(history1.getContentId()),
+            () -> assertThat(actual).isSameAs(history1)
+        );
+    }
+
+    @Test
+    void test_삭제한_id로_조회() {
+        DeleteHistory actual = deleteHistoryRepository.findByDeletedById(history1.getDeletedById())
+            .orElse(null);
+
+        assertAll(
+            () -> assertThat(actual).isNotNull(),
+            () -> assertThat(actual.getDeletedById()).isEqualTo(history1.getDeletedById()),
+            () -> assertThat(actual).isSameAs(history1)
+        );
+    }
+
+    @Test
+    void test_content_type으로_조회() {
+        DeleteHistory actual = deleteHistoryRepository.findByContentType(history1.getContentType())
+            .orElse(null);
+
+        assertAll(
+            () -> assertThat(actual).isNotNull(),
+            () -> assertThat(actual.getContentType()).isEqualTo(history1.getContentType()),
+            () -> assertThat(actual).isSameAs(history1)
+        );
+    }
+}
