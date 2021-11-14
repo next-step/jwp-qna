a79f6548d0fa0760c90ccd0a8846da26c448964e
Junhong-Kim <jhk1005jhk@gmail.com>
2021-11-14 21:39:16 +0900

    test: 학습 테스트 추가

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,65 @@
 package qna.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+@DataJpaTest
 public class QuestionTest {
-    public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
-    public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+    public static final Question Q1 = new Question(1L, "title1", "contents1").writeBy(UserTest.JAVAJIGI);
+    public static final Question Q2 = new Question(2L, "title2", "contents2").writeBy(UserTest.SANJIGI);
+
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    @DisplayName("save 테스트")
+    @Test
+    void save() {
+        // when
+        Question newQuestion = questionRepository.save(Q1);
+
+        // then
+        assertAll(
+                () -> assertThat(newQuestion.getId()).isNotNull()
+                , () -> assertThat(newQuestion.getTitle()).isEqualTo(Q1.getTitle())
+                , () -> assertThat(newQuestion.getContents()).isEqualTo(Q1.getContents())
+                , () -> assertThat(newQuestion.getWriterId()).isEqualTo(Q1.getWriterId())
+        );
+    }
+
+    @DisplayName("findByDeletedFalse 테스트")
+    @Test
+    void findByDeletedFalse() {
+        // when
+        Question newQuestion = questionRepository.save(Q1);
+        List<Question> questions = questionRepository.findByDeletedFalse();
+
+        // then
+        assertAll(
+                () -> assertThat(questions).hasSize(1)
+                , () -> assertThat(questions).containsExactly(newQuestion)
+        );
+    }
+
+    @DisplayName("findByIdAndDeletedFalse 테스트")
+    @Test
+    void findByIdAndDeletedFalse() {
+        // when
+        Question newQuestion = questionRepository.save(Q1);
+        Optional<Question> optionalQuestion = questionRepository.findByIdAndDeletedFalse(1L);
+
+        // then
+        assertAll(
+                () -> assertThat(optionalQuestion.get()).isNotNull()
+                , () -> assertThat(optionalQuestion.get()).isEqualTo(newQuestion)
+        );
+    }
 }
--- src/test/java/qna/domain/UserTest.java
+++ src/test/java/qna/domain/UserTest.java
@@ -1,6 +1,50 @@
 package qna.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+@DataJpaTest
 public class UserTest {
     public static final User JAVAJIGI = new User(1L, "javajigi", "password", "name", "javajigi@slipp.net");
     public static final User SANJIGI = new User(2L, "sanjigi", "password", "name", "sanjigi@slipp.net");
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @DisplayName("save 테스트")
+    @Test
+    void save() {
+        // when
+        User newUser = userRepository.save(JAVAJIGI);
+
+        // then
+        assertAll(
+                () -> assertThat(newUser.getId()).isNotNull()
+                , () -> assertThat(newUser.getUserId()).isEqualTo(JAVAJIGI.getUserId())
+                , () -> assertThat(newUser.getPassword()).isEqualTo(JAVAJIGI.getPassword())
+                , () -> assertThat(newUser.getName()).isEqualTo(JAVAJIGI.getName())
+                , () -> assertThat(newUser.getEmail()).isEqualTo(JAVAJIGI.getEmail())
+        );
+    }
+
+    @DisplayName("findByUserId 테스트")
+    @Test
+    void findByUserId() {
+        // when
+        User newUser = userRepository.save(JAVAJIGI);
+        Optional<User> optionalUser = userRepository.findByUserId(JAVAJIGI.getUserId());
+
+        // then
+        assertAll(
+                () -> assertThat(optionalUser.get()).isNotNull()
+                , () -> assertThat(optionalUser.get()).isEqualTo(newUser)
+        );
+    }
 }
