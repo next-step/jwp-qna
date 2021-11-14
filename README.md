32db57ceb5cfb372fc598739565382d174fbcbc7
changjin.park <pcjin118@gmail.com>
2021-11-14 18:31:16 +0900

    feat: DeleteHistory Entity 클래스 수정 및 테스트

--- README.md
+++ README.md
@@ -3,9 +3,9 @@
 [x] Answer Entity 클래스 수정 및 테스트
     [x] 요구 ddl에 맞춰서 클래스 수정
     [x] @DataJpaTest를 사용하여 코드 작성
-[] DeleteHistory Entity 클래스 수정 및 테스트
-    [] 요구 ddl에 맞춰서 클래스 수정
-    [] @DataJpaTest를 사용하여 코드 작성
+[x] DeleteHistory Entity 클래스 수정 및 테스트
+    [x] 요구 ddl에 맞춰서 클래스 수정
+    [x] @DataJpaTest를 사용하여 코드 작성
 [] Question Entity 클래스 수정 및 테스트
     [] 요구 ddl에 맞춰서 클래스 수정
     [] @DataJpaTest를 사용하여 코드 작성
---
+++ src/test/java/qna/domain/DeleteHistoryTest.java
@@ -0,0 +1,53 @@
+package qna.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+public class DeleteHistoryTest {
+
+    public static final DeleteHistory D1 = new DeleteHistory(ContentType.QUESTION, UserTest.JAVAJIGI.getId(), UserTest.SANJIGI.getId(), LocalDateTime.now());
+
+    @Autowired
+    private DeleteHistoryRepository deleteHistoryRepository;
+
+    @DisplayName("Create 및 ID 생성 테스트")
+    @Test
+    void save() {
+        DeleteHistory save = deleteHistoryRepository.save(D1);
+        assertThat(save.getId()).isNotNull();
+    }
+
+    @DisplayName("Read 테스트")
+    @Test
+    void read() {
+        DeleteHistory save = deleteHistoryRepository.save(D1);
+        DeleteHistory found = deleteHistoryRepository.findById(save.getId()).orElse(null);
+        assertThat(found).isEqualTo(save);
+    }
+
+    @DisplayName("Update 테스트")
+    @Test
+    void update() {
+        DeleteHistory save = deleteHistoryRepository.save(D1);
+        save.setContentType(ContentType.ANSWER);
+        DeleteHistory found = deleteHistoryRepository.findById(save.getId()).orElseThrow(() -> new NullPointerException("테스트실패"));
+        assertThat(found.getContents()).isEqualTo(ContentType.ANSWER);
+    }
+
+    @DisplayName("Delete 테스트")
+    @Test
+    void delete() {
+        DeleteHistory save = deleteHistoryRepository.save(D1);
+        deleteHistoryRepository.delete(save);
+        deleteHistoryRepository.flush();
+        DeleteHistory found = deleteHistoryRepository.findById(save.getId()).orElse(null);
+        assertThat(found).isNull();
+    }
+}
