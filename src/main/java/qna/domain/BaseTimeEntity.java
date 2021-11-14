e593819929f81f1642a316fc0c1b5a94baec9045
이승민 <sml7179@naver.com>
2021-11-14 12:11:26 +0900

    refactor(BaseTimeEntity): @CreationTimestamp -> @CreatedDate 로 통일

--- src/main/java/qna/domain/BaseTimeEntity.java
+++ src/main/java/qna/domain/BaseTimeEntity.java
@@ -1,7 +1,7 @@
 package qna.domain;
 
-import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.Column;
@@ -13,9 +13,9 @@ import java.time.LocalDateTime;
 @EntityListeners(AuditingEntityListener.class)
 public class BaseTimeEntity {
 
-    @CreationTimestamp
+    @CreatedDate
     @Column(nullable = false, updatable = false)
-    private LocalDateTime createdAt;
+    private LocalDateTime createdAt  = LocalDateTime.now();
 
     @UpdateTimestamp
     private LocalDateTime updatedAt;
