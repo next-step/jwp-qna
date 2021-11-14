a341385b2e852ebcca08848f0efb5620c8ef3ef2
jerry92k <jens92.k@gmail.com>
2021-11-14 10:09:48 +0900

    style: 코드 포맷팅

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -14,69 +14,68 @@ import qna.CannotDeleteException;
 import qna.NotFoundException;
 import qna.UnAuthorizedException;
 
-
 @DataJpaTest
 public class AnswerTest {
 
-    public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
-    public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+	public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
+	public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
 
-    @Autowired
-    EntityManager em;
+	@Autowired
+	EntityManager em;
 
-    @Test
-    public void 엔티티_생성(){
-        User user = new User("jerry92k", "12345678","jerrykim","jerry@gmail.com");
-        em.persist(user);
-        Question question = new Question("title1", "contents1").writeBy(user);
-        em.persist(question);
+	@Test
+	public void 엔티티_생성() {
+		User user = new User("jerry92k", "12345678", "jerrykim", "jerry@gmail.com");
+		em.persist(user);
+		Question question = new Question("title1", "contents1").writeBy(user);
+		em.persist(question);
 
-        Answer answer = new Answer(user,question, "Answer entity unit test");
-        em.persist(answer);
-        Answer findAnswer = em.createQuery("select a from Answer a where a.id=:id",Answer.class)
-            .setParameter("id",answer.getId())
-            .getSingleResult();
-        assertThat(findAnswer).isEqualTo(answer);
-    }
+		Answer answer = new Answer(user, question, "Answer entity unit test");
+		em.persist(answer);
+		Answer findAnswer = em.createQuery("select a from Answer a where a.id=:id", Answer.class)
+			.setParameter("id", answer.getId())
+			.getSingleResult();
+		assertThat(findAnswer).isEqualTo(answer);
+	}
 
-    @Test
-    public void 엔티티_생성_User_객체_NUll_예외(){
-        assertThatThrownBy(()->{
-            Answer answer = new Answer(null, QuestionTest.Q1, "Answer entity unit test");
-        }).isInstanceOf(UnAuthorizedException.class);
-    }
+	@Test
+	public void 엔티티_생성_User_객체_NUll_예외() {
+		assertThatThrownBy(() -> {
+			Answer answer = new Answer(null, QuestionTest.Q1, "Answer entity unit test");
+		}).isInstanceOf(UnAuthorizedException.class);
+	}
 
-    @Test
-    public void 엔티티_생성_Question_객체_NUll_예외(){
-        assertThatThrownBy(()->{
-            Answer answer = new Answer(UserTest.JAVAJIGI, null, "Answer entity unit test");
-        }).isInstanceOf(NotFoundException.class);
-    }
+	@Test
+	public void 엔티티_생성_Question_객체_NUll_예외() {
+		assertThatThrownBy(() -> {
+			Answer answer = new Answer(UserTest.JAVAJIGI, null, "Answer entity unit test");
+		}).isInstanceOf(NotFoundException.class);
+	}
 
-    @Test
-    public void delete_작성자와_삭제요청자가_다른_경우_예외(){
-        User writer = new User("jerry92k", "12345678","jerrykim","jerry@gmail.com");
-        User deleter = new User("tom", "34323f","tom","tom@gmail.com");
-        em.persist(writer);
-        em.persist(deleter);
+	@Test
+	public void delete_작성자와_삭제요청자가_다른_경우_예외() {
+		User writer = new User("jerry92k", "12345678", "jerrykim", "jerry@gmail.com");
+		User deleter = new User("tom", "34323f", "tom", "tom@gmail.com");
+		em.persist(writer);
+		em.persist(deleter);
 
-        Answer answer = new Answer(deleter, QuestionTest.Q1, "Answer entity unit test");
+		Answer answer = new Answer(deleter, QuestionTest.Q1, "Answer entity unit test");
 
-        assertThatThrownBy(()->{
-            answer.delete(writer);
-        }).isInstanceOf(CannotDeleteException.class);
-    }
+		assertThatThrownBy(() -> {
+			answer.delete(writer);
+		}).isInstanceOf(CannotDeleteException.class);
+	}
 
-    @Test
-    public void delete_작성자와_삭제요청자가_다르지만_이미_삭제된_경우_정상_처리() throws CannotDeleteException {
-        User writer = new User("jerry92k", "12345678","jerrykim","jerry@gmail.com");
-        User deleter = new User("tom", "34323f","tom","tom@gmail.com");
-        em.persist(writer);
-        em.persist(deleter);
+	@Test
+	public void delete_작성자와_삭제요청자가_다르지만_이미_삭제된_경우_정상_처리() throws CannotDeleteException {
+		User writer = new User("jerry92k", "12345678", "jerrykim", "jerry@gmail.com");
+		User deleter = new User("tom", "34323f", "tom", "tom@gmail.com");
+		em.persist(writer);
+		em.persist(deleter);
 
-        Answer answer = new Answer(writer, QuestionTest.Q1, "Answer entity unit test");
-        answer.delete(writer);
-        answer.delete(deleter);
-        assertThat(answer.isDeleted()).isTrue();
-    }
+		Answer answer = new Answer(writer, QuestionTest.Q1, "Answer entity unit test");
+		answer.delete(writer);
+		answer.delete(deleter);
+		assertThat(answer.isDeleted()).isTrue();
+	}
 }
