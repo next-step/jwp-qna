cc9f7126ae78dc36e48a15388ea1357e4da100cb
jennie267 <jennieee267@gmail.com>
2021-11-14 20:42:56 +0900

    refactor : Title 체크로직 util로 변경

--- src/main/java/qna/domain/Title.java
+++ src/main/java/qna/domain/Title.java
@@ -3,6 +3,8 @@ package qna.domain;
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 
+import qna.util.ValidationUtils;
+
 @Embeddable
 public class Title {
     public static final int TITLE_MAX_SIZE = 100;
@@ -23,15 +25,7 @@ public class Title {
     }
     
     private static void checkValidation(String title) {
-        if (isEmpty(title)) {
-            throw new IllegalArgumentException("제목을 입력하지 않았습니다.");
-        }
-        if (title.length() > TITLE_MAX_SIZE) {
-            throw new IllegalArgumentException(String.format("제목의 길이가 %d자를 초과했습니다.", TITLE_MAX_SIZE));
-        }
-    }
-    
-    private static boolean isEmpty(String title) {
-        return title.isEmpty();
+        ValidationUtils.checkEmpty(title);
+        ValidationUtils.checkLength(title, TITLE_MAX_SIZE);
     }
 }
