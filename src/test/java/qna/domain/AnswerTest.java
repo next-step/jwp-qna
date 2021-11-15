02d920690af5cf24816b3c6cfa5011970130dba4
ddabizz <ddabizz@gmail.com>
2021-11-15 17:09:13 +0900

    test: 테스트 코드 작성

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,15 +1,54 @@
 package qna.domain;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import qna.NotFoundException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DataJpaTest
 public class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
 
-    @Test
-    void name() {
+    @Autowired
+    AnswerRepository answerRepository;
 
+    @Autowired
+    QuestionRepository questionRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Test
+    void save() {
+        final Long id = answerRepository.save(A1).getId();
+        assertThat(id).isEqualTo(1);
+    }
+
+    @Test
+    public void findById() {
+        final Answer answer = answerRepository.findById(answerRepository.save(A1).getId())
+                .orElseThrow(NotFoundException::new);
+        assertThat(answer.getWriterId()).isEqualTo(1L);
+    }
+
+    @Test
+    void findByIdAndDeletedFalse() {
+        final Answer answer = answerRepository.save(A1);
+        answer.setDeleted(true);
+        assertThatThrownBy(() -> answerRepository.findByIdAndDeletedFalse(answer.getId())
+                .orElseThrow(NotFoundException::new))
+                .isInstanceOf(NotFoundException.class);
+    }
+
+    @Test
+    void findByQuestionIdAndDeletedFalse() {
+        final Question question = questionRepository.save(QuestionTest.Q1);
+        final Answer answer = answerRepository.save(new Answer(UserTest.SANJIGI, question, "answer 111"));
+        answer.setDeleted(true);
+        assertThat(answerRepository.findByQuestionIdAndDeletedFalse(question.getId())).hasSize(0);
     }
 }
--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,47 @@
 package qna.domain;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import qna.NotFoundException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@DataJpaTest
 public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+    @Autowired
+    QuestionRepository questionRepository;
+
+    @Test
+    void save() {
+        final String title = questionRepository.save(Q1).getTitle();
+        assertThat(title).isEqualTo("title1");
+    }
+
+    @Test
+    void findById() {
+        final Question question = questionRepository.findById(questionRepository.save(Q1).getId())
+                .orElseThrow(NotFoundException::new);
+        assertThat(question.getTitle()).isEqualTo("title1");
+    }
+
+    @Test
+    void findByDeletedFalse() {
+        final Question question = questionRepository.save(Q1);
+        question.setDeleted(true);
+        assertThat(questionRepository.findByDeletedFalse()).hasSize(0);
+    }
+
+    @Test
+    void findByIdAndDeletedFalse() {
+        final Question question = questionRepository.save(Q1);
+        question.setDeleted(true);
+        assertThatThrownBy(() -> questionRepository.findByIdAndDeletedFalse(question.getId())
+                .orElseThrow(NotFoundException::new))
+                .isInstanceOf(NotFoundException.class);
+    }
 }
