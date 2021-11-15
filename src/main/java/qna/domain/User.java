b5187c467dc1261305ee169b7126439658a8d467
JiHunHong <hongji3354@gmail.com>
2021-11-15 23:34:32 +0900

    refactor : User Entity에 equals, hashcode override

--- src/main/java/qna/domain/User.java
+++ src/main/java/qna/domain/User.java
@@ -90,32 +90,33 @@ public class User extends BaseTimeEntity {
         return userId;
     }
 
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
     public String getPassword() {
         return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final User user = (User) o;
+        return Objects.equals(id, user.id)
+                && Objects.equals(userId, user.userId)
+                && Objects.equals(password, user.password)
+                && Objects.equals(name, user.name)
+                && Objects.equals(email, user.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, userId, password, name, email);
     }
 
     @Override
