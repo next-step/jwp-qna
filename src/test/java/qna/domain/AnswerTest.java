f03d65c5a83067694be49fe4a87f78fc69ab60a1
moon <hoopster@airi.kr>
2021-11-15 15:23:01 +0900

    feat (Answer) : 질문 유저와 로그인 유저가 다를때 삭제시 예외처리 기능 추가

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Optional;
 
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -13,37 +13,46 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 @DataJpaTest
 public class AnswerTest {
 
-	public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
-	public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+	private Answer a1;
+	private Answer a2;
+	private User user1;
+	private User user2;
+	private Question q1;
+	private Question q2;
 
 	@Autowired
 	private AnswerRepository answerRepository;
 
-	@BeforeAll
-	static void setUp(@Autowired UserRepository userRepository, @Autowired QuestionRepository questionRepository) {
-		userRepository.save(UserTest.JAVAJIGI);
-		userRepository.save(UserTest.SANJIGI);
-		questionRepository.save(QuestionTest.Q1);
-		questionRepository.save(QuestionTest.Q2);
+	@Autowired
+	private UserRepository userRepository;
+
+	@BeforeEach
+	void setUp() {
+		user1 = new User("test1", "test1", "test1", "test1");
+		user2 = new User("test2", "test2", "test2", "test2");
+		q1 = new Question("test", "test").writeBy(user1);
+		q2 = new Question("test2", "test2").writeBy(user2);
+		a1 = new Answer(user1, q1, "Answers Contents1");
+		a2 = new Answer(user2, q2, "Answers Contents2");
 	}
 
 	@Test
 	void 답변_저장_테스트() {
 		// given // when
-		Answer answer = answerRepository.save(A1);
+		Answer answer = answerRepository.save(a1);
 
 		// then
 		assertAll(
-			() -> assertThat(answer.getCreatedDate()).isEqualTo(A1.getCreatedDate()),
-			() -> assertThat(answer.getId()).isEqualTo(A1.getId()),
-			() -> assertThat(answer.getQuestion()).isEqualTo(A1.getQuestion())
+			() -> assertThat(answer.getCreatedDate()).isEqualTo(a1.getCreatedDate()),
+			() -> assertThat(answer.getId()).isEqualTo(a1.getId()),
+			() -> assertThat(answer.getQuestion()).isEqualTo(a1.getQuestion())
 		);
 	}
 
 	@Test
 	void 답변_조회_테스트() {
 		// given
-		Answer expectAnswer = answerRepository.save(A2);
+		Answer expectAnswer = answerRepository.save(a2);
 
 		// when
 		Optional<Answer> answer = answerRepository.findById(expectAnswer.getId());
@@ -58,24 +67,25 @@ public class AnswerTest {
 	@Test
 	void 답변_수정_테스트() {
 		// given
-		Answer answer = answerRepository.save(new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Test1"));
+		Answer answer = answerRepository.save(new Answer(user1, q1, "Test1"));
 
 		// when
-		answer.setQuestion(QuestionTest.Q2);
+		answer.setQuestion(q2);
 		Answer expectAnswer = answerRepository.save(answer);
 
 		// then
-		assertThat(expectAnswer.getQuestion()).isEqualTo(QuestionTest.Q2);
+		assertThat(expectAnswer.getQuestion()).isEqualTo(q2);
 	}
 
 	@Test
 	void 답변_삭제_테스트() {
 		// given
-		Answer answer = answerRepository.save(new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Test1"));
+		User expectUser = userRepository.save(new User("testuser", "test", "test", "test"));
+		Answer answer = answerRepository.save(new Answer(expectUser, q2, "Test1"));
 
 		// when
 		answerRepository.delete(answer);
-		Optional<Answer> expectAnswer = answerRepository.findById(UserTest.SANJIGI.getId());
+		Optional<Answer> expectAnswer = answerRepository.findById(expectUser.getId());
 
 		// then
 		assertThat(expectAnswer.isPresent()).isFalse();
