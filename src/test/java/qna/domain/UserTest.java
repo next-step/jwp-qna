e2b83492d914347ca90bfefab61775f89a39c6c5
shryu <>
2021-11-17 00:24:39 +0900

    test user entity 연관관계 테스트 작성

--- src/test/java/qna/domain/UserTest.java
+++ src/test/java/qna/domain/UserTest.java
@@ -7,6 +7,8 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import javax.persistence.EntityManager;
 
+import java.time.LocalDateTime;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
@@ -50,6 +52,38 @@ public class UserTest {
     void 검색() {
         User foundUser = userRepository.findById(user1.getId()).get();
         assertThat(foundUser).isEqualTo(user1);
+    }
+
+    @Test
+    void 연관관계_답변() {
+        questionRepository.save(question1);
+        Answer savedAnswer = answerRepository.save(answer1);
+        user1.addAnswer(savedAnswer);
+        em.flush();
+        em.clear();
+        User foundUser = userRepository.findById(user1.getId()).get();
+        assertThat(foundUser.getAnswers().get(0).getId()).isEqualTo(savedAnswer.getId());
+    }
+
+    @Test
+    void 연관관계_질문() {
+        Question savedQuestion = questionRepository.save(question1);
+        user1.addAQuestion(savedQuestion);
+        em.flush();
+        em.clear();
+        User foundUser = userRepository.findById(user1.getId()).get();
+        assertThat(foundUser.getQuestions().get(0).getId()).isEqualTo(savedQuestion.getId());
+    }
+
+    @Test
+    void 연관관계_삭제히스토리() {
+        Question savedQuestion = questionRepository.save(question1);
+        DeleteHistory savedDeleteHistory = deleteHistoryRepository.save(new DeleteHistory(ContentType.QUESTION, savedQuestion.getId(), user1, LocalDateTime.now()));
+        user1.addDeleteHistory(savedDeleteHistory);
+        em.flush();
+        em.clear();
+        User foundUser = userRepository.findById(user1.getId()).get();
+        assertThat(foundUser.getDeleteHistories().get(0).getId()).isEqualTo(savedDeleteHistory.getId());
     }
 
     @Test
