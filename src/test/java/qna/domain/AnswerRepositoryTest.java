270adcc7b3af1484b8bf27ad46d3df3364aa9398
vicjed <vicjed@icloud.com>
2021-11-17 02:31:21 +0900

    fix : AnswerRepositoryTest 수정

--- src/test/java/qna/domain/AnswerRepositoryTest.java
+++ src/test/java/qna/domain/AnswerRepositoryTest.java
@@ -1,72 +1,42 @@
 package qna.domain;
 
-import static java.util.Arrays.*;
 import static org.assertj.core.api.Assertions.*;
-import static qna.domain.UserTest.*;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-@DataJpaTest
-public class AnswerRepositoryTest {
+public class AnswerRepositoryTest extends QnATest {
 
-	@Autowired
-	AnswerRepository answerRepository;
+    @Test
+    void 삭제되지_않은_대상건중_한건을_true변경시_제외한_대상건만_조회() {
+        //given
+        User user = createUser();
+        Question question = createQuestion(user, TITLE_1, CONTENTS_1);
+        createAnswer(user, question, ANSWER_1);
+        createAnswer(user, question, ANSWER_2);
 
-	@Autowired
-	QuestionRepository questionRepository;
+        // when
+        List<Answer> answers = answerRepository.findByQuestionIdAndDeletedFalse(question.getId());
+        Answer answer = answers.get(0);
+        answer.setDeleted(true);
 
-	@Autowired
-	UserRepository userRepository;
+        List<Answer> expectedAnswers = answerRepository.findByDeletedFalse();
 
-	User user1;
-	User user2;
-	Question question1;
-	Question question2;
+        // then
+        assertThat(expectedAnswers.size() == 1).isTrue();
+    }
 
-	@BeforeEach
-	void setUp() {
-		user1 = userRepository.save(JAVAJIGI);
-		user2 = userRepository.save(SANJIGI);
-		question1 = new Question("공지사항1", "테스트1").writeBy(user1);
-		question2 = new Question("공지사항2", "테스트2").writeBy(user2);
+    @Test
+    void 여러건을_저장할때_리스트에_데이터를_담는경우_동일한_객체라도_모두_저장된다() {
+        User user = createUser();
+        Question question = createQuestion(user, TITLE_1, CONTENTS_1);
+        createAnswer(user, question, ANSWER_1);
+        createAnswer(user, question, ANSWER_1);
+        createAnswer(user, question, ANSWER_2);
 
-		questionRepository.saveAll(asList(question1, question2));
-	}
-
-	@Test
-	void 삭제되지_않은_대상건중_한건을_true변경시_제외한_대상건만_조회() {
-		//given
-		answerRepository.save(new Answer(user1, question1, "안녕하세요"));
-		answerRepository.save(new Answer(user2, question2, "안녕하세요2"));
-
-		// when
-		List<Answer> answers = answerRepository.findByQuestionIdAndDeletedFalse(question1.getId());
-		Answer answer = answers.get(0);
-		answer.setDeleted(true);
-
-		List<Answer> expectedAnswers = answerRepository.findByDeletedFalse();
-
-		// then
-		assertThat(expectedAnswers.size() == 1).isTrue();
-	}
-
-	@Test
-	void 여러건을_저장할때_리스트에_데이터를_담는경우_동일한_객체라도_모두_저장된다() {
-		List<Answer> answers = asList(
-			new Answer(user1, question1, "안녕하세요"),
-			new Answer(user1, question1, "안녕하세요"),
-			new Answer(user1, question1, "안녕하세요"));
-
-		List<Answer> answers1 = answerRepository.saveAll(answers);
-		assertThat(answers1.size()).isEqualTo(3);
-	}
+        List<Answer> findAnswer = answerRepository.findAll();
+        assertThat(findAnswer.size()).isEqualTo(3);
+    }
 
 }
