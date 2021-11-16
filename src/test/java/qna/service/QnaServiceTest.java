fd17c611763509390e27eddcfa2df1f8d1c42ed7
shryu <>
2021-11-17 00:18:40 +0900

    style : 연관관계 매핑으로 인한 entity 수정으로 인한 코드 수정

--- src/test/java/qna/service/QnaServiceTest.java
+++ src/test/java/qna/service/QnaServiceTest.java
@@ -90,8 +90,8 @@ class QnaServiceTest {
     private void verifyDeleteHistories() {
         LocalDateTime now = LocalDateTime.now();
         List<DeleteHistory> deleteHistories = Arrays.asList(
-                new DeleteHistory(ContentType.QUESTION, question.getId(), question.getWriterId(), now),
-                new DeleteHistory(ContentType.ANSWER, answer.getId(), answer.getWriterId(), now)
+                new DeleteHistory(ContentType.QUESTION, question.getId(), question.getWriter(), now),
+                new DeleteHistory(ContentType.ANSWER, answer.getId(), answer.getWriter(), now)
         );
         verify(deleteHistoryService).saveAll(deleteHistories);
     }
