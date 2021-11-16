63336a333c9b12f4bbe58606cd42a3133c715cfc
Summer <31176781+livedetuc@users.noreply.github.com>
2021-11-16 13:24:48 +0900

    Step1 - 엔티티 매핑 (#307)
    - JPA로 도메인 모델 구성: Answer, Question, DeleteHistory, User
    - @DataJpaTest를 이용하여 CRUD 테스트 구성
    
    Co-authored-by: 유한결 <hangyul.yu@42dot.io>

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,6 +1,70 @@
 package qna.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import java.util.Optional;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
 public class AnswerTest {
-    public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
-    public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+  @Autowired
+  AnswerRepository answers;
+
+  public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1,
+      "Answers Contents1");
+  public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1,
+      "Answers Contents2");
+
+  @Test
+  void saveAnswer() {
+    Answer savedAnswer = answers.save(A1);
+
+    assertAll(
+        ()-> assertThat(savedAnswer.getId()).isNotNull(),
+        ()-> assertThat(savedAnswer.getContents()).isEqualTo(A1.getContents())
+    );
+  }
+
+  @Test
+  void findAnswerByWriterId() {
+    Answer savedAnswer = answers.save(A1);
+    Answer foundAnswer = answers.findAnswerByWriterId(A1.getWriterId());
+
+    assertAll(
+        ()-> assertThat(foundAnswer.getId()).isNotNull(),
+        ()-> assertThat(foundAnswer.getQuestionId()).isEqualTo(savedAnswer.getQuestionId())
+    );
+  }
+
+  @Test
+  void updateAnswer() {
+    Answer savedAnswer = answers.save(A1);
+    Answer foundAnswer = answers.findById(savedAnswer.getId()).get();
+
+    foundAnswer.setContents(A2.getContents());
+    Optional<Answer> foundAnswerOptional = answers.findById(savedAnswer.getId());
+
+    assertAll(
+        ()-> assertThat(foundAnswerOptional).isPresent(),
+        ()-> assertThat(foundAnswerOptional.get().getContents()).isEqualTo(A2.getContents())
+    );
+  }
+
+  @Test
+  void deleteAnswer() {
+    Answer savedAnswer = answers.save(A1);
+    answers.delete(savedAnswer);
+    Optional<Answer> answerOptional = answers.findById(savedAnswer.getId());
+
+    assertThat(answerOptional).isNotPresent();
+
+  }
+
 }
--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,70 @@
 package qna.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
 public class QuestionTest {
-    public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
-    public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+  @Autowired
+  private QuestionRepository questions;
+
+  public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
+  public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+  @Test
+  void saveQuestion() {
+    Question savedQuestion = questions.save(Q1);
+
+    assertAll(
+        () -> assertThat(savedQuestion.getId()).isNotNull(),
+        () -> assertThat(savedQuestion.getContents()).isEqualTo(Q1.getContents())
+    );
+  }
+
+  @Test
+  void findByTitle() {
+    Question savedQuestion = questions.save(Q1);
+    Question foundQuestion = questions.findFirstByTitle(Q1.getTitle());
+
+    assertAll(
+        () -> assertThat(foundQuestion.getId()).isNotNull(),
+        () -> assertThat(foundQuestion.getContents()).isEqualTo(savedQuestion.getContents())
+    );
+  }
+
+  @Test
+  void updateQuestion() {
+    Question savedQuestion = questions.save(Q1);
+    Question foundQuestion = questions.findByIdAndDeletedFalse(savedQuestion.getId()).get();
+    foundQuestion.setDeleted(true);
+
+    Optional<Question> foundQuestionById = questions.findById(savedQuestion.getId());
+    Optional<Question> foundQuestionByIdAndDeletedFalse = questions
+        .findByIdAndDeletedFalse(savedQuestion.getId());
+
+    assertAll(
+        () -> assertThat(foundQuestionById).isPresent(),
+        () -> assertThat(foundQuestionByIdAndDeletedFalse).isNotPresent()
+    );
+  }
+
+  @Test
+  void deleteQuestion() {
+    Question savedQuestion = questions.save(Q1);
+    Question foundQuestion = questions.findById(savedQuestion.getId()).get();
+
+    questions.delete(foundQuestion);
+
+    Optional<Question> foundQuestionById = questions.findById(savedQuestion.getId());
+
+    assertAll(
+        () -> assertThat(foundQuestionById).isNotPresent()
+    );
+  }
 }
