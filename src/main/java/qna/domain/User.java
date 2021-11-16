a61f61af0bc255734b088859d89d1493bb91cd56
lewisseo91 <togokr@naver.com>
2021-11-17 04:16:38 +0900

    chore(User): 필요없는 import 정리
    필요없는 import 정리

--- src/main/java/qna/domain/User.java
+++ src/main/java/qna/domain/User.java
@@ -4,14 +4,10 @@ import qna.UnAuthorizedException;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 
 @Entity
