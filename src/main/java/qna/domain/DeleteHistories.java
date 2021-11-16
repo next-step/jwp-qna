8333d7d3c5d68b28fed8c7e7f7e07de99f314a8d
ChangSubKwak <paradise1732@gmail.com>
2021-11-17 02:03:08 +0900

    feat: DeleteHistories의 불변 클래스 변경

--- src/main/java/qna/domain/DeleteHistories.java
+++ src/main/java/qna/domain/DeleteHistories.java
@@ -1,24 +1,17 @@
 package qna.domain;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class DeleteHistories {
-    private List<DeleteHistory> deleteHistories = new ArrayList<>();
+    private final List<DeleteHistory> deleteHistories;
 
-    public DeleteHistories() {
+    private DeleteHistories(final List<DeleteHistory> deleteHistories) {
+        this.deleteHistories = Collections.unmodifiableList(deleteHistories);
     }
 
-    private DeleteHistories(List<DeleteHistory> deleteHistories) {
-        this.deleteHistories = deleteHistories;
-    }
-
-    public static DeleteHistories from(List<DeleteHistory> deleteHistories) {
+    public static DeleteHistories from(final List<DeleteHistory> deleteHistories) {
         return new DeleteHistories(deleteHistories);
-    }
-
-    public void add(DeleteHistory deleteHistory) {
-        deleteHistories.add(deleteHistory);
     }
 
     public List<DeleteHistory> getDeleteHistories() {
