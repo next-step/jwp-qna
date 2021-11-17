651c81fd6fcf1125f24b0828c719df5cd6905aa8
InmookJeong <jeonginmook@gmail.com>
2021-11-17 23:42:38 +0900

    test: add sign up test code

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO user (id, user_id, password, name, email, created_at)
+VALUES
+    (1L, 'javajigi', 'password', 'name', 'javajigi@slipp.net', CURRENT_TIMESTAMP()),
+    (2L, 'sanjigi', 'password', 'name', 'sanjigi@slipp.net', CURRENT_TIMESTAMP());
--- src/test/java/qna/domain/UserTest.java
+++ src/test/java/qna/domain/UserTest.java
@@ -1,5 +1,6 @@
 package qna.domain;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -9,6 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 
 @DataJpaTest
 public class UserTest {
+    // data.sql을 통해 Insert 자동화
     public static final User JAVAJIGI = new User(1L, "javajigi", "password", "name", "javajigi@slipp.net");
     public static final User SANJIGI = new User(2L, "sanjigi", "password", "name", "sanjigi@slipp.net");
 
@@ -16,11 +18,13 @@ public class UserTest {
     private UserRepository userRepository;
 
     @Test
-    void save() {
-        User actual = userRepository.save(JAVAJIGI);
+    @DisplayName("회원가입 : 사용자 정보 등록")
+    void signUp() {
+        User actual = userRepository.save(new User(3L, "inmookjeong", "password", "inmookjeong", "jeonginmook@gmail.com"));
         assertAll(
                 () -> assertThat(actual).isNotNull(),
-                () -> assertThat(actual.getId()).isEqualTo(JAVAJIGI.getId())
+                () -> assertThat(actual.getId()).isEqualTo(3L),
+                () -> assertThat(actual.getUserId()).isEqualTo("inmookjeong")
         );
     }
 }
