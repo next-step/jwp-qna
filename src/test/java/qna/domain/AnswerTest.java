b336056d2ae5449aee42f9cd95c0d52d209fc07c
Hanee Park <hanea8199@gmail.com>
2021-11-18 01:05:24 +0900

    1단계 - 엔티티 매핑 (#334)
    * feat: 수업시간 Hands on
    
    * feat(Answer): Answer 엔티티 클래스 작성
    
    * feat(Answer, BaseEntity): Answer 엔티티 클래스의 createdAt, updatedAt을 JPA Auditing을 이용해 구현
    
    * feat: 나머지 Entity 클래스 작성
    
    * Revert "feat: 수업시간 Hands on"
    
    This reverts commit e33b89ffd253ce1ad7c6cd564b5ed1646f3c9a1b.

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,6 +1,75 @@
 package qna.domain;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
 public class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @Autowired
+    private AnswerRepository answers;
+
+    @Test
+    void instantiate() {
+        assertThat(A1.getId()).isNull();
+        assertThat(A1.getWriterId()).isEqualTo(UserTest.JAVAJIGI.getId());
+        assertThat(A1.getQuestionId()).isEqualTo(QuestionTest.Q1.getId());
+        assertThat(A1.getContents()).isEqualTo("Answers Contents1");
+        assertThat(A1.isDeleted()).isFalse();
+        assertThat(A1.getCreatedAt()).isNull();
+        assertThat(A1.getUpdatedAt()).isNull();
+    }
+
+    @Test
+    void save() {
+        LocalDateTime now = LocalDateTime.now();
+        final Answer actual = answers.save(A1);
+        assertThat(actual).isNotNull();
+        assertThat(actual.getId()).isNotNull();
+        assertThat(actual.getWriterId()).isEqualTo(A1.getWriterId());
+        assertThat(actual.getQuestionId()).isEqualTo(A1.getQuestionId());
+        assertThat(actual.getContents()).isEqualTo(A1.getContents());
+        assertThat(actual.isDeleted()).isEqualTo(A1.isDeleted());
+        assertThat(actual.getCreatedAt()).isAfterOrEqualTo(now);
+        assertThat(actual.getUpdatedAt()).isAfterOrEqualTo(now);
+    }
+
+    @Test
+    void findByName() {
+        final Answer expected = answers.save(A1);
+        final Answer actual = answers.findById(expected.getId())
+                .orElseThrow(RuntimeException::new);
+        assertThat(actual).isNotNull();
+        assertThat(actual.getId()).isEqualTo(expected.getId());
+        assertThat(actual.getWriterId()).isEqualTo(expected.getWriterId());
+    }
+
+    @Test
+    void update() {
+        final LocalDateTime now = LocalDateTime.now();
+        final Answer answer = answers.save(A1);
+        String content = "Updated Content";
+
+        answer.setContents(content);
+
+        final Answer actual = answers.findById(answer.getId()).get();
+        assertThat(actual.getContents()).isEqualTo(content);
+        assertThat(actual.getUpdatedAt()).isAfterOrEqualTo(now);
+    }
+
+    @Test
+    void delete() {
+        final Answer answer = answers.save(A1);
+        assertThat(answers.findById(answer.getId())).isNotNull();
+        answers.deleteById(answer.getId());
+        assertThat(answers.findById(answer.getId())).isEmpty();
+    }
+
 }
--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,40 @@
 package qna.domain;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
 public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+    @Autowired
+    private QuestionRepository questions;
+
+    @Test
+    void save() {
+        final Question question = questions.save(Q1);
+        assertThat(question.getId()).isNotNull();
+        assertThat(question.getTitle()).isEqualTo(Q1.getTitle());
+    }
+
+    @Test
+    void findByDeletedFalse_Empty() {
+        List<Question> questions = this.questions.findByDeletedFalse();
+        assertThat(questions).isEmpty();
+    }
+
+    @Test
+    void findByDeletedFalse() {
+        questions.save(Q1);
+        questions.save(Q2);
+
+        List<Question> questions = this.questions.findByDeletedFalse();
+        assertThat(questions.size()).isEqualTo(2);
+    }
 }
