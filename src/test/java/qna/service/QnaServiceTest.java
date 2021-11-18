b981306ed2e65c6104e6faf62d9d8bc9b7a78ed0
jongwoo <jongwoo.dev@gmail.com>
2021-11-19 01:19:34 +0900

    test: 지금까지의 변경점에 맞춰서 QnaServiceTest 수정
    1. 연관관계 매핑 설정 구현시 자동으로 Answer가 Question에 들어가게 처리된 것을 반영하여 테스트코드에서 addAnswer 라인 제거
    2. Question Entity의 delete 피드백 적용(삭제 순서 변경)에 따른 테스트 검증 데이터 변경(deleteHistories의 순서 변경)

--- src/test/java/qna/service/QnaServiceTest.java
+++ src/test/java/qna/service/QnaServiceTest.java
@@ -46,7 +46,7 @@ class QnaServiceTest {
     public void setUp() throws Exception {
         question = new Question(1L, "title1", "contents1").writeBy(UserTest.JAVAJIGI);
         answer = new Answer(1L, UserTest.JAVAJIGI, question, "Answers Contents1");
-        question.addAnswer(answer);
+        //question.addAnswer(answer);
     }
 
     @Test
@@ -95,8 +95,8 @@ class QnaServiceTest {
 
     private void verifyDeleteHistories() {
         List<DeleteHistory> deleteHistories = Arrays.asList(
-                new DeleteHistory(ContentType.QUESTION, question.getId(), question.getWriter(), LocalDateTime.now()),
-                new DeleteHistory(ContentType.ANSWER, answer.getId(), answer.getWriter(), LocalDateTime.now())
+            new DeleteHistory(ContentType.ANSWER, answer.getId(), answer.getWriter(), LocalDateTime.now()),
+            new DeleteHistory(ContentType.QUESTION, question.getId(), question.getWriter(), LocalDateTime.now())
         );
         verify(deleteHistoryService).saveAll(deleteHistories);
     }
