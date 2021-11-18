8797e4f58c8e76a8afb6d5327646c7cf00d3b0ea
InmookJeong <jeonginmook@gmail.com>
2021-11-18 19:55:02 +0900

    test: find users count and list test code

--- src/test/java/qna/domain/UserTest.java
+++ src/test/java/qna/domain/UserTest.java
@@ -1,9 +1,13 @@
 package qna.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -17,10 +21,16 @@ public class UserTest {
     @Autowired
     private UserRepository userRepository;
 
+    User actual = null;
+
+    @BeforeEach
+    void makeDefaultUser() {
+        actual = userRepository.save(new User("inmookjeong", "password", "inmookjeong", "jeonginmook@gmail.com"));
+    }
+
     @Test
     @DisplayName("회원가입 : 사용자 정보 등록")
     void signUp() {
-        User actual = userRepository.save(new User("inmookjeong", "password", "inmookjeong", "jeonginmook@gmail.com"));
         assertAll(
                 () -> assertThat(actual).isNotNull(),
                 () -> assertThat(actual.getId()).isEqualTo(3L),
@@ -31,7 +41,6 @@ public class UserTest {
     @Test
     @DisplayName("회원정보 수정 : 회원 이름, 이메일")
     void update() {
-        User actual = userRepository.save(new User("inmookjeong", "password", "inmookjeong", "jeonginmook@gmail.com"));
         actual.setName("mook");
         actual.setEmail("jeonginmook2@gmail.com");
         actual.update(actual, actual);
@@ -46,12 +55,29 @@ public class UserTest {
     @Test
     @DisplayName("회원정보 수정 : 회원 패스워드")
     void updatePassword() {
-        User inmookjeong = userRepository.save(new User("inmookjeong", "password", "inmookjeong", "jeonginmook@gmail.com"));
-        inmookjeong.updatePassword(inmookjeong, "newPassword");
+        actual.updatePassword(actual, "newPassword");
         assertAll(
-                () -> assertThat(inmookjeong).isNotNull(),
-                () -> assertThat(inmookjeong.getId()).isEqualTo(3L),
-                () -> assertThat(inmookjeong.getPassword()).isEqualTo("newPassword")
+                () -> assertThat(actual).isNotNull(),
+                () -> assertThat(actual.getId()).isEqualTo(3L),
+                () -> assertThat(actual.getPassword()).isEqualTo("newPassword")
         );
+    }
+
+    @Test
+    @DisplayName("전체 회원 수 조회")
+    void countUsers() {
+        Long count = userRepository.count();
+        assertThat(count).isEqualTo(3L);
+    }
+
+    @Test
+    @DisplayName("전체 회원 목록 조회")
+    void getUsers() {
+        String orderByColumn = "id";
+        List<User> users = userRepository.findAll(Sort.by(Sort.Direction.ASC, orderByColumn));
+        assertThat(users.size()).isEqualTo(3L);
+        assertThat(users.get(0).getUserId()).isEqualTo("javajigi");
+        assertThat(users.get(1).getUserId()).isEqualTo("sanjigi");
+        assertThat(users.get(2).getUserId()).isEqualTo("inmookjeong");
     }
 }
