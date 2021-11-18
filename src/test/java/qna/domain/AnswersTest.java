1547ef3e29f18d7d5fbc1dcbf5d6b6af7e1e1732
이승민 <sml7179@naver.com>
2021-11-18 23:00:58 +0900

    refactor: QnaService 리팩토링

--- src/test/java/qna/domain/AnswersTest.java
+++ src/test/java/qna/domain/AnswersTest.java
@@ -1,32 +1,35 @@
 package qna.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import qna.CannotDeleteException;
-
-import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class AnswersTest {
 
+    private final User user = new User(1L,"lsm", "password", "이승민", "test@test.com");
+    private final Answer A1 = new Answer(1L, user, QuestionTest.Q1, "Answers Contents1");
+    private final Answer A2 = new Answer(2L, user, QuestionTest.Q1, "Answers Contents2");
+    private final Answers answers = Answers.of();
+
+    @BeforeEach
+    void setUp() {
+        answers.add(A1);
+        answers.add(A2);
+    }
+
     @DisplayName("Answers일급 콜렉션 객체 생성")
     @Test
     void create() {
-        Answers answers = new Answers(Arrays.asList(AnswerTest.A1, AnswerTest.A2));
-
         assertThat(answers.size()).isEqualTo(2);
     }
 
     @DisplayName("답변들을 삭제 상태로 변경시켜준다.")
     @Test
     void delete() throws CannotDeleteException {
-        final User user = new User(1L,"lsm", "password", "이승민", "test@test.com");
-        final Answer A1 = new Answer(1L, user, QuestionTest.Q1, "Answers Contents1");
-        final Answer A2 = new Answer(2L, user, QuestionTest.Q1, "Answers Contents2");
-        Answers answers = new Answers(Arrays.asList(A1, A2));
-
         answers.delete(user);
 
         assertThat(answers.getAnswerGroup().get(0).isDeleted()).isTrue();
@@ -34,26 +37,18 @@ public class AnswersTest {
     }
 
 
-    @DisplayName("답변 등록자가 삭제하려는 사람과 같을때 삭제 되는지 검증")
+    @DisplayName("답변들 삭제시 작성자와 사용자가 같을때 삭제 되는지 검증")
     @Test
     void deleteAnswersByUser() throws CannotDeleteException {
-        final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
-        final Answer A2 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents2");
-        Answers answers = new Answers(Arrays.asList(A1, A2));
-
         answers.delete(A1.getWriter());
 
         assertThat(answers.getAnswerGroup().get(0).isDeleted()).isTrue();
         assertThat(answers.getAnswerGroup().get(1).isDeleted()).isTrue();
     }
 
-    @DisplayName("답변 등록자가 삭제하려는 사람과 다를때 에러가 생기는지 검증")
+    @DisplayName("답변들 삭제시 작성자와 사용자가 다를때 에러가 생기는지 검증")
     @Test
     void deleteAnswersByUserError() throws CannotDeleteException {
-        final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
-        final Answer A2 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents2");
-        Answers answers = new Answers(Arrays.asList(A1, A2));
-
         assertThatThrownBy(() -> {
             answers.delete(new User("lsm", "password", "이승민", "test@test.com"));
         }).isInstanceOf(CannotDeleteException.class)
