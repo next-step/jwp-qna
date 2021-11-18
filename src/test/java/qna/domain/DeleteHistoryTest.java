929f344d86e88f370b336d948d0a58ff8803da27
jongwoo <jongwoo.dev@gmail.com>
2021-11-19 00:47:12 +0900

    test: 피드백적용 - DeleteHistory Entity Test에서 사용하지않는 @DataJpaTest 제거

--- src/test/java/qna/domain/DeleteHistoryTest.java
+++ src/test/java/qna/domain/DeleteHistoryTest.java
@@ -4,7 +4,6 @@ import java.time.LocalDateTime;
 
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-@DataJpaTest
 public class DeleteHistoryTest {
     public static final DeleteHistory D1 = new DeleteHistory(ContentType.QUESTION, QuestionTest.Q1.getId(), UserTest.JAVAJIGI,
         LocalDateTime.now());
