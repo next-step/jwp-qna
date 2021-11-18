9ff2f478f924311df6721e6b0e0b9f36ae88cda7
jongwoo <jongwoo.dev@gmail.com>
2021-11-19 01:01:22 +0900

    test: 피드백적용 - User Entity Test에 단위 테스트 추가

--- src/test/java/qna/domain/UserTest.java
+++ src/test/java/qna/domain/UserTest.java
@@ -1,7 +1,43 @@
 package qna.domain;
 
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import qna.UnAuthorizedException;
+
 public class UserTest {
     public static final User JAVAJIGI = new User(1L, "javajigi", "password", "name", "javajigi@slipp.net");
     public static final User SANJIGI = new User(2L, "sanjigi", "password", "name", "sanjigi@slipp.net");
 
+
+    @Test
+    @DisplayName("유저정보 업데이트(성공)")
+    void update() {
+        User userOld = new User(1L, "javajigi", "password", "name", "javajigi@slipp.net");
+        User userNewInfo = new User(1L, "javajigi", "password", "changed", "changed@slipp.net");
+        User userLogin = new User(1L, "javajigi", "password", "name", "javajigi@slipp.net");
+
+        userOld.update(userLogin, userNewInfo);
+        assertThat(userOld.getName()).isEqualTo("changed");
+        assertThat(userOld.getEmail()).isEqualTo("changed@slipp.net");
+    }
+
+    @Test
+    @DisplayName("유저정보 업데이트(실패)")
+    void updateFail() {
+        User userOld = new User(1L, "javajigi", "password", "name", "javajigi@slipp.net");
+        User userNewInfo = new User(1L, "javajigi", "password", "changed", "changed@slipp.net");
+        User userNewInfoInvalidPW = new User(1L, "javajigi", "invalid", "changed", "changed@slipp.net");
+        User userLogin = new User(1L, "invalid", "password", "name", "javajigi@slipp.net");
+
+        assertThatThrownBy(()->{
+            userOld.update(userLogin, userNewInfo);
+        }).isInstanceOf(UnAuthorizedException.class);
+
+        assertThatThrownBy(()->{
+            userOld.update(userLogin, userNewInfoInvalidPW);
+        }).isInstanceOf(UnAuthorizedException.class);
+    }
 }
