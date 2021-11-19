ab78f93ff11857248fbb75378d5d4ba5eafa9a9a
shryu <>
2021-11-19 18:35:49 +0900

    refactor : Collections.unmodifiableList 사용

--- src/main/java/qna/domain/DeleteHistories.java
+++ src/main/java/qna/domain/DeleteHistories.java
@@ -1,12 +1,13 @@
 package qna.domain;
 
+import java.util.Collections;
 import java.util.List;
 
 public class DeleteHistories {
     private final List<DeleteHistory> deleteHistories;
 
     public DeleteHistories(List<DeleteHistory> deleteHistories) {
-        this.deleteHistories = deleteHistories;
+        this.deleteHistories = Collections.unmodifiableList(deleteHistories);
     }
 
     public List<DeleteHistory> getValue() {
