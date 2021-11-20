d7be740ea01de2cd68880ed74e552567a3417262
sungwoo <ssw890418@gmail.com>
2021-11-20 20:28:23 +0900

    refactor:연관관계 메핑에 따른 테스트 로직 수정

--- src/test/java/qna/domain/QuestionRepositoryTest.java
+++ src/test/java/qna/domain/QuestionRepositoryTest.java
@@ -1,13 +1,14 @@
 package qna.domain;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DataIntegrityViolationException;
 
-import java.time.LocalDateTime;
+import javax.persistence.EntityManager;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
@@ -16,23 +17,29 @@ class QuestionRepositoryTest {
     @Autowired
     private QuestionRepository questionRepository;
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private EntityManager entityManager;
+
     @Test
     @DisplayName("save하면 id가 자동으로 생성되야 한다.")
     void saveTest() {
-        assertThat(QuestionTest.Q1.getId()).isNull();
-        Question save = questionRepository.save(QuestionTest.Q1);
+        userRepository.save(UserTest.JAVAJIGI);
+        Question save = questionRepository.save(new Question("title", "content").writeBy(UserTest.JAVAJIGI));
 
         assertAll(
                 () -> assertThat(save.getId()).isNotNull(),
-                () -> assertThat(QuestionTest.Q1.getId()).isNotNull(),
-                () -> assertThat(save.getWriterId()).isEqualTo(QuestionTest.Q1.getWriterId())
+                () -> assertThat(save.getWriter()).isEqualTo(UserTest.JAVAJIGI)
         );
     }
-    
+
     @Test
     @DisplayName("findById 결과는 동일성이 보장되어야한다.")
     void findByIdTest() {
-        Question save = questionRepository.save(QuestionTest.Q1);
+
+        Question save = questionRepository.save(new Question("title", "contents"));
 
         Question find = questionRepository.findById(save.getId()).get();
 
@@ -42,13 +49,10 @@ class QuestionRepositoryTest {
     @Test
     @DisplayName("nullable false 칼럼은 반드시 값이 있어야한다.")
     void nullableTest() {
-        Question save = questionRepository.save(QuestionTest.Q1);
-        Question find = questionRepository.findById(save.getId()).get();
 
-        assertAll(
-                () -> assertThat(find.getCreatedAt()).isBefore(LocalDateTime.now()),
-                () -> assertThat(find.isDeleted()).isFalse(),
-                () -> assertThat(find.getTitle()).isNotNull()
-        );
+        Question question = new Question(null, "contents");
+
+        assertThatThrownBy(() -> questionRepository.save(question))
+                .isInstanceOf(DataIntegrityViolationException.class);
     }
 }
