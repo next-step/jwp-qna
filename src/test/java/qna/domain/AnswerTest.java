2674228849ea2b3e91bf51200c27c49c420c9c3b
coder013 <wnssh607@gmail.com>
2021-11-20 17:24:44 +0900

    test: 테스트 코드 작성

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,6 +1,29 @@
 package qna.domain;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
 public class AnswerTest {
+
+    @Autowired
+    private AnswerRepository answerRepository;
+
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @Test
+    void 답변_저장_후_조회_테스트() {
+        Answer answer = answerRepository.save(A1);
+
+        Optional<Answer> opAnswer = answerRepository.findByIdAndDeletedFalse(answer.getId());
+
+        assertThat(opAnswer).isNotEmpty();
+        assertThat(opAnswer.get()).isEqualTo(answer);
+    }
 }
