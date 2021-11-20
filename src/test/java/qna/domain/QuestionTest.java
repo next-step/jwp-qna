85fc1324e8b75e9824531a8a5979a0004f9d8b12
coder013 <wnssh607@gmail.com>
2021-11-20 15:50:55 +0900

    test: 학습 테스트 작성

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,29 @@
 package qna.domain;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
 public class QuestionTest {
+
+    @Autowired
+    private QuestionRepository questionRepository;
+
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+    @Test
+    void 질문_저장_후_조회_테스트() {
+        Question question = questionRepository.save(Q1);
+
+        Optional<Question> opQuestion = questionRepository.findByIdAndDeletedFalse(question.getId());
+
+        assertThat(opQuestion).isNotEmpty();
+        assertThat(opQuestion.get()).isEqualTo(question);
+    }
 }
--- src/test/java/qna/domain/UserTest.java
+++ src/test/java/qna/domain/UserTest.java
@@ -1,6 +1,53 @@
 package qna.domain;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
 public class UserTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
     public static final User JAVAJIGI = new User(1L, "javajigi", "password", "name", "javajigi@slipp.net");
     public static final User SANJIGI = new User(2L, "sanjigi", "password", "name", "sanjigi@slipp.net");
+
+    @Test
+    void 사용자_저장_테스트() {
+        User user = userRepository.save(JAVAJIGI);
+
+        assertThat(user).isNotNull();
+    }
+
+    @Test
+    void 사용자_조회_테스트() {
+        userRepository.save(JAVAJIGI);
+
+        Optional<User> user = userRepository.findByUserId(JAVAJIGI.getUserId());
+
+        assertThat(user).isNotEmpty();
+        assertThat(user.get().getUserId()).isEqualTo(JAVAJIGI.getUserId());
+    }
+
+    @Test
+    void 사용자_삭제_테스트() {
+        userRepository.save(JAVAJIGI);
+        assertThat(userRepository.findByUserId(JAVAJIGI.getUserId())).isNotEmpty();
+
+        userRepository.delete(JAVAJIGI);
+
+        assertThat(userRepository.findByUserId(JAVAJIGI.getUserId())).isEmpty();
+    }
+
+    @Test
+    void 객체_동일성_테스트() {
+        User user = userRepository.save(JAVAJIGI);
+
+        assertThat(userRepository.findByUserId(JAVAJIGI.getUserId()).get()).isEqualTo(user);
+    }
 }
