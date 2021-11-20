2a4078f4774db74e97543e3dce2f9d88a1c59fb0
InmookJeong <jeonginmook@gmail.com>
2021-11-20 09:46:16 +0900

    test: update answer's content

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,5 +1,7 @@
 package qna.domain;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -15,12 +17,42 @@ public class AnswerTest {
     @Autowired
     private AnswerRepository answerRepository;
 
+    private Answer answer1;
+    private Answer answer2;
+
+    @BeforeEach
+    void saveDefaultAnswer() {
+        answer1 = answerRepository.save(A1);
+        answer2 = answerRepository.save(A2);
+    }
+
     @Test
+    @DisplayName("답변 등록")
     void save() {
-        Answer actual = answerRepository.save(A1);
+        Answer actual = answer1;
         assertAll(
                 () -> assertThat(actual).isNotNull(),
                 () -> assertThat(actual.getId()).isEqualTo(A1.getId())
+        );
+    }
+    
+    /*
+    1. Update : Contents 없데이터
+    2. Question을 이용한 조회 및 조회 수 가져오기
+    3. 상세 조회
+    4. 삭제
+        - Question 삭제 시 하위에 있는 Answer도 삭제 테스트
+     */
+
+    @Test
+    @DisplayName("질문 수정")
+    void update() {
+        String contents = "질문 내용을 수정합니다.";
+        answer1.setContents(contents);
+        Answer actual = answerRepository.save(answer1);
+        assertAll(
+                () -> assertThat(actual).isNotNull(),
+                () -> assertThat(actual.getContents()).isEqualTo(contents)
         );
     }
 }
