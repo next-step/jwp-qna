4bb09f9997e0c92d84b6bc0cf173901a8dc8c391
sungwoo <ssw890418@gmail.com>
2021-11-22 00:03:39 +0900

    refactor:도메인 영역 로직의 단위테스트 추가

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,6 +1,34 @@
 package qna.domain;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, Contents.of("Answers Contents1"));
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, Contents.of("Answers Contents2"));
+
+    private Question question;
+    private Answer answer;
+
+    @BeforeEach
+    void setUp() {
+        question = new Question(1L, "title1", Contents.of("contents1")).writeBy(UserTest.JAVAJIGI);
+        answer = new Answer(1L, UserTest.JAVAJIGI, question, Contents.of("Answers Contents1"));
+    }
+
+    @Test
+    @DisplayName("답변이 생기면 질문에도 답변이 생겨야한다.")
+    void toQuestionTest() {
+        assertThat(question.getAnswers()).isEqualTo(Answers.of(answer));
+    }
+
+    @Test
+    void 질문_삭제시_답변_삭제상태_변경_되어야한다() throws Exception {
+        question.delete(UserTest.JAVAJIGI);
+
+        assertThat(answer.isDeleted()).isTrue();
+    }
 }
--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,42 @@
 package qna.domain;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import qna.CannotDeleteException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@DataJpaTest
 public class QuestionTest {
     public static final Question Q1 = new Question("title1", Contents.of("contents1")).writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", Contents.of("contents2")).writeBy(UserTest.SANJIGI);
+
+    @Test
+    void 질문_삭제시_삭제상태가_False로_변경되야한다() throws Exception {
+        Question question = new Question("title", "contents").writeBy(UserTest.JAVAJIGI);
+        assertThat(question.isDeleted()).isFalse();
+
+        question.delete(UserTest.JAVAJIGI);
+        assertThat(question.isDeleted()).isTrue();
+    }
+
+    @Test
+    void 질문자와_다른_사용자가_질문을_삭제할_수_없다() throws Exception {
+        Question question = new Question("title", "contents").writeBy(UserTest.JAVAJIGI);
+
+        assertThatThrownBy(() -> question.delete(UserTest.SANJIGI))
+                .isInstanceOf(CannotDeleteException.class);
+    }
+
+    @Test
+    void 답변이_없는_경우_삭제_가능하다() throws Exception{
+        Question question = new Question("title", "contents").writeBy(UserTest.JAVAJIGI);
+        Answer answer = new Answer(1L, UserTest.SANJIGI, question, Contents.of("Answers Contents1"));
+
+        assertThatThrownBy(() -> question.delete(UserTest.JAVAJIGI))
+                .isInstanceOf(CannotDeleteException.class)
+                .hasMessage("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
+
+    }
 }
