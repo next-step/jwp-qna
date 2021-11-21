eb2efc87961107177b33d2899ef9ebfc02b24525
lovethefeel <lovethefeel@gmail.com>
2021-11-21 23:42:46 +0900

    refactor: 단위테스트 사용을 위한 정적 메소드 추가, 삭제 시 삭제 시간 추가

--- src/main/java/qna/domain/Answers.java
+++ src/main/java/qna/domain/Answers.java
@@ -2,6 +2,7 @@ package qna.domain;
 
 import javax.persistence.Embeddable;
 import javax.persistence.OneToMany;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,6 +11,12 @@ public class Answers {
     @OneToMany(mappedBy = "question")
     private List<Answer> answers = new ArrayList<>();
 
+    public static Answers of(Answer answer) {
+        final Answers answers = new Answers();
+        answers.addAnswer(answer);
+        return answers;
+    }
+
     protected Answers() {
     }
 
@@ -17,10 +24,10 @@ public class Answers {
         answers.add(answer);
     }
 
-    public List<DeleteHistory> deleteAnswers(User writer) {
+    public List<DeleteHistory> deleteAnswers(User writer, LocalDateTime localDateTime) {
         List<DeleteHistory> deleteHistories = new ArrayList<>();
         for( Answer answer: answers ) {
-            deleteHistories.add(answer.deleteAnswer(writer));
+            deleteHistories.add(answer.deleteAnswer(writer, localDateTime));
         }
         return deleteHistories;
     }
