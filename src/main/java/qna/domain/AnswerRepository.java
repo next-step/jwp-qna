70aa2ff31b85daa8cea7bc11d229503f304c9368
박창혁 <changchang73@pionnet.co.kr>
2021-11-22 00:53:06 +0900

    chore : 사용하지 않는 클래스 제거

--- src/main/java/qna/domain/AnswerRepository.java
+++ src/main/java/qna/domain/AnswerRepository.java
@@ -6,8 +6,6 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
-    List<Answer> findByQuestionIdAndDeletedFalse(Long questionId);
-
     Optional<Answer> findByIdAndDeletedFalse(Long id);
 
     Answer findByWriterId(Long id);
--- src/main/java/subway/domain/MemberRepository.java
+++
@@ -1,6 +0,0 @@
-package subway.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-}
