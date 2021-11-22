46251e20dd216bf5958d4e2ba06e6c4486002892
Sung Du Kim <sungdu.alex.kim@gmail.com>
2021-11-23 08:21:40 +0900

    test: Question fixture ID 부여

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -9,8 +9,8 @@ import org.junit.jupiter.api.Test;
 
 public class QuestionTest {
 
-    public static final Question Q1 = new Question("title1", "contents1", UserTest.JAVAJIGI);
-    public static final Question Q2 = new Question("title2", "contents2", UserTest.SANJIGI);
+    public static final Question Q1 = new Question(1L, "title1", "contents1", UserTest.JAVAJIGI);
+    public static final Question Q2 = new Question(2L, "title2", "contents2", UserTest.SANJIGI);
 
     @Test
     void equals() {
