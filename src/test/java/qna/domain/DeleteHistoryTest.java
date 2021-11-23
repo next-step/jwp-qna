5137bfc003ec7645ed1d22578f61c605446ff0a0
daehyun0 <dd70296@gmail.com>
2021-11-24 02:45:54 +0900

    test(DeleteHistoryTest): 잘못된 테스트 수정
    - 변수의 순서를 의미적으로 올바른 순서로 변경
    - isSameAs를 이용한 테스트 삭제 (너무 엄격함)
    - Optional 강제 unwrap하여 사용하지 않도록 변경
    - 의미없는 변수 삭제

--- src/test/java/qna/domain/DeleteHistoryTest.java
+++ src/test/java/qna/domain/DeleteHistoryTest.java
@@ -3,6 +3,7 @@ package qna.domain;
 import static org.assertj.core.api.Assertions.*;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,10 +19,10 @@ public class DeleteHistoryTest {
 	@Test
 	void save() {
 		questionRepository.save(QuestionTest.Q1);
-		DeleteHistory deleteHistory = new DeleteHistory(ContentType.QUESTION, QuestionTest.Q1.getId(),
-			UserTest.JAVAJIGI.getId(),
-			LocalDateTime.now());
-		DeleteHistory actual = deleteHistoryRepository.save(deleteHistory);
+		DeleteHistory actual = deleteHistoryRepository.save(
+			new DeleteHistory(ContentType.QUESTION, QuestionTest.Q1.getId(),
+				UserTest.JAVAJIGI.getId(),
+				LocalDateTime.now()));
 
 		assertThat(actual).isNotNull();
 	}
@@ -29,13 +30,13 @@ public class DeleteHistoryTest {
 	@Test
 	void findById() {
 		questionRepository.save(QuestionTest.Q2);
-		DeleteHistory expected = new DeleteHistory(ContentType.QUESTION, QuestionTest.Q2.getId(),
-			QuestionTest.Q2.getWriterId(),
-			LocalDateTime.now());
-		deleteHistoryRepository.save(expected);
+		DeleteHistory expected = deleteHistoryRepository.save(
+			new DeleteHistory(ContentType.QUESTION, QuestionTest.Q2.getId(),
+				QuestionTest.Q2.getWriterId(),
+				LocalDateTime.now()));
 
-		DeleteHistory actual = deleteHistoryRepository.findById(1L).get();
-		assertThat(actual).isEqualTo(expected);
-		assertThat(actual).isSameAs(expected);
+		Optional<DeleteHistory> actual = deleteHistoryRepository.findById(1L);
+
+		assertThat(actual).hasValue(expected);
 	}
 }
