d2d084dbb921c7e2f94113f505ae386708ebb5c9
daehyun0 <dd70296@gmail.com>
2021-11-25 00:57:41 +0900

    refactor: DeleteHistories 클래스 추가
    - 기존 커밋에서 빼먹은것 추가

---
+++ src/main/java/qna/domain/DeleteHistories.java
@@ -0,0 +1,27 @@
+package qna.domain;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DeleteHistories {
+	private final List<DeleteHistory> deleteHistories = new ArrayList<>();
+
+	public void add(Question question, LocalDateTime dateTime) {
+		DeleteHistory deleteHistory = new DeleteHistory(ContentType.QUESTION, question.getId(), question.getWriterId(),
+			dateTime);
+		deleteHistories.add(deleteHistory);
+	}
+
+	public void add(Answers answers, LocalDateTime dateTime) {
+		for (Answer answer: answers.getAnswers()) {
+			DeleteHistory deleteHistory = new DeleteHistory(ContentType.ANSWER, answer.getId(), answer.getWriterId(),
+				dateTime);
+			deleteHistories.add(deleteHistory);
+		}
+	}
+
+	public List<DeleteHistory> get() {
+		return deleteHistories;
+	}
+}
