a34bf8d92d255c57cc5db120e9d8c645b4090a2d
InmookJeong <jeonginmook@gmail.com>
2021-11-30 00:28:47 +0900

    refactor: rename AnswersDaoTest class
    - Before : test.java.qna.domain.AnswersTest.java
    - After : test.java.qna.dao.AnswersDaoTest.java

--- src/test/java/qna/dao/AnswersDaoTest.java
+++ src/test/java/qna/dao/AnswersDaoTest.java
@@ -1,17 +1,18 @@
-package qna.domain;
+package qna.dao;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import qna.domain.*;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
-public class AnswersTest {
+public class AnswersDaoTest {
 
 	@Autowired
 	private QuestionRepository questionRepository;
