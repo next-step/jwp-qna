952b4f328914bcbb9f3801f396621e853e51bfb7
coder013 <wnssh607@gmail.com>
2021-11-30 23:26:20 +0900

    test: 테스트 코드 수정

--- src/test/java/qna/domain/AnswerRepositoryTest.java
+++ src/test/java/qna/domain/AnswerRepositoryTest.java
@@ -12,10 +12,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class AnswerRepositoryTest {
 
     @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    @Autowired
     private AnswerRepository answerRepository;
 
     @Test
     void 답변_저장_후_조회_테스트() {
+        userRepository.save(AnswerTest.A1.getWriter());
+        questionRepository.save(AnswerTest.A1.getQuestion());
         Answer answer = answerRepository.save(AnswerTest.A1);
 
         Optional<Answer> opAnswer = answerRepository.findByIdAndDeletedFalse(answer.getId());
