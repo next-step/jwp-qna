126d39554e686f381b94109f0cee7ebaa98893ad
coder013 <wnssh607@gmail.com>
2021-12-05 21:35:51 +0900

    test: 테스트 코드 수정 및 추가

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,8 +1,11 @@
 package qna.domain;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+import qna.CannotDeleteException;
 
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class QuestionTest {
@@ -14,5 +17,10 @@ public class QuestionTest {
     @NullAndEmptySource
     void title_null_테스트(String title) {
         assertThatIllegalArgumentException().isThrownBy(() -> new Question(title, "contents1").writeBy(UserTest.JAVAJIGI));
+    }
+
+    @Test
+    void 다른_사람이_작성한_질문_삭제_테스트() {
+        assertThatExceptionOfType(CannotDeleteException.class).isThrownBy(() -> Q1.delete(UserTest.SANJIGI));
     }
 }
