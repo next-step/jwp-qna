c998cd4e3b0ca4eb1dfd9fb4a00c7bf523a33e58
KangSeongHyo <abc9023@naver.com>
2022-05-14 21:27:42 +0900

    refactor : 전체 Repository 인터페이스 패키지 이동 (domain.repository)

--- src/test/java/qna/service/QnaServiceTest.java
+++ src/test/java/qna/service/QnaServiceTest.java
@@ -13,6 +13,8 @@ import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import qna.domain.repository.AnswerRepository;
+import qna.domain.repository.QuestionRepository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
