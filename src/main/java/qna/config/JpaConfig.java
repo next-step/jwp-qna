a347ef0b6dd1452550b4897ee8f124f352c44199
heowc <heowc1992@gmail.com>
2022-05-14 20:20:16 +0900

    refactor: JpaConfig 제거

--- src/main/java/qna/config/JpaConfig.java
+++
@@ -1,9 +0,0 @@
-package qna.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-@EnableJpaAuditing
-@Configuration
-class JpaConfig {
-}
