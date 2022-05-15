4229ba362ed48bfa759c6d3fb20512dbb0f64f37
ssonsh <chosunci@gmail.com>
2022-05-16 08:31:02 +0900

    feat(Question) : Question 도메인에 대한 엔티티 매핑 작업

--- src/main/java/qna/domain/Question.java
+++ src/main/java/qna/domain/Question.java
@@ -1,10 +1,28 @@
 package qna.domain;
 
-public class Question {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "question")
+public class Question extends BaseDateTimeEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "title", length = 100, nullable = false)
     private String title;
+    @Lob
+    @Column(name = "contents")
     private String contents;
+    @Column(name = "writer_id")
     private Long writerId;
+    @Column(name = "deleted", nullable = false)
     private boolean deleted = false;
 
     public Question(String title, String contents) {
@@ -16,6 +34,8 @@ public class Question {
         this.title = title;
         this.contents = contents;
     }
+
+    protected Question() {}
 
     public Question writeBy(User writer) {
         this.writerId = writer.getId();
