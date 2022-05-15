edf75ff9b53aa5f69af77b50c6ce6bee1b6d4a02
ssonsh <chosunci@gmail.com>
2022-05-16 08:53:13 +0900

    test(UserTest) : User 엔티티에 대한 생성처리 테스트 코드 작성
    - 기존에 static 변수로 선언되어 있는 JAVAJIGI, SANJIGI를 userRepository로 save 하고
    
    - save 된 정보를 다시 select 하여 그 결과가 일치하는지 테스트 코드로 작성

--- src/test/java/qna/domain/UserTest.java
+++ src/test/java/qna/domain/UserTest.java
@@ -1,6 +1,57 @@
 package qna.domain;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Optional;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
 public class UserTest {
     public static final User JAVAJIGI = new User(1L, "javajigi", "password", "name", "javajigi@slipp.net");
     public static final User SANJIGI = new User(2L, "sanjigi", "password", "name", "sanjigi@slipp.net");
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    @DisplayName("User 도메인 생성 테스트")
+    void generate01(){
+        // given & when
+        User javajigi = userRepository.save(JAVAJIGI);
+        User sanjigi = userRepository.save(SANJIGI);
+
+        em.flush();
+        em.clear();
+
+        // then
+        Optional<User> findJavajigi = userRepository.findByUserId(javajigi.getUserId());
+        Optional<User> findSanjigi = userRepository.findByUserId(sanjigi.getUserId());
+
+        assertAll(
+            () -> assertTrue(findJavajigi.isPresent()),
+            () -> assertEqualsUser(javajigi, findJavajigi.get()),
+            () -> assertTrue(findSanjigi.isPresent()),
+            () -> assertEqualsUser(sanjigi, findSanjigi.get())
+        );
+    }
+
+    private void assertEqualsUser(User expect, User actual) {
+        assertAll(
+            () -> assertEquals(expect.getId(), actual.getId()),
+            () -> assertEquals(expect.getPassword(), actual.getPassword()),
+            () -> assertEquals(expect.getUserId(), actual.getUserId()),
+            () -> assertEquals(expect.getEmail(), actual.getEmail()),
+            () -> assertEquals(expect.getName(), actual.getName())
+        );
+    }
 }
