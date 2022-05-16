d559dd719d847beafc21eb8a9e9bec26ef4a8539
heowc <heowc1992@gmail.com>
2022-05-16 20:08:35 +0900

    refacor: fixture 데이터 추가

--- src/test/java/qna/domain/FixtureUser.java
+++ src/test/java/qna/domain/FixtureUser.java
@@ -3,6 +3,7 @@ package qna.domain;
 public final class FixtureUser {
     public static final User JAVAJIGI = new User(1L, "javajigi", "password", "name", "javajigi@slipp.net");
     public static final User SANJIGI = new User(2L, "sanjigi", "password", "name", "sanjigi@slipp.net");
+    public static final User HEOWC = new User(3L, "heowc", "1234", "wonchul", "heowc1992@gmail.com");
 
     private FixtureUser() {
     }
