6c149fb20d69a83a894904b508befc8fd2992edb
kwonyongil <ksgkms23@likelion.org>
2022-05-17 00:32:09 +0900

    feat(jpa) Answer Entity 설정추가

--- src/main/java/qna/domain/Answer.java
+++ src/main/java/qna/domain/Answer.java
@@ -1,22 +1,31 @@
 package qna.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import qna.NotFoundException;
 import qna.UnAuthorizedException;
 
 import java.util.Objects;
-
+@Entity
 public class Answer extends BaseEntity {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Long writerId;
     private Long questionId;
+    @Lob
     private String contents;
+    @Column(nullable = false)
     private boolean deleted = false;
 
-    public Answer(User writer, Question question, String contents) {
+    protected Answer() {
+    }
+
+    protected Answer(User writer, Question question, String contents) {
         this(null, writer, question, contents);
     }
 
