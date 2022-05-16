67821222d5ba6a9b3878094d24d1c1045ddfad5d
mmtos <js223kr92@naver.com>
2022-05-17 02:04:59 +0900

    test : Jpa Respository 기본 CRD 메서드 및 각 repository 메서드 테스트 작성

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,6 +1,67 @@
 package qna.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import java.util.Optional;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
 public class AnswerTest {
+
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @Autowired
+    private AnswerRepository answerRepository;
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    @Test
+    void save_테스트() {
+        Answer managed = answerRepository.save(A1);
+        Assertions.assertAll(
+                () -> assertThat(managed == A1).isTrue()
+        );
+    }
+
+    @Test
+    void findById_테스트() {
+        answerRepository.save(A1);
+        Optional<Answer> a1 = answerRepository.findById(A1.getId());
+        assertThat(a1.isPresent()).isTrue();
+        assertThat(a1.get() == A1).isTrue();
+    }
+
+    @Test
+    void deleteById_테스트() {
+        answerRepository.save(A1);
+        answerRepository.deleteById(A1.getId());
+        Optional<Answer> a1 = answerRepository.findById(A1.getId());
+        assertThat(a1.isPresent()).isFalse();
+    }
+
+    @Test
+    void findByIdAndDeletedFalse_테스트() {
+        answerRepository.save(A1);
+        A1.setDeleted(true);
+        answerRepository.save(A2);
+        Optional<Answer> a1 = answerRepository.findByIdAndDeletedFalse(A1.getId());
+        Optional<Answer> a2 = answerRepository.findByIdAndDeletedFalse(A2.getId());
+        assertThat(a1.isPresent()).isFalse();
+        assertThat(a2.isPresent()).isTrue();
+        assertThat(a2.get() == A2).isTrue();
+    }
+
+    @Test
+    void findByQuestionIdAndDeletedFalse_테스트() {
+        answerRepository.save(A1);
+        answerRepository.save(A2);
+        questionRepository.save(QuestionTest.Q1);
+        List<Answer> answerList = answerRepository.findByQuestionIdAndDeletedFalse(A1.getQuestionId());
+        assertThat(answerList).containsExactly(A1, A2);
+    }
 }
--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,65 @@
 package qna.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import java.util.Optional;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
 public class QuestionTest {
+
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    @Test
+    void save_테스트() {
+        Question managed = questionRepository.save(Q1);
+        Assertions.assertAll(
+                () -> assertThat(managed == Q1).isTrue()
+        );
+    }
+
+    @Test
+    void findById_테스트() {
+        questionRepository.save(Q1);
+        Optional<Question> q1 = questionRepository.findById(Q1.getId());
+        assertThat(q1.isPresent()).isTrue();
+        assertThat(q1.get() == Q1).isTrue();
+    }
+
+    @Test
+    void deleteById_테스트() {
+        questionRepository.save(Q1);
+        questionRepository.deleteById(Q1.getId());
+        Optional<Question> q1 = questionRepository.findById(Q1.getId());
+        assertThat(q1.isPresent()).isFalse();
+    }
+
+    @Test
+    void findByIdAndDeletedFalse_테스트() {
+        questionRepository.save(Q1);
+        Q1.setDeleted(true);
+        questionRepository.save(Q2);
+        Optional<Question> q1 = questionRepository.findByIdAndDeletedFalse(Q1.getId());
+        Optional<Question> q2 = questionRepository.findByIdAndDeletedFalse(Q2.getId());
+        assertThat(q1.isPresent()).isFalse();
+        assertThat(q2.isPresent()).isTrue();
+        assertThat(q2.get() == Q2).isTrue();
+    }
+
+    @Test
+    void findByQuestionIdAndDeletedFalse_테스트() {
+        questionRepository.save(Q1);
+        questionRepository.save(Q2);
+        questionRepository.findByDeletedFalse();
+        List<Question> questionList = questionRepository.findByDeletedFalse();
+        assertThat(questionList).containsExactly(Q1, Q2);
+    }
 }
