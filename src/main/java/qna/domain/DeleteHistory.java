df9ab8bc9e567f88ee89bd5f3b7ea3db79aeae3e
Donghee Han <handh0413@gmail.com>
2022-05-17 21:31:12 +0900

    feat: 삭제 이력을 표현한 DeleteHistory 엔티티 클래스

--- src/main/java/qna/domain/DeleteHistory.java
+++ src/main/java/qna/domain/DeleteHistory.java
@@ -1,14 +1,29 @@
 package qna.domain;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
+@Entity
 public class DeleteHistory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Enumerated(EnumType.STRING)
     private ContentType contentType;
+
+    @Column(name = "CONTENT_ID")
     private Long contentId;
+
+    @Column(name = "DELETED_BY_ID")
     private Long deletedById;
+
+    @Column(name = "CREATE_DATE")
     private LocalDateTime createDate = LocalDateTime.now();
+
+    public DeleteHistory() {
+    }
 
     public DeleteHistory(ContentType contentType, Long contentId, Long deletedById, LocalDateTime createDate) {
         this.contentType = contentType;
