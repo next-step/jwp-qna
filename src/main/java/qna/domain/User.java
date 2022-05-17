80e247310cd28e382afabe3a94c5930e0f7d24d7
tasklet1579 <tasklet1579@gmail.com>
2022-05-17 19:21:20 +0900

    refactor : User 클래스 요구 사항에 맞게 수정 - 데이터베이스 테이블과 매핑 - 기본 생성자 선언 - 컬럼 속성 변경 - 컬럼 추가

--- src/main/java/qna/domain/User.java
+++ src/main/java/qna/domain/User.java
@@ -2,18 +2,31 @@ package qna.domain;
 
 import qna.UnAuthorizedException;
 
+import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.Objects;
 
+@Entity
+@Table(name = "user")
 public class User {
     public static final GuestUser GUEST_USER = new GuestUser();
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(length = 20, nullable = false, unique = true)
     private String userId;
+    @Column(length = 20, nullable = false)
     private String password;
+    @Column(length = 20, nullable = false)
     private String name;
+    @Column(length = 50)
     private String email;
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 
-    private User() {
+    protected User() {
     }
 
     public User(String userId, String password, String name, String email) {
