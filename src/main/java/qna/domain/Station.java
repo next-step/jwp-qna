314eb126e7976ae025fe40188893ecf48a5ed835
tasklet1579 <tasklet1579@gmail.com>
2022-05-17 18:24:08 +0900

    test : JPA Hands-on Part 1 실습 - Station 엔티티 클래스 선언 - save, findById, findByName 메서드 사용

---
+++ src/main/java/qna/domain/Station.java
@@ -0,0 +1,35 @@
+package qna.domain;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "station")
+public class Station {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    protected Station() {
+    }
+
+    public Station(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void changeName(String name) {
+        this.name = name;
+    }
+}
