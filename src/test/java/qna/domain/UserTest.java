115f7fc9e33af0019f3623f9c61c236f81015c8f
Donghee Han <handh0413@gmail.com>
2022-05-17 22:07:51 +0900

    feat: JPA Auditing으로 생성시간/수정시간 자동화하기

--- src/test/java/qna/domain/UserTest.java
+++ src/test/java/qna/domain/UserTest.java
@@ -1,5 +1,10 @@
 package qna.domain;
 
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@DataJpaTest
 public class UserTest {
     public static final User JAVAJIGI = new User(1L, "javajigi", "password", "name", "javajigi@slipp.net");
     public static final User SANJIGI = new User(2L, "sanjigi", "password", "name", "sanjigi@slipp.net");
