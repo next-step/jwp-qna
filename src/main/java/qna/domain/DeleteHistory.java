74fb7c77df1b529a09fdbeb54e0427d78660bad2
sihyun <sihyun.park@skylabs.io>
2022-05-17 17:15:40 +0900

    feat: Entity 어노테이션 적용. 생성, 수정 날짜 관련 BaseEntity 적용

--- src/main/java/qna/domain/DeleteHistory.java
+++ src/main/java/qna/domain/DeleteHistory.java
@@ -1,14 +1,24 @@
 package qna.domain;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
+@Entity
 public class DeleteHistory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private ContentType contentType;
+
     private Long contentId;
     private Long deletedById;
     private LocalDateTime createDate = LocalDateTime.now();
+
+    public DeleteHistory() {
+
+    }
 
     public DeleteHistory(ContentType contentType, Long contentId, Long deletedById, LocalDateTime createDate) {
         this.contentType = contentType;
