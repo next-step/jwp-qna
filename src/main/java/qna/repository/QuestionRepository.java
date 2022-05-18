382da093196de0203ea37dad067b82a655fb940d
kwonyongil <ksgkms23@likelion.org>
2022-05-19 01:20:48 +0900

    refactor(jpa) 클래스 코드 컨벤션 적용

--- src/main/java/qna/repository/QuestionRepository.java
+++ src/main/java/qna/repository/QuestionRepository.java
@@ -1,9 +1,8 @@
 package qna.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import qna.domain.Question;
 
