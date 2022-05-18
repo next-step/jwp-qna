24ab94c7e80a68157f76ee520abee009e46e9e1f
tyakamyz <tyakamyz@naver.com>
2022-05-18 22:20:13 +0900

    feat: updatable 속성 추가

--- src/main/java/qna/domain/BaseEntity.java
+++ src/main/java/qna/domain/BaseEntity.java
@@ -13,7 +13,7 @@ import java.time.LocalDateTime;
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
     @CreatedDate
-    @Column(nullable = false)
+    @Column(updatable = false, nullable = false)
     private LocalDateTime createdAt;
 
     @LastModifiedDate
