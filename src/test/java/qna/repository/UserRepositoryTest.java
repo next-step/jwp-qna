709fde7567983e6f94cad8260453278e350ceea5
june2 <>
2022-05-18 18:19:03 +0900

    feat: base entity, user 클래스 추가 맵핑 & jpaAuditing 활성

---
+++ src/test/java/qna/repository/UserRepositoryTest.java
@@ -0,0 +1,38 @@
+package qna.repository;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import qna.domain.User;
+import qna.domain.UserTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+@DataJpaTest
+public class UserRepositoryTest {
+    @Autowired
+    UserRepository userRepository;
+
+    User user;
+
+    @BeforeEach
+    void setUp() {
+        user = new User("javajigi", "password", "name", "javajigi@slipp.net");
+        userRepository.save(user);
+    }
+
+    @Test
+    void findByUserId_test() {
+        // when
+        User user = userRepository.findByUserId(UserTest.JAVAJIGI.getUserId()).get();
+
+        // then
+        assertAll(
+                () -> assertThat(user).isNotNull(),
+                () -> assertThat(user.getEmail()).isEqualTo(UserTest.JAVAJIGI.getEmail()),
+                () -> assertThat(user.getName()).isEqualTo(UserTest.JAVAJIGI.getName())
+        );
+    }
+}
