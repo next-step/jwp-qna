6641f9a8db53997677276b13290b5856dc926fef
Yul Jeon <notkhaki@gmail.com>
2022-05-19 12:56:05 +0900

    refactor(Answer, Question, User): remove unnecessary setter setId()

--- src/main/java/qna/domain/Answer.java
+++ src/main/java/qna/domain/Answer.java
@@ -68,10 +68,6 @@ public class Answer {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public Long getWriterId() {
         return writerId;
     }
--- src/main/java/qna/domain/Question.java
+++ src/main/java/qna/domain/Question.java
@@ -60,10 +60,6 @@ public class Question {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getTitle() {
         return title;
     }
