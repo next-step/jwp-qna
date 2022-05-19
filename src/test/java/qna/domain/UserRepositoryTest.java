ace8aa2ef4b355abbf7431ad9a9de282dede8c6c
jiyeonghwang <gyp0288@naver.com>
2022-05-19 23:18:51 +0900

    test: user repository test

---
+++ src/test/java/qna/domain/UserRepositoryTest.java
@@ -0,0 +1,34 @@
+package qna.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+@DataJpaTest
+public class UserRepositoryTest {
+    @Autowired
+    UserRepository userRepository;
+
+    @Test
+    void save() {
+        User expected = new User("tester", "Tester1!", "테스터", "tester@test.com");
+        User actual = userRepository.save(expected);
+        assertAll(
+                () -> assertThat(expected.getUserId()).isEqualTo(actual.getUserId()),
+                () -> assertThat(expected.equalsNameAndEmail(actual)).isEqualTo(true)
+        );
+    }
+
+    @Test
+    void findAll() {
+        User expected = new User("tester", "Tester1!", "테스터", "tester@test.com");
+        userRepository.save(expected);
+        List<User> actual = userRepository.findAll();
+        assertThat(actual.get(0).getUserId()).isNotNull();
+    }
+}
