6f207729d17a5ef41ad062f8e131713a9cdb5595
jiyeonghwang <gyp0288@naver.com>
2022-05-19 23:39:53 +0900

    test: QuestionRepository 테스트

---
+++ src/test/java/qna/domain/QuestionRepositoryTest.java
@@ -0,0 +1,36 @@
+package qna.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+@DataJpaTest
+public class QuestionRepositoryTest {
+    @Autowired
+    QuestionRepository questionRepository;
+
+    @Test
+    void save() {
+        Question expected = new Question("제목", "내용");
+        Question actual = questionRepository.save(expected);
+        assertAll(
+                () -> assertThat(expected.getContents()).isEqualTo(actual.getContents()),
+                () -> assertThat(expected.getTitle()).isEqualTo(actual.getTitle())
+        );
+    }
+
+    @Test
+    void findAll() {
+        Question expected = new Question("제목", "내용");
+        questionRepository.save(expected);
+        List<Question> actual = questionRepository.findAll();
+        assertThat(actual.get(0).getContents()).isNotNull();
+        assertThat(actual.get(0).getCreatedAt()).isNotNull();
+        System.out.println(actual.get(0).toString());
+    }
+}
