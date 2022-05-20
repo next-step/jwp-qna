6367b9dbb34feba6d89311c1eb3bf5012afce79a
박시현 <33593855+tyakamyz@users.noreply.github.com>
2022-05-20 22:42:50 +0900

    1단계 - 엔티티 매핑 (#399)
    * docs: README 작성
    
    * feat: Entity 어노테이션 적용. 생성, 수정 날짜 관련 BaseEntity 적용
    
    * feat: DDL 확인을 위한 설정 추가
    
    * feat: @Enumerated 어노테이션 추가
    
    * feat: @EnableJpaAuditing 어노테이션 적용
    
    * test: Jpa 테스트코드 작성
    
    * test: findByDeletedFalse 테스트 코드 추가
    
    * test: QuestionId를 null로 테스트하던 버그 수정
    
    * test: AnswerTest.A1.Deleted 값 초기화
    
    * feat: updatable 속성 추가
    
    * refactor: 기본 생성자 public -> protected 으로 변경
    
    * feat: jpa parameter 로그 확인 옵션 추가
    
    * feat: enum 저장 시 넘버링 값이 아닌 value 값으로 저장될 수 있도록 변경
    
    * test: 테스트 순서를 변경하여 전체 테스트시 통과될 수 있도록 변경
    
    Co-authored-by: sihyun <sihyun.park@skylabs.io>

---
+++ src/test/java/qna/domain/AnswerRepositoryTest.java
@@ -0,0 +1,73 @@
+package qna.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+@DataJpaTest
+class AnswerRepositoryTest {
+
+    @Autowired
+    private AnswerRepository answerRepository;
+
+    @Test
+    void Answer_저장() {
+        Answer answer = AnswerTest.A1;
+        Answer result = answerRepository.save(AnswerTest.A1);
+
+        assertAll(
+                () -> assertThat(result.getId()).isNotNull(),
+                () -> assertThat(result.getContents()).isEqualTo(answer.getContents())
+        );
+    }
+
+    @Test
+    void Answer_단건_조회() {
+        Answer answer = answerRepository.save(AnswerTest.A1);
+
+        assertThat(answerRepository.findById(answer.getId()).get())
+                .usingRecursiveComparison()
+                .isEqualTo(answer);
+    }
+
+    @Test
+    void Answer_전체_조회() {
+        answerRepository.save(AnswerTest.A1);
+        answerRepository.save(AnswerTest.A2);
+
+        assertThat(answerRepository.findAll()).hasSize(2);
+    }
+
+    @Test
+    void Answer_삭제여부_컬럼이_false인_단건_조회() {
+        Answer answer = answerRepository.save(AnswerTest.A1);
+
+        assertThat(answerRepository.findByIdAndDeletedFalse(answer.getId()).get()).isEqualTo(answer);
+
+        Answer findAnswer = answerRepository.findById(answer.getId()).get();
+        findAnswer.setDeleted(true);
+
+        assertThat(answerRepository.findByIdAndDeletedFalse(answer.getId()).isPresent()).isFalse();
+
+    }
+
+    @Test
+    void Answer_findByQuestionId_삭제여부_컬럼이_false인_전체_조회() {
+        AnswerTest.A1.setDeleted(false);
+        Answer answer = AnswerTest.A1;
+        answer.setQuestionId(1L);
+
+        Answer result = answerRepository.save(answer);
+
+        assertThat(answerRepository.findByQuestionIdAndDeletedFalse(1L)).hasSize(1);
+
+        Answer findAnswer = answerRepository.findById(result.getId()).get();
+        findAnswer.setDeleted(true);
+
+        assertThat(answerRepository.findByQuestionIdAndDeletedFalse(findAnswer.getQuestionId()).size()).isZero();
+    }
+
+}
