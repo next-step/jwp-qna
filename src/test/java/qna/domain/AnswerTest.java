e3bffdbd37c35919e4c93f5a65694d2fa4a2af12
minzzang <minjjang1117@gmail.com>
2022-05-21 00:11:06 +0900

    feat(AnswerTest): 작성자가 null일 경우 예외가 발생한다

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,6 +1,19 @@
 package qna.domain;
 
+import org.junit.jupiter.api.Test;
+import qna.UnAuthorizedException;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @Test
+    void 작성자가_null일_경우_예외가_발생한다() {
+        // when and then
+        assertThatThrownBy(() ->
+                new Answer(1L, null, QuestionTest.Q1, "test")
+        ).isInstanceOf(UnAuthorizedException.class);
+    }
 }
