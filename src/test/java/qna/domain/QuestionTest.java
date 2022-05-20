7a183fb4023e87f9bb91f03103e167e4d3e6ffab
minzzang <minjjang1117@gmail.com>
2022-05-21 00:35:14 +0900

    feat(QuestionTest): 작성자를 설정한다

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,19 @@
 package qna.domain;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+    @Test
+    void 작성자를_설정한다() {
+        Question question = new Question("title", "contents");
+        // when
+        Question result = question.writeBy(UserTest.SANJIGI);
+        // then
+        assertThat(result.getWriterId()).isEqualTo(2L);
+    }
 }
