7a931eae935545c1fcd967b62bd38009515b3678
tyakamyz <tyakamyz@naver.com>
2022-05-20 21:42:12 +0900

    feat: enum 저장 시 넘버링 값이 아닌 value 값으로 저장될 수 있도록 변경

--- src/main/java/qna/domain/DeleteHistory.java
+++ src/main/java/qna/domain/DeleteHistory.java
@@ -10,7 +10,7 @@ public class DeleteHistory {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Enumerated
+    @Enumerated(EnumType.STRING)
     private ContentType contentType;
 
     private Long contentId;
