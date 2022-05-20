5c3fddc7ecc286635f61c0d9162a120a9c07efbc
limwoobin <drogba02@naver.com>
2022-05-20 21:10:51 +0900

    test: Answer 에 대한 테스트 코드 리팩토링
    - auto_increment 값을 초기화 하는대신 상태값을 변경하여 처리

--- src/test/java/qna/repository/AnswerRepositoryTest.java
+++ src/test/java/qna/repository/AnswerRepositoryTest.java
@@ -9,11 +9,8 @@ import static qna.domain.AnswerTest.A2;
 
 import java.util.List;
 import java.util.Optional;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -26,25 +23,23 @@ class AnswerRepositoryTest {
     @Autowired
     private AnswerRepository answerRepository;
 
-    @PersistenceContext
-    EntityManager em;
+    private Answer answer;
+    private Answer answer2;
 
-    @AfterEach
-    void teardown() {
-        answerRepository.deleteAll();
+    @BeforeEach
+    void setUp() {
+        answer = A1;
+        answer2 = A2;
         A1.setDeleted(false);
         A2.setDeleted(false);
 
-        em.createNativeQuery(
-                "ALTER TABLE answer ALTER COLUMN `id` RESTART WITH 1")
-            .executeUpdate();
+        answer = answerRepository.save(answer);
+        answer2 = answerRepository.save(answer2);
     }
 
     @DisplayName("엔티티를 저장하면 정상적으로 저장되어 id 값이 채번되어야 한다")
     @Test
     void save_test() {
-        Answer answer = A1;
-
         answerRepository.save(answer);
         assertThat(answer.getId()).isNotNull();
     }
@@ -52,12 +47,6 @@ class AnswerRepositoryTest {
     @DisplayName("같은 questionId 를 가진 엔티티를 questionId 로 조회하면 모두 조회되어야 한다")
     @Test
     void find_test() {
-        Answer answer = A1;
-        Answer answer2 = A2;
-
-        answerRepository.save(answer);
-        answerRepository.save(answer2);
-
         List<Answer> answers = answerRepository.findByQuestionIdAndDeletedFalse(A1.getQuestionId());
 
         assertAll(
@@ -69,11 +58,7 @@ class AnswerRepositoryTest {
     @DisplayName("id 와 deleted 가 false 인 엔티티를 동일 조건으로 조회하면 모두 조회되어야 한다")
     @Test
     void find_test2() {
-        Answer answer = A1;
-        Answer answer2 = A2;
         answer2.setDeleted(true);
-
-        answerRepository.save(answer);
         answerRepository.save(answer2);
 
         Optional<Answer> result = answerRepository.findByIdAndDeletedFalse(answer.getId());
@@ -85,11 +70,7 @@ class AnswerRepositoryTest {
 
     @DisplayName("엔티티의 deleted 를 변경하고 다시 조회하면 변경된 상태로 조회되어야 한다")
     @Test
-    @Order(5)
     void update_test() {
-        Answer answer = A1;
-        answerRepository.save(answer);
-
         answer.setDeleted(true);
         answerRepository.save(answer);
 
@@ -99,11 +80,6 @@ class AnswerRepositoryTest {
     @DisplayName("entity 를 삭제하면 정상적으로 삭제되어야 한다")
     @Test
     void delete_test() {
-        Answer answer = A1;
-        Answer answer2 = A2;
-        answerRepository.save(answer);
-        answerRepository.save(answer2);
-
         List<Answer> answers = answerRepository.findAll();
         answerRepository.delete(answer);
 
