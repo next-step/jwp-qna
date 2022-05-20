6071b250816cf9538503ad5c8d2c15471b4e1d55
heowc <heowc1992@gmail.com>
2022-05-20 10:45:29 +0900

    refactor(Answer): 메소드 분리

--- src/main/java/qna/domain/Answer.java
+++ src/main/java/qna/domain/Answer.java
@@ -83,11 +83,15 @@ public class Answer extends BaseEntity {
     }
 
     public DeleteHistory delete(User loginUser) {
+        validateOwner(loginUser);
+        deleted = true;
+        return DeleteHistory.ofAnswer(this);
+    }
+
+    private void validateOwner(User loginUser) {
         if (!isOwner(loginUser)) {
             throw new CannotDeleteException("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
         }
-        deleted = true;
-        return DeleteHistory.ofAnswer(this);
     }
 
     @Override
