8b3858a8d171219779682f79b80abdd4c3fb0596
Ttungga <torb2002@gmail.com>
2022-05-21 00:29:54 +0900

    feat: make User entity

--- src/main/java/qna/domain/User.java
+++ src/main/java/qna/domain/User.java
@@ -1,22 +1,40 @@
 package qna.domain;
 
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import qna.UnAuthorizedException;
 
-import java.util.Objects;
-
-public class User {
+@Entity
+@Table(name = "user")
+public class User extends BaseTimeEntity {
     public static final GuestUser GUEST_USER = new GuestUser();
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "user_id")
     private String userId;
+
+    @Column(name = "password")
     private String password;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "email")
     private String email;
 
-    private User() {
+    public User() {
     }
 
-    public User(String userId, String password, String name, String email) {
+    User(String userId, String password, String name, String email) {
         this(null, userId, password, name, email);
     }
 
@@ -110,6 +128,8 @@ public class User {
                 ", password='" + password + '\'' +
                 ", name='" + name + '\'' +
                 ", email='" + email + '\'' +
+                ", createdAt=" + super.getCreatedAt() +
+                ", modifiedAt=" + super.getModifiedAt() +
                 '}';
     }
 
