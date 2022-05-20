ed869a6ae32f9c4e7073498b96eb7d5b1821e25e
mond <revive29@gmail.com>
2022-05-21 04:37:35 +0900

    fix(DeleteHistoryRepositoryTest): 테스트 코드 실패 수정

--- src/test/java/qna/domain/DeleteHistoryRepositoryTest.java
+++ src/test/java/qna/domain/DeleteHistoryRepositoryTest.java
@@ -2,7 +2,7 @@ package qna.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
-import static qna.domain.QuestionTest.Q1;
+import static qna.domain.QuestionTest.Q2;
 import static qna.domain.UserTest.MOND;
 
 import java.util.Optional;
@@ -58,8 +58,8 @@ class DeleteHistoryRepositoryTest {
     private void initUserAndQuestionSetting() {
         User mond = userRepository.findByUserId(MOND.getUserId())
                 .orElseGet(() -> userRepository.save(MOND));
-        Question question = questionRepository.findById(Q1.getId())
-                .orElseGet(() -> questionRepository.save(Q1));
+        Question question = questionRepository.findById(Q2.writeBy(mond).getId())
+                .orElseGet(() -> questionRepository.save(Q2.writeBy(mond)));
         deleteHistory = new DeleteHistory(DeleteHistoryContent.removeQuestion(question), mond);
     }
 
