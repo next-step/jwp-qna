59ccbe894990ee6a7b99e577a05875cd4db27167
Ttungga <torb2002@gmail.com>
2022-05-21 00:22:53 +0900

    feat: add ContentTypeConverter and make DeleteHistory entity

--- src/main/java/qna/domain/ContentType.java
+++ src/main/java/qna/domain/ContentType.java
@@ -1,5 +1,17 @@
 package qna.domain;
 
 public enum ContentType {
-    QUESTION, ANSWER
+    QUESTION("QUESTION"),
+    ANSWER("ANSWER"),
+    ;
+
+    private final String type;
+
+    ContentType(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
 }
