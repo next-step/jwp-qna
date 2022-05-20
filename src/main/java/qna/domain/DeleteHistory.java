57100145f5254ef5b90452b63ae0e52da6debebd
june2 <>
2022-05-20 16:06:32 +0900

    fix: deposithistory entity에서 baseEntity제거

--- src/main/java/qna/domain/DeleteHistory.java
+++ src/main/java/qna/domain/DeleteHistory.java
@@ -5,7 +5,7 @@ import java.time.LocalDateTime;
 import java.util.Objects;
 
 @Entity
-public class DeleteHistory extends BaseEntity {
+public class DeleteHistory {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
