dab866f77fe28359f8c42508d45090aaa7963f56
minzzang <minjjang1117@gmail.com>
2022-05-20 22:53:34 +0900

    feat(Answer): Answer 엔티티 매핑

--- src/main/java/qna/domain/Answer.java
+++ src/main/java/qna/domain/Answer.java
@@ -3,14 +3,27 @@ package qna.domain;
 import qna.NotFoundException;
 import qna.UnAuthorizedException;
 
+import javax.persistence.*;
 import java.util.Objects;
 
-public class Answer {
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Entity
+public class Answer extends BaseEntity {
+    @Id @GeneratedValue(strategy = IDENTITY)
     private Long id;
+    @Column(name = "writer_id")
     private Long writerId;
+    @Column(name = "question_id")
     private Long questionId;
+    @Lob
+    @Column(name = "contents")
     private String contents;
+    @Column(name = "deleted", nullable = false)
     private boolean deleted = false;
+
+    protected Answer() {
+    }
 
     public Answer(User writer, Question question, String contents) {
         this(null, writer, question, contents);
