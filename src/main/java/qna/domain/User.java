a2c3d547154e8f31bbe5978476f3ef77642d483c
minzzang <minjjang1117@gmail.com>
2022-05-20 23:22:47 +0900

    feat(User): User 엔티티 매핑

--- src/main/java/qna/domain/User.java
+++ src/main/java/qna/domain/User.java
@@ -2,18 +2,29 @@ package qna.domain;
 
 import qna.UnAuthorizedException;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.util.Objects;
 
-public class User {
-    public static final GuestUser GUEST_USER = new GuestUser();
+import static javax.persistence.GenerationType.IDENTITY;
 
+@Entity
+public class User extends BaseEntity {
+    public static final GuestUser GUEST_USER = new GuestUser();
+    @Id @GeneratedValue(strategy = IDENTITY)
     private Long id;
+    @Column(name = "user_id", unique = true, nullable = false, length = 20)
     private String userId;
+    @Column(name = "password", nullable = false, length = 20)
     private String password;
+    @Column(name = "name", nullable = false, length = 20)
     private String name;
+    @Column(name = "email", length = 50)
     private String email;
 
-    private User() {
+    protected User() {
     }
 
     public User(String userId, String password, String name, String email) {
