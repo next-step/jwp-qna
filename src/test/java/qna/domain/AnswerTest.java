717f0f279b3c8cd1cdd9b06df8bc25f1cfd5b7cb
mond <revive29@gmail.com>
2022-05-21 02:00:35 +0900

    test(AnswerTest): Answer 엔티티 테스트 코드 작성

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,6 +1,56 @@
 package qna.domain;
 
-public class AnswerTest {
-    public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
-    public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static qna.domain.QuestionTest.Q1;
+import static qna.domain.QuestionTest.Q2;
+import static qna.domain.UserTest.JAVAJIGI;
+import static qna.domain.UserTest.SANJIGI;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import qna.exception.NotFoundException;
+import qna.exception.UnAuthorizedException;
+
+class AnswerTest {
+    public static final Answer A1 = new Answer(JAVAJIGI, Q1, "Answers Contents1");
+    public static final Answer A2 = new Answer(SANJIGI, Q1, "Answers Contents2");
+
+    @Test
+    @DisplayName("객체 생성 확인")
+    void verifyAnswer() {
+        assertThat(new Answer(JAVAJIGI, Q1, "contents")).isEqualTo(new Answer(JAVAJIGI, Q1, "contents"));
+    }
+
+    @Test
+    @DisplayName("유저 정보가 없으면 UnAuthorizedException이 발생")
+    void inputNullUserThenUnAuthorizedException() {
+        assertThatExceptionOfType(UnAuthorizedException.class)
+                .isThrownBy(() -> new Answer(null, Q1, "contents"));
+    }
+
+    @Test
+    @DisplayName("질문 정보가 없으면 NotFoundException이 발생")
+    void inputNullQuestionThenNotFoundException() {
+        assertThatExceptionOfType(NotFoundException.class)
+                .isThrownBy(() -> new Answer(JAVAJIGI, null, "contents"));
+    }
+
+    @Test
+    @DisplayName("질문 연결 후 동일한지 검증")
+    void verifyMappingQuestion() {
+        Answer answer = new Answer(JAVAJIGI, Q1, "contents");
+        answer.mappingQuestion(Q2);
+
+        assertThat(answer.getQuestion()).isEqualTo(Q2);
+    }
+
+    @Test
+    @DisplayName("삭제 상태 값 변경 후 바뀌었는지 확인")
+    void verifyChangeDeleteStatus() {
+        Answer answer = new Answer(JAVAJIGI, Q1, "contents");
+        answer.changeDeleteStatus();
+
+        assertThat(answer.isDeleted()).isTrue();
+    }
 }
