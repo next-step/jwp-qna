819a967bc50cba0ac93902e72acc036f332c2433
서윤철 <woody@29cm.co.kr>
2022-05-20 21:27:45 +0900

    test : 테스트 코드 작성

---
+++ src/test/java/qna/domain/repository/UserRepositoryTest.java
@@ -0,0 +1,32 @@
+package qna.domain.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import qna.domain.User;
+import qna.domain.UserTest;
+import qna.repository.UserRepository;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+@DataJpaTest
+class UserRepositoryTest {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Test
+    void findByUserId_조회_테스트() {
+        User user = userRepository.save(UserTest.JAVAJIGI);
+
+        Optional<User> optionalUser = userRepository.findByUserId(user.getUserId());
+
+        assertAll(
+                () -> assertThat(optionalUser.isPresent()).isTrue(),
+                () -> assertThat(optionalUser.get()).isSameAs(user)
+        );
+    }
+}
