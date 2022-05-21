4c0efe016308c60ea6e508607368e8f3014342e2
minzzang <minjjang1117@gmail.com>
2022-05-21 16:57:20 +0900

    feat(BaseEntityTest): 엔티티가 생성되면 생성일이 저장된다

---
+++ src/test/java/qna/domain/BaseEntityTest.java
@@ -0,0 +1,30 @@
+package qna.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import qna.config.JpaAuditingConfig;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@Import(JpaAuditingConfig.class)
+class BaseEntityTest {
+
+    @Autowired
+    private AnswerRepository answerRepository;
+
+    @Test
+    void 엔티티가_생성되면_생성일이_저장된다() {
+        // given
+        Answer answer = new Answer();
+        // when
+        Answer result = answerRepository.save(answer);
+        // then
+        assertThat(result.createdAt()).isBefore(LocalDateTime.now());
+    }
+
+}
