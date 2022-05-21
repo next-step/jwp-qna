f1b46ed8c18b547353d4f371f381eb414041d943
minzzang <minjjang1117@gmail.com>
2022-05-21 14:38:57 +0900

    feat(QuestionRepositoryTest): id로 삭제되지 않은 질문 찾기

--- src/test/java/qna/domain/QuestionRepositoryTest.java
+++ src/test/java/qna/domain/QuestionRepositoryTest.java
@@ -1,15 +1,21 @@
 package qna.domain;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 import qna.config.JpaAuditingConfig;
 
 import java.util.Optional;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static qna.domain.QuestionTest.Q1;
+import static qna.domain.QuestionTest.Q2;
 
 @DataJpaTest
 @Import(JpaAuditingConfig.class)
@@ -71,5 +77,31 @@ class QuestionRepositoryTest {
         assertThat(result)
                 .map(Question::getContents)
                 .hasValue("update contents");
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void id로_삭제되지_않은_질문_찾기(Question question, boolean expected) {
+        // given
+        Question saved = questionRepository.save(question);
+        // when
+        Optional<Question> result = questionRepository.findByIdAndDeletedFalse(saved.getId());
+        // then
+        assertThat(result.isPresent()).isEqualTo(expected);
+    }
+
+    static Stream<Arguments> id로_삭제되지_않은_질문_찾기() {
+        Q1.setDeleted(true);
+
+        return Stream.of(
+                Arguments.of(
+                        Q1,
+                        false
+                ),
+                Arguments.of(
+                        Q2,
+                        true
+                )
+        );
     }
 }
