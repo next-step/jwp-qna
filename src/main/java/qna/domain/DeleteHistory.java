a435bc1926bb0656fe471e2eb3364f7411a12bba
sy001 <suyounlake@naver.com>
2022-05-21 14:44:39 +0900

    feat: 엔티티 기본 매핑
    - Answer, DeleteHistory, Question, User 엔티티 매핑
    - @Entity 애노테이션 사용
    - @Id 애노테이션으로 Id 및 Id 생성전략 지정

--- src/main/java/qna/domain/DeleteHistory.java
+++ src/main/java/qna/domain/DeleteHistory.java
@@ -1,9 +1,16 @@
 package qna.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
+@Entity
 public class DeleteHistory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private ContentType contentType;
     private Long contentId;
