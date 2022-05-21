5e1f78945d2e4656f68435f98678e79062015c4a
TakJong Oh <43510811+tj3828@users.noreply.github.com>
2022-05-21 23:04:14 +0900

    1단계 - 엔티티 매핑 PR입니다. (#416)
    * docs(step1.md): step1 - 요구사항 정리
    
    * feat(application.properties): step1 - DB, console 환경설정
    
    * docs(step1.md): step1 - 요구사항 Common Entity 추가
    
    * feat(Common): step1 - Common Entity 생성
    
    * feat(User): step1 - User(class -> entity)
    
    * feat(Question): step1 - Question(class -> entity)
    
    * feat(Answer): step1 - Answer(class -> entity)
    
    * feat(DeleteHistory): step1 - DeleteHistory(class -> entity)
    
    * feat(env): step1 - mysql 설정 변경
    
    * feat(env): step1 - audit 설정 추가
    
    * test(User): step1 - UserTest 추가
    
    * test(User): step1 - UserTest select, delete 추가
    
    * test(Question): step1 - QuestionTest
    
    * test(Answer): step1 - AnswerTest
    
    * refactor(directory): step1 - 디렉토리 구조 변경(repository 추가)
    
    * refactor(class): step1(피드백) - Common 추상클래스로 변환

---
+++ docs/step1.md
@@ -0,0 +1,12 @@
+# 1단계 - 엔티티 매핑
+
+---
+
+## 요구사항
+1. 각 클래스를 엔티티로 변환(아래 순서대로)
+   1. Common 
+   2. User
+   3. Question
+   4. Answer
+   5. DeleteHistory
+2. Repository CRUD 테스트
--- src/main/java/qna/domain/DeleteHistory.java
+++ src/main/java/qna/domain/DeleteHistory.java
@@ -2,13 +2,32 @@ package qna.domain;
 
 import java.time.LocalDateTime;
 import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import org.springframework.data.annotation.CreatedDate;
 
+@Entity
 public class DeleteHistory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Enumerated(EnumType.STRING)
     private ContentType contentType;
+
     private Long contentId;
     private Long deletedById;
-    private LocalDateTime createDate = LocalDateTime.now();
+
+    @CreatedDate
+    private LocalDateTime createDate;
+
+    protected DeleteHistory() {
+    }
 
     public DeleteHistory(ContentType contentType, Long contentId, Long deletedById, LocalDateTime createDate) {
         this.contentType = contentType;
