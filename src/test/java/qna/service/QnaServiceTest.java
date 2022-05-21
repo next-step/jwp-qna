c802107d5d63246d964b7b7d64bd705ef003b771
kwonyongil <ksgkms23@likelion.org>
2022-05-22 00:07:47 +0900

    refactor(jpa) 유사한 테스트 중 하나를 '작성자와 로그인 사용자가 같고 답변이 없는 질문 삭제'를 검증하도록 변경

--- src/test/java/qna/service/QnaServiceTest.java
+++ src/test/java/qna/service/QnaServiceTest.java
@@ -7,9 +7,11 @@ import static org.mockito.Mockito.when;
 
 import java.time.LocalDateTime;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -44,75 +46,77 @@ class QnaServiceTest {
     User sanjigi;
 
     @BeforeEach
-    public void setUp() {
+    void setUp() {
         javajigi = User.builder("javajigi", "password", "name")
+                .id(1L)
                 .email("javajigi@slipp.net")
                 .build();
         sanjigi = User.builder("sanjigi", "password", "name")
+                .id(2L)
                 .email("sanjigi@slipp.net")
                 .build();
         question = Question.builder("title1")
+                .id(1L)
                 .contents("contents1")
                 .build()
                 .writeBy(javajigi);
         answer = Answer.builder(javajigi, question)
+                .id(1L)
                 .contents("Answers Contents1")
                 .build();
         question.addAnswer(answer);
     }
 
+    @DisplayName("작성자와 로그인 사용자가 같고 답변이 없는 질문 삭제 테스트")
     @Test
-    public void deleteSuccess() throws Exception {
+    void deleteSuccessNoneAnswer() throws Exception {
         when(questionRepository.findByIdAndDeletedFalse(question.getId())).thenReturn(Optional.of(question));
-        when(answerRepository.findByQuestionIdAndDeletedFalse(question.getId())).thenReturn(Arrays.asList(answer));
-
+        when(answerRepository.findByQuestionIdAndDeletedFalse(question.getId())).thenReturn(Collections.emptyList());
         assertThat(question.isDeleted()).isFalse();
         qnaService.deleteQuestion(javajigi, question.getId());
-
         assertThat(question.isDeleted()).isTrue();
-        verifyDeleteHistories();
+        verifyDeleteHistoriesAtDeleteSuccessNoneAnswer();
     }
 
+    @DisplayName("다른 작성자의 질문 삭제시 예외 테스트")
     @Test
     public void deleteQuestionOtherWriter() {
         when(questionRepository.findByIdAndDeletedFalse(question.getId())).thenReturn(Optional.of(question));
-
         assertThatThrownBy(() -> qnaService.deleteQuestion(sanjigi, question.getId()))
-                .isInstanceOf(CannotDeleteException.class);
+                .isInstanceOf(CannotDeleteException.class)
+                .hasMessage("질문을 삭제할 권한이 없습니다.");
     }
 
+    @DisplayName("로그인 유저와 질문, 답변이 같은 작성자인경우 질문, 답변글 삭제 테스트")
     @Test
-    public void deleteQuestionSameWriter() throws Exception {
-        when(questionRepository.findByIdAndDeletedFalse(question.getId())).thenReturn(Optional.of(question));
-        when(answerRepository.findByQuestionIdAndDeletedFalse(question.getId())).thenReturn(Arrays.asList(answer));
-
-        qnaService.deleteQuestion(javajigi, question.getId());
-
-        assertThat(question.isDeleted()).isTrue();
-        assertThat(answer.isDeleted()).isTrue();
-        verifyDeleteHistories();
-    }
-
-    @Test
-    public void deleteHavingContentsOfOther() {
-        Question q1 = Question.builder("title1")
-                .contents("contents1")
-                .build()
-                .writeBy(javajigi);
-        Answer answer2 = Answer.builder(sanjigi, q1)
-                .contents("Answers Contents1")
-                .build();
-        question.addAnswer(answer2);
-
+    void deleteQuestionSameWriter() throws Exception {
         when(questionRepository.findByIdAndDeletedFalse(question.getId())).thenReturn(Optional.of(question));
         when(answerRepository.findByQuestionIdAndDeletedFalse(question.getId())).thenReturn(
-                Arrays.asList(answer, answer2));
-
-        assertThatThrownBy(() -> qnaService.deleteQuestion(javajigi, question.getId()))
-                .isInstanceOf(CannotDeleteException.class);
+                Collections.singletonList(answer));
+        qnaService.deleteQuestion(javajigi, question.getId());
+        assertThat(question.isDeleted()).isTrue();
+        assertThat(answer.isDeleted()).isTrue();
+        verifyDeleteHistoriesAtDeleteQuestionSameWriter();
     }
 
-    private void verifyDeleteHistories() {
+    @DisplayName("다른 사람의 답변이 있는 질문 삭제시 예외 테스트")
+    @Test
+    void deleteHavingContentsOfOther() {
+        Answer answer = Answer.builder(sanjigi, question)
+                .contents("Answers Contents1")
+                .build();
+        question.addAnswer(answer);
+
+        when(questionRepository.findByIdAndDeletedFalse(question.getId())).thenReturn(Optional.of(this.question));
+        when(answerRepository.findByQuestionIdAndDeletedFalse(question.getId())).thenReturn(
+                Arrays.asList(this.answer, answer));
+
+        assertThatThrownBy(() -> qnaService.deleteQuestion(javajigi, question.getId()))
+                .isInstanceOf(CannotDeleteException.class)
+                .hasMessage("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
+    }
+
+    void verifyDeleteHistoriesAtDeleteQuestionSameWriter() {
         List<DeleteHistory> deleteHistories = Arrays.asList(
                 DeleteHistory.builder()
                         .contentType(ContentType.QUESTION)
@@ -124,6 +128,18 @@ class QnaServiceTest {
                         .contentType(ContentType.ANSWER)
                         .contentId(answer.getId())
                         .deletedBy(answer.getWriter())
+                        .createDate(LocalDateTime.now())
+                        .build()
+        );
+        verify(deleteHistoryService).saveAll(deleteHistories);
+    }
+
+    void verifyDeleteHistoriesAtDeleteSuccessNoneAnswer() {
+        List<DeleteHistory> deleteHistories = Collections.singletonList(
+                DeleteHistory.builder()
+                        .contentType(ContentType.QUESTION)
+                        .contentId(question.getId())
+                        .deletedBy(question.getWriter())
                         .createDate(LocalDateTime.now())
                         .build()
         );
