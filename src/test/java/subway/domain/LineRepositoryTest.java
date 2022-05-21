15ce283bb38d1ba2677fd8413ff073c1cd8bf790
sangho lee <sangholee.dev@gmail.com>
2022-05-21 23:21:00 +0900

    feat: 프로젝트 설정 변경에 따른 hands-on 테스트 수정

--- src/test/java/subway/domain/LineRepositoryTest.java
+++ src/test/java/subway/domain/LineRepositoryTest.java
@@ -5,9 +5,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Sql("/subway-data.sql")
 class LineRepositoryTest {
     @Autowired
     LineRepository lines;
--- src/test/java/subway/domain/StationRepositoryTest.java
+++ src/test/java/subway/domain/StationRepositoryTest.java
@@ -4,9 +4,11 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class StationRepositoryTest {
     @Autowired
     private StationRepository stations;
