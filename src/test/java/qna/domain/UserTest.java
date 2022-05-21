21d7ffb6764b6eb60fd058fab1413786d634e00d
piopoi <kangsw1988@gmail.com>
2022-05-21 21:26:58 +0900

    test(UserTest): 테스트 케이스 추가

--- src/test/java/qna/domain/UserTest.java
+++ src/test/java/qna/domain/UserTest.java
@@ -1,6 +1,22 @@
 package qna.domain;
 
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class UserTest {
     public static final User JAVAJIGI = new User(1L, "javajigi", "password", "name", "javajigi@slipp.net");
     public static final User SANJIGI = new User(2L, "sanjigi", "password", "name", "sanjigi@slipp.net");
+
+    @DisplayName("name과 email이 같은지 확인한다.")
+    @Test
+    void equalsNameAndEmail() {
+        //given & when
+        boolean actual = JAVAJIGI.equalsNameAndEmail(new User("test", "test", "name", "javajigi@slipp.net"));
+
+        //then
+        assertThat(actual).isTrue();
+    }
+
 }
