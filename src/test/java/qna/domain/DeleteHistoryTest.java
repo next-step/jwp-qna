3fd527c22126d068889b5fe2912f975ae569ecdd
mannue <sppe12@gmail.com>
2022-05-21 10:58:22 +0900

    feat(DeleteHistory): DeleteHistory Entity 등록

---
+++ src/test/java/qna/domain/DeleteHistoryTest.java
@@ -0,0 +1,27 @@
+package qna.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+@DataJpaTest
+public class DeleteHistoryTest {
+
+    @Autowired
+    DeleteHistoryRepository deleteHistoryRepository;
+
+    @DisplayName("identityTest 테스트")
+    @Test
+    void identityTest() {
+        DeleteHistory history = new DeleteHistory(ContentType.ANSWER, 1L, 1L, LocalDateTime.now());
+        DeleteHistory deleteHistory = deleteHistoryRepository.save(history);
+        List<DeleteHistory> deleteHistoryList = deleteHistoryRepository.findByContentType(ContentType.ANSWER);
+        assertThat(deleteHistoryList.size()).isEqualTo(1);
+    }
+}
