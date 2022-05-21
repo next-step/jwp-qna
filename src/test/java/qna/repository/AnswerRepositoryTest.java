56474d851bd1d9ca5f7eb6c8a8ced2079f7b7c79
limwoobin <drogba02@naver.com>
2022-05-21 15:13:49 +0900

    test: Answer 의 연관관계를 테스트 코드를 작성해 검증하라

--- src/test/java/qna/repository/AnswerRepositoryTest.java
+++ src/test/java/qna/repository/AnswerRepositoryTest.java
@@ -4,18 +4,23 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static qna.domain.AnswerTest.A1;
-import static qna.domain.AnswerTest.A2;
+import static qna.domain.QuestionTest.Q1;
+import static qna.domain.UserTest.JAVAJIGI;
 
 import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import qna.domain.Answer;
 import qna.domain.AnswerRepository;
+import qna.domain.Question;
+import qna.domain.QuestionRepository;
+import qna.domain.User;
+import qna.domain.UserRepository;
 
 @DataJpaTest
 class AnswerRepositoryTest {
@@ -23,71 +28,133 @@ class AnswerRepositoryTest {
     @Autowired
     private AnswerRepository answerRepository;
 
-    private Answer answer;
-    private Answer answer2;
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private User writer;
+    private Question question;
 
     @BeforeEach
     void setUp() {
-        answer = A1;
-        answer2 = A2;
-        A1.setDeleted(false);
-        A2.setDeleted(false);
-
-        answer = answerRepository.save(answer);
-        answer2 = answerRepository.save(answer2);
+        writer = userRepository.save(JAVAJIGI);
+        question = Q1;
+        question.writeBy(writer);
+        question = questionRepository.save(question);
     }
 
-    @DisplayName("엔티티를 저장하면 정상적으로 저장되어 id 값이 채번되어야 한다")
-    @Test
-    void save_test() {
-        answerRepository.save(answer);
-        assertThat(answer.getId()).isNotNull();
+    @DisplayName("데이터 추가,수정,삭제 테스트")
+    @Nested
+    class CommandTest {
+
+        @DisplayName("엔티티를 저장하면 정상적으로 저장되어 id 값이 채번되어야 한다")
+        @Test
+        void save_test() {
+            Answer answer = new Answer(writer, question, "test-contents");
+            answerRepository.save(answer);
+
+            assertThat(answer.getId()).isNotNull();
+        }
+
+        @DisplayName("엔티티의 deleted 를 변경하고 다시 조회하면 변경된 상태로 조회되어야 한다")
+        @Test
+        void update_test() {
+            Answer answer = new Answer(writer, question, "test-contents");
+            answerRepository.save(answer);
+
+            answer.setDeleted(true);
+            answerRepository.save(answer);
+
+            assertThat(answer.isDeleted()).isTrue();
+        }
+
+        @DisplayName("entity 를 삭제하면 정상적으로 삭제되어야 한다")
+        @Test
+        void delete_test() {
+            Answer answer = new Answer(writer, question, "test-contents");
+            Answer answer2 = new Answer(writer, question, "test-contents2");
+
+            answerRepository.save(answer);
+            answerRepository.save(answer2);
+
+            List<Answer> answers = answerRepository.findAll();
+            answerRepository.delete(answer);
+
+            List<Answer> deleted_answers = answerRepository.findAll();
+
+            assertAll(
+                () -> assertThat(answers).hasSize(2),
+                () -> assertThat(deleted_answers).hasSize(1)
+            );
+        }
     }
 
-    @DisplayName("같은 questionId 를 가진 엔티티를 questionId 로 조회하면 모두 조회되어야 한다")
-    @Test
-    void find_test() {
-        List<Answer> answers = answerRepository.findByQuestionIdAndDeletedFalse(A1.getQuestionId());
+    @DisplayName("데이터 조회 테스트")
+    @Nested
+    class QueryTest {
 
-        assertAll(
-            () -> assertThat(answers.get(0).getId()).isEqualTo(answer.getId()),
-            () -> assertThat(answers.get(1).getId()).isEqualTo(answer2.getId())
-        );
-    }
+        @DisplayName("같은 questionId 를 가진 엔티티를 questionId 로 조회하면 모두 조회되어야 한다")
+        @Test
+        void find_test() {
+            Answer answer = new Answer(writer, question, "test-contents");
+            Answer answer2 = new Answer(writer, question, "test-contents2");
 
-    @DisplayName("id 와 deleted 가 false 인 엔티티를 동일 조건으로 조회하면 모두 조회되어야 한다")
-    @Test
-    void find_test2() {
-        answer2.setDeleted(true);
-        answerRepository.save(answer2);
+            answerRepository.save(answer);
+            answerRepository.save(answer2);
 
-        Optional<Answer> result = answerRepository.findByIdAndDeletedFalse(answer.getId());
-        Optional<Answer> result2 = answerRepository.findByIdAndDeletedFalse(answer2.getId());
+            List<Answer> answers = answerRepository.findByQuestionAndDeletedFalse(question);
 
-        assertTrue(result.isPresent());
-        assertFalse(result2.isPresent());
-    }
+            assertAll(
+                () -> assertThat(answers.get(0).getId()).isEqualTo(answer.getId()),
+                () -> assertThat(answers.get(1).getId()).isEqualTo(answer2.getId())
+            );
+        }
 
-    @DisplayName("엔티티의 deleted 를 변경하고 다시 조회하면 변경된 상태로 조회되어야 한다")
-    @Test
-    void update_test() {
-        answer.setDeleted(true);
-        answerRepository.save(answer);
+        @DisplayName("id 와 deleted 가 false 인 엔티티를 동일 조건으로 조회하면 모두 조회되어야 한다")
+        @Test
+        void find_test2() {
+            Answer answer = new Answer(writer, question, "test-contents");
+            Answer answer2 = new Answer(writer, question, "test-contents2");
 
-        assertThat(answer.isDeleted()).isTrue();
-    }
+            answerRepository.save(answer);
+            answerRepository.save(answer2);
 
-    @DisplayName("entity 를 삭제하면 정상적으로 삭제되어야 한다")
-    @Test
-    void delete_test() {
-        List<Answer> answers = answerRepository.findAll();
-        answerRepository.delete(answer);
+            answer2.setDeleted(true);
+            answerRepository.save(answer2);
 
-        List<Answer> deleted_answers = answerRepository.findAll();
+            Optional<Answer> result = answerRepository.findByIdAndDeletedFalse(answer.getId());
+            Optional<Answer> result2 = answerRepository.findByIdAndDeletedFalse(answer2.getId());
 
-        assertAll(
-            () -> assertThat(answers).hasSize(2),
-            () -> assertThat(deleted_answers).hasSize(1)
-        );
+            assertTrue(result.isPresent());
+            assertFalse(result2.isPresent());
+        }
+
+        @DisplayName("Answer 를 조회하면 연관관계인 Writer 도 정상적으로 조회되어야 한다")
+        @Test
+        void find_relation_test() {
+            Answer answer = new Answer(writer, question, "test-contents");
+            answerRepository.save(answer);
+
+            Answer result = answerRepository.findById(answer.getId()).get();
+            User 연관관계_writer = result.getWriter();
+
+            assertThat(연관관계_writer).isNotNull();
+            assertThat(연관관계_writer.getId()).isEqualTo(writer.getId());
+        }
+
+        @DisplayName("Answer 를 조회하면 연관관계인 Question 도 정상적으로 조회되어야 한다")
+        @Test
+        void find_relation_test2() {
+            Answer answer = new Answer(writer, question, "test-contents");
+            answerRepository.save(answer);
+
+            Answer result = answerRepository.findById(answer.getId()).get();
+            Question 연관관계_question = result.getQuestion();
+
+            assertThat(연관관계_question).isNotNull();
+            assertThat(연관관계_question.getId()).isEqualTo(writer.getId());
+        }
     }
 }
