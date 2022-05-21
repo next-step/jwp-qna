f015b164669e2204b26acd47a5698e0f72219f52
minzzang <minjjang1117@gmail.com>
2022-05-21 14:15:12 +0900

    feat(QuestionRepositoryTest): id로 조회

---
+++ src/test/java/qna/domain/QuestionRepositoryTest.java
@@ -0,0 +1,38 @@
+package qna.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import qna.config.JpaAuditingConfig;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@Import(JpaAuditingConfig.class)
+class QuestionRepositoryTest {
+
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    @Test
+    void id로_조회() {
+        // given
+        Question question = new Question("title", "contents");
+        Question saved = questionRepository.save(question);
+        // when
+        Optional<Question> result = questionRepository.findById(saved.getId());
+        // then
+        assertAll(
+                () -> assertThat(result)
+                        .map(Question::getTitle)
+                        .hasValue("title"),
+                () -> assertThat(result)
+                        .map(Question::getContents)
+                        .hasValue("contents")
+        );
+    }
+}
