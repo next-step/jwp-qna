b92040d6527f1de6426cf2add8aee4230cc79c97
piopoi <kangsw1988@gmail.com>
2022-05-21 16:53:25 +0900

    test(Question): 테스트 케이스 추가

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,18 @@
 package qna.domain;
 
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+    @DisplayName("Owner 확인")
+    @Test
+    void isOwner() {
+        assertThat(Q1.isOwner(UserTest.JAVAJIGI)).isTrue();
+        assertThat(Q2.isOwner(UserTest.SANJIGI)).isTrue();
+    }
 }
