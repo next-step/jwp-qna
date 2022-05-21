6ca7517f071da43d6ac3e08a7ef3b4e7b8633829
minzzang <minjjang1117@gmail.com>
2022-05-21 16:38:17 +0900

    feat(UserRepositoryTest): userId가 null인 user를 저장하면 예외가 발생한다

--- src/test/java/qna/domain/UserRepositoryTest.java
+++ src/test/java/qna/domain/UserRepositoryTest.java
@@ -4,11 +4,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.dao.DataIntegrityViolationException;
 import qna.config.JpaAuditingConfig;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static qna.domain.UserTest.JAVAJIGI;
 
 @DataJpaTest
@@ -26,5 +28,15 @@ class UserRepositoryTest {
         Optional<User> result = userRepository.findByUserId(JAVAJIGI.getUserId());
         // then
         assertThat(result.isPresent()).isTrue();
+    }
+
+    @Test
+    void userId가_null인_user를_저장하면_예외가_발생한다() {
+        // given
+        User user = new User(null, "password", "name", "email");
+        // when
+        assertThatThrownBy(() ->
+            userRepository.save(user)
+        ).isInstanceOf(DataIntegrityViolationException.class);
     }
 }
