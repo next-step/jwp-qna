c7e8bf936e366a4bddfbc69adb4573e465123b4f
seungin yang <rhfpdk92@naver.com>
2022-05-21 23:41:01 +0900

    fix: 요구사항 충족하도록 엔티티 작성

--- src/main/java/qna/domain/Answer.java
+++ src/main/java/qna/domain/Answer.java
@@ -3,14 +3,32 @@ package qna.domain;
 import qna.NotFoundException;
 import qna.UnAuthorizedException;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import java.time.LocalDateTime;
 import java.util.Objects;
 
+@Entity
 public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Long writerId;
-    private Long questionId;
+    @Lob
     private String contents;
+    @Column(nullable = false)
+    private LocalDateTime createAt;
+    @Column(nullable = false)
     private boolean deleted = false;
+    private Long questionId;
+    private LocalDateTime updatedAt;
+    private Long writerId;
+
+    public Answer() {
+    }
 
     public Answer(User writer, Question question, String contents) {
         this(null, writer, question, contents);
