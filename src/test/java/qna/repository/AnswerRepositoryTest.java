607a5e3e42632dd22d66d003cf9ea112600694ff
june2 <>
2022-05-22 19:59:11 +0900

    chore: remove unused import

--- src/test/java/qna/repository/AnswerRepositoryTest.java
+++ src/test/java/qna/repository/AnswerRepositoryTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import qna.domain.Answer;
-import qna.domain.AnswerTest;
 import qna.domain.Question;
 import qna.domain.User;
 
