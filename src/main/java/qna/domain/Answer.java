57814d31f2836ac8860e3d06d71ced7cda606d56
오진혁 <hyeok7777@gmail.com>
2022-05-23 00:22:54 +0900

    feat(Answer.java):annotation 설정 test(AnswerTest.java):저장 및 수정 테스트

--- src/main/java/qna/domain/Answer.java
+++ src/main/java/qna/domain/Answer.java
@@ -1,15 +1,22 @@
 package qna.domain;
 
+import org.springframework.lang.Nullable;
 import qna.NotFoundException;
 import qna.UnAuthorizedException;
 
+import javax.persistence.*;
 import java.util.Objects;
 
-public class Answer {
+@Entity
+public class Answer extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Long writerId;
     private Long questionId;
+    @Lob
     private String contents;
+    @Nullable
     private boolean deleted = false;
 
     public Answer(User writer, Question question, String contents) {
@@ -30,6 +37,10 @@ public class Answer {
         this.writerId = writer.getId();
         this.questionId = question.getId();
         this.contents = contents;
+    }
+
+    protected Answer() {
+
     }
 
     public boolean isOwner(User writer) {
@@ -90,4 +101,5 @@ public class Answer {
                 ", deleted=" + deleted +
                 '}';
     }
+
 }
--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,6 +1,40 @@
 package qna.domain;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
 public class AnswerTest {
+
+    @Autowired
+    AnswerRepository answerRepository;
+
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @Test
+    void 저장_테스트() {
+        Answer expected = AnswerTest.A1;
+        Answer result = answerRepository.save(expected);
+
+        assertThat(result.getId()).isNotNull();
+    }
+
+    @Test
+    void 아이디로_조회() {
+        Answer save = answerRepository.save(A1);
+        Answer expected = answerRepository.findByIdAndDeletedFalse(save.getId()).get();
+        assertThat(expected.getId()).isEqualTo(save.getId());
+    }
+
+    @Test
+    void 수정하기() {
+        Answer answer = answerRepository.save(A1);
+        answer.setContents("Answer Contents3");
+        Answer expected = answerRepository.findById(answer.getId()).get();
+        assertThat(expected.getContents()).isEqualTo("Answer Contents3");
+    }
 }
