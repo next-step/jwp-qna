3b86e3aa333882423980b839a3e1566aa9a2c70f
14km <k.connor614@gmail.com>
2022-05-23 00:05:24 +0900

    test: AnswerRepositoryTest 테스트 수정

--- src/test/java/qna/repository/AnswerRepositoryTest.java
+++ src/test/java/qna/repository/AnswerRepositoryTest.java
@@ -1,39 +1,63 @@
 package qna.repository;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import qna.domain.Answer;
+import qna.domain.Question;
+import qna.domain.User;
 
-import static qna.domain.AnswerTest.A1;
-import static qna.domain.AnswerTest.A2;
+import static qna.domain.UserTest.JAVAJIGI;
 
 @DataJpaTest
 class AnswerRepositoryTest {
 
+    private User user;
+
+    private Question question;
+
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
     @Autowired
     private AnswerRepository answerRepository;
+
+    @BeforeEach
+    void setUp() {
+        user = userRepository.save(JAVAJIGI);
+        question = questionRepository.save(new Question("title1", "contents1").writeBy(user));
+    }
 
     @Test
     @DisplayName("답변이 정상적으로 등록되었는지 확인 테스트")
     void save() {
-        answerRepository.save(A1);
-        answerRepository.save(A2);
+        // given
+        Answer answer1 = new Answer(user, question, "답변입니다.");
+        Answer answer2 = new Answer(user, question, "답변입니다.");
 
+        // when
+        answerRepository.save(answer1);
+        answerRepository.save(answer2);
+
+        // then
         Assertions.assertThat(answerRepository.findAll()).size().isEqualTo(2);
     }
 
     @Test
     @DisplayName("답변 등록 후 조회 테스트")
     void find() {
-        answerRepository.save(A1);
+        Answer answer1 = answerRepository.save(new Answer(user, question, "답변입니다."));
 
-        Answer answer = answerRepository.findByIdAndDeletedFalse(A1.getId()).get();
+        Answer answer = answerRepository.findByIdAndDeletedFalse(answer1.getId()).get();
 
         Assertions.assertThat(answer).isNotNull();
-        Assertions.assertThat(answer.getId()).isEqualTo(A1.getId());
+        Assertions.assertThat(answer.getId()).isEqualTo(answer1.getId());
         Assertions.assertThat(answer.isDeleted()).isFalse();
     }
 
