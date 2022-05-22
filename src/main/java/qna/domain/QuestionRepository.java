1fe5aeb6b2b8adcc0ef7d4e70107cdf35aef51f2
minzzang <minjjang1117@gmail.com>
2022-05-23 02:33:08 +0900

    refactor(QuestionRepository): n+1 문제 해결

--- src/main/java/qna/domain/QuestionRepository.java
+++ src/main/java/qna/domain/QuestionRepository.java
@@ -1,6 +1,8 @@
 package qna.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -9,4 +11,7 @@ public interface QuestionRepository extends JpaRepository<Question, Long> {
     List<Question> findByDeletedFalse();
 
     Optional<Question> findByIdAndDeletedFalse(Long id);
+
+    @Query("select q from Question q join fetch q.answers where q.id = :id")
+    Optional<Question> findQuestionById(@Param(value = "id") Long id);
 }
