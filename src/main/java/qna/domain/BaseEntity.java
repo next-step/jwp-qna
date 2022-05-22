e7c44abe0cc34b8cd263a748e6b75ecd4f887407
msm <msmasd@naver.com>
2022-05-22 21:19:23 +0900

    refac: BaseEntity 추상클래스로 변경

--- src/main/java/qna/domain/BaseEntity.java
+++ src/main/java/qna/domain/BaseEntity.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 
 @EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
-public class BaseEntity {
+public abstract class BaseEntity {
     @CreatedDate
     @Column(nullable = false)
     protected LocalDateTime createdAt;
