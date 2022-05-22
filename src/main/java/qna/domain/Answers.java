d54e1a03a5b9220bc2e4d84a91b7954a61aebe64
Woody <yuncheol92@naver.com>
2022-05-22 16:22:11 +0900

    feat : Answers 일급컬렉션 추가

---
+++ src/main/java/qna/domain/Answers.java
@@ -0,0 +1,28 @@
+package qna.domain;
+
+
+import javax.persistence.Embeddable;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Embeddable
+public class Answers {
+
+    @OneToMany(mappedBy = "answer")
+    private List<Answer> values = new ArrayList<>();
+
+    protected Answers() {
+    }
+
+    public void add(Answer answer) {
+        values.add(answer);
+    }
+
+    public List<DeleteHistory> delete(User loginUser) {
+        return values.stream()
+                .map(answer -> answer.delete(loginUser))
+                .collect(Collectors.toList());
+    }
+}
