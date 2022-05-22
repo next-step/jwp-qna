342e6e36d1b452c095c4a8cd7e204a32dacc6dea
minzzang <minjjang1117@gmail.com>
2022-05-23 00:25:47 +0900

    fix(answer): user 생성자 파라미터에 id 제외

--- src/test/java/qna/domain/DeleteHistoryRepositoryTest.java
+++ src/test/java/qna/domain/DeleteHistoryRepositoryTest.java
@@ -20,7 +20,7 @@ class DeleteHistoryRepositoryTest {
     @Test
     void id로_조회한다() {
         // given
-        User user = new User(1L, "user1", "password", "name", "user1@com");
+        User user = new User("user1", "password", "name", "user1@com");
         userRepository.save(user);
         DeleteHistory deleteHistory = new DeleteHistory(ContentType.QUESTION, 1L, user, LocalDateTime.now());
         DeleteHistory saved = deleteHistoryRepository.save(deleteHistory);
