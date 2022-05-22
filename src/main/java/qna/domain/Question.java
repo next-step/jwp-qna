962cd9f33c2f8d051e2bbf011475a192bbc0754f
otakjong <tj3828@naver.com>
2022-05-22 12:12:48 +0900

    test(Question): step2 - Question test 분리

--- src/main/java/qna/domain/Question.java
+++ src/main/java/qna/domain/Question.java
@@ -1,5 +1,6 @@
 package qna.domain;
 
+import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -105,6 +106,25 @@ public class Question extends Common {
             ", deleted=" + deleted +
             ", writer=" + writer +
             '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Question question = (Question) o;
+        return deleted == question.deleted && Objects.equals(id, question.id) && Objects.equals(title,
+            question.title) && Objects.equals(contents, question.contents) && Objects.equals(writer,
+            question.writer);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, contents, deleted, writer);
     }
 
 }
