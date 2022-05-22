d36c1e59ed3370648e77e3ec89d8a10e2ecc22d2
bestsilver <dkvkxm99@gmail.com>
2022-05-22 23:32:50 +0900

    test: 연관 관계 매핑에 따른 테스트 케이스 작성

--- src/test/java/qna/repository/AnswerRepositoryTest.java
+++ src/test/java/qna/repository/AnswerRepositoryTest.java
@@ -4,11 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import qna.domain.Answer;
-import qna.domain.AnswerTest;
-import qna.domain.QuestionTest;
-import qna.domain.UserTest;
-
+import qna.domain.*;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,31 +14,41 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 @DataJpaTest
 public class AnswerRepositoryTest {
     @Autowired
-    AnswerRepository answerRepository;
+    private AnswerRepository answerRepository;
+    @Autowired
+    private QuestionRepository questionRepository;
+    @Autowired
+    private UserRepository userRepository;
+
+    private User user;
+    private Question question;
 
     @BeforeEach
     public void setup() {
-        answerRepository.save(AnswerTest.A1);
-        answerRepository.save(AnswerTest.A2);
+        user = userRepository.save(new User("bestsilver", "password", "name", "bestsilver@ggg.net"));
+        question = questionRepository.save(new Question("title1", "contents1").writeBy(user));
+        Answer answer = new Answer(user, question, "Answers Contents1");
+
+        answerRepository.save(answer);
     }
 
     @Test
     void 답변_등록_테스트() {
-        Answer answer3 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents3");
-        answerRepository.save(answer3);
-
-        assertThat(answer3.getContents()).isEqualTo("Answers Contents3");
+        Answer answer = answerRepository.save(new Answer(user, question, "Answers Contents3"));
+        assertThat(answer.getContents()).isEqualTo("Answers Contents3");
     }
 
     @Test
     void findByQuestionIdAndDeletedFalse_테스트() {
-        List<Answer> byQuestionIdAndDeletedFalse = answerRepository.findByQuestionIdAndDeletedFalse(QuestionTest.Q1.getId());
+        answerRepository.save(new Answer(user, question, "Answers Content2"));
+
+        List<Answer> byQuestionIdAndDeletedFalse = answerRepository.findByQuestionIdAndDeletedFalse(question.getId());
         assertThat(byQuestionIdAndDeletedFalse.size()).isEqualTo(2);
     }
 
     @Test
     void findByIdAndDeletedFalse_테스트() {
-        Answer answer3 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents3");
+        Answer answer3 = new Answer(user, question, "Answers Contents3");
         answerRepository.save(answer3);
         Optional<Answer> byIdAndDeletedFalse = answerRepository.findByIdAndDeletedFalse(answer3.getId());
 
--- src/test/java/qna/repository/QuestionRepositoryTest.java
+++ src/test/java/qna/repository/QuestionRepositoryTest.java
@@ -9,8 +9,10 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.dao.DataIntegrityViolationException;
 import qna.domain.Question;
 import qna.domain.QuestionTest;
+import qna.domain.User;
 import qna.domain.UserTest;
 
+import javax.persistence.EntityManager;
 import java.util.List;
 import java.util.Optional;
 import java.util.Random;
@@ -21,26 +23,27 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 @DataJpaTest
 public class QuestionRepositoryTest {
     @Autowired
-    QuestionRepository questionRepository;
+    private QuestionRepository questionRepository;
+    @Autowired
+    private UserRepository userRepository;
+    private User writer;
 
     @BeforeEach
     void setup() {
-        questionRepository.save(QuestionTest.Q1);
-        questionRepository.save(QuestionTest.Q2);
+        writer = userRepository.save(new User(3L, "bestsilver", "password", "name", "bestsilver@ggg.net"));
     }
 
     @Test
     void 질문_등록_테스트() {
-        Question question = new Question("title3", "contents3").writeBy(UserTest.JAVAJIGI);
-        questionRepository.save(question);
-        assertThat(question.isOwner(UserTest.JAVAJIGI)).isTrue();
+        Question question = new Question("title1", "contents1").writeBy(writer);
+        assertThat(question.isOwner(writer)).isTrue();
     }
 
     @DisplayName("not null인 필드에 null을 넣었을 때")
     @Test
     void 질문_title_null_등록_테스트() {
         assertThrows(DataIntegrityViolationException.class, () -> {
-            Question question = new Question(null, "content3").writeBy(UserTest.JAVAJIGI);
+            Question question = new Question(null, "content3").writeBy(writer);
             questionRepository.save(question);
         });
     }
@@ -48,6 +51,12 @@ public class QuestionRepositoryTest {
     @DisplayName("deleted가 false인 객체들 검색")
     @Test
     void findByDeletedFalse_테스트() {
+        Question question1 = new Question("title1", "content1").writeBy(writer);
+        Question question2 = new Question("title3", "content3").writeBy(writer);
+
+        questionRepository.save(question1);
+        questionRepository.save(question2);
+
         List<Question> byDeletedFalse = questionRepository.findByDeletedFalse();
 
         assertThat(byDeletedFalse.size()).isEqualTo(2);
@@ -55,12 +64,12 @@ public class QuestionRepositoryTest {
 
     @Test
     void findByIdAndDeletedFalse_테스트() {
-        Question question3 = new Question("title3", "contents3").writeBy(UserTest.JAVAJIGI);
-        questionRepository.save(question3);
-        Optional<Question> byIdAndDeletedFalse = questionRepository.findByIdAndDeletedFalse(question3.getId());
+        Question question = new Question("title1", "contents1").writeBy(writer);
+        questionRepository.save(question);
+        Optional<Question> byIdAndDeletedFalse = questionRepository.findByIdAndDeletedFalse(question.getId());
 
         assertThat(byIdAndDeletedFalse.isPresent()).isTrue();
-        assertThat(byIdAndDeletedFalse.get() == question3).isTrue();
+        assertThat(byIdAndDeletedFalse.get() == question).isTrue();
     }
 
     @Test
@@ -72,7 +81,7 @@ public class QuestionRepositoryTest {
                 .toString();
 
         assertThrows(DataIntegrityViolationException.class, () -> {
-            Question question = new Question(title,"content3").writeBy(UserTest.JAVAJIGI);
+            Question question = new Question(title,"content1").writeBy(writer);
             questionRepository.save(question);
         });
     }
