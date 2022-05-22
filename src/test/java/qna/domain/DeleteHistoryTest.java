742fe6076bf8419cf92de73b165e9b447153ac67
seongyoon <ksy091104@gmail.com>
2022-05-22 11:01:05 +0900

    feat: deleteHistory-user 연관관계 추가 (N:1)

--- src/test/java/qna/domain/DeleteHistoryTest.java
+++ src/test/java/qna/domain/DeleteHistoryTest.java
@@ -4,7 +4,7 @@ import java.time.LocalDateTime;
 
 public class DeleteHistoryTest {
     public static final DeleteHistory D1 = new DeleteHistory(ContentType.ANSWER, AnswerTest.A1.getId(),
-            UserTest.JAVAJIGI.getId(), LocalDateTime.now());
+            UserTest.JAVAJIGI, LocalDateTime.now());
     public static final DeleteHistory D2 = new DeleteHistory(ContentType.QUESTION, QuestionTest.Q1.getId(),
-            UserTest.SANJIGI.getId(), LocalDateTime.now());
+            UserTest.SANJIGI, LocalDateTime.now());
 }
--- src/test/java/qna/repository/DeleteHistoryRepositoryTest.java
+++ src/test/java/qna/repository/DeleteHistoryRepositoryTest.java
@@ -4,51 +4,50 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import qna.domain.AnswerTest;
 import qna.domain.ContentType;
 import qna.domain.DeleteHistory;
 import qna.domain.DeleteHistoryRepository;
-import qna.domain.DeleteHistoryTest;
-import qna.domain.UserTest;
+import qna.domain.User;
+import qna.domain.UserRepository;
 
 @DataJpaTest
 class DeleteHistoryRepositoryTest {
     @Autowired
     private DeleteHistoryRepository deleteHistories;
 
-    @Test
-    @DisplayName("저장")
-    void create() {
-        DeleteHistory savedDeleteHistory = deleteHistories.save(DeleteHistoryTest.D1);
-        Optional<DeleteHistory> actual = deleteHistories.findById(savedDeleteHistory.getId());
-        assertThat(actual).isPresent();
+    @Autowired
+    private UserRepository users;
+
+    private DeleteHistory deleteHistory;
+
+    @BeforeEach
+    void setup() {
+        User user = users.save(new User("sykim", "password", "sykim", "sykim@sykim.com"));
+        deleteHistory = deleteHistories.save(new DeleteHistory(ContentType.ANSWER, 1L, user, LocalDateTime.now()));
     }
 
     @Test
     @DisplayName("없는 id 조회")
     void findNotExistAnswer() {
-        DeleteHistory deleteHistory = new DeleteHistory(ContentType.ANSWER, AnswerTest.A1.getId(),
-                UserTest.JAVAJIGI.getId(), LocalDateTime.now());
-        deleteHistories.save(deleteHistory);
-        Optional<DeleteHistory> actual = deleteHistories.findById(9999L);
-        assertThat(actual).isNotPresent();
+        assertThat(deleteHistories.findById(9999L)).isNotPresent();
+    }
+
+    @Test
+    @DisplayName("저장")
+    void create() {
+        Optional<DeleteHistory> actual = deleteHistories.findById(deleteHistory.getId());
+        assertThat(actual).isPresent();
     }
 
     @Test
     @DisplayName("삭제 후 조회")
     void delete() {
-        DeleteHistory deleteHistory = new DeleteHistory(ContentType.ANSWER, AnswerTest.A1.getId(),
-                UserTest.JAVAJIGI.getId(), LocalDateTime.now());
-        deleteHistories.save(deleteHistory);
-        Optional<DeleteHistory> actual = deleteHistories.findById(deleteHistory.getId());
-        assertThat(actual).isPresent();
-
-        deleteHistories.deleteById(actual.get().getId());
-        actual = deleteHistories.findById(actual.get().getId());
-        assertThat(actual).isNotPresent();
+        deleteHistories.delete(deleteHistory);
+        assertThat(deleteHistories.findById(deleteHistory.getId())).isNotPresent();
     }
 }
