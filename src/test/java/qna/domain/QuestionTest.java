d49d4a7da95dc13fb1eb2914c6e40ff1fc7304b1
hyun <jhyun_yi@naver.com>
2022-05-22 18:03:32 +0900

    feat : 엔티티 객체 수정

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,62 @@
 package qna.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import java.util.Optional;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
 public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+    @Autowired
+    AnswerRepository answerRepository;
+
+    @Autowired
+    QuestionRepository questionRepository;
+
+    @Test
+    @DisplayName("질문에 답변 달기")
+    void addAnswer() {
+        Answer answer = answerRepository.save(AnswerTest.A1);
+        Question question = questionRepository.save(Q2);
+
+        question.addAnswer(answer);
+        Answer answerFind = answerRepository.findById(answer.getId()).get();
+        assertThat(answerFind.getQuestionId()).isEqualTo(question.getId());
+    }
+
+    @Test
+    @DisplayName("저장하기")
+    void save() {
+        Question question = questionRepository.save(Q1);
+        Optional<Question> questionOptional = questionRepository.findById(question.getId());
+
+        assertThat(questionOptional.get().getId()).isEqualTo(question.getId());
+    }
+
+    @Test
+    @DisplayName("미삭제 건 전체 조회")
+    void notDeleted() {
+        Question question1 = questionRepository.save(Q1);
+        Question question2 = questionRepository.save(Q2);
+        List<Question> deletedFalse = questionRepository.findByDeletedFalse();
+        assertThat(deletedFalse).contains(question1, question2);
+    }
+
+    @Test
+    @DisplayName("미삭제 질문 id로 조회")
+    void findByIdAndNotDeleted() {
+        Question question1 = questionRepository.save(Q1);
+        Question question2 = questionRepository.save(Q2);
+
+        Optional<Question> optionalQuestion = questionRepository.findByIdAndDeletedFalse(question1.getId());
+        assertThat(optionalQuestion.get().getId()).isEqualTo(question1.getId());
+    }
+
 }
--- src/test/java/qna/domain/UserTest.java
+++ src/test/java/qna/domain/UserTest.java
@@ -1,6 +1,52 @@
 package qna.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
 public class UserTest {
     public static final User JAVAJIGI = new User(1L, "javajigi", "password", "name", "javajigi@slipp.net");
     public static final User SANJIGI = new User(2L, "sanjigi", "password", "name", "sanjigi@slipp.net");
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Test
+    @DisplayName("이름, 이메일 수정")
+    void updateNameAndEmail() {
+        User changeUser = new User(1L, "javajigi", "password", "name2", "javajigi2@slipp.net");
+        changeUser.update(JAVAJIGI, JAVAJIGI);
+        assertThat(changeUser.getName()).isEqualTo(JAVAJIGI.getName());
+        assertThat(changeUser.getEmail()).isEqualTo(JAVAJIGI.getEmail());
+    }
+
+    @Test
+    @DisplayName("저장 후 이름, 이메일,비밀번호 동일 저장 확인")
+    void saveMatch() {
+        User user = userRepository.save(JAVAJIGI);
+        assertThat(user.matchPassword(JAVAJIGI.getPassword())).isTrue();
+        assertThat(user.equalsNameAndEmail(JAVAJIGI)).isTrue();
+    }
+
+    @Test
+    @DisplayName("빈값으로 id 세팅 시 자동 입력 확인")
+    void save() {
+        User user = new User("", "password", "hyun", "hyun@slipp.net");
+        User save = userRepository.save(user);
+        assertThat(save.getId()).isNotNull();
+    }
+
+    @Test
+    @DisplayName("이메일 수정")
+    void updateEmail() {
+        User save = userRepository.save(new User("", "password", "hyun", "hyun@slipp.net"));
+        save.setEmail("hyun22@slipp.net");
+
+        User one = userRepository.getOne(save.getId());
+        assertThat(one.getEmail()).isEqualTo("hyun22@slipp.net");
+    }
 }
