0903bc5a0e1f52e0af9f5655b80185d1d0cead27
millennion-jin <Jadestern2@>
2022-05-24 01:33:35 +0900

    test(QuestionTest) : Question 객체관련 테스트 작성

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,30 @@
 package qna.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+    @DisplayName("Question 객체 기본 API 테스트")
+    @Test
+    void questionNormal() {
+        assertAll(
+                () -> assertThat(Q1.isOwner(UserTest.JAVAJIGI)).isTrue(),
+                () -> assertThat(Q1.isOwner(UserTest.SANJIGI)).isFalse()
+        );
+    }
+
+    @DisplayName("Question 객체 deleted 변경 테스트")
+    @Test
+    void deleteQuestion() {
+        Question question = new Question("title3", "contents3");
+        question.setDeleted(true);
+
+        assertThat(question.isDeleted()).isTrue();
+    }
 }
