8d2e56a113eb938e81c06094875bc71865a9ea0c
mannue <sppe12@gmail.com>
2022-05-23 14:09:21 +0900

    test(AnswerTEst): 불필요한 테스트 제거

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,13 +1,10 @@
 package qna.domain;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
@@ -15,18 +12,13 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 
 @DataJpaTest
 public class AnswerTest {
-    public static Answer A1;
-    public static Answer A2;
+    public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
+    public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
 
 
     @Autowired
     private AnswerRepository answerRepository;
 
-    @BeforeEach
-    void setUp() {
-        A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
-        A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
-    }
 
     @DisplayName("create_at, deleted 필드 값을 null 을 가질수 없다.")
     @Test
@@ -43,7 +35,7 @@ public class AnswerTest {
     void identityTest() {
         Answer expected = answerRepository.save(A1);
         answerRepository.flush();
-        Answer answer = answerRepository.findById(A1.getId()).get();
+        Answer answer = answerRepository.findById(expected.getId()).get();
         assertThat(expected).isSameAs(answer);
     }
 
@@ -56,13 +48,5 @@ public class AnswerTest {
         Optional<Answer> isSavedAnswer = answerRepository.findByIdAndDeletedFalse(savedAnswer.getId());
         assertThat(isSavedAnswer.isPresent()).isTrue();
         assertThat(isSavedAnswer.get().getContents()).isEqualTo(savedAnswer.getContents());
-    }
-
-    @DisplayName("동일 질문 이고 삭제가 안된 정보 가져오기 테스트")
-    @Test
-    void getSameQuestionListTest() {
-        answerRepository.saveAll(Arrays.asList(A1, A2));
-        List<Answer> byQuestionIdAndDeletedFalse = answerRepository.findByQuestionIdAndDeletedFalse(QuestionTest.Q1.getId());
-        assertThat(byQuestionIdAndDeletedFalse).contains(A1, A2);
     }
 }
