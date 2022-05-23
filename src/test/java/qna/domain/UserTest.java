1fd3e3cf3ca7a37a601a5cdc2b110d552754e62d
millennion-jin <Jadestern2@>
2022-05-24 01:34:39 +0900

    test(UserTest) : User 객체관련 테스트 작성

--- src/test/java/qna/domain/UserTest.java
+++ src/test/java/qna/domain/UserTest.java
@@ -1,6 +1,35 @@
 package qna.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class UserTest {
-    public static final User JAVAJIGI = new User(1L, "javajigi", "password", "name", "javajigi@slipp.net");
-    public static final User SANJIGI = new User(2L, "sanjigi", "password", "name", "sanjigi@slipp.net");
+    public static final User JAVAJIGI = new User(1L, "javajigi", "password", "name1", "javajigi@slipp.net");
+    public static final User SANJIGI = new User(2L, "sanjigi", "password", "name2", "sanjigi@slipp.net");
+
+    @DisplayName("User 객체 기본 API 테스트")
+    @Test
+    void userNormal() {
+        assertAll(
+                () -> assertThat(JAVAJIGI.matchPassword("password")).isTrue(),
+                () -> assertThat(JAVAJIGI.matchPassword("abcdefgh")).isFalse(),
+                () -> assertThat(SANJIGI.equalsNameAndEmail(JAVAJIGI)).isFalse()
+        );
+    }
+
+    @DisplayName("User 객체 update 테스트")
+    @Test
+    void updateUser() {
+        User user = new User(3L, "baejigi", "password", "name3", "javajigi@slipp.net");
+        user.update(user, SANJIGI);
+
+        assertAll(
+                () -> assertThat(user.getName()).isEqualTo("name2"),
+                () -> assertThat(user.getEmail()).isEqualTo("sanjigi@slipp.net")
+        );
+    }
+
 }
