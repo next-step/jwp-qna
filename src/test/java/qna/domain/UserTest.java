6c8f1dc8117edd2beadc65a777ea4bebc84de032
limwoobin <drogba02@naver.com>
2022-05-23 19:22:58 +0900

    feat: User 에 대해 단위테스트를 작성하고 fixture 자원을 제거하라

--- src/test/java/qna/domain/UserTest.java
+++ src/test/java/qna/domain/UserTest.java
@@ -1,6 +1,41 @@
 package qna.domain;
 
-public class UserTest {
-    public static final User JAVAJIGI = new User(1L, "javajigi", "password", "name", "javajigi@slipp.net");
-    public static final User SANJIGI = new User(2L, "sanjigi", "password", "name", "sanjigi@slipp.net");
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import qna.UnAuthorizedException;
+
+@DisplayName("유저에 대한 단위 테스트")
+class UserTest {
+
+    private User user;
+    private User other;
+
+    @BeforeEach
+    void setUp() {
+        user = new User(1L, "woobeen", "password", "name", "drogba02@naver.com");
+        other = new User(2L, "other", "other-password", "name", "other@naver.com");
+    }
+
+    @DisplayName("유저의 id 가 다른 user 로 update 를 시도하면 예외가 발생해야 한다")
+    @Test
+    void user_update_test() {
+        User owner = user;
+
+        assertThatThrownBy(() -> {
+            owner.update(other, other);
+        }).isInstanceOf(UnAuthorizedException.class);
+    }
+
+    @DisplayName("패스워드가 다른 user 가 update 를 시도하면 예외가 발생해야 한다")
+    @Test
+    void user_update_test2() {
+        User owner = user;
+
+        assertThatThrownBy(() -> {
+            owner.update(owner, other);
+        }).isInstanceOf(UnAuthorizedException.class);
+    }
 }
