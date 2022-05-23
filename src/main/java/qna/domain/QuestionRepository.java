61323c60f12f44fd35dfff6f9589f59b10721151
limwoobin <drogba02@naver.com>
2022-05-23 19:17:42 +0900

    feat: Question 에 대해 단위 테스트를 작성하고 repository test 를 수정하라
    - Question 은 List<Answer> 를 같이 조회해야 하므로 성능을 위해 fetch join 으로 조회한다

--- src/main/java/qna/domain/QuestionRepository.java
+++ src/main/java/qna/domain/QuestionRepository.java
@@ -4,9 +4,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.Query;
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
     List<Question> findByDeletedFalse();
 
+    @Query(value = "select distinct q from Question q "
+        + "left join fetch q.answers.answers "
+        + "where q.id = :id "
+        + "and q.deleted = false ")
     Optional<Question> findByIdAndDeletedFalse(Long id);
 }
--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,42 @@
 package qna.domain;
 
-public class QuestionTest {
-    public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
-    public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
+@DisplayName("질문에 대한 단위 테스트")
+class QuestionTest {
+
+    private User writer;
+    private User other;
+
+    @BeforeEach
+    void setUp() {
+        writer = new User(1L, "woobeen", "password", "name", "drogba02@naver.com");
+        other = new User(2L, "other", "password", "name", "other@naver.com");
+    }
+
+    @Test
+    @DisplayName("질문을 작성한 자가 아닌 사람이 질문을 삭제하려 하면 예외가 발생한다")
+    void delete_exception_test() {
+        Question question = new Question("test-title", "test-contents").writeBy(writer);
+
+        assertThatThrownBy(() -> {
+            question.delete(other);
+        }).isInstanceOf(CannotDeleteException.class)
+            .hasMessageContaining("질문을 삭제할 권한이 없습니다.");
+    }
+
+    @Test
+    @DisplayName("질문을 작성한 자가 질문을 삭제하려 하면 정상적으로 삭제되어야 한다")
+    void delete_test() {
+        Question question = new Question("test-title", "test-contents").writeBy(writer);
+        question.delete(writer);
+
+        assertTrue(question.isDeleted());
+    }
 }
