d3a8b52cb46ea67400ba51fcdabde7cbc14d4e4e
soob-forest <dltntnqo@naver.com>
2022-05-24 04:09:57 +0900

    test(QuestionRepositoryTest): QuestionRepository 테스트 추가

---
+++ src/test/java/qna/domain/QuestionRepositoryTest.java
@@ -0,0 +1,62 @@
+package qna.domain;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import java.util.List;
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+class QuestionRepositoryTest {
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    @Test
+    void 질문_생성() {
+        Question expected = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
+        Question actual = questionRepository.save(expected);
+        assertAll(
+                () -> assertThat(actual.getId()).isEqualTo(expected.getId()),
+                () -> assertThat(actual.getTitle()).isEqualTo(expected.getTitle()),
+                () -> assertThat(actual.getContents()).isEqualTo(expected.getContents()),
+                () -> assertThat(actual.getWriterId()).isEqualTo(expected.getWriterId()),
+                () -> assertThat(actual.isDeleted()).isEqualTo(expected.isDeleted())
+        );
+    }
+
+    @Test
+    void 삭제_되지_않은_질문목록_조회() {
+
+        Question deletedQuestion = questionRepository.save(
+                new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI));
+        Question notDeletedQuestion = questionRepository.save(
+                new Question("title2", "contents2").writeBy(UserTest.SANJIGI));
+
+        deletedQuestion.setDeleted(true);
+
+        List<Question> foundQuestions = questionRepository.findByDeletedFalse();
+        assertThat(foundQuestions).containsExactly(notDeletedQuestion);
+    }
+
+    @Test
+    void 아이디로_삭제_되지_않은_질문_조회() {
+        Question deletedQuestion = questionRepository.save(
+                new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI));
+        Question notDeletedQuestion = questionRepository.save(
+                new Question("title2", "contents2").writeBy(UserTest.SANJIGI));
+
+        deletedQuestion.setDeleted(true);
+
+        Optional<Question> foundDeletedQuestion = questionRepository.findByIdAndDeletedFalse(deletedQuestion.getId());
+        Optional<Question> foundNotDeletedQuestion = questionRepository.findByIdAndDeletedFalse(
+                notDeletedQuestion.getId());
+        assertAll(
+                () -> assertThat(foundDeletedQuestion.isPresent()).isFalse(),
+                () -> assertThat(foundNotDeletedQuestion.isPresent()).isTrue()
+        );
+    }
+}
