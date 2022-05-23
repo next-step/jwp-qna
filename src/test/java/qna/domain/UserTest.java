87d6797683d96fb745de00612e23aeb77748977f
soob-forest <dltntnqo@naver.com>
2022-05-24 01:26:22 +0900

    feat(User): User 도메인 엔티티 jpa 적용

--- src/test/java/qna/domain/UserTest.java
+++ src/test/java/qna/domain/UserTest.java
@@ -1,6 +1,30 @@
 package qna.domain;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
+
 public class UserTest {
     public static final User JAVAJIGI = new User(1L, "javajigi", "password", "name", "javajigi@slipp.net");
     public static final User SANJIGI = new User(2L, "sanjigi", "password", "name", "sanjigi@slipp.net");
+
+    @Test
+    void 유저_이름_이메일_업데이트() {
+        User beforeUpdatedUser = new User("soob", "password", "name", "email@email.com");
+        User afterUpdatedUser = new User("soob", "password", "name2", "email2@email.com");
+
+        beforeUpdatedUser.update(beforeUpdatedUser, afterUpdatedUser);
+
+        assertThat(beforeUpdatedUser.equalsNameAndEmail(afterUpdatedUser)).isTrue();
+    }
+
+    @Test
+    void 게스트_유저_확인인() {
+        User guestUser = User.GUEST_USER;
+        assertAll(
+                () -> assertThat(guestUser.isGuestUser()).isTrue(),
+                () -> assertThat(JAVAJIGI.isGuestUser()).isFalse()
+        );
+    }
 }
