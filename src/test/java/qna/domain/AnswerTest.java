64befc4b85330a89e3782d65191cfcba5d325465
millennion-jin <Jadestern2@>
2022-05-24 01:33:00 +0900

    test(AnswerTest) : Answer 객체관련 테스트 작성

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,19 +1,19 @@
 package qna.domain;
 
 
-import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-class AnswerTest {
+public class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "This is apple.");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q2, "Wryyyyyyyyyyyyyyy!!!!");
 
     @DisplayName("Answer 객체 기본 API 테스트")
     @Test
-    private void answerNormal() {
+    void answerNormal() {
         assertAll(
                 () -> assertThat(A1.isOwner(UserTest.JAVAJIGI)).isTrue(),
                 () -> assertThat(A2.isOwner(UserTest.JAVAJIGI)).isFalse(),
@@ -22,9 +22,9 @@ class AnswerTest {
         );
     }
 
-    @DisplayName("deleted 변경 테스트")
+    @DisplayName("Answer 객체 deleted 변경 테스트")
     @Test
-    private void deleteAnswer() {
+    void deleteAnswer() {
         Answer answer = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "ABC");
         answer.setDeleted(true);
 
