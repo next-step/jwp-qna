4d4061b310c4a3b673f826c2a36ac9d3d44571bd
jiyeonghwang <gyp0288@naver.com>
2022-05-24 22:24:46 +0900

    feat: findDeleteHistoryByDeletedById 추가

--- src/main/java/qna/domain/DeleteHistoryRepository.java
+++ src/main/java/qna/domain/DeleteHistoryRepository.java
@@ -2,5 +2,8 @@ package qna.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DeleteHistoryRepository extends JpaRepository<DeleteHistory, Long> {
+    public List<DeleteHistory> findDeleteHistoryByDeletedById(User user);
 }
