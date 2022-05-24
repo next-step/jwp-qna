4d87dceed3ec8e46b4eec0c43abb41b7b3ebe7eb
breezeunn <je7689@gmail.com>
2022-05-24 23:24:52 +0900

    feat(Question): entity 및 test 생성

---
+++ src/test/java/qnamission/domain/QuestionRepositoryTest.java
@@ -0,0 +1,22 @@
+package qnamission.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class QuestionRepositoryTest {
+
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    @Test
+    void save() {
+        final Question expected = new Question();
+        expected.setTitle("test");
+        final Question actual = questionRepository.save(expected);
+        assertNotNull(actual.getId());
+    }
+}
