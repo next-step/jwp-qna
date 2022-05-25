89cda02f4d9b291d36ac4ca5485ece1b79d95724
sangho lee <sangholee.dev@gmail.com>
2022-05-25 23:18:11 +0900

    refactor: Answer 에서 의존성 사이클링 제거

--- src/main/java/qna/domain/Answer.java
+++ src/main/java/qna/domain/Answer.java
@@ -11,7 +11,6 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
-import qna.NotFoundException;
 import qna.UnAuthorizedException;
 import qna.base.BaseTimeEntity;
 
@@ -23,9 +22,6 @@ public class Answer extends BaseTimeEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "writer_id", foreignKey = @ForeignKey(name = "fk_answer_writer"))
     private User writer;
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "question_id", foreignKey = @ForeignKey(name = "fk_answer_to_question"))
-    private Question question;
     @Lob
     private String contents;
     @Column(nullable = false)
@@ -34,32 +30,23 @@ public class Answer extends BaseTimeEntity {
     protected Answer() {
     }
 
-    public Answer(User writer, Question question, String contents) {
-        this(null, writer, question, contents);
+    public Answer(User writer, String contents) {
+        this(null, writer, contents);
     }
 
-    public Answer(Long id, User writer, Question question, String contents) {
+    public Answer(Long id, User writer, String contents) {
         this.id = id;
 
         if (Objects.isNull(writer)) {
             throw new UnAuthorizedException();
         }
 
-        if (Objects.isNull(question)) {
-            throw new NotFoundException();
-        }
-
         this.writer = writer;
-        this.question = question;
         this.contents = contents;
     }
 
     public boolean isOwner(User writer) {
         return this.writer.equals(writer);
-    }
-
-    public void toQuestion(Question question) {
-        this.question = question;
     }
 
     public Long getId() {
@@ -72,10 +59,6 @@ public class Answer extends BaseTimeEntity {
 
     public User getWriter() {
         return writer;
-    }
-
-    public Question getQuestion() {
-        return question;
     }
 
     public Answer writeBy(final User writer) {
@@ -99,12 +82,13 @@ public class Answer extends BaseTimeEntity {
         this.deleted = true;
     }
 
+
+
     @Override
     public String toString() {
         return "Answer{" +
                 "id=" + id +
                 ", writer=" + writer +
-                ", question=" + question +
                 ", contents='" + contents + '\'' +
                 ", deleted=" + deleted +
                 '}';
