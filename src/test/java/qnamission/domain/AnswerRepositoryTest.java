e0544ecc90b306d8ce42f3dab6d2826cb8ef09c7
breezeunn <je7689@gmail.com>
2022-05-25 09:40:28 +0900

    JPA 1단계 - 엔티티 매핑 PR 입니다 (#504)
    * feat(answer): entity 및 test 생성
    
    * feat(DeleteHistory): entity 및 test 생성
    
    * refactor(answer): 변수명 변경
    
    * feat(Question): entity 및 test 생성
    
    * feat(User): entity 및 test 생성

---
+++ src/test/java/qnamission/domain/AnswerRepositoryTest.java
@@ -0,0 +1,22 @@
+package qnamission.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class AnswerRepositoryTest {
+
+    @Autowired
+    private AnswerRepository answerRepository;
+
+    @Test
+    void save() {
+        final Answer expected = new Answer();
+        expected.setContents("hello");
+        final Answer actual = answerRepository.save(expected);
+        assertNotNull(actual.getId());
+    }
+}
