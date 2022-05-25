6fbba5520472cd1c99f53d6c789d3379987b1fe7
ssamzag <ssamzag@gmail.com>
2022-05-25 19:27:28 +0900

    chore(BaseEntity.java): 미사용 import 삭제

--- src/main/java/qna/domain/BaseEntity.java
+++ src/main/java/qna/domain/BaseEntity.java
@@ -1,11 +1,8 @@
 package qna.domain;
 
-import com.sun.istack.NotNull;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
