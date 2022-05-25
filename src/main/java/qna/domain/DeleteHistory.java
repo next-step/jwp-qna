1bfcf3bbed0e0cf94d66b9677dec0939abede382
sangho lee <sangholee.dev@gmail.com>
2022-05-25 23:19:03 +0900

    refactor: DeleteHistory 에서 User 를 lazy 로딩 하도록 수정

--- src/main/java/qna/domain/DeleteHistory.java
+++ src/main/java/qna/domain/DeleteHistory.java
@@ -5,6 +5,7 @@ import java.util.Objects;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -20,7 +21,7 @@ public class DeleteHistory {
     @Enumerated(EnumType.STRING)
     private ContentType contentType;
     private Long contentId;
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "deleted_by_id", foreignKey = @ForeignKey(name = "fk_delete_history_to_user"))
     private User deletedBy;
     private LocalDateTime createDate = LocalDateTime.now();
@@ -33,14 +34,6 @@ public class DeleteHistory {
         this.contentId = contentId;
         this.deletedBy = deletedBy;
         this.createDate = createDate;
-    }
-
-    public DeleteHistory(final Question question) {
-        this(ContentType.QUESTION, question.getId(), question.getWriter(), LocalDateTime.now());
-    }
-
-    public DeleteHistory(final Answer answer) {
-        this(ContentType.ANSWER, answer.getId(), answer.getWriter(), LocalDateTime.now());
     }
 
     public Long getId() {
@@ -58,13 +51,12 @@ public class DeleteHistory {
         DeleteHistory that = (DeleteHistory) o;
         return Objects.equals(id, that.id) &&
                 contentType == that.contentType &&
-                Objects.equals(contentId, that.contentId) &&
-                Objects.equals(deletedBy, that.deletedBy);
+                Objects.equals(contentId, that.contentId);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, contentType, contentId, deletedBy);
+        return Objects.hash(id, contentType, contentId);
     }
 
     @Override
