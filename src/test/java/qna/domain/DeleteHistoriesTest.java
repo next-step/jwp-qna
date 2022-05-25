bad5a9b94657a174e9435679521d4b6e975dd755
piopoi <kangsw1988@gmail.com>
2022-05-25 23:13:23 +0900

    test(DeleteHistoriesTest): 불필요한 객체 생성 제거

--- src/test/java/qna/domain/DeleteHistoriesTest.java
+++ src/test/java/qna/domain/DeleteHistoriesTest.java
@@ -10,25 +10,19 @@ import org.junit.jupiter.api.Test;
 
 class DeleteHistoriesTest {
 
-    private Question question;
-    private Answer answer;
     private DeleteHistories deleteHistories;
 
     @BeforeEach
-    public void setUp() {
-        question = new Question(1L, "title1", "contents1").writeBy(UserTest.JAVAJIGI);
-        answer = new Answer(1L, UserTest.JAVAJIGI, question, "Answers Contents1");
-        question.addAnswer(answer);
-
+    void setUp() {
         deleteHistories = new DeleteHistories();
-        deleteHistories.add(DeleteHistory.ofQuestion(question.getId(), question.writer(), LocalDateTime.now()));
+        deleteHistories.add(DeleteHistory.ofQuestion(1L, UserTest.JAVAJIGI, LocalDateTime.now()));
     }
 
     @DisplayName("DeleteHistories에 DeleteHistory를 추가한다.")
     @Test
     void add() {
         //given & when
-        deleteHistories.add(DeleteHistory.ofAnswer(answer.getId(), answer.writer(), LocalDateTime.now()));
+        deleteHistories.add(DeleteHistory.ofAnswer(1L, UserTest.JAVAJIGI, LocalDateTime.now()));
 
         //then
         assertThat(deleteHistories.elements()).hasSize(2);
@@ -38,11 +32,9 @@ class DeleteHistoriesTest {
     @Test
     void addAll() {
         //given
-        Answer answer2 = new Answer(2L, UserTest.JAVAJIGI, question, "Answers Contents2");
-        question.addAnswer(answer2);
         DeleteHistories addDeleteHistories = new DeleteHistories(Arrays.asList(
-                DeleteHistory.ofAnswer(answer.getId(), answer.writer(), LocalDateTime.now()),
-                DeleteHistory.ofAnswer(answer2.getId(), answer2.writer(), LocalDateTime.now())
+                DeleteHistory.ofAnswer(2L, UserTest.JAVAJIGI, LocalDateTime.now()),
+                DeleteHistory.ofAnswer(3L, UserTest.SANJIGI, LocalDateTime.now())
         ));
 
         //when
