a429443039a28996877962a1617121d6a7da6c60
songsimo <chy.song89@gmail.com>
2022-05-27 02:23:37 +0900

    refactor: DeleteHistoryRepositoryTest 리팩터링

--- src/test/java/qna/domain/DeleteHistoryRepositoryTest.java
+++ src/test/java/qna/domain/DeleteHistoryRepositoryTest.java
@@ -1,10 +1,11 @@
 package qna.domain;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.LocalDateTime;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,14 +13,30 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 @DataJpaTest
 class DeleteHistoryRepositoryTest {
+	private Question question;
+	private User javajigi;
+	private User sanjigi;
 
 	@Autowired
 	DeleteHistoryRepository deleteHistoryRepository;
 
+	@Autowired
+	QuestionRepository questionRepository;
+
+	@Autowired
+	UserRepository userRepository;
+
+	@BeforeEach
+	void init() {
+		javajigi = userRepository.save(UserTest.JAVAJIGI);
+		sanjigi = userRepository.save(UserTest.SANJIGI);
+		question = questionRepository.save(QuestionTest.Q1);
+	}
+
 	@Test
 	@DisplayName("삭제 History 생성")
 	void save() {
-		DeleteHistory expected = new DeleteHistory(ContentType.ANSWER, QuestionTest.Q1.getId(), UserTest.JAVAJIGI, LocalDateTime.now());
+		DeleteHistory expected = new DeleteHistory(ContentType.ANSWER, question.getId(), javajigi, LocalDateTime.now());
 		DeleteHistory actual = deleteHistoryRepository.save(expected);
 
 		assertAll(
@@ -31,8 +48,8 @@ class DeleteHistoryRepositoryTest {
 	@Test
 	@DisplayName("삭제 History 조회")
 	void findAll() {
-		DeleteHistory actual1 = deleteHistoryRepository.save(new DeleteHistory(ContentType.ANSWER, QuestionTest.Q1.getId(), UserTest.JAVAJIGI, LocalDateTime.now()));
-		DeleteHistory actual2 = deleteHistoryRepository.save(new DeleteHistory(ContentType.ANSWER, QuestionTest.Q1.getId(), UserTest.SANJIGI, LocalDateTime.now()));
+		DeleteHistory actual1 = deleteHistoryRepository.save(new DeleteHistory(ContentType.ANSWER, question.getId(), javajigi, LocalDateTime.now()));
+		DeleteHistory actual2 = deleteHistoryRepository.save(new DeleteHistory(ContentType.ANSWER, question.getId(), sanjigi, LocalDateTime.now()));
 
 		assertThat(deleteHistoryRepository.findAll()).contains(actual1, actual2);
 	}
