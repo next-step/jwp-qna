d87186db1b42d8402551b36e8b3f943e487b1a18
hyun <jhyun_yi@naver.com>
2022-05-26 19:33:53 +0900

    feat : subway hands-on mission

---
+++ src/test/java/subway/domain/LineRepositoryTest.java
@@ -0,0 +1,53 @@
+package subway.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+class LineRepositoryTest {
+    @Autowired
+    private LineRepository lines;
+
+    @Autowired
+    private StationRepository stations;
+
+    @Test
+    void saveWithLine() {
+        final Station station = new Station("잠실역");
+        station.setLine(lines.save(new Line("2호선")));
+        Station actual = stations.save(station);
+        stations.flush();
+    }
+
+    @Test
+    void findByNameWithLine() {
+        Station station = stations.findByName("교대역");
+        assertThat(station).isNotNull();
+        assertThat(station.getLine().getName()).isEqualTo("3호선");
+    }
+
+    @Test
+    void updateWithLine() {
+        Station station = stations.findByName("교대역");
+        station.setLine(lines.save(new Line("2호선")));
+        stations.flush();
+    }
+
+    @Test
+    void findByName() {
+        lines.save(new Line("3호선"));
+        Line line = lines.findByName("3호선");
+        assertThat(line.getStations()).hasSize(1);
+    }
+
+    @Test
+    void save() {
+        Line line = new Line("2호선");
+        line.addStation(new Station("잠실역"));
+        lines.save(line);
+    }
+
+}
