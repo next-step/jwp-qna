9a2135ba32d38bef09e035e97709be4f34ce2993
songsimo <chy.song89@gmail.com>
2022-05-27 00:31:27 +0900

    refactor: UserRepositoryTest 리팩터링

--- src/test/java/qna/domain/UserRepositoryTest.java
+++ src/test/java/qna/domain/UserRepositoryTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 
 import java.util.Optional;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,25 +13,31 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 @DataJpaTest
 class UserRepositoryTest {
+	private User javajigi;
+
 	@Autowired
 	UserRepository userRepository;
+
+	@BeforeEach
+	void init() {
+		javajigi = UserTest.JAVAJIGI;
+	}
 
 	@Test
 	@DisplayName("User 생성")
 	void save() {
-		User expected = UserTest.JAVAJIGI;
-		User actual = userRepository.save(expected);
+		User actual = userRepository.save(javajigi);
 
 		assertAll(
 			() -> assertThat(actual.getId()).isNotNull(),
-			() -> assertThat(actual.getUserId()).isEqualTo(expected.getUserId())
+			() -> assertThat(actual.getUserId()).isEqualTo(javajigi.getUserId())
 		);
 	}
 
 	@Test
 	@DisplayName("User Id로 조회")
 	void findByUserId() {
-		User expected = userRepository.save(new User(1L, "javajigi", "password", "name", "javajigi@slipp.net"));
+		User expected = userRepository.save(javajigi);
 		Optional<User> actual = userRepository.findByUserId(expected.getUserId());
 		
 		assertThat(actual.isPresent()).isTrue();
