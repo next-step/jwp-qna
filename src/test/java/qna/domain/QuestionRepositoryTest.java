c3bfccc7328f601f0907a7f04dc6e021afb758bb
Lisc <qqwer153@gmail.com>
2022-05-27 23:52:15 +0900

    feat: Question 연관 매핑 적용

--- src/test/java/qna/domain/QuestionRepositoryTest.java
+++ src/test/java/qna/domain/QuestionRepositoryTest.java
@@ -1,5 +1,6 @@
 package qna.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,32 +16,37 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 public class QuestionRepositoryTest {
     @Autowired
     private QuestionRepository questionRepository;
+    @Autowired
+    private UserRepository userRepository;
+
+    private Question question1, question2;
+
+    @BeforeEach
+    void init() {
+        //given
+        final User writer = userRepository.save(new User("javajigi", "password", "name", "javajigi@slipp.net"));
+        question1 = questionRepository.save(new Question("title1", "contents1").writeBy(writer));
+        question2 = questionRepository.save(new Question("title2", "contents2").writeBy(writer));
+    }
 
     @Test
     @DisplayName("질문 저장 및 값 비교 테스트")
     void save() {
-        //given
-        final Question expected = QuestionTest.Q1;
-
         //when
-        final Question actual = questionRepository.save(expected);
+        final Question actual = questionRepository.save(question1);
 
         //then
         assertAll(
                 () -> assertThat(actual.getId()).isNotNull(),
-                () -> assertThat(actual.getTitle()).isEqualTo(expected.getTitle()),
-                () -> assertThat(actual.getContents()).isEqualTo(expected.getContents()),
-                () -> assertThat(actual.getWriterId()).isEqualTo(expected.getWriterId())
+                () -> assertThat(actual.getTitle()).isEqualTo(question1.getTitle()),
+                () -> assertThat(actual.getContents()).isEqualTo(question1.getContents()),
+                () -> assertThat(actual.getWriter()).isEqualTo(question1.getWriter())
         );
     }
 
     @Test
     @DisplayName("삭제되지 않은 질문 목록 조회")
     void findByDeletedFalse() {
-        //given
-        Question question1 = questionRepository.save(QuestionTest.Q1);
-        Question question2 = questionRepository.save(QuestionTest.Q2);
-
         //when
         question1.setDeleted(true);
         List<Question> founds = questionRepository.findByDeletedFalse();
@@ -56,10 +62,6 @@ public class QuestionRepositoryTest {
     @Test
     @DisplayName("id로 삭제되지 않은 질문 목록 조회")
     void findByIdAndDeletedFalse() {
-        //given
-        Question question1 = questionRepository.save(QuestionTest.Q1);
-        Question question2 = questionRepository.save(QuestionTest.Q2);
-
         //when
         question1.setDeleted(true);
         Optional<Question> foundsQuestion1 = questionRepository.findByIdAndDeletedFalse(question1.getId());
