1a938f7541a9147b407014fdfe534e6db71d5fb0
vvsungho <vvsungho@gmail.com>
2022-05-27 11:32:18 +0900

    refactor(DeleteHistory.java): equals 메서드 수정
    식별자 하나만으로 동등 비교하도록 수정

--- src/main/java/qna/domain/DeleteHistory.java
+++ src/main/java/qna/domain/DeleteHistory.java
@@ -40,17 +40,9 @@ public class DeleteHistory {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         DeleteHistory that = (DeleteHistory) o;
-        return Objects.equals(id, that.id)
-                && contentType == that.contentType
-                && Objects.equals(contentId, that.contentId)
-                && Objects.equals(user, that.user);
+        return Objects.equals(id, that.id);
     }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, contentType, contentId, user, createDate);
-    }
-
+    
     @Override
     public String toString() {
         return "DeleteHistory{" +
