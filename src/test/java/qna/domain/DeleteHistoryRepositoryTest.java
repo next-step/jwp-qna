82286f7f07f66acd6a424716b64e02107c7f8db7
Lisc <qqwer153@gmail.com>
2022-05-28 00:50:40 +0900

    refactor: DeleteHistory id getter 추가 및 테스트 코드 수정

--- src/test/java/qna/domain/DeleteHistoryRepositoryTest.java
+++ src/test/java/qna/domain/DeleteHistoryRepositoryTest.java
@@ -18,24 +18,14 @@ public class DeleteHistoryRepositoryTest {
     private DeleteHistoryRepository deleteHistoryRepository;
     @Autowired
     private UserRepository userRepository;
-    @Autowired
-    private QuestionRepository questionRepository;
-    @Autowired
-    private AnswerRepository answerRepository;
-
-    private Question question;
-    private Answer answer;
     private DeleteHistory answerHistory, questionHistory;
 
     @BeforeEach
     void init() {
         //given
         User writer = userRepository.save(new User("javajigi", "password", "name", "javajigi@slipp.net"));
-        question = questionRepository.save(new Question("title1", "contents1").writeBy(writer));
-        answer = answerRepository.save(new Answer(writer, question, "Answers Contents1"));
-
-        questionHistory = new DeleteHistory(ContentType.QUESTION, question.getWriter().getId(), question.getWriter(), LocalDateTime.now());
-        answerHistory = new DeleteHistory(ContentType.ANSWER, answer.getWriter().getId(), answer.getWriter(), LocalDateTime.now());
+        questionHistory = new DeleteHistory(ContentType.QUESTION, 1L, writer, LocalDateTime.now());
+        answerHistory = new DeleteHistory(ContentType.ANSWER, 1L, writer, LocalDateTime.now());
     }
 
     @Test
@@ -65,13 +55,11 @@ public class DeleteHistoryRepositoryTest {
     }
 
     @Test
-    @DisplayName("질문 작성자 id로 삭제기록 목록 조회")
-    void findById_answer_writer() {
+    @DisplayName("질문 타입 저장 후 id로 삭제기록 목록 조회")
+    void findById_question_type() {
         //when
         DeleteHistory expected = deleteHistoryRepository.save(questionHistory);
-
-        //when
-        Optional<DeleteHistory> actual = deleteHistoryRepository.findById(question.getWriter().getId());
+        Optional<DeleteHistory> actual = deleteHistoryRepository.findById(expected.getId());
 
         //then
         assertAll(
@@ -81,13 +69,13 @@ public class DeleteHistoryRepositoryTest {
     }
 
     @Test
-    @DisplayName("답변 작성자 id로 삭제기록 목록 조회")
-    void findById_question_writer() {
+    @DisplayName("답변 타입 저장 후 id로 삭제기록 목록 조회")
+    void findById_answer_type() {
         //when
         DeleteHistory expected = deleteHistoryRepository.save(answerHistory);
 
         //when
-        Optional<DeleteHistory> actual = deleteHistoryRepository.findById(answer.getWriter().getId());
+        Optional<DeleteHistory> actual = deleteHistoryRepository.findById(expected.getId());
 
         //then
         assertAll(
