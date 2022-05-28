023467e5e5e5737b3283f855706bba8929ee106f
mannue <sppe12@gmail.com>
2022-05-28 14:31:18 +0900

    feat(DeleteHistories ): getter 추가

--- src/main/java/qna/domain/DeleteHistories.java
+++ src/main/java/qna/domain/DeleteHistories.java
@@ -23,6 +23,10 @@ public class DeleteHistories {
         return deleteHistories.size();
     }
 
+    public Set<DeleteHistory> getDeleteHistories() {
+        return deleteHistories;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
