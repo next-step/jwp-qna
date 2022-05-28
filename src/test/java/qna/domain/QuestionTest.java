9070f3cd63f48d8902fdc009367555f69816a7df
millennion-jin <Jadestern2@>
2022-05-29 04:22:27 +0900

    test(QuestionTest) : Question 객체 삭제시 Exception 발생 케이스에 대한 테스트 추가

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,7 @@
 package qna.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 import org.junit.jupiter.api.DisplayName;
@@ -20,7 +21,7 @@ public class QuestionTest {
         );
     }
 
-    @DisplayName("Question 객체 deleted 변경 테스트")
+    @DisplayName("Question 객체 delete 테스트")
     @Test
     void deleteQuestion() throws CannotDeleteException {
         Question question = new Question("title3", "contents3");
@@ -29,5 +30,33 @@ public class QuestionTest {
         question.delete(writer);
 
         assertThat(question.isDeleted()).isTrue();
+    }
+
+    @DisplayName("Question 객체 delete 를 writer 와 다른 유저가 동작시 Exception 발생 확인")
+    @Test
+    void deleteQuestionByOtherUser() {
+        Question question = new Question("title3", "contents3");
+        User writer = new User("onepunch", "abc", "name", "abc@gmail.com");
+        question.writeBy(writer);
+        User other = new User("hoho", "1234", "js", "att@gmail.com");
+
+        assertThatThrownBy(() -> question.delete(other)).isInstanceOf(CannotDeleteException.class);
+    }
+
+    @DisplayName("다른 유저가 작성한 Answer가 존재하는 Question 객체 delete 시 Exception 발생 확인")
+    @Test
+    void deleteQuestionWithOtherAnswer() {
+        User writer = new User("onepunch", "abc", "name", "abc@gmail.com");
+        User other = new User("hoho", "1234", "js", "att@gmail.com");
+
+        Question question = new Question("title3", "contents3");
+        question.writeBy(writer);
+
+        Answer answerFirst = new Answer(writer, question, "abc");
+        Answer answerSecond = new Answer(other, question, "att");
+        question.addAnswer(answerFirst);
+        question.addAnswer(answerSecond);
+
+        assertThatThrownBy(() -> question.delete(writer)).isInstanceOf(CannotDeleteException.class);
     }
 }
