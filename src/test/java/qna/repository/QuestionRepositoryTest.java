bd44234fbdc25248b80021aa8588eaab218ee0ed
jiyeonghwang <gyp0288@naver.com>
2022-05-30 21:31:32 +0900

    test: answers 조회 테스트 추가

--- src/test/java/qna/repository/QuestionRepositoryTest.java
+++ src/test/java/qna/repository/QuestionRepositoryTest.java
@@ -3,9 +3,12 @@ package qna.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Description;
 import qna.domain.*;
 
+import javax.persistence.EntityManager;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -17,6 +20,12 @@ public class QuestionRepositoryTest {
 
     @Autowired
     UserRepository userRepository;
+
+    @Autowired
+    AnswerRepository answerRepository;
+
+    @Autowired
+    EntityManager entityManager;
 
     @Test
     void save() {
@@ -53,5 +62,19 @@ public class QuestionRepositoryTest {
         questionRepository.save(expected);
         List<Question> actual = questionRepository.findByWriterId(user);
         assertThat(actual.get(0)).isEqualTo(expected);
+    }
+
+    @Test
+    @Description(value = "answers 조회 테스트")
+    void getAnswers() {
+        User user = userRepository.save(UserTest.JAVAJIGI);
+        Question expected = new Question("제목", "내용").writeBy(user);
+        questionRepository.save(expected);
+        answerRepository.save(new Answer(1L, user, expected, "답변1"));
+        answerRepository.save(new Answer(2L, user, expected, "답변2"));
+        entityManager.flush(); // (1)
+        entityManager.clear();
+        Optional<Question> actual2 = questionRepository.findByIdAndDeletedFalse(1L);
+        assertThat(actual2.get().getAnswers().size()).isEqualTo(2);
     }
 }
