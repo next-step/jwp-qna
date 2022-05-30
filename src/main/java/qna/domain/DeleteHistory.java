51ee69a9f38df93213190e7f290b1a0d8913318a
choi-ys <project.log.062@gmail.com>
2022-05-30 17:30:43 +0900

    feat: 삭제이력 테이블을 중심으로 삭제이력 Entity 매핑 및 Repository TC 작성
    delete_history 테이블과 DeleteHistory Entity 매핑
    - @Id를 이용한 PK 컬럼 매핑 및 PK 컬럼 값 설정 전략 적용
    - @GeneratedValue IDENTITY 설정을 이용한 PK 컬럼 값 생성 전략 적용
      - Insert 시점에 DB에서 값을 부여
    - @Enumerated를 이용한 열거형 타입 매핑
    
    TC 작성
    - Entity 및 Repository 동작 확인을 위한 TC 작성

--- src/main/java/qna/domain/DeleteHistory.java
+++ src/main/java/qna/domain/DeleteHistory.java
@@ -2,13 +2,33 @@ package qna.domain;
 
 import java.time.LocalDateTime;
 import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "delete_history")
 public class DeleteHistory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Enumerated(EnumType.STRING)
     private ContentType contentType;
+
     private Long contentId;
+
     private Long deletedById;
+
     private LocalDateTime createDate = LocalDateTime.now();
+
+    protected DeleteHistory() {
+    }
 
     public DeleteHistory(ContentType contentType, Long contentId, Long deletedById, LocalDateTime createDate) {
         this.contentType = contentType;
@@ -17,15 +37,23 @@ public class DeleteHistory {
         this.createDate = createDate;
     }
 
+    public Long getId() {
+        return id;
+    }
+
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         DeleteHistory that = (DeleteHistory) o;
         return Objects.equals(id, that.id) &&
-                contentType == that.contentType &&
-                Objects.equals(contentId, that.contentId) &&
-                Objects.equals(deletedById, that.deletedById);
+            contentType == that.contentType &&
+            Objects.equals(contentId, that.contentId) &&
+            Objects.equals(deletedById, that.deletedById);
     }
 
     @Override
@@ -36,11 +64,11 @@ public class DeleteHistory {
     @Override
     public String toString() {
         return "DeleteHistory{" +
-                "id=" + id +
-                ", contentType=" + contentType +
-                ", contentId=" + contentId +
-                ", deletedById=" + deletedById +
-                ", createDate=" + createDate +
-                '}';
+            "id=" + id +
+            ", contentType=" + contentType +
+            ", contentId=" + contentId +
+            ", deletedById=" + deletedById +
+            ", createDate=" + createDate +
+            '}';
     }
 }
---
+++ src/test/java/qna/domain/DeleteHistoryTest.java
@@ -0,0 +1,31 @@
+package qna.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.LocalDateTime;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author : choi-ys
+ * @date : 2022/05/26 2:33 오후
+ */
+@DisplayName("Domain:DeleteHistory")
+class DeleteHistoryTest {
+
+    @Test
+    @DisplayName("삭제 이력 생성")
+    public void createDeleteHistoryTest() {
+        // Given
+        final ContentType contentType = ContentType.ANSWER;
+        final Long contentId = 1L;
+        final Long deletedById = 1L;
+        final LocalDateTime createDate = LocalDateTime.now();
+
+        // When
+        DeleteHistory given = new DeleteHistory(contentType, contentId, deletedById, createDate);
+
+        // Then
+        assertThat(given).isEqualTo(new DeleteHistory(contentType, contentId, deletedById, createDate));
+    }
+}
