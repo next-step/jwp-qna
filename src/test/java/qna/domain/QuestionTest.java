c4bb5765b4322d7fc7efd63e9fbaf04f0bae0a05
jiyeonghwang <gyp0288@naver.com>
2022-06-01 15:41:36 +0900

    test: Question 테스트 추가

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,40 @@
 package qna.domain;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+    @BeforeEach
+    void setUp() {
+        Q1.addAnswer(AnswerTest.A1);
+    }
+
+    @Test
+    void delete_다른_사람이_쓴_글() {
+        assertThatThrownBy(() -> {
+            Q1.delete(UserTest.SANJIGI);
+        }).isInstanceOf(CannotDeleteException.class)
+                .hasMessageContaining("질문을 삭제할 권한이 없습니다.");
+    }
+
+    @Test
+    void delete_성공_질문자_답변자_같음() throws CannotDeleteException {
+        assertThat(Q1.delete(UserTest.JAVAJIGI).size()).isEqualTo(2);
+    }
+
+    @Test
+    void delete_답변_중_다른_사람이_쓴_글() {
+        Q1.addAnswer(AnswerTest.A2);
+        assertThatThrownBy(() -> {
+            Q1.delete(UserTest.JAVAJIGI);
+        }).isInstanceOf(CannotDeleteException.class)
+                .hasMessageContaining("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
+    }
 }
