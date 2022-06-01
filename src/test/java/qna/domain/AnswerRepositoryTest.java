f670545967338db984af780c2bdf9c33ec095886
오진혁 <hyeok7777@gmail.com>
2022-06-02 02:32:02 +0900

    feat(Answer.java):연관관계 매핑 test(AnswerRepository.java):저장 조회 테스트

--- src/test/java/qna/domain/AnswerRepositoryTest.java
+++ src/test/java/qna/domain/AnswerRepositoryTest.java
@@ -1,8 +1,11 @@
 package qna.domain;
 
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,8 +17,16 @@ class AnswerRepositoryTest {
     @Autowired
     AnswerRepository answerRepository;
 
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    QuestionRepository questionRepository;
+
+
     @Test
     void 저장_테스트() {
+        User user = userRepository.save(UserTest.JAVAJIGI);
         Answer expected = A1;
         Answer result = answerRepository.save(expected);
 
@@ -24,9 +35,14 @@ class AnswerRepositoryTest {
 
     @Test
     void 아이디로_조회() {
+        User user = userRepository.save(UserTest.JAVAJIGI);
+        Question question = questionRepository.save(QuestionTest.Q1).writeBy(user);
+
         Answer save = answerRepository.save(A1);
-        Answer expected = answerRepository.findByIdAndDeletedFalse(save.getId()).get();
-        assertThat(expected.getId()).isEqualTo(save.getId());
+        Answer expected = answerRepository.findByIdAndDeletedFalse(user.getId()).get();
+
+        assertThat(expected.getWriter()).isEqualTo(user);
     }
+
 
 }
--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -13,5 +13,22 @@ public class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
 
+    @Test
+    void 답변_작성자_확인() {
+        assertThat(A1.isOwner(UserTest.JAVAJIGI)).isTrue();
+    }
+
+    @Test
+    void 답변_작성자_확인2() {
+        assertThat(A2.isOwner(UserTest.JAVAJIGI)).isFalse();
+    }
+
+    @Test
+    void 답변_추가() {
+        A1.toQuestion(QuestionTest.Q1);
+
+        assertThat(A1.getQuestion()).isEqualTo(QuestionTest.Q1);
+
+    }
 
 }
