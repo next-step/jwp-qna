76dea5e6099889c4af70e27d79562cc7e4ac50cb
오진혁 <hyeok7777@gmail.com>
2022-06-02 02:30:01 +0900

    feat(User.java):unique 제약조건 추가 test(UserRepository.java):User 검색 테스트

--- src/test/java/qna/domain/UserRepositoryTest.java
+++ src/test/java/qna/domain/UserRepositoryTest.java
@@ -1,7 +1,24 @@
 package qna.domain;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+@DataJpaTest
 class UserRepositoryTest {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Test
+    void findByUserId() {
+        User savedUser = userRepository.save(UserTest.JAVAJIGI);
+        User user = userRepository.findByUserId(savedUser.getUserId()).get();
+
+        assertThat(user).isNotNull();
+    }
 
 }
