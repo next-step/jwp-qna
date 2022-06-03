4efd726aea94a6d058cbcd551f18a7e5482b2885
Philip <pch4623@gmail.com>
2022-06-04 00:37:35 +0900

    test: 답변 삭제 기능 테스트

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -2,12 +2,14 @@ package qna.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
 import qna.NotFoundException;
 import qna.UnAuthorizedException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static qna.domain.QuestionTest.Q1;
+import static qna.domain.UserTest.JAVAJIGI;
 
 public class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, Q1, "Answers Contents1");
@@ -45,5 +47,34 @@ public class AnswerTest {
         assertThat(A1.getQuestion())
                 .isEqualTo(question)
                 .hasFieldOrPropertyWithValue("id", 10L);
+    }
+
+    @Test
+    @DisplayName("답변을 삭제한다.")
+    void test() throws CannotDeleteException {
+        DeleteHistory deleteHistory = A1.delete(UserTest.JAVAJIGI);
+
+        assertThat(A1.isDeleted())
+                .isTrue();
+        assertThat(deleteHistory)
+                .isNotNull();
+    }
+
+    @Test
+    @DisplayName("다른 사람이 쓴 답변인 경우 예외를 발생시킨다.")
+    void delete_wrong_writer() {
+        assertThatThrownBy(() -> A1.delete(UserTest.SANJIGI))
+                .isExactlyInstanceOf(CannotDeleteException.class)
+                .hasMessage("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("이미 삭제한 답변이 있으면 예외를 발생시킨다.")
+    void delete_already_deleted() {
+        A1.setDeleted(true);
+
+        assertThatThrownBy(() -> A1.delete(JAVAJIGI))
+                .isExactlyInstanceOf(CannotDeleteException.class)
+                .hasMessage("이미 삭제된 답변 입니다.");
     }
 }
