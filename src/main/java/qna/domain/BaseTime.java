a93d3dae98fe8cdbdf716f70c4220007e5a05532
tasklet1579 <tasklet1579@gmail.com>
2022-06-09 14:17:04 +0900

    refactor : 추상 클래스로 변경한다

--- src/main/java/qna/domain/BaseTime.java
+++ src/main/java/qna/domain/BaseTime.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-public class BaseTime {
+public abstract class BaseTime {
     @CreatedDate
     @Column(nullable = false)
     private LocalDateTime createdAt;
