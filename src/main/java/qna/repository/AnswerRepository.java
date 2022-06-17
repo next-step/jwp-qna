3ccc35ace811e9dc1d38e3fb9de9dc7ed5852974
choi-ys <project.log.062@gmail.com>
2022-06-17 19:08:27 +0900

    chore(polish): 불필요 코드 삭제

--- src/main/java/qna/repository/AnswerRepository.java
+++ src/main/java/qna/repository/AnswerRepository.java
@@ -1,10 +1,8 @@
 package qna.repository;
 
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import qna.domain.Answer;
 
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
 
-//    Optional<Answer> findByIdAndDeletedFalse(Long id);
 }
