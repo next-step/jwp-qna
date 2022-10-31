a136d4d3c77d575b19cf2dc22f1fd4ad33c06860
namhyun.sung <namhyun.sung@yanolja.com>
2022-10-31 22:47:56 +0900

    feat(baseEntity): baseEntity 생성, JPA Auditing 활성화

---
+++ src/main/java/qna/domain/BaseEntity.java
@@ -0,0 +1,38 @@
+package qna.domain;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners({AuditingEntityListener.class})
+public class BaseEntity {
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    protected LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    protected LocalDateTime updatedAt;
+
+    protected BaseEntity() {
+    }
+
+    public BaseEntity(LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+}
