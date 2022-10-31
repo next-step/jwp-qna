33d2cf250189df2f5f4096a9f3f180f5ba3da8ad
jhkang <serdozz@naver.com>
2022-10-31 18:59:54 +0900

    feat: BaseEntity 생성
    - 공통으로 사용되는 필드 속성을 상속 받도록 추가

---
+++ src/main/java/qna/domain/BaseEntity.java
@@ -0,0 +1,23 @@
+package qna.domain;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @CreatedDate
+    private LocalDateTime createdAt;
+    
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+    
+}
