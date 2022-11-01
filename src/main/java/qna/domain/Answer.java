dd91c37fc91dc0dfc573d873b0a479d61b3f31df
jisu1211 <dbdnzl1211@gmail.com>
2022-11-02 08:13:23 +0900

    feat: answer entity 설정 test: AnswerTest, AnswerRepositoryTest 추가

--- src/main/java/qna/domain/Answer.java
+++ src/main/java/qna/domain/Answer.java
@@ -2,15 +2,27 @@ package qna.domain;
 
 import qna.NotFoundException;
 import qna.UnAuthorizedException;
+import qna.common.AuditingEntity;
 
+import javax.persistence.*;
 import java.util.Objects;
 
-public class Answer {
+@Entity
+public class Answer extends AuditingEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private Long writerId;
     private Long questionId;
+
+    @Lob
     private String contents;
+
+    @Column(nullable = false)
     private boolean deleted = false;
+
+    public Answer() {}
 
     public Answer(User writer, Question question, String contents) {
         this(null, writer, question, contents);
@@ -78,6 +90,26 @@ public class Answer {
 
     public void setDeleted(boolean deleted) {
         this.deleted = deleted;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Answer answer = (Answer) o;
+        return Objects.equals(id, answer.id) && Objects.equals(writerId, answer.writerId)
+                && Objects.equals(questionId, answer.questionId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, writerId, questionId);
     }
 
     @Override
