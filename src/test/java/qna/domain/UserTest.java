3710367e0fd81438f598883f36d3903c8de26264
지영진 <youngjin.jee@nhn.com>
2022-11-01 22:11:53 +0900

    feature: User 클래스 jpa Test 추가

--- src/test/java/qna/domain/UserTest.java
+++ src/test/java/qna/domain/UserTest.java
@@ -1,6 +1,25 @@
 package qna.domain;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
 public class UserTest {
     public static final User JAVAJIGI = new User(1L, "javajigi", "password", "name", "javajigi@slipp.net");
     public static final User SANJIGI = new User(2L, "sanjigi", "password", "name", "sanjigi@slipp.net");
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void 사용자_아이디로_조회() {
+        User user = userRepository.save(JAVAJIGI);
+
+        User retrievedUser = userRepository.findByUserId(user.getUserId()).get();
+
+        assertThat(retrievedUser).isEqualTo(user);
+    }
 }
