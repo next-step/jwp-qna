eeb60ae38f8d3b02b1cd5393ae9e326584d9e800
earth-h <earth.h.183@gmail.com>
2022-11-01 16:05:38 +0900

    test(repository): QuestionRepositoryTest
    QuestionRepositoryTest
    - 테스트명을 좀 더 명확하게 수정
    - beforeEach 구문 내 deleteAllInBatch() 제거
    - 피드백을 통해 받은 DynamicTest를 상태 공유 필요 테스트에 적용

--- src/test/java/qna/repository/QuestionRepositoryTest.java
+++ src/test/java/qna/repository/QuestionRepositoryTest.java
@@ -5,10 +5,13 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 import static qna.domain.QuestionTest.Q1;
 import static qna.domain.QuestionTest.Q2;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import qna.domain.Question;
@@ -19,14 +22,12 @@ public class QuestionRepositoryTest {
     @Autowired
     QuestionRepository questionRepository;
 
-    @BeforeEach
-    void setUp() {
-        questionRepository.deleteAllInBatch();
-    }
-
     @Test
-    void 질문_저장_테스트() {
+    void 질문을_저장하면_저장한_질문_객체를_반환한다() {
+        //when
         Question question = questionRepository.save(Q1);
+
+        //then
         assertAll(
                 () -> assertThat(question.getId()).isNotNull(),
                 () -> assertThat(question.getTitle()).isEqualTo(Q1.getTitle()),
@@ -35,39 +36,70 @@ public class QuestionRepositoryTest {
     }
 
     @Test
-    void 질문_저장_후_삭제되지_않은_질문들_조회_테스트() {
+    void 질문을_저장한_후_삭제되지_않은_질문들을_조회하면_삭제여부가_false인_질문_리스트가_반환된다() {
+        //given
         Question saveQuestion1 = questionRepository.save(Q1);
         Question saveQuestion2 = questionRepository.save(Q2);
-        List<Question> findQuestion = questionRepository.findByDeletedFalse();
+        saveQuestion1.setDeleted(false);
+        saveQuestion2.setDeleted(true);
 
+        //when
+        List<Question> findQuestions = questionRepository.findByDeletedFalse();
+
+        //then
         assertAll(
-                () -> assertThat(findQuestion.size()).isEqualTo(2),
-                () -> assertThat(findQuestion).contains(saveQuestion1),
-                () -> assertThat(findQuestion).contains(saveQuestion2)
+                () -> assertThat(findQuestions.size()).isEqualTo(1),
+                () -> assertThat(findQuestions).contains(saveQuestion1),
+                () -> assertThat(saveQuestion2).isNotIn(findQuestions)
         );
     }
 
-    @Test
-    void 질문_저장_후_삭제여부_true로_업데이트하여_조회_안됨_테스트() {
-        Question saveQuestion = questionRepository.save(Q1);
-        Long saveQuestionId = saveQuestion.getId();
-
-        saveQuestion.setDeleted(true);
-        Optional<Question> findQuestion = questionRepository.findByIdAndDeletedFalse(saveQuestionId);
-
-        assertThat(findQuestion.isPresent()).isFalse();
-    }
-
-    @Test
-    void 질문_조회_후_삭제하면_더_이상_조회가_되지_않음_테스트() {
+    @TestFactory
+    Collection<DynamicTest> 질문을_저장한_후_삭제여부값에_따라_findByIdAndDeletedFalse메소드_조회_결과가_달라진다() {
+        //given
         Question saveQuestion = questionRepository.save(Q2);
         Long saveQuestionId = saveQuestion.getId();
-        Optional<Question> findQuestion = questionRepository.findByIdAndDeletedFalse(saveQuestionId);
+        return Arrays.asList(
+                DynamicTest.dynamicTest("저장한 질문은 findByIdAndDeletedFalse()로 조회하면 정상적으로 조회가 된다.", () -> {
+                    //when
+                    saveQuestion.setDeleted(false);
+                    Optional<Question> findQuestion = questionRepository.findByIdAndDeletedFalse(saveQuestionId);
 
-        assertThat(findQuestion.isPresent()).isTrue();
-        findQuestion.ifPresent(question -> questionRepository.delete(question));
-        Optional<Question> deletedQuestion = questionRepository.findByIdAndDeletedFalse(saveQuestionId);
+                    //then
+                    assertThat(findQuestion).isPresent();
+                }),
+                DynamicTest.dynamicTest("저장한 질문의 삭제여부가 true이면 findByIdAndDeletedFalse() 조회 시 조회되지 않는다.", () -> {
+                    //when
+                    saveQuestion.setDeleted(true);
+                    Optional<Question> findQuestion = questionRepository.findByIdAndDeletedFalse(saveQuestionId);
 
-        assertThat(deletedQuestion.isPresent()).isFalse();
+                    //then
+                    assertThat(findQuestion).isNotPresent();
+                })
+        );
+    }
+
+    @TestFactory
+    Collection<DynamicTest> 질문을_저장하면_조회가_되지만_해당_질문을_삭제하고_조회하면_더_이상_조회되지_않는다() {
+        //given
+        Question saveQuestion = questionRepository.save(Q2);
+        Long saveQuestionId = saveQuestion.getId();
+        return Arrays.asList(
+                DynamicTest.dynamicTest("저장한 질문의 id로 질문을 조회하면 정상적으로 조회가 된다.", () -> {
+                    //when
+                    Optional<Question> findQuestion = questionRepository.findById(saveQuestionId);
+
+                    //then
+                    assertThat(findQuestion).isPresent();
+                }),
+                DynamicTest.dynamicTest("저장한 질문을 삭제하고, 다시 조회하면 해당 질문이 조회되지 않는다.", () -> {
+                    //when
+                    questionRepository.delete(saveQuestion);
+                    Optional<Question> deleteQuestion = questionRepository.findById(saveQuestionId);
+
+                    //then
+                    assertThat(deleteQuestion).isNotPresent();
+                })
+        );
     }
 }
