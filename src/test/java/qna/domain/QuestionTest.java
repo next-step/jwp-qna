c5506cc0b7ff8de6c32fd8192750631156cd81b3
earth-h <earth.h.183@gmail.com>
2022-11-01 15:27:23 +0900

    test(domain): QuestionTest
    QuestionTest
    - 테스트명을 좀 더 명확하게 수정

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -10,8 +10,11 @@ public class QuestionTest {
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
 
     @Test
-    void Question_객체에_Writer_등록_시_Writer가_비어있으면_에러_발생() {
+    void 질문_객체_생성_후_Writer_등록_할_때_Writer가_null이면_UnAuthorizedException_예외가_발생한다() {
+        //given
         Question question = new Question("title3", "Writer가 비어있음");
+
+        //when
         assertThatThrownBy(() -> question.writeBy(null))
                 .isInstanceOf(UnAuthorizedException.class);
     }
