10eda4da8b8deafe2d5e632efa7d2912d91f9f78
ilmare-cbk <cbk104325@gmail.com>
2022-11-01 23:18:00 +0900

    refactor(DeleteHistory.java): DeleteHistory 엔티티 클래스 작성
    - 엔티티의 공통 매팽정보를 상속하기 위해 BaseTimeEntity 클래스 상속
    - primary key 매핑을 위해 @Id 사용
    - @GenerateValue 를 통해 primary key 생성전략 설정
    - enum 매핑을 위해 @Enumerated 사용

--- src/main/java/qna/domain/DeleteHistory.java
+++ src/main/java/qna/domain/DeleteHistory.java
@@ -2,9 +2,19 @@ package qna.domain;
 
 import java.time.LocalDateTime;
 import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class DeleteHistory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Enumerated(EnumType.STRING)
     private ContentType contentType;
     private Long contentId;
     private Long deletedById;
@@ -17,10 +27,18 @@ public class DeleteHistory {
         this.createDate = createDate;
     }
 
+    protected DeleteHistory() {
+
+    }
+
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         DeleteHistory that = (DeleteHistory) o;
         return Objects.equals(id, that.id) &&
                 contentType == that.contentType &&
