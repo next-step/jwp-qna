07ae4a3448a143fd486bc7c6b19e87285e06a50e
sonyoon7 <sonyoon7@gmail.com>
2022-11-02 00:13:07 +0900

    revert

---
+++ src/main/java/qna/domain/AbstractTimestampEntity.java
@@ -0,0 +1,29 @@
+package qna.domain;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+public abstract class AbstractTimestampEntity {
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        LocalDateTime now = LocalDateTime.now();
+        createdAt = now;
+        updatedAt = now;
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+
+}
