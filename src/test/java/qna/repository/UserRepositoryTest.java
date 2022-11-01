88ca7e1fead718cadd1e08d41742e2fc15d5949f
earth-h <earth.h.183@gmail.com>
2022-11-01 15:12:06 +0900

    test(repository): UserRepositoryTest 리팩토링
    UserRepositoryTest
    - beforeEach 구문 내 deleteAllInBatch() 제거
      - @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE) 설정을 하지 않았기 때문에 안심하고 날려도 되어서 넣었으나, 기본적으로 @Transactional이 설정된 @DataJpaTest를 사용했기 때문에 불필요한 설정이라 생각하여 제거
    - 피드백을 통해 받은 DynamicTest를 상태 공유 필요 테스트에 적용
    - 테스트명을 좀 더 명확하도록 수정

--- src/test/java/qna/repository/UserRepositoryTest.java
+++ src/test/java/qna/repository/UserRepositoryTest.java
@@ -6,9 +6,12 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 import static qna.domain.UserTest.JAVAJIGI;
 import static qna.domain.UserTest.SANJIGI;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Optional;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -20,25 +23,27 @@ public class UserRepositoryTest {
     @Autowired
     UserRepository userRepository;
 
-    @BeforeEach
-    void setUp() {
-        userRepository.deleteAllInBatch();
-    }
-
     @Test
-    void 유저_저장_테스트() {
-        User user = userRepository.save(JAVAJIGI);
+    void 유저를_저장하면_저장한_유저_객체를_반환한다() {
+        //when
+        User saveUser = userRepository.save(JAVAJIGI);
+
+        //then
         assertAll(
-                () -> assertThat(user.getUserId()).isEqualTo(JAVAJIGI.getUserId()),
-                () -> assertThat(user.getEmail()).isEqualTo(JAVAJIGI.getEmail())
+                () -> assertThat(saveUser.getUserId()).isEqualTo(JAVAJIGI.getUserId()),
+                () -> assertThat(saveUser.getEmail()).isEqualTo(JAVAJIGI.getEmail())
         );
     }
 
     @Test
-    void 유저_저장_후_유저ID로_조회_테스트() {
+    void 유저를_저장한_후_해당_userId로_조회하면_저장한_유저가_조회된다() {
+        //given
         User saveUser = userRepository.save(JAVAJIGI);
+
+        //when
         Optional<User> findUser = userRepository.findByUserId(saveUser.getUserId());
 
+        //then
         assertThat(findUser.isPresent()).isTrue();
         findUser.ifPresent(user -> assertAll(
                 () -> assertThat(user).isEqualTo(saveUser),
@@ -47,31 +52,48 @@ public class UserRepositoryTest {
     }
 
     @Test
-    void 유저_저장시_동일한_userId를_넣으면_예외_발생_테스트() {
-        userRepository.save(SANJIGI);
+    void 동일한_userId를_가진_유저가_레포지토리에_존재하면_예외를_발생시킨다() {
+        //given
+        User saveUser = userRepository.save(SANJIGI);
+        User duplicateUser = new User(null, saveUser.getUserId(), "password", "name", "email@gmail.com");
 
+        //when
         assertThatExceptionOfType(DataIntegrityViolationException.class)
-                .isThrownBy(() -> userRepository.save(SANJIGI)).withMessageContaining("constraint");
+                .isThrownBy(() -> userRepository.save(duplicateUser)).withMessageContaining("constraint");
     }
 
     @Test
-    void 유저_저장시_id_값을_null로_보내면_DB가_id값을_넣어줌_테스트() {
-        User user = new User("javajigi", "password", "name", "javajigi@slipp.net");
+    void id가_null인_유저를_저장하면_null이_아닌_id를_가진_유저를_반환한다() {
+        //given
+        User user = new User(null, "javajigi", "password", "name", "javajigi@slipp.net");
+
+        //when
         User saveUser = userRepository.save(user);
 
+        //then
         assertThat(saveUser.getId()).isNotNull();
     }
 
-    @Test
-    void 유저_조회_후_삭제하면_더_이상_조회가_되지_않음_테스트() {
+    @TestFactory
+    Collection<DynamicTest> 유저를_저장하면_조회가_되지만_해당_유저를_삭제하고_조회하면_해당_유저가_조회되지_않는다() {
+        //given
         User saveUser = userRepository.save(SANJIGI);
         Long saveUserId = saveUser.getId();
-        Optional<User> findUser = userRepository.findById(saveUserId);
+        return Arrays.asList(
+                DynamicTest.dynamicTest("저장한 유저의 id로 유저를 조회하면 정상적으로 조회가 된다.", () -> {
+                    //when
+                    Optional<User> findUser = userRepository.findById(saveUserId);
 
-        assertThat(findUser.isPresent()).isTrue();
-        findUser.ifPresent(user -> userRepository.delete(user));
-        Optional<User> deletedUser = userRepository.findById(saveUserId);
+                    //then
+                    assertThat(findUser.isPresent()).isTrue();
+                }),
+                DynamicTest.dynamicTest("저장한 유저를 삭제하고, 다시 조회하면 해당 유저가 조회되지 않는다.", () -> {
+                    //when
+                    userRepository.delete(saveUser);
+                    Optional<User> deleteUser = userRepository.findById(saveUserId);
 
-        assertThat(deletedUser.isPresent()).isFalse();
+                    //then
+                    assertThat(deleteUser.isPresent()).isFalse();
+                }));
     }
 }
