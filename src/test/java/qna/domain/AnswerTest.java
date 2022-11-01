d4229cdbfe4f80b8d58aad46eebaade2b8d1af91
earth-h <earth.h.183@gmail.com>
2022-11-01 15:25:35 +0900

    test(domain): AnswerTest 내 오타 수정
    AnswerTest
    - 오타수정

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -12,14 +12,14 @@ public class AnswerTest {
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
 
     @Test
-    void 질문_객체_생성_시_writer가_비어있으면_UnAuthorizedException_예외가_발생한다() {
+    void 답변_객체_생성_시_writer가_비어있으면_UnAuthorizedException_예외가_발생한다() {
         //when
         assertThatThrownBy(() -> new Answer(null, QuestionTest.Q1, "Writer가 비어있음"))
                 .isInstanceOf(UnAuthorizedException.class);
     }
 
     @Test
-    void 질문_객체_생성_시_question이_비어있으면_NotFoundException_예외가_발생한다() {
+    void 답변_객체_생성_시_question이_비어있으면_NotFoundException_예외가_발생한다() {
         //when
         assertThatThrownBy(() -> new Answer(UserTest.JAVAJIGI, null, "Question이 비어있음"))
                 .isInstanceOf(NotFoundException.class);
