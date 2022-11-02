317f5008844a86a55d8713edaed44e9b2f776e8e
paki1019 <paki1019@gmail.com>
2022-11-02 18:10:35 +0900

    test: AnswerRepository 테스트 코드 추가, BasEntity getter 추가

--- src/main/java/qna/domain/BaseEntity.java
+++ src/main/java/qna/domain/BaseEntity.java
@@ -21,4 +21,11 @@ public abstract class BaseEntity {
 	@LastModifiedDate
 	private LocalDateTime updatedAt;
 
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
 }
