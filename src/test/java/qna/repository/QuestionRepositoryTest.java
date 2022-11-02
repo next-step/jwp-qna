c83de9d62a7619b692d36c79b93661b06fe9b383
anwjrrp33 <anwjrrp33@gmail.com>
2022-11-02 23:48:16 +0900

    style: 경로 변경

--- src/test/java/qna/repository/QuestionRepositoryTest.java
+++ src/test/java/qna/repository/QuestionRepositoryTest.java
@@ -1,4 +1,4 @@
-package qna.domain;
+package qna.repository;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,6 +9,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import qna.domain.Question;
+import qna.domain.QuestionTest;
+import qna.repository.QuestionRepository;
 
 @DataJpaTest
 class QuestionRepositoryTest {
