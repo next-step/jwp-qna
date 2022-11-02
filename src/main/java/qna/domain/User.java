3ff5c8c69fd3f46046c8d7497086395137fcd816
earth-h <earth.h.183@gmail.com>
2022-11-02 22:17:51 +0900

    refactor(domain): User에 대해 equals, hashCode 오버라이딩 추가
    User
    - Question과 Answer 작성자인지 여부를 판단할 때 equals() 사용
      - 영속성 컨텍스트를 비우고 다시 동일 객체를 조회하면, 내부 내용은 같아도 다른 객체를 반환해주기 때문에 equals 오버라이딩

--- src/main/java/qna/domain/User.java
+++ src/main/java/qna/domain/User.java
@@ -92,6 +92,25 @@ public class User extends BaseEntity {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return Objects.equals(getUserId(), user.getUserId()) && Objects.equals(getPassword(),
+                user.getPassword()) && Objects.equals(getName(), user.getName()) && Objects.equals(
+                getEmail(), user.getEmail());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getUserId(), getPassword(), getName(), getEmail());
+    }
+
+    @Override
     public String toString() {
         return "User{" +
                 "id=" + id +
