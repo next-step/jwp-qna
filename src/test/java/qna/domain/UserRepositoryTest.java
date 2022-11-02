786f7b518c5c3b025e7eeafdf6d6277e39d4497a
HJ Kim <hjkim880527@gmail.com>
2022-11-02 17:40:15 +0900

    fix: 사용자 엔티티, 리포지토리 매핑

---
+++ src/test/java/qna/domain/UserRepositoryTest.java
@@ -0,0 +1,63 @@
+package qna.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import qna.NotFoundException;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static qna.domain.UserTest.JAVAJIGI;
+
+@DataJpaTest
+public class UserRepositoryTest {
+    
+    @Autowired
+    UserRepository userRepository;
+    
+    @Test
+    @DisplayName("사용자가 정상적으로 등록되있는지 테스트 한다")
+    void saveUserTest(){
+        User user = userRepository.save(JAVAJIGI);
+        assertAll(
+            () -> assertThat(user.getId()).isEqualTo(JAVAJIGI.getId()),
+            () -> assertThat(user.getUserId()).isEqualTo(JAVAJIGI.getUserId()),
+            () -> assertThat(user.getName()).isEqualTo(JAVAJIGI.getName()),
+            () -> assertThat(user.getPassword()).isEqualTo(JAVAJIGI.getPassword()),
+            () -> assertThat(user.getEmail()).isEqualTo(JAVAJIGI.getEmail()),
+            () -> assertThat(user.getCreatedAt()).isEqualTo(JAVAJIGI.getCreatedAt()),
+            () -> assertThat(user.getUpdatedAt()).isEqualTo(JAVAJIGI.getUpdatedAt())
+        );       
+    }
+
+    @Test
+    @DisplayName("사용자를 등록하고 조회되는지 테스트한다")
+    void byIdAndDeletedFalseTest(){
+        User user = userRepository.save(JAVAJIGI);
+        User getUser = userRepository.findById(user.getId())
+                .orElseThrow(() -> new NotFoundException());
+        assertAll(
+                () -> assertThat(getUser).isNotNull(),
+                () -> assertThat(getUser.getId()).isEqualTo(JAVAJIGI.getId()),
+                () -> assertThat(getUser.getUserId()).isEqualTo(JAVAJIGI.getUserId()),
+                () -> assertThat(getUser.getName()).isEqualTo(JAVAJIGI.getName()),
+                () -> assertThat(getUser.getPassword()).isEqualTo(JAVAJIGI.getPassword()),
+                () -> assertThat(getUser.getEmail()).isEqualTo(JAVAJIGI.getEmail()),
+                () -> assertThat(getUser.getCreatedAt()).isEqualTo(JAVAJIGI.getCreatedAt()),
+                () -> assertThat(getUser.getUpdatedAt()).isEqualTo(JAVAJIGI.getUpdatedAt())
+        );
+    }
+    
+    @Test
+    @DisplayName("사용자를 id로 삭제되는지 테스트한다")
+    void deleteByIdTest(){
+        User user = userRepository.save(JAVAJIGI);
+        userRepository.deleteById(user.getId());
+        Optional<User> getUser = userRepository.findById(user.getId());
+        assertThat(getUser.isPresent()).isFalse();
+    }
+    
+}
