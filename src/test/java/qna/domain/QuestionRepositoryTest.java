d0e17df38f79a2ae17d8a97a1686fa2dce010417
namhyun.sung <namhyun.sung@yanolja.com>
2022-11-03 01:52:56 +0900

    test(QuestionRepositoryTest): 테스트 수정

--- src/test/java/qna/domain/QuestionRepositoryTest.java
+++ src/test/java/qna/domain/QuestionRepositoryTest.java
@@ -1,5 +1,6 @@
 package qna.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,10 +18,21 @@ class QuestionRepositoryTest {
     @Autowired
     private QuestionRepository questionRepository;
 
+    @Autowired
+    private UserRepository userRepository;
+
+    private User user;
+
+    @BeforeEach
+    void setUp() {
+        questionRepository.deleteAll();
+        userRepository.deleteAll();
+        user = userRepository.save(new User("test1234", "1234", "테스트", "test1234@gmail.com"));
+    }
     @Test
     @DisplayName("질문을 저장한다")
     void save_question_test() {
-        Question question = QuestionTest.Q1;
+        Question question = QuestionTest.Q1.writeBy(user);
 
         Question saveQuestion = questionRepository.save(question);
 
@@ -28,7 +40,7 @@ class QuestionRepositoryTest {
             () -> assertThat(saveQuestion.getId()).isNotNull(),
             () -> assertThat(saveQuestion.getTitle()).isEqualTo(question.getTitle()),
             () -> assertThat(saveQuestion.getContents()).isEqualTo(question.getContents()),
-            () -> assertThat(saveQuestion.getWriterId()).isEqualTo(question.getWriterId()),
+            () -> assertThat(saveQuestion.getWriteBy()).isEqualTo(question.getWriteBy()),
             () -> assertThat(saveQuestion.getCreatedAt()).isNotNull()
         );
     }
@@ -36,7 +48,7 @@ class QuestionRepositoryTest {
     @Test
     @DisplayName("전체 질문을 조회한다.")
     void query_all_questions_test() {
-        List<Question> questions = Arrays.asList(QuestionTest.Q1, QuestionTest.Q2);
+        List<Question> questions = Arrays.asList(QuestionTest.Q1.writeBy(user), QuestionTest.Q2.writeBy(user));
         questionRepository.saveAll(questions);
 
         List<Question> findAllQuestionByDeletedFalse = questionRepository.findByDeletedFalse();
@@ -47,7 +59,7 @@ class QuestionRepositoryTest {
     @Test
     @DisplayName("삭제된 질문은 조회되지 않는다.")
     void delete_question_test() {
-        List<Question> questions = questionRepository.saveAll(Arrays.asList(QuestionTest.Q1, QuestionTest.Q2));
+        List<Question> questions = questionRepository.saveAll(Arrays.asList(QuestionTest.Q1.writeBy(user), QuestionTest.Q2.writeBy(user)));
 
         questionRepository.delete(questions.get(0));
         List<Question> findAllQuestionByDeletedFalse = questionRepository.findByDeletedFalse();
@@ -58,7 +70,7 @@ class QuestionRepositoryTest {
     @DisplayName("Question의 title을 변경한다.")
     @Test
     void update_question_title_test() {
-        Question question = QuestionTest.Q2;
+        Question question = QuestionTest.Q2.writeBy(user);
         Question saveQuestion = questionRepository.save(question);
 
         saveQuestion.setTitle("테스트1");
