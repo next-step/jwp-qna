ca5c8c5478a0ea92c7d492d2cd9f0b4098910f4c
earth-h <earth.h.183@gmail.com>
2022-11-02 21:30:53 +0900

    fix(domain): 테스트 픽스쳐 public static으로 변경
    TestAnswerFactory
    - public static으로 변경하여 정적 팩토리 메소드로 사용하고자 함

--- src/test/java/qna/domain/TestAnswerFactory.java
+++ src/test/java/qna/domain/TestAnswerFactory.java
@@ -2,7 +2,7 @@ package qna.domain;
 
 public class TestAnswerFactory {
 
-    Answer create(User writer, Question question) {
+    public static Answer create(User writer, Question question) {
         return new Answer(writer, question, "contents");
     }
 }
