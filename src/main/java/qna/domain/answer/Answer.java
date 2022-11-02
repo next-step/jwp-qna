cb6eb39a6fe9337a12c2fd281c589f4d19708b87
bky4564 <bky4564@hunet.co.kr>
2022-11-03 08:37:19 +0900

    refactor: 불필요한 set 메서드 제거

--- src/main/java/qna/domain/answer/Answer.java
+++ src/main/java/qna/domain/answer/Answer.java
@@ -76,16 +76,8 @@ public class Answer extends BaseEntity {
 		return id;
 	}
 
-	public void setId(Long id) {
-		this.id = id;
-	}
-
 	public User getWriter() {
 		return writer;
-	}
-
-	public void setWriter(User writer) {
-		this.writer = writer;
 	}
 
 	public Question getQuestion() {
@@ -98,10 +90,6 @@ public class Answer extends BaseEntity {
 
 	public String getContents() {
 		return contents;
-	}
-
-	public void setContents(String contents) {
-		this.contents = contents;
 	}
 
 	public boolean isDeleted() {
