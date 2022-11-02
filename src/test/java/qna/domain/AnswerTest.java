496ef4f519921faaa775da38f1b0297b7f8d8143
paki1019 <paki1019@gmail.com>
2022-11-02 18:05:15 +0900

    feat: Answer 도메인 테이블 매핑
    - Answer 도메인 테이블 매핑
    - createAt, updateAt을 자동 생성하는 BaseEntity 추가
    - Answer 도메인 테스트 코드 추가

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,6 +1,53 @@
 package qna.domain;
 
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import qna.NotFoundException;
+import qna.UnAuthorizedException;
+
 public class AnswerTest {
-    public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
-    public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+	public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
+	public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+	@Test
+	void 객체_생성_유저_null일경우_UnAuthorizedException() {
+		assertThatThrownBy(() -> new Answer(null, QuestionTest.Q1, "Answers Contents"))
+			.isInstanceOf(UnAuthorizedException.class);
+	}
+
+	@Test
+	void 객체_생성_질문_null일경우_UnAuthorizedException() {
+		assertThatThrownBy(() -> new Answer(UserTest.JAVAJIGI, null, "Answers Contents"))
+			.isInstanceOf(NotFoundException.class);
+	}
+
+
+
+	@Test
+	void 동등성() {
+		assertThat(new Answer(1L, UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents"))
+			.isEqualTo(new Answer(1L, UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents"));
+
+		assertThat(new Answer(1L, UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents"))
+			.isNotEqualTo(new Answer(2L, UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents"));
+	}
+
+	@Test
+	void 작성자_확인() {
+		assertThat(A1.isOwner(UserTest.JAVAJIGI)).isTrue();
+		assertThat(A1.isOwner(UserTest.SANJIGI)).isFalse();
+	}
+
+	@Test
+	void 질문_변경() {
+		Question question1 = new Question(1L, "title1", "contents1").writeBy(UserTest.JAVAJIGI);
+		Question question2 = new Question(2L, "title2", "contents2").writeBy(UserTest.SANJIGI);
+		Answer answer = new Answer(1L, UserTest.JAVAJIGI, question1, "Answers Contents");
+
+		answer.toQuestion(question2);
+		assertThat(answer.getQuestionId()).isNotEqualTo(question1.getId());
+		assertThat(answer.getQuestionId()).isEqualTo(question2.getId());
+	}
 }
