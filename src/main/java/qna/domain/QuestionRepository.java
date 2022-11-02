774442ef0c9914ff85ecda0570cd17668ec93087
손윤 <sonyoon7@gmail.com>
2022-11-02 18:26:21 +0900

    test (QuestionTest) 질문 CRUD 테스트

--- src/main/java/qna/domain/QuestionRepository.java
+++ src/main/java/qna/domain/QuestionRepository.java
@@ -2,7 +2,6 @@ package qna.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 import java.util.Optional;
 
@@ -11,4 +10,11 @@ public interface QuestionRepository extends JpaRepository<Question, Long> {
     List<Question> findByDeletedFalse();
 
     Optional<Question> findByIdAndDeletedFalse(Long id);
+
+    @Override
+    List<Question> findAll();
+
+    Question findByContents(String contents);
+
+
 }
