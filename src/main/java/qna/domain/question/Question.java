918994b0930126afaf622ff01662738c622467ba
bky4564 <bky4564@hunet.co.kr>
2022-11-03 11:21:44 +0900

    feat: 질문, 답변 equals & hashcode 재정의

--- src/main/java/qna/domain/question/Question.java
+++ src/main/java/qna/domain/question/Question.java
@@ -1,5 +1,7 @@
 package qna.domain.question;
 
+import java.util.Objects;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -84,6 +86,23 @@ public class Question extends BaseEntity {
 
     public void setDeleted(boolean deleted) {
         this.deleted = deleted;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+
+        Question question = (Question)o;
+
+        return Objects.equals(id, question.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
     }
 
     @Override
