bdccf977210d2a10c9fb7e1fa6c4e10db9b20884
bky4564 <bky4564@hunet.co.kr>
2022-11-03 11:00:14 +0900

    refactor: User entity equals & hash code 재정의

--- src/main/java/qna/domain/user/User.java
+++ src/main/java/qna/domain/user/User.java
@@ -103,6 +103,23 @@ public class User extends BaseEntity {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+
+        User user = (User)o;
+
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    @Override
     public String toString() {
         return "User{" +
                 "id=" + id +
