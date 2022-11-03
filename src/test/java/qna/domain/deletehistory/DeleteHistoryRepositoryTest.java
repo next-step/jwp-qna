74b9086015af274ac777da827a747c2e4aca81e6
bky4564 <bky4564@hunet.co.kr>
2022-11-03 13:21:12 +0900

    test: 삭제 내역 테스트 수정
    - 삭제 테스트 삭제 (요구사항 x)
    - 답변, 질문 삭제 내역 테스트 추가
    - Generator 활용

--- src/test/java/qna/domain/deletehistory/DeleteHistoryRepositoryTest.java
+++ src/test/java/qna/domain/deletehistory/DeleteHistoryRepositoryTest.java
@@ -1,55 +1,103 @@
 package qna.domain.deletehistory;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
-import static qna.domain.deletehistory.DeleteHistoryTest.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.TestConstructor;
+
+import qna.domain.ContentType;
+import qna.domain.answer.Answer;
+import qna.domain.generator.QuestionGenerator;
+import qna.domain.generator.UserGenerator;
+import qna.domain.question.AnswerGenerator;
+import qna.domain.question.Question;
+import qna.domain.user.User;
 
 @DataJpaTest
+@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
+@Import({UserGenerator.class, QuestionGenerator.class, AnswerGenerator.class})
 @DisplayName("DeleteHistory Repository 테스트")
 class DeleteHistoryRepositoryTest {
 
-	@Autowired
-	private DeleteHistoryRepository deleteHistoryRepository;
+	private final DeleteHistoryRepository deleteHistoryRepository;
+	private final AnswerGenerator answerGenerator;
+	private final QuestionGenerator questionGenerator;
+	private final UserGenerator userGenerator;
+
+	public DeleteHistoryRepositoryTest(
+		DeleteHistoryRepository deleteHistoryRepository,
+		AnswerGenerator answerGenerator,
+		QuestionGenerator questionGenerator,
+		UserGenerator userGenerator
+	) {
+		this.deleteHistoryRepository = deleteHistoryRepository;
+		this.answerGenerator = answerGenerator;
+		this.questionGenerator = questionGenerator;
+		this.userGenerator = userGenerator;
+	}
 
 	@Test
-	@DisplayName("삭제 이력 저장 테스트")
-	void saveTest() {
-		DeleteHistory deleteHistory = deleteHistoryRepository.save(D1);
-		assertThat(deleteHistory).isNotNull();
+	@DisplayName("질문 삭제 이력 저장 테스트")
+	void saveQuestionTest() {
+		// given
+		User user = userGenerator.savedUser();
+		Question question = questionGenerator.savedQuestion(user);
+		DeleteHistory deleteHistory =
+			new DeleteHistory(ContentType.QUESTION, question.getId(), user, LocalDateTime.now());
+
+		// when
+		DeleteHistory actual = deleteHistoryRepository.save(deleteHistory);
+
+		// then
+		assertThat(actual).isNotNull();
+	}
+
+	@Test
+	@DisplayName("답변 삭제 이력 저장 테스트")
+	void saveAnswerTest() {
+		// given
+		User questionWriter = userGenerator.savedUser();
+		Question question = questionGenerator.savedQuestion(questionWriter);
+		User answerWriter = userGenerator.savedUser(UserGenerator.answerWriter());
+		Answer answer = answerGenerator.savedAnswer(answerWriter, question, "answer contents");
+
+		DeleteHistory deleteHistory =
+			new DeleteHistory(ContentType.ANSWER, answer.getId(), answerWriter, LocalDateTime.now());
+
+		// when
+		DeleteHistory actual = deleteHistoryRepository.save(deleteHistory);
+
+		// then
+		assertThat(actual).isNotNull();
 	}
 
 	@Test
 	@DisplayName("삭제 이력 저장 후 조회 테스트")
 	void findByIdTest() {
-		DeleteHistory deleteHistory1 = deleteHistoryRepository.save(D1);
-		DeleteHistory deleteHistory2 = deleteHistoryRepository.save(D2);
+		// given
+		User questionWriter = userGenerator.savedUser();
+		Question question = questionGenerator.savedQuestion(questionWriter);
+		DeleteHistory questionDeleteHistory =
+			new DeleteHistory(ContentType.QUESTION, question.getId(), questionWriter, LocalDateTime.now());
+		DeleteHistory actualQuestionDeleteHistory = deleteHistoryRepository.save(questionDeleteHistory);
 
+		User answerWriter = userGenerator.savedUser(UserGenerator.answerWriter());
+		Answer answer = answerGenerator.savedAnswer(answerWriter, question, "answer contents");
+		DeleteHistory answerDeleteHistory =
+			new DeleteHistory(ContentType.ANSWER, answer.getId(), answerWriter, LocalDateTime.now());
+		DeleteHistory actualAnswerDeleteHistory = deleteHistoryRepository.save(answerDeleteHistory);
+
+		// when
 		List<DeleteHistory> deleteHistories = deleteHistoryRepository.findAll();
 
-		assertThat(deleteHistories).hasSize(2);
+		// then
+		assertThat(deleteHistories).hasSize(2)
+				.containsExactly(actualQuestionDeleteHistory, actualAnswerDeleteHistory);
 	}
-
-	@Test
-	@DisplayName("삭제 이력 저장 후 삭제 테스트")
-	void deleteTest() {
-		DeleteHistory deleteHistory1 = deleteHistoryRepository.save(D1);
-		DeleteHistory deleteHistory2 = deleteHistoryRepository.save(D2);
-
-		deleteHistoryRepository.delete(deleteHistory1);
-
-		List<DeleteHistory> deleteHistories = deleteHistoryRepository.findAll();
-
-		assertAll(
-			() -> assertThat(deleteHistories).hasSize(1),
-			() -> assertThat(deleteHistories).containsExactly(deleteHistory2)
-		);
-	}
-
 }
