548b8530a4a96bc43b26334a4dd51509c52c865c
taewonh <40269387+taewonh@users.noreply.github.com>
2022-11-04 11:25:28 +0900

    refactor(BaseEntity): Jpa Auditing 적용

--- src/main/java/qna/domain/DateEntity.java
+++ src/main/java/qna/domain/DateEntity.java
@@ -2,14 +2,21 @@ package qna.domain;
 
 import java.time.LocalDateTime;
 import javax.persistence.Column;
+import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class DateEntity extends BaseEntity {
 
     @Column(nullable = false, updatable = false)
-    protected final LocalDateTime createdAt = LocalDateTime.now();
+    @CreatedDate
+    protected LocalDateTime createdAt;
 
     @Column(nullable = true)
+    @LastModifiedDate
     protected LocalDateTime updatedAt;
 }
