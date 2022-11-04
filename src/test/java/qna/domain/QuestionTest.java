20e923ddda79b4e3d5857f80fde37f30a557ea39
sixthou <justyuk@naver.com>
2022-11-05 02:22:25 +0900

    [sixthou] - Step1 PR 리뷰 부탁드립니다! (#594)
    * docs : README 작성
    
    * feat : jpa 옵션 추가 설정
    
    * feat : BaseEntity 생성
    - 생성일자, 업데이트일자 공통 Entity생성 및 자동 저장을 위해 Auditing 설정
    - Auditing 기능 화성화
    
    * feat : Answer 엔티티 맵핑
    - BaseEntity 상속 처리
    
    * feat : DeleteHistory 엔티티 매핑
    
    * feat : Question 엔티티 매핑
    - BaseEntity 상속처리
    
    * feat : User 엔티니매핑
    - BaseEntity 상속처리
    
    * style : 컨벤션 정리
    
    * docs : Answer, AnswerRepository 테스트 목록 작성
    
    * test : Answer 테스트 작성
    
    * test : AnswerRepositoryTest 테스트 작성
    
    * test : DeleteHistory 테스트 작성
    
    * test : DeleteHistoryRepositoryTest 테스트 작성
    - DeleteHistoryTest - 테스트용 전역 변수 선언
    - DeleteHistory - 조회를 위한 getId 메서드 추가
    
    * docs : Question, QuestionRepository 테스트 항목 작성
    
    * test : Question 테스트 작성
    
    * test : QuestionRepository 테스트 작성
    
    * docs : User 관련 테스트 작성
    
    * test : UserTest 테스트 작성
    
    * feat : properties 로깅 옵션 추가
    - sql 실행시 실 변수 확인 가능하도록 추가
    
    * test : UserRepositoryTest 테스트 작성
    
    * refactor : DeleteHistory 피드백 반영
    - ContentType : Enum타입에 맞게 변경
    - equals() : 엔티티의 경우 id 값의 유니크함이 보장되기 때문에 id값으로만 변경되게 수정
    
    * refactor : Repository 패키지 분리

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,21 @@
 package qna.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+    @DisplayName("Question의 작성자를 변경할 수 있다.")
+    @Test
+    void write_by() {
+        Question actual = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
+
+        actual.writeBy(UserTest.SANJIGI);
+
+        assertThat(actual.isOwner(UserTest.SANJIGI)).isTrue();
+    }
 }
