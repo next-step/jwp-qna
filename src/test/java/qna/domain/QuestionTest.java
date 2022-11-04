d5221c5aa7a610660cb8bbecacc52564f0ae54fa
lunatic <lsklsk45@naver.com>
2022-11-04 17:05:25 +0900

    refactor(Question) : Question 클래스 entity 매핑 작업

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,19 @@
 package qna.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+    @Test
+    @DisplayName("질문 작성자 동일한지 테스트")
+    void is_owner_check() {
+        Question question = new Question("test", "test");
+        question.writeBy(UserTest.JAVAJIGI);
+        assertTrue(question.isOwner(UserTest.JAVAJIGI));
+    }
 }
