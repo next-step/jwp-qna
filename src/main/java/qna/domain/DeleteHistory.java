8598c6d98e08d0750d1a2de5001add3a439a5b79
velcronicity <velcronicity@gmail.com>
2022-11-05 00:15:54 +0900

    refactor: style guide(리뷰)
    ide가 자동생성해준 코드도 컨벤션 유지

--- src/main/java/qna/domain/DeleteHistory.java
+++ src/main/java/qna/domain/DeleteHistory.java
@@ -54,10 +54,12 @@ public class DeleteHistory {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o)
+        if (this == o) {
             return true;
-        if (o == null || getClass() != o.getClass())
+        }
+        if (o == null || getClass() != o.getClass()) {
             return false;
+        }
         DeleteHistory that = (DeleteHistory)o;
         return Objects.equals(id, that.id) &&
             contentType == that.contentType &&
