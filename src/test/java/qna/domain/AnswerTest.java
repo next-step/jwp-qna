923cdf3c9e03c908cf2a3bec50bca37436bc10bf
choiyongseok90 <badaelephant>
2022-11-04 11:53:52 +0900

    test: 기본 엔티티에 대한 테스트 코드를 작성한다. - AnswerTest - DeleteHistoryTest - QuestionTest - UserTest

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,6 +1,51 @@
 package qna.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import qna.NotFoundException;
+import qna.UnAuthorizedException;
+
 public class AnswerTest {
+
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+
+    @DisplayName("답변 생성시 작성자가 없으면 UnAuthorizedException 발생시킨다")
+    @Test
+    void createAnswer_withoutWriter_exception() {
+        assertThatThrownBy(() -> new Answer(null, QuestionTest.Q1, "contents"))
+                .isInstanceOf(UnAuthorizedException.class);
+    }
+
+    @DisplayName("답변 생성시 질문이 없으면 NotFoundException 발생시킨다")
+    @Test
+    void createAnswer_withoutQuestion_exception() {
+        assertThatThrownBy(() -> new Answer(UserTest.JAVAJIGI, null, "contents"))
+                .isInstanceOf(NotFoundException.class);
+    }
+
+    @DisplayName("답변의 작성자가 주어진 작성자가 맞는지 확인할 수 있다")
+    @Test
+    void isOwner_test() {
+        Answer answer = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "contents");
+        assertAll(
+                () -> assertTrue(answer.isOwner(UserTest.JAVAJIGI)),
+                () -> assertFalse(answer.isOwner(UserTest.SANJIGI))
+        );
+    }
+
+    @DisplayName("답변에 해당하는 질문을 변경할 수 있다")
+    @Test
+    void toQuestion_test() {
+        Answer answer = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "contents");
+        answer.toQuestion(QuestionTest.Q2);
+        assertThat(answer.getQuestionId()).isEqualTo(QuestionTest.Q2.getId());
+    }
 }
--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,44 @@
 package qna.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class QuestionTest {
+
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+    @DisplayName("질문의 작성자를 변경할수 있다")
+    @Test
+    void writeBy_test() {
+        Question question = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
+
+        question.writeBy(UserTest.SANJIGI);
+
+        assertEquals(question.getWriterId(), UserTest.SANJIGI.getId());
+    }
+
+    @DisplayName("질문의 작성자를 확인할 수 있다")
+    @Test
+    void isOwner_test() {
+        Question question = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
+        assertTrue(question.isOwner(UserTest.JAVAJIGI));
+    }
+
+    @DisplayName("질문의 답변을 변경할 수 있다")
+    @Test
+    void addAnswer_test() {
+        // given
+        Question question1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
+        Question question2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+        Answer answer = new Answer(UserTest.JAVAJIGI, question1, "Answers Contents1");
+        // when
+        question2.addAnswer(answer);
+        // then
+        assertThat(answer.getQuestionId()).isEqualTo(question2.getId());
+    }
 }
