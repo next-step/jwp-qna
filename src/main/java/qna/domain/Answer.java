85d9edc80688c300bc831c2f042c1e417ad585af
giraffelim <taeyang7704@naver.com>
2022-11-05 20:58:54 +0900

    feature(Answer): equals & hashCode 오버라이딩

--- src/main/java/qna/domain/Answer.java
+++ src/main/java/qna/domain/Answer.java
@@ -97,6 +97,24 @@ public class Answer extends BaseEntity {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Answer answer = (Answer) o;
+        return Objects.equals(id, answer.id) && Objects.equals(writerId, answer.writerId)
+                && Objects.equals(questionId, answer.questionId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, writerId, questionId);
+    }
+
+    @Override
     public String toString() {
         return "Answer{" +
                 "id=" + id +
