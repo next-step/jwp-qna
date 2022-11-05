977d1e89efeacbc612d384679ab2ff6b64494633
ilmare-cbk <cbk104325@gmail.com>
2022-11-05 22:45:24 +0900

    test(test package): 도메인 모델에 대한 테스트 작성

--- src/main/java/qna/domain/User.java
+++ src/main/java/qna/domain/User.java
@@ -6,11 +6,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import qna.UnAuthorizedException;
 
 @Entity
 public class User extends BaseTimeEntity {
-    public static final GuestUser GUEST_USER = new GuestUser();
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -37,40 +35,6 @@ public class User extends BaseTimeEntity {
         this.password = password;
         this.name = name;
         this.email = email;
-    }
-
-    public void update(User loginUser, User target) {
-        if (!matchUserId(loginUser.userId)) {
-            throw new UnAuthorizedException();
-        }
-
-        if (!matchPassword(target.password)) {
-            throw new UnAuthorizedException();
-        }
-
-        this.name = target.name;
-        this.email = target.email;
-    }
-
-    private boolean matchUserId(String userId) {
-        return this.userId.equals(userId);
-    }
-
-    public boolean matchPassword(String targetPassword) {
-        return this.password.equals(targetPassword);
-    }
-
-    public boolean equalsNameAndEmail(User target) {
-        if (Objects.isNull(target)) {
-            return false;
-        }
-
-        return name.equals(target.name) &&
-                email.equals(target.email);
-    }
-
-    public boolean isGuestUser() {
-        return false;
     }
 
     public Long getId() {
@@ -109,10 +73,4 @@ public class User extends BaseTimeEntity {
                 '}';
     }
 
-    private static class GuestUser extends User {
-        @Override
-        public boolean isGuestUser() {
-            return true;
-        }
-    }
 }
--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,32 @@
 package qna.domain;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+    @Test
+    @DisplayName("질문 작성자 확인 테스트")
+    void owner() {
+        Question question = new Question(1L, "title", "contents").writeBy(UserTest.JAVAJIGI);
+        Assertions.assertThat(question.isOwner(UserTest.JAVAJIGI)).isTrue();
+    }
+
+    @Test
+    @DisplayName("삭제 여부 true 테스트")
+    void delete() {
+        Question question = new Question(1L, "title", "contents");
+        question.deleted();
+        Assertions.assertThat(question.isDeleted()).isTrue();
+    }
+
+    @Test
+    @DisplayName("삭제 여부 false 테스트")
+    void delete2() {
+        Question question = new Question(1L, "title", "contents");
+        Assertions.assertThat(question.isDeleted()).isFalse();
+    }
 }
