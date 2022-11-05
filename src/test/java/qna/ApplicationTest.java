f59406e3cec5cca3a4c9a898c03159b742fc8933
baeksoo <kyubeom03@naver.com>
2022-11-05 13:19:14 +0900

    fix: 테스트 레벨에서만 show-sql 활성화

--- src/test/java/qna/ApplicationTest.java
+++ src/test/java/qna/ApplicationTest.java
@@ -2,8 +2,10 @@ package qna;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "/application-data.properties")
 class ApplicationTest {
     @Test
     void contextLoads() {
