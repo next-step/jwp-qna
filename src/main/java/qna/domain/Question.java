1fe9e6b3a7968ceb0be4bf91278e0157c3cd173e
Minjae Kim <minjae.kim@mytrum.com>
2022-11-05 17:05:47 +0900

    feat: 질문 도메인 엔티티 매핑
    * ```Question``` 엔티티 매핑
    * 테스트 케이스 생성

--- src/main/java/qna/domain/Question.java
+++ src/main/java/qna/domain/Question.java
@@ -1,11 +1,43 @@
 package qna.domain;
 
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "question")
+@EntityListeners(AuditingEntityListener.class)
 public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "title", length = 100, nullable = false)
     private String title;
+    @Lob
+    @Column(name = "contents")
     private String contents;
+    @Column(name = "writer_id")
     private Long writerId;
+    @Column(name = "deleted", nullable = false)
     private boolean deleted = false;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 
     public Question(String title, String contents) {
         this(null, title, contents);
@@ -15,6 +47,16 @@ public class Question {
         this.id = id;
         this.title = title;
         this.contents = contents;
+    }
+
+    public Question(Long id, String title, String contents, LocalDateTime createdAt) {
+        this.id = id;
+        this.title = title;
+        this.contents = contents;
+        this.createdAt = createdAt;
+    }
+
+    protected Question() {
     }
 
     public Question writeBy(User writer) {
