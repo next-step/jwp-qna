939c1a501384209e8c721d4cf40a35e18110d383
baeksoo <kyubeom03@naver.com>
2022-11-05 23:59:37 +0900

    fix: 기본 설정 어노테이션 제거

--- src/test/java/qna/ApplicationTest.java
+++ src/test/java/qna/ApplicationTest.java
@@ -2,10 +2,8 @@ package qna;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@TestPropertySource(locations = "/application.properties")
 class ApplicationTest {
     @Test
     void contextLoads() {
