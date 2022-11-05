b05a3fb3a48acd600e3a446de97b22edde7ee391
earth-h <earth.h.183@gmail.com>
2022-11-05 17:51:56 +0900

    refactor(service): deleteQuestion() 메소드 리팩토링
    QnaService
    - question.delete()의 반환값을 바로 deleteHistoryService.saveAll()의 인자값으로 사용하도록 리팩토링

--- src/main/java/qna/service/QnaService.java
+++ src/main/java/qna/service/QnaService.java
@@ -1,12 +1,10 @@
 package qna.service;
 
-import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import qna.NotFoundException;
-import qna.domain.DeleteHistory;
 import qna.domain.Question;
 import qna.domain.User;
 import qna.repository.QuestionRepository;
@@ -32,7 +30,6 @@ public class QnaService {
     @Transactional
     public void deleteQuestion(User loginUser, Long questionId) {
         Question question = findQuestionById(questionId);
-        List<DeleteHistory> deleteHistories = question.delete(loginUser);
-        deleteHistoryService.saveAll(deleteHistories);
+        deleteHistoryService.saveAll(question.delete(loginUser));
     }
 }
