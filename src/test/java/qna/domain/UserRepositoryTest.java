c7da5487df4f76264544c604b3598b0277e22428
Haseop Lee <haservi@naver.com>
2022-11-05 20:53:45 +0900

    style(Import): 사용하지 않는 import 제거

--- src/test/java/qna/domain/UserRepositoryTest.java
+++ src/test/java/qna/domain/UserRepositoryTest.java
@@ -1,7 +1,5 @@
 package qna.domain;
 
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -10,7 +8,6 @@ import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 import java.util.stream.Stream;
