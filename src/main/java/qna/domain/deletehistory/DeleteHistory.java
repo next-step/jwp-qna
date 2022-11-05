3f95bc88e91f2abd5c33a81ddb7f9c49312f913f
sonyoon7 <sonyoon7@gmail.com>
2022-11-05 22:15:05 +0900

    refactor (DeleteHistory) ManyToOne 옵션 추가

--- src/main/java/qna/domain/deletehistory/DeleteHistory.java
+++ src/main/java/qna/domain/deletehistory/DeleteHistory.java
@@ -20,7 +20,7 @@ public class DeleteHistory {
 
     private Long contentId;
 
-    @ManyToOne
+    @ManyToOne(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
     @JoinColumn(name = "deleted_by_id", foreignKey = @ForeignKey(name = "fk_delete_history_to_user"))
     private User deletedBy;
 
