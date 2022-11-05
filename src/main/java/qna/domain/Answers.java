8700b7d6413839df26f6a7cb2c199dfbdc610641
ilmare-cbk <cbk104325@gmail.com>
2022-11-06 04:33:46 +0900

    test(AnswersTest.java): Answers 의 삭제 기능 테스트 추가
    - 질문자와 딥변자가 다른 경우 예외발생 테스트 추가
    - 질문자와 모든 답변자가 같은 경우 삭제 가능 테스트 추가

--- src/main/java/qna/domain/Answers.java
+++ src/main/java/qna/domain/Answers.java
@@ -51,6 +51,10 @@ public class Answers {
         values.forEach(Answer::deleted);
     }
 
+    public boolean isAllDeleted() {
+        return values.stream().allMatch(Answer::isDeleted);
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) {
--- src/test/java/qna/domain/AnswersTest.java
+++ src/test/java/qna/domain/AnswersTest.java
@@ -7,6 +7,7 @@ import java.util.Arrays;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
 
 class AnswersTest {
 
@@ -63,6 +64,29 @@ class AnswersTest {
         Answers expected = Answers.init();
 
         Assertions.assertThat(actual).isNotEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("질문자와 답변자가 다른 경우 삭제 불가 (예외발생)")
+    void deleteException() {
+        User questionWriter = UserTest.SANJIGI;
+        Answer a1 = new Answer(1L, UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
+        Answer a2 = new Answer(2L, UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
+        Answers answers = Answers.from(Arrays.asList(a1, a2));
+
+        Assertions.assertThatThrownBy(() -> answers.deleted(questionWriter))
+                .isInstanceOf(CannotDeleteException.class);
+    }
+
+    @Test
+    @DisplayName("질문자와 답변자가 모두 같은 경우 삭제 가능")
+    void delete() throws CannotDeleteException {
+        User questionWriter = UserTest.JAVAJIGI;
+        Answers answers = Answers.from(Arrays.asList(AnswerTest.A1));
+
+        answers.deleted(questionWriter);
+
+        Assertions.assertThat(answers.isAllDeleted()).isTrue();
     }
 
     private Answers getAnswers() {
