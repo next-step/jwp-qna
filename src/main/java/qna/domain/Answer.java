c6be7340ea6b3cda6497f26ab7ef570ae8630fb6
jimbaemon <jimbae@kakao.com>
2022-11-06 01:43:04 +0900

    feat: 답변 삭제 정의.

--- src/main/java/qna/domain/Answer.java
+++ src/main/java/qna/domain/Answer.java
@@ -1,29 +1,22 @@
 package qna.domain;
 
+import qna.CannotDeleteException;
 import qna.NotFoundException;
 import qna.UnAuthorizedException;
 
 import java.time.LocalDateTime;
 import java.util.Objects;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.Lob;
-import javax.persistence.ManyToOne;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
+import javax.persistence.*;
 
 @Entity
 public class Answer extends BaseEntity{
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private Long writerId;
-	private Long questionId;
+	@OneToOne
+	@JoinColumn(name = "writer_id")
+	private User writer;
 	@Lob
 	private String contents;
 	private boolean deleted = false;
@@ -46,62 +39,27 @@ public class Answer extends BaseEntity{
 			throw new NotFoundException();
 		}
 
-		this.writerId = writer.getId();
-		this.questionId = question.getId();
+		this.writer = writer;
 		this.contents = contents;
 	}
 
-	public boolean isOwner(User writer) {
-		return this.writerId.equals(writer.getId());
-	}
-
-	public void toQuestion(Question question) {
-		this.questionId = question.getId();
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public Long getWriterId() {
-		return writerId;
-	}
-
-	public void setWriterId(Long writerId) {
-		this.writerId = writerId;
-	}
-
-	public Long getQuestionId() {
-		return questionId;
-	}
-
-	public void setQuestionId(Long questionId) {
-		this.questionId = questionId;
-	}
-
-	public String getContents() {
-		return contents;
-	}
-
-	public void setContents(String contents) {
-		this.contents = contents;
+	public DeleteHistory delete(final User loginUser) throws CannotDeleteException {
+		if (!writer.equals(loginUser)) {
+			throw new CannotDeleteException("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
+		}
+		this.deleted = true;
+		return new DeleteHistory(ContentType.ANSWER, id, loginUser.getId(), LocalDateTime.now());
 	}
 
 	public boolean isDeleted() {
 		return deleted;
 	}
 
-	public void setDeleted(boolean deleted) {
-		this.deleted = deleted;
+	public Long getId() {
+		return id;
 	}
 
-	@Override
-	public String toString() {
-		return "Answer{" + "id=" + id + ", writerId=" + writerId + ", questionId=" + questionId + ", contents='"
-			+ contents + '\'' + ", deleted=" + deleted + '}';
+	public Long getWriterId() {
+		return writer.getId();
 	}
 }
