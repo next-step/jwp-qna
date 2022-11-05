343ab9ef8323d4c5729e681cab3d8dd276b95f84
Minjae Kim <minjae.kim@mytrum.com>
2022-11-05 17:04:47 +0900

    feat: 답변 도메인 엔티티 매핑
    * Answer.class Entity 매핑
    * TestCase 생성

--- src/main/java/qna/domain/Answer.java
+++ src/main/java/qna/domain/Answer.java
@@ -1,16 +1,47 @@
 package qna.domain;
 
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import qna.NotFoundException;
 import qna.UnAuthorizedException;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
 import java.util.Objects;
 
+@Entity
+@Table(name = "answer")
+@EntityListeners(AuditingEntityListener.class)
 public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "writer_id")
     private Long writerId;
+    @Column(name = "question_id")
     private Long questionId;
+    @Lob
+    @Column(name = "contents")
     private String contents;
+
+    @Column(name = "deleted", nullable = false)
     private boolean deleted = false;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 
     public Answer(User writer, Question question, String contents) {
         this(null, writer, question, contents);
@@ -30,6 +61,9 @@ public class Answer {
         this.writerId = writer.getId();
         this.questionId = question.getId();
         this.contents = contents;
+    }
+
+    protected Answer() {
     }
 
     public boolean isOwner(User writer) {
@@ -80,6 +114,14 @@ public class Answer {
         this.deleted = deleted;
     }
 
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
     @Override
     public String toString() {
         return "Answer{" +
@@ -89,5 +131,25 @@ public class Answer {
                 ", contents='" + contents + '\'' +
                 ", deleted=" + deleted +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Answer answer = (Answer) o;
+        return deleted == answer.deleted && Objects.equals(id, answer.id) && Objects.equals(writerId,
+                answer.writerId) && Objects.equals(questionId, answer.questionId) && Objects.equals(
+                contents, answer.contents) && Objects.equals(createdAt, answer.createdAt)
+                && Objects.equals(updatedAt, answer.updatedAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, writerId, questionId, contents, deleted, createdAt, updatedAt);
     }
 }
--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,6 +1,72 @@
 package qna.domain;
 
-public class AnswerTest {
-    public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
-    public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatNoException;
+
+@DisplayName("answer 엔티티 테스트")
+@DataJpaTest
+class AnswerTest {
+    public static final Answer A1 = new Answer(1L, UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
+    public static final Answer A2 = new Answer(2L, UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @Autowired
+    private AnswerRepository answerRepository;
+
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    @DisplayName("save 성공")
+    @Test
+    void save_answer_success() {
+        assertThatNoException().isThrownBy(() -> answerRepository.save(A1));
+    }
+
+    @DisplayName("createdAt 자동 생성 여부 테스트")
+    @Test
+    void save_answer_createdAt() {
+        //given, when:
+        final Answer answer = answerRepository.save(A1);
+        //then:
+        assertThat(answer.getCreatedAt()).isNotNull();
+    }
+
+    @DisplayName("updatedAt 데이터 변경 여부 테스트")
+    @Test
+    void save_answer_changedUpdatedAt() {
+        //given:
+        final Answer answer = answerRepository.save(A1);
+        final LocalDateTime before = answer.getUpdatedAt();
+        //when:
+        answer.setContents("modified");
+        final Answer updatedAnswer = answerRepository.save(answer);
+        testEntityManager.flush();
+        //then:
+        assertThat(updatedAnswer.getUpdatedAt()).isAfter(before);
+    }
+
+    @DisplayName("findByQuestionIdAndDeletedFalse 메서드 테스트")
+    @Test
+    void findByQuestionIdAndDeletedFalse_answer_success() {
+        //given:
+        Answer answer = answerRepository.save(A1);
+        //when, then:
+        assertThat(answerRepository.findByQuestionIdAndDeletedFalse(answer.getQuestionId())).contains(answer);
+    }
+
+    @DisplayName("findByIdAndDeletedFalse 메서드 테스트")
+    @Test
+    void findByIdAndDeletedFalse_answer_success() {
+        //given:
+        Answer answer = answerRepository.save(A1);
+        //when, then:
+        assertThat(answerRepository.findByIdAndDeletedFalse(answer.getId())).containsSame(answer);
+    }
 }
