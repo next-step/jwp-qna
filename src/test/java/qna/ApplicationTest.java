609ec4161199a354f6692368bf7f8e219b42d793
baeksoo <kyubeom03@naver.com>
2022-11-05 16:08:32 +0900

    fix: 프로퍼티명 수정

--- src/test/java/qna/ApplicationTest.java
+++ src/test/java/qna/ApplicationTest.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@TestPropertySource(locations = "/application-data.properties")
+@TestPropertySource(locations = "/application.properties")
 class ApplicationTest {
     @Test
     void contextLoads() {
