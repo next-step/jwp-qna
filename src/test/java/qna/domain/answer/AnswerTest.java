c17d4a6f666c33492794a53f01d501b9d457a326
hybrid.simpler@gmail.com <hybrid.simpler@gmail.com>
2022-11-06 15:10:50 +0900

    fix: 누락된 어노테이션 추가

--- src/test/java/qna/domain/answer/AnswerTest.java
+++ src/test/java/qna/domain/answer/AnswerTest.java
@@ -1,8 +1,15 @@
 package qna.domain.answer;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.DirtiesContext;
 import qna.domain.question.QuestionTest;
 import qna.domain.user.User;
 
+@DirtiesContext
+@DataJpaTest
 public class AnswerTest {
 
     public static Answer createAnswer(User writeUser) {
