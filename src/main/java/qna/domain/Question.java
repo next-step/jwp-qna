dd59b5e7d2d51f5288e0651ae04acf7993b43a6b
giraffelim <taeyang7704@naver.com>
2022-11-06 21:54:03 +0900

    refactor(Question): setDeleted 메소드를 delete 메소드로 변경

--- src/main/java/qna/domain/Question.java
+++ src/main/java/qna/domain/Question.java
@@ -71,8 +71,8 @@ public class Question extends BaseEntity {
         return deleted;
     }
 
-    public void setDeleted(boolean deleted) {
-        this.deleted = deleted;
+    public void delete() {
+        this.deleted = true;
     }
 
     public User getWriter() {
