0a8a4ae71d1e1113e5fd9c58657e72c49820431d
jimbaemon <jimbae@kakao.com>
2022-11-06 19:14:32 +0900

    fix: 질문 조회후 삭제 테스트 추가.

--- src/test/java/qna/domain/QuestionRepositoryTest.java
+++ src/test/java/qna/domain/QuestionRepositoryTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import javax.persistence.EntityManager;
+
 import static qna.domain.AnswerTest.A1;
 import static qna.domain.QuestionTest.Q1;
 import static qna.domain.UserTest.JAVAJIGI;
@@ -16,12 +18,21 @@ class QuestionRepositoryTest {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private EntityManager entityManager;
 
     @Test
-    void 답변_저장() {
+    void 답변_저장_조회후_삭제() {
         userRepository.save(JAVAJIGI);
         Q1.addAnswer(A1);
         questionRepository.save(Q1);
+
+        entityManager.clear();
+
+        Question question = questionRepository.findById(Q1.getId()).get();
+        question.delete(JAVAJIGI);
+
+        entityManager.flush();
     }
 
 }
