6575fb1fe953194b3873b07a125e930ecc5007dd
iamsojung <sojungkid@gmail.com>
2022-11-06 21:36:15 +0900

    refactor: properties 추가,answer 코드정리

--- src/main/java/qna/domain/Answer.java
+++ src/main/java/qna/domain/Answer.java
@@ -1,31 +1,25 @@
 package qna.domain;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Lob;
 import qna.NotFoundException;
 import qna.UnAuthorizedException;
 
+import javax.persistence.*;
 import java.util.Objects;
 
 @Entity
 public class Answer extends BaseEntity {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private Long writerId;
     private Long questionId;
-
     @Lob
     private String contents;
-
     @Column(nullable = false)
     private boolean deleted = false;
+
+    protected Answer() {
+    }
 
     public Answer(User writer, Question question, String contents) {
         this(null, writer, question, contents);
@@ -45,10 +39,6 @@ public class Answer extends BaseEntity {
         this.writerId = writer.getId();
         this.questionId = question.getId();
         this.contents = contents;
-    }
-
-    protected Answer() {
-
     }
 
     public boolean isOwner(User writer) {
@@ -108,25 +98,5 @@ public class Answer extends BaseEntity {
             ", contents='" + contents + '\'' +
             ", deleted=" + deleted +
             '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-
-        Answer answer = (Answer) o;
-        return Objects.equals(id, answer.id) && Objects.equals(writerId, answer.writerId)
-            && Objects.equals(questionId, answer.questionId);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, writerId, questionId);
     }
 }
