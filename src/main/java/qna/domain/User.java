dd2904bee55c55c52782725700dfb192577b21bd
jisu1211 <dbdnzl1211@gmail.com>
2022-11-06 13:08:24 +0900

    refactor: User 클래스 리팩터링 - 사용하지 않는 getter, setter 제거 - 데이터 입력/수정에 대한 validation 추가 - 생성자 protected로 변경
    test: UserTest, UserRepositoryTest 수정

--- src/main/java/qna/domain/User.java
+++ src/main/java/qna/domain/User.java
@@ -12,7 +12,6 @@ import java.util.Objects;
         columnNames = {"user_id"}
 )})
 public class User extends AuditingEntity {
-    public static final GuestUser GUEST_USER = new GuestUser();
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -30,13 +29,15 @@ public class User extends AuditingEntity {
     @Column(length = 50)
     private String email;
 
-    public User() {}
+    protected User() {}
 
     public User(String userId, String password, String name, String email) {
         this(null, userId, password, name, email);
     }
 
     public User(Long id, String userId, String password, String name, String email) {
+        validateForInserts(userId, password, name);
+
         this.id = id;
         this.userId = userId;
         this.password = password;
@@ -44,25 +45,52 @@ public class User extends AuditingEntity {
         this.email = email;
     }
 
-    public void update(User loginUser, User target) {
-        if (!matchUserId(loginUser.userId)) {
-            throw new UnAuthorizedException();
-        }
+    private void validateForInserts(String userId, String password, String name) {
+        validateUserId(userId);
+        validatePassword(password);
+        validateName(name);
+    }
 
-        if (!matchPassword(target.password)) {
-            throw new UnAuthorizedException();
+    private void validateUserId(String userId) {
+        if(Objects.isNull(userId)) {
+            throw new IllegalArgumentException();
         }
+    }
+
+    private void validatePassword(String password) {
+        if(Objects.isNull(password)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateName(String name) {
+        if(Objects.isNull(name)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void update(User loginUser, User target) {
+        validateForUpdates(loginUser, target);
 
         this.name = target.name;
         this.email = target.email;
     }
 
-    private boolean matchUserId(String userId) {
-        return this.userId.equals(userId);
+    private void validateForUpdates(User loginUser, User target) {
+        matchUserId(loginUser.userId);
+        matchPassword(loginUser.password);
     }
 
-    public boolean matchPassword(String targetPassword) {
-        return this.password.equals(targetPassword);
+    private void matchUserId(String userId) {
+        if(!this.userId.equals(userId)) {
+            throw new UnAuthorizedException();
+        }
+    }
+
+    private void matchPassword(String targetPassword) {
+        if(!this.password.equals(targetPassword)) {
+            throw new UnAuthorizedException();
+        }
     }
 
     public boolean equalsNameAndEmail(User target) {
@@ -74,48 +102,16 @@ public class User extends AuditingEntity {
                 email.equals(target.email);
     }
 
-    public boolean isGuestUser() {
-        return false;
-    }
-
     public Long getId() {
         return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public String getUserId() {
         return userId;
     }
 
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
     @Override
@@ -135,23 +131,5 @@ public class User extends AuditingEntity {
     @Override
     public int hashCode() {
         return Objects.hash(id, userId);
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", userId='" + userId + '\'' +
-                ", password='" + password + '\'' +
-                ", name='" + name + '\'' +
-                ", email='" + email + '\'' +
-                '}';
-    }
-
-    private static class GuestUser extends User {
-        @Override
-        public boolean isGuestUser() {
-            return true;
-        }
     }
 }
--- src/test/java/qna/domain/UserRepositoryTest.java
+++ src/test/java/qna/domain/UserRepositoryTest.java
@@ -1,10 +1,15 @@
 package qna.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import qna.domain.content.*;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -17,50 +22,137 @@ class UserRepositoryTest {
     @Autowired
     private UserRepository userRepository;
 
-    @Test
-    void user_저장_테스트() {
-        User actual = userRepository.save(UserTest.JAVAJIGI);
+    @Autowired
+    private QuestionRepository questionRepository;
 
-        assertAll(
-                () -> assertThat(actual.getId()).isNotNull(),
-                () -> assertEquals("javajigi", actual.getUserId()),
-                () -> assertEquals("password", actual.getPassword()),
-                () -> assertEquals("name", actual.getName()),
-                () -> assertEquals("javajigi@slipp.net", actual.getEmail()),
-                () -> assertThat(actual.getCreatedAt()).isNotNull(),
-                () -> assertThat(actual.getUpdatedAt()).isNotNull()
-        );
+    @Autowired
+    private AnswerRepository answerRepository;
+
+    @Autowired
+    private DeleteHistoryRepository deleteHistoryRepository;
+
+    @Autowired
+    private TestEntityManager manager;
+
+    private User user;
+
+    @BeforeEach
+    void setUp() {
+        user = new User("user1", "password", "username1", "user1@test.com");
     }
 
     @Test
-    void user_동일성_보장_테스트() {
-        User userSaved = userRepository.save(UserTest.JAVAJIGI);
-        User userFound = userRepository.findById(userSaved.getId()).get();
+    @DisplayName("사용자를 저장한다")
+    void save() {
+        User actual = userRepository.save(user);
+
+        assertThat(actual.getId()).isNotNull();
+        assertEquals("user1", actual.getUserId());
+    }
+
+    @Test
+    @DisplayName("UserId로 사용자를 검색한다")
+    void findByUserId() {
+        userRepository.save(user);
+        User actual = userRepository.findByUserId("user1").get();
+
+        assertThat(actual).isNotNull();
+    }
+
+    @Test
+    @DisplayName("User entity의 동일성을 확인한다")
+    void identity() {
+        User userSaved = userRepository.save(user);
+        User userFound = userRepository.findById(user.getId()).get();
 
         assertTrue(userSaved == userFound);
     }
 
     @Test
-    void user_업데이트_테스트() {
-        User userSaved = userRepository.save(
-                new User("user1", "password", "user1", "user1@test.com")
-        );
-        userSaved.setName("userName_changed");
+    @DisplayName("사용자를 수정한다(JPQL)")
+    void update() {
+        User userSaved = userRepository.save(user);
+        userSaved.update(userSaved,
+                new User("user1", "password", "changeName", "user1@test.com"));
 
-        userRepository.saveAndFlush(userSaved);
+        User userFound = userRepository.findByUserId("user1").get();
 
         assertAll(
-                () -> assertEquals("userName_changed", userSaved.getName()),
+                () -> assertThat(userFound).isNotNull(),
+                () -> assertEquals("changeName", userFound.getName()),
+                () -> assertTrue(userFound.getUpdatedAt().isAfter(userFound.getCreatedAt()))
+        );
+    }
+
+    @Test
+    @DisplayName("사용자를 수정한다(flush)")
+    void updateAndFlush() {
+        User userSaved = userRepository.save(user);
+        userSaved.update(userSaved,
+                new User("user1", "password", "changeName", "user1@test.com"));
+
+        userRepository.flush();
+        assertAll(
+                () -> assertEquals("changeName", userSaved.getName()),
                 () -> assertTrue(userSaved.getUpdatedAt().isAfter(userSaved.getCreatedAt()))
         );
     }
 
     @Test
-    void userId로_user_검색() {
-        User userSaved = userRepository.save(UserTest.JAVAJIGI);
-        User userFound = userRepository.findByUserId(userSaved.getUserId()).get();
+    @DisplayName("사용자를 삭제한다")
+    void delete() {
+        User userSaved = userRepository.save(user);
+        userRepository.delete(userSaved);
+        userRepository.flush();
 
-        assertEquals(userSaved, userFound);
+        Optional<User> userFound = userRepository.findById(user.getId());
+
+        assertFalse(userFound.isPresent());
     }
 
+    @Test
+    @DisplayName("등록한 질문이 있는 사용자를 삭제한다")
+    void deleteWithQuestion() {
+        User userSaved = userRepository.save(user);
+        Question question = questionRepository.save(
+                new Question(userSaved, "test title", "test contents"));
+
+        questionRepository.delete(question);
+        userRepository.delete(userSaved);
+        flushAndClear();
+    }
+
+    @Test
+    @DisplayName("등록한 답변이 있는 사용자를 삭제한다")
+    void deleteWithQuestionAndAnswer() {
+        User userSaved = userRepository.save(user);
+        Question question = questionRepository.save(
+                new Question(userSaved, "test title", "test contents"));
+        answerRepository.save(
+                new Answer(userSaved, question, "test contents"));
+
+        questionRepository.delete(question);
+        userRepository.delete(userSaved);
+        flushAndClear();
+    }
+
+    @Test
+    @DisplayName("삭제 히스토리에 포함된 질문의 작성자인 사용자를 삭제한다")
+    void deleteWithQuestionAndAnswerAndDeleteHistory() {
+        User userSaved = userRepository.save(user);
+        Question question = questionRepository.save(
+                new Question(userSaved, "test title", "test contents"));
+        DeleteHistory questionDeleteHistory = deleteHistoryRepository.save(
+                new DeleteHistory(ContentType.QUESTION, question.getId(), userSaved));
+
+        deleteHistoryRepository.delete(questionDeleteHistory);
+        questionRepository.delete(question);
+        userRepository.delete(user);
+        flushAndClear();
+    }
+
+    private void flushAndClear() {
+        manager.flush();
+        manager.clear();
+    }
 }
