ce9d9d48300e69f7175bff11579589dd357ed845
earth-h <earth.h.183@gmail.com>
2022-11-06 20:29:22 +0900

    test(domain): 원시값 포장(Contents) 테스트 추가
    ContentsTest
    - 테스트코드 추가 및 테스트코드 보강

--- src/test/java/qna/domain/ContentsTest.java
+++ src/test/java/qna/domain/ContentsTest.java
@@ -19,11 +19,24 @@ public class ContentsTest {
     }
 
     @Test
-    void 컨텐츠_toString() {
+    void 컨텐츠_다름_테스트() {
         //given
         Contents actual = Contents.of("컨텐츠");
 
+        //when
+        Contents expect = Contents.of("컨텐츠 다름");
+
         //then
-        assertThat(actual.toString()).contains("Contents{contents=");
+        assertThat(actual).isNotEqualTo(expect);
+    }
+
+    @Test
+    void 컨텐츠_toString() {
+        //given
+        String actual = "컨텐츠";
+        Contents contents = Contents.of(actual);
+
+        //then
+        assertThat(contents.toString()).contains("Contents{contents='" + actual);
     }
 }
