f16605d155ca42869eae5f497e08ba7e3c2eb904
sixthou <justyuk@naver.com>
2022-11-07 20:24:56 +0900

    refactor : 미사용 메서드 삭제

--- src/main/java/qna/domain/User.java
+++ src/main/java/qna/domain/User.java
@@ -77,24 +77,12 @@ public class User extends BaseEntity {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getUserId() {
         return userId;
     }
 
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
     public String getPassword() {
         return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 
     public String getName() {
@@ -107,10 +95,6 @@ public class User extends BaseEntity {
 
     public String getEmail() {
         return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
     @Override
