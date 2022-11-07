c66d539fdb7e134dc1dd07fb87b630e58402d6c6
dongha-byun <byunsw4@naver.com>
2022-11-08 01:33:02 +0900

    Test: 리파지토리 테스트 코드 작성

---
+++ src/test/java/qna/domain/UserRepositoryTest.java
@@ -0,0 +1,38 @@
+package qna.domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+public class UserRepositoryTest {
+
+    @Autowired
+    UserRepository userRepository;
+
+
+    @Test
+    @DisplayName("아이디로 사용자 정보 조회 테스트")
+    void findByUserIdTest(){
+        User saveUser = userRepository.save(UserTest.JAVAJIGI);
+        User findUser = userRepository.findByUserId(UserTest.JAVAJIGI.getUserId())
+                .orElse(null);
+
+        assertThat(findUser).isNotNull();
+        assertThat(findUser).isEqualTo(saveUser);
+    }
+
+    @Test
+    @DisplayName("이름으로 사용자 정보 조회 테스트")
+    void findByNameTest(){
+        User saveUser = userRepository.save(UserTest.SANJIGI);
+        User findUser = userRepository.findByName(UserTest.SANJIGI.getName())
+                .orElse(null);
+
+        assertThat(findUser).isNotNull();
+        assertThat(findUser).isEqualTo(saveUser);
+    }
+}
