e4ef27e81956fc31bcf3cc1db2cc6dc036fa5bf2
sjk5766 <wjdrnr6662@gmail.com>
2022-11-07 20:56:12 +0900

    test(domain): domain에 대한 테스트 코드 추가

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -2,9 +2,14 @@ package qna.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
 import qna.NotFoundException;
 import qna.UnAuthorizedException;
+import qna.fixture.TestAnswerFactory;
+import qna.fixture.TestQuestionFactory;
+import qna.fixture.TestUserFactory;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class AnswerTest {
@@ -20,5 +25,27 @@ public class AnswerTest {
     void no_question_exception_test() {
         assertThatThrownBy(() -> new Answer(UserTest.JAVAJIGI, null, "컨텐츠"))
                 .isInstanceOf(NotFoundException.class);
+    }
+
+    @DisplayName("답변은 삭제될 수 있다")
+    @Test
+    void delete_test() throws CannotDeleteException {
+        User writer = TestUserFactory.create("서정국");
+        Question question = TestQuestionFactory.create(writer);
+        Answer answer = TestAnswerFactory.create(writer, question);
+
+        answer.delete(writer);
+
+        assertThat(answer.isDeleted()).isTrue();
+    }
+
+    @DisplayName("답변이 삭제되면 삭제이력이 반환된다")
+    @Test
+    void delete_history_test() throws CannotDeleteException {
+        User writer = TestUserFactory.create("서정국");
+        Question question = TestQuestionFactory.create(writer);
+        Answer answer = TestAnswerFactory.create(writer, question);
+
+        assertThat(answer.delete(writer)).isInstanceOf(DeleteHistory.class);
     }
 }
--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,5 +1,49 @@
 package qna.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+import qna.UnAuthorizedException;
+import qna.fixture.TestAnswerFactory;
+import qna.fixture.TestQuestionFactory;
+import qna.fixture.TestUserFactory;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
+
+    @DisplayName("질문은 삭제될 수 있다")
+    @Test
+    void delete_test() throws CannotDeleteException {
+        User writer = TestUserFactory.create("서정국");
+        Question question = TestQuestionFactory.create(writer);
+
+        question.delete(writer);
+
+        assertThat(question.isDeleted()).isTrue();
+    }
+
+    @DisplayName("질문이 삭제되면 답변도 삭제된다")
+    @Test
+    void delete_with_answer_test() throws CannotDeleteException {
+        User writer = TestUserFactory.create("서정국");
+        Question question = TestQuestionFactory.create(writer);
+        Answer answer = TestAnswerFactory.create(writer, question);
+
+        question.delete(writer);
+
+        assertThat(question.isDeleted()).isTrue();
+        assertThat(answer.isDeleted()).isTrue();
+    }
+
+    @DisplayName("질문이 삭제되면 삭제이력이 반환된다")
+    @Test
+    void delete_history_test() throws CannotDeleteException {
+        User writer = TestUserFactory.create("서정국");
+        Question question = TestQuestionFactory.create(writer);
+
+        assertThat(question.delete(writer)).isInstanceOf(DeleteHistories.class);
+    }
 }
