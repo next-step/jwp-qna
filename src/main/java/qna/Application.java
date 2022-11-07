287bf3d37cc802d4dcc4ca9752920385eb36e214
deokmoon <deok2moon@gmail.com>
2022-11-08 02:08:04 +0900

    feat(main): EnableJpaAuditing 어노테이션 적용

--- src/main/java/qna/Application.java
+++ src/main/java/qna/Application.java
@@ -2,10 +2,14 @@ package qna;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import qna.domain.Answer;
+import qna.domain.Question;
+import qna.domain.User;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
     }
 }
