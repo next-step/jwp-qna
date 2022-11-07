9c94b55169653247e9b534d964586b0fa3e7aafc
iamsojung <sojungkid@gmail.com>
2022-11-07 23:18:11 +0900

    refactor: domain 테스트 수정

--- src/test/java/qna/domain/DeleteHistoryTest.java
+++ src/test/java/qna/domain/DeleteHistoryTest.java
@@ -1,16 +1,7 @@
 package qna.domain;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import java.time.LocalDateTime;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-@DataJpaTest
 public class DeleteHistoryTest {
 
     public static final DeleteHistory questionDeleted =
--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,10 +1,6 @@
 package qna.domain;
 
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-
-@DataJpaTest
 public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
-
 }
