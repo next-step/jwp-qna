d65d24643f45db62b270c2c315f173ef59581dbc
lunatic <lsklsk45@naver.com>
2022-11-07 19:48:26 +0900

    refactor(AnswerRepository , AnswerTest) : 연관 관계 수정에 따른 테스트 코드 수정

--- src/test/java/qna/repository/AnswerRepositoryTest.java
+++ src/test/java/qna/repository/AnswerRepositoryTest.java
@@ -1,5 +1,8 @@
 package qna.repository;
 
+import org.hibernate.Hibernate;
+import org.hibernate.proxy.HibernateProxy;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,14 +10,12 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import qna.NotFoundException;
 import qna.domain.Answer;
 import qna.domain.Question;
-import qna.domain.QuestionTest;
 import qna.domain.UserTest;
 
 import javax.persistence.EntityManager;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -28,28 +29,46 @@ public class AnswerRepositoryTest {
     @Autowired
     private EntityManager entityManager;
 
+    private Answer answer;
+    private Question question;
+
+    @BeforeEach
+    void init() {
+        question = new Question("test_title", "contents");
+        questions.save(question);
+        answer = new Answer(UserTest.JAVAJIGI, question, "contents");
+        answers.save(answer);
+        entityManager.flush();
+        entityManager.clear();
+    }
+
+
     @Test
     @DisplayName("answer 저장")
     void answer_save() {
-        Answer answer = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "contents");
-        answers.saveAndFlush(answer);
-        entityManager.clear();
         Answer expect = answers.findById(answer.getId()).orElseThrow(NotFoundException::new);
         assertAll(
                 () -> assertThat(answer.getId()).isEqualTo(expect.getId()),
                 () -> assertThat(answer.getWriterId()).isEqualTo(expect.getWriterId()),
-                () -> assertThat(answer.getQuestionId()).isEqualTo(expect.getQuestionId()),
+                () -> assertThat(answer.getQuestion()).isEqualTo(Hibernate.unproxy(expect.getQuestion())),
                 () -> assertThat(answer.getContents()).isEqualTo(expect.getContents()),
                 () -> assertThat(answer.isDeleted()).isEqualTo(expect.isDeleted())
         );
     }
 
     @Test
+    @DisplayName("answer의 question lazy 로딩 확인")
+    void answer_to_question_proxy() {
+        Answer expect = answers.findById(answer.getId()).orElseThrow(NotFoundException::new);
+        Question question = expect.getQuestion();
+        assertThat(Hibernate.isInitialized(question)).isFalse();
+    }
+
+    @Test
     @DisplayName("answer 삭제(deleted 상태 변경)")
     void answer_delete() {
-        Answer answer = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "contents");
-        answers.saveAndFlush(answer);
-        answer.setDeleted(true);
+        Answer dbSave = answers.findById(answer.getId()).orElseThrow(NotFoundException::new);
+        dbSave.setDeleted(true);
         entityManager.flush();
         entityManager.clear();
         Answer expect = answers.findById(answer.getId()).orElseThrow(NotFoundException::new);
@@ -59,10 +78,6 @@ public class AnswerRepositoryTest {
     @Test
     @DisplayName("question id에 일치하는 삭제되지 않은 Answer을 조회")
     void find_question_id_and_delete_false() {
-        Question question = questions.saveAndFlush(new Question("title", "contents"));
-        Answer answer = new Answer(UserTest.JAVAJIGI, question, "contents");
-        answers.saveAndFlush(answer);
-        entityManager.clear();
         List<Answer> expect = answers.findByQuestionIdAndDeletedFalse(question.getId());
         assertThat(expect).hasSize(1).containsExactly(answer);
     }
@@ -70,21 +85,15 @@ public class AnswerRepositoryTest {
     @Test
     @DisplayName("question id에 일치하는 삭제되지 않은 Answer을 조회(해당 결과 없음 일차하는 questionId가 없음)")
     void find_question_id_and_delete_false_return_nothing() {
-        Question question = questions.saveAndFlush(new Question("title", "contents"));
-        Answer answer = new Answer(UserTest.JAVAJIGI, question, "contents");
-        answers.saveAndFlush(answer);
-        entityManager.clear();
-        long diffQuestionId = 3L;
-        List<Answer> expect = answers.findByQuestionIdAndDeletedFalse(diffQuestionId);
+        Question diffQuestion = new Question("diff", "diff");
+        questions.save(diffQuestion);
+        List<Answer> expect = answers.findByQuestionIdAndDeletedFalse(diffQuestion.getId());
         assertThat(expect).isEmpty();
     }
 
     @Test
     @DisplayName("answer id에 일치하는 삭제되지 않은 Answer을 조회")
     void find_answer_id_and_delete_false() {
-        Answer answer = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "contents");
-        answers.saveAndFlush(answer);
-        entityManager.clear();
         Answer expect = answers.findByIdAndDeletedFalse(answer.getId()).orElseThrow(NotFoundException::new);
         assertThat(answer).isEqualTo(expect);
     }
@@ -92,9 +101,9 @@ public class AnswerRepositoryTest {
     @Test
     @DisplayName("answer id에 일치하는 삭제되지 않은 Answer을 조회(삭제된 Answer 이라 조회결과 없음)")
     void find_answer_id_and_delete_false_return_nothing() {
-        Answer answer = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "contents");
-        answer.setDeleted(true);
-        answers.saveAndFlush(answer);
+        Answer dbSave = answers.findById(answer.getId()).orElseThrow(NotFoundException::new);
+        dbSave.setDeleted(true);
+        entityManager.flush();
         entityManager.clear();
         assertThat(answers.findByIdAndDeletedFalse(answer.getId())).isEmpty();
     }
