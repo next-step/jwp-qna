e55fef3cb630ad342a34660a28f06a03e570aab7
dongha-byun <byunsw4@naver.com>
2022-11-08 01:27:48 +0900

    Docs: 기초데이터 sql 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,4 @@
-INSERT INTO line (id, name) VALUES (1, '3호선')
-INSERT INTO station (id, line_id, name) VALUES (1, 1, '교대역')
+--INSERT INTO line (id, name) VALUES (1, '3호선')
+--INSERT INTO station (id, line_id, name) VALUES (1, 1, '교대역')
+
+INSERT INTO user (id, name, user_id, password, created_at) values(3L, '테스터', 'tester', 'tester!', sysdate)
