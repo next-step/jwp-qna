3ae2db3e11c0711ce9bc7aeebdcf7a592acd6ba1
victor-jo <joasjn45@gmail.com>
2022-11-07 23:01:20 +0900

    test(question): 질문 엔티티 테스트

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,47 @@
 package qna.domain;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
 public class QuestionTest {
-    public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
-    public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+    private static final String TITLE_1 = "title1";
+    private static final String TITLE_2 = "title2";
+
+    private static final String CONTENTS_1 = "contents1";
+    private static final String CONTENTS_2 = "contents2";
+
+    public static final Question Q1 = new Question(TITLE_1, CONTENTS_1).writeBy(UserTest.JAVAJIGI);
+    public static final Question Q2 = new Question(TITLE_2, CONTENTS_2).writeBy(UserTest.SANJIGI);
+
+
+    @Autowired
+    QuestionRepository questions;
+
+    @ParameterizedTest(name = "save_테스트")
+    @MethodSource("saveTestFixture")
+    void save_테스트(Question question) {
+        Question saved = questions.save(question);
+        assertThat(saved).isEqualTo(question);
+        assertThat(saved.getContents()).isEqualTo(question.getContents());
+    }
+
+    @ParameterizedTest(name = "save_후_findById_테스트")
+    @MethodSource("saveTestFixture")
+    void save_후_findById_테스트(Question question) {
+        Question question1 = questions.save(question);
+        Question question2 = questions.findById(question1.getId()).get();
+        assertThat(question1).isEqualTo(question2);
+        assertThat(question1.getContents()).isEqualTo(question2.getContents());
+    }
+
+    static Stream<Question> saveTestFixture() {
+        return Stream.of(Q1, Q2);
+    }
 }
