77e54a7cc32610b3ec3bad4e8458b08576f2c888
iamsojung <sojungkid@gmail.com>
2022-11-07 18:40:41 +0900

    refactor: domain 테스트 수정

--- src/test/java/qna/domain/DeleteHistoryTest.java
+++ src/test/java/qna/domain/DeleteHistoryTest.java
@@ -13,25 +13,9 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 @DataJpaTest
 public class DeleteHistoryTest {
 
-    private static final DeleteHistory questionDeleted =
+    public static final DeleteHistory questionDeleted =
         new DeleteHistory(ContentType.QUESTION, 1L, 1L, LocalDateTime.now());
     private static final DeleteHistory answerDeleted =
         new DeleteHistory(ContentType.ANSWER, 1L, 1L, LocalDateTime.now());
 
-    @Autowired
-    private DeleteHistoryRepository deleteHistoryRepository;
-
-    @DisplayName("save 테스트")
-    @Test
-    void saveTest() {
-        DeleteHistory actual = deleteHistoryRepository.save(questionDeleted);
-
-        assertAll(
-            () -> assertThat(actual.getId()).isNotNull(),
-            () -> assertEquals(ContentType.QUESTION, actual.getContentType()),
-            () -> assertEquals(1L, actual.getContentId()),
-            () -> assertEquals(1L, actual.getDeletedById()),
-            () -> assertThat(actual.getCreateDate()).isNotNull()
-        );
-    }
 }
--- src/test/java/qna/domain/UserTest.java
+++ src/test/java/qna/domain/UserTest.java
@@ -13,30 +13,4 @@ public class UserTest {
     public static final User SANJIGI = new User(2L, "sanjigi", "password", "name",
         "sanjigi@slipp.net");
 
-    @Autowired
-    private UserRepository userRepository;
-
-
-    @Test
-    @DisplayName("save 테스트")
-    void saveTest() {
-        User userSaved = userRepository.save(JAVAJIGI);
-
-        assertAll(
-            () -> assertNotNull(userSaved.getId()),
-            () -> assertEquals(userSaved.getUserId(), JAVAJIGI.getUserId()),
-            () -> assertEquals(userSaved.getPassword(), JAVAJIGI.getPassword()),
-            () -> assertEquals(userSaved.getName(), JAVAJIGI.getName()),
-            () -> assertEquals(userSaved.getEmail(), JAVAJIGI.getEmail())
-        );
-    }
-
-    @Test
-    @DisplayName("findByUserId 테스트")
-   void findByUserIdTest() {
-        User userSaved = userRepository.save(UserTest.JAVAJIGI);
-        User userFound = userRepository.findByUserId(userSaved.getUserId()).get();
-
-        assertEquals(userSaved, userFound);
-    }
 }
