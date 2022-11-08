7b9106eefe456f2b60111c00ef2d353c3396df37
choiyongseok90 <badaelephant>
2022-11-08 19:43:02 +0900

    refactor: 답변 테스트코드 리팩토링. 1. 인자를 private으로 수정 2. 답변의 삭제를 답변 도메인 안으로 가져옴 3. 답변을 삭제하면 삭제이력이 반환되도록 구현

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -4,48 +4,93 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import qna.NotFoundException;
-import qna.UnAuthorizedException;
+import qna.exception.CannotDeleteException;
+import qna.exception.NotFoundException;
+import qna.exception.UnAuthorizedException;
 
 public class AnswerTest {
 
-    public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
-    public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    private User u1;
+    private User u2;
+    private Question q1;
+    private Question q2;
+    private Answer a1;
+
+    @BeforeEach
+    void setup() {
+        u1 = new User(1L, "javajigi", "password", "name", "javajigi@slipp.net");
+        u2 = new User(2L, "sanjigi", "password", "name", "sanjigi@slipp.net");
+        q1 = new Question(1L, "title1", "contents1").writeBy(u1);
+        q2 = new Question(2L, "title1", "contents1").writeBy(u2);
+        a1 = new Answer(1L, u1, q1, "contents");
+    }
 
 
     @DisplayName("답변 생성시 작성자가 없으면 UnAuthorizedException 발생시킨다")
     @Test
     void createAnswer_withoutWriter_exception() {
-        assertThatThrownBy(() -> new Answer(null, QuestionTest.Q1, "contents"))
+        assertThatThrownBy(() -> new Answer(null, q1, "contents"))
                 .isInstanceOf(UnAuthorizedException.class);
     }
 
     @DisplayName("답변 생성시 질문이 없으면 NotFoundException 발생시킨다")
     @Test
     void createAnswer_withoutQuestion_exception() {
-        assertThatThrownBy(() -> new Answer(UserTest.JAVAJIGI, null, "contents"))
+        assertThatThrownBy(() -> new Answer(u1, null, "contents"))
                 .isInstanceOf(NotFoundException.class);
     }
 
     @DisplayName("답변의 작성자가 주어진 작성자가 맞는지 확인할 수 있다")
     @Test
     void isOwner_test() {
-        Answer answer = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "contents");
+        Answer answer = new Answer(u1, q1, "contents");
         assertAll(
-                () -> assertTrue(answer.isOwner(UserTest.JAVAJIGI)),
-                () -> assertFalse(answer.isOwner(UserTest.SANJIGI))
+                () -> assertTrue(answer.isOwner(u1)),
+                () -> assertFalse(answer.isOwner(u2))
         );
     }
 
     @DisplayName("답변에 해당하는 질문을 변경할 수 있다")
     @Test
     void toQuestion_test() {
-        Answer answer = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "contents");
-        answer.toQuestion(QuestionTest.Q2);
-        assertThat(answer.getQuestion()).isEqualTo(QuestionTest.Q2);
+        Answer answer = new Answer(u1, q1, "contents");
+        answer.toQuestion(q2);
+        assertThat(answer.getQuestion()).isEqualTo(q2);
+    }
+
+    @DisplayName("삭제되지 않은 답변을 삭제하면 DeleteHistory가 리턴 된다.")
+    @Test
+    void delete_non_deleted_answer_test() throws CannotDeleteException {
+
+        assertFalse(a1.isDeleted());
+        DeleteHistory deleteHistory = a1.delete(u1);
+        assertNotNull(deleteHistory);
+    }
+
+    @DisplayName("삭제된 답변을 또 삭제하려고 하면 이미 CannotDeleteException")
+    @Test
+    void delete_answer_already_deleted_test() throws CannotDeleteException {
+
+        assertFalse(a1.isDeleted());
+        DeleteHistory deleteHistory = a1.delete(u1);
+        assertNotNull(deleteHistory);
+        assertThatThrownBy(() -> a1.delete(u1))
+                .isInstanceOf(CannotDeleteException.class);
+    }
+
+    @DisplayName("삭제할때 로그인 유저와 id가 다르면 CannotDeleteException을 발생시킨다.")
+    @Test
+    void delete_answer_wrong_user_exception() {
+
+        assertThatThrownBy(() -> a1.delete(u2))
+                .isInstanceOf(CannotDeleteException.class);
+
     }
 }
