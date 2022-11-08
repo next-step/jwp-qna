a5cc6c4cd96b7d4f0e8fad28b4345774d38da0c4
Heonseop <shinsub6120@gmail.com>
2022-11-08 23:44:53 +0900

    fix : toString, hashCode 연관관계 작성전 삭제

--- src/main/java/qna/domain/DeleteHistory.java
+++ src/main/java/qna/domain/DeleteHistory.java
@@ -1,7 +1,6 @@
 package qna.domain;
 
 import java.time.LocalDateTime;
-import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -42,21 +41,6 @@ public class DeleteHistory {
         this.createDate = createDate;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        DeleteHistory that = (DeleteHistory) o;
-        return Objects.equals(id, that.id) &&
-                contentType == that.contentType &&
-                Objects.equals(contentId, that.contentId) &&
-                Objects.equals(deletedById, that.deletedById);
-    }
-
     public Long getId() {
         return id;
     }
@@ -75,11 +59,6 @@ public class DeleteHistory {
 
     public LocalDateTime getCreateDate() {
         return createDate;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, contentType, contentId, deletedById);
     }
 
     @Override
