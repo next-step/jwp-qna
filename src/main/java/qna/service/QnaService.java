4bec2db8089193e4c38403973752089d1a284444
sixthou <justyuk@naver.com>
2022-11-09 00:02:01 +0900

    refactor : deleteQuestion 리팩토링 및 테스트 수정

--- src/main/java/qna/service/QnaService.java
+++ src/main/java/qna/service/QnaService.java
@@ -1,19 +1,14 @@
 package qna.service;
 
-import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import qna.CannotDeleteException;
 import qna.NotFoundException;
-import qna.domain.Answer;
-import qna.domain.ContentType;
 import qna.domain.DeleteHistories;
-import qna.domain.DeleteHistory;
 import qna.domain.Question;
 import qna.domain.User;
-import qna.repository.AnswerRepository;
 import qna.repository.QuestionRepository;
 
 @Service
@@ -21,13 +16,10 @@ public class QnaService {
     private static final Logger log = LoggerFactory.getLogger(QnaService.class);
 
     private QuestionRepository questionRepository;
-    private AnswerRepository answerRepository;
     private DeleteHistoryService deleteHistoryService;
 
-    public QnaService(QuestionRepository questionRepository, AnswerRepository answerRepository,
-                      DeleteHistoryService deleteHistoryService) {
+    public QnaService(QuestionRepository questionRepository, DeleteHistoryService deleteHistoryService) {
         this.questionRepository = questionRepository;
-        this.answerRepository = answerRepository;
         this.deleteHistoryService = deleteHistoryService;
     }
 
@@ -39,19 +31,8 @@ public class QnaService {
 
     @Transactional
     public void deleteQuestion(User loginUser, Long questionId) throws CannotDeleteException {
-
-        DeleteHistories deleteHistories = new DeleteHistories();
-
         Question question = findQuestionById(questionId);
-
-        deleteHistories.add(question.delete(loginUser));
-
-        List<Answer> answers = answerRepository.findByQuestionIdAndDeletedFalse(questionId);
-
-        answers.stream()
-                .map(answer -> answer.delete(loginUser))
-                .forEach(deleteHistories::add);
-
+        DeleteHistories deleteHistories = question.delete(loginUser);
         deleteHistoryService.saveAll(deleteHistories);
     }
 }
