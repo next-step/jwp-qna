b4a6daee5abab3819d95c95f8c56a1373092d18b
deveo-pyo <jungpyo9@gmail.com>
2022-11-09 00:15:19 +0900

    fix: 마지막라인 개행 추가

--- src/test/java/qna/domain/DeleteHistoryTest.java
+++ src/test/java/qna/domain/DeleteHistoryTest.java
@@ -1,5 +1,6 @@
 package qna.domain;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -15,6 +16,7 @@ class DeleteHistoryTest {
     DeleteHistoryRepository deleteHistoryRepository;
 
     @Test
+    @DisplayName("저장 테스트")
     public void save() {
         DeleteHistory deleteHistory = new DeleteHistory(ContentType.ANSWER, 1L, 1L, LocalDateTime.now());
         DeleteHistory savedDeleteHistory = deleteHistoryRepository.save(deleteHistory);
