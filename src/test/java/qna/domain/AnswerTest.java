8ca30febc6db52ac1e0e60f18e2b73f303aa4d87
seogineer <dgseo8981@gmail.com>
2022-11-08 21:13:07 +0900

    refactor: 도메인 테스트에만 집중, assertAll 사용

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,11 +1,9 @@
 package qna.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
-import java.util.List;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 @DataJpaTest
@@ -13,50 +11,15 @@ public class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
 
-    @Autowired
-    private AnswerRepository answerRepository;
-
-    @BeforeEach
-    void beforeEach() {
-        answerRepository.save(A1);
-        answerRepository.save(A2);
-    }
-
     @Test
-    void findById() {
-        Answer answer1 = answerRepository.findById(1L).get();
-        Answer answer2 = answerRepository.findById(2L).get();
-        assertThat(answer1.getWriterId()).isEqualTo(A1.getId());
-        assertThat(answer2.getWriterId()).isEqualTo(A2.getId());
-        assertThat(answer1.getQuestionId()).isEqualTo(A1.getQuestionId());
-        assertThat(answer2.getQuestionId()).isEqualTo(A2.getQuestionId());
-        assertThat(answer1.getContents()).isEqualTo(A1.getContents());
-        assertThat(answer2.getContents()).isEqualTo(A2.getContents());
-    }
-
-    @Test
-    void findByQuestionIdAndDeletedFalse() {
-        List<Answer> answers = answerRepository.findByQuestionIdAndDeletedFalse(QuestionTest.Q1.getId());
-        assertThat(answers).isNotEmpty();
-        assertThat(answers.get(0).getQuestionId()).isEqualTo(A1.getQuestionId());
-        assertThat(answers.get(1).getQuestionId()).isEqualTo(A2.getQuestionId());
-        assertThat(answers.get(0).getWriterId()).isEqualTo(A1.getWriterId());
-        assertThat(answers.get(1).getWriterId()).isEqualTo(A2.getWriterId());
-        assertThat(answers.get(0).getContents()).isEqualTo(A1.getContents());
-        assertThat(answers.get(1).getContents()).isEqualTo(A2.getContents());
-    }
-
-    @Test
-    void findByIdAndDeletedFalse() {
-        Answer answer1 = answerRepository.findByIdAndDeletedFalse(1L).get();
-        Answer answer2 = answerRepository.findByIdAndDeletedFalse(2L).get();
-        assertThat(answer1).isNotNull();
-        assertThat(answer2).isNotNull();
-        assertThat(answer1.getQuestionId()).isEqualTo(A1.getQuestionId());
-        assertThat(answer2.getQuestionId()).isEqualTo(A2.getQuestionId());
-        assertThat(answer1.getWriterId()).isEqualTo(A1.getWriterId());
-        assertThat(answer2.getWriterId()).isEqualTo(A2.getWriterId());
-        assertThat(answer1.getContents()).isEqualTo(A1.getContents());
-        assertThat(answer2.getContents()).isEqualTo(A2.getContents());
+    void 값_검증() {
+        assertAll(
+                () -> assertThat(A1.getWriterId()).isEqualTo(UserTest.JAVAJIGI.getId()),
+                () -> assertThat(A2.getWriterId()).isEqualTo(UserTest.SANJIGI.getId()),
+                () -> assertThat(A1.getQuestionId()).isEqualTo(QuestionTest.Q1.getId()),
+                () -> assertThat(A2.getQuestionId()).isEqualTo(QuestionTest.Q1.getId()),
+                () -> assertThat(A1.getContents()).isEqualTo("Answers Contents1"),
+                () -> assertThat(A2.getContents()).isEqualTo("Answers Contents2")
+        );
     }
 }
--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,11 +1,9 @@
 package qna.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
-import java.util.List;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 @DataJpaTest
@@ -13,52 +11,15 @@ public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
 
-    @Autowired
-    private QuestionRepository questionRepository;
-
-    @BeforeEach
-    void beforeEach() {
-        questionRepository.save(Q1);
-        questionRepository.save(Q2);
-    }
-
     @Test
-    void findById() {
-        Question question1 = questionRepository.findById(1L).get();
-        Question question2 = questionRepository.findById(2L).get();
-        assertThat(question1).isNotNull();
-        assertThat(question2).isNotNull();
-        assertThat(question1.getTitle()).isEqualTo(Q1.getTitle());
-        assertThat(question2.getTitle()).isEqualTo(Q2.getTitle());
-        assertThat(question1.getContents()).isEqualTo(Q1.getContents());
-        assertThat(question2.getContents()).isEqualTo(Q2.getContents());
-        assertThat(question1.getWriterId()).isEqualTo(Q1.getWriterId());
-        assertThat(question2.getWriterId()).isEqualTo(Q2.getWriterId());
-    }
-
-    @Test
-    void findByDeletedFalse() {
-        List<Question> questions = questionRepository.findByDeletedFalse();
-        assertThat(questions).isNotEmpty();
-        assertThat(questions.get(0).getTitle()).isEqualTo(Q1.getTitle());
-        assertThat(questions.get(1).getTitle()).isEqualTo(Q2.getTitle());
-        assertThat(questions.get(0).getContents()).isEqualTo(Q1.getContents());
-        assertThat(questions.get(1).getContents()).isEqualTo(Q2.getContents());
-        assertThat(questions.get(0).getWriterId()).isEqualTo(Q1.getWriterId());
-        assertThat(questions.get(1).getWriterId()).isEqualTo(Q2.getWriterId());
-    }
-
-    @Test
-    void findByIdAndDeletedFalse() {
-        Question question1 = questionRepository.findByIdAndDeletedFalse(1L).get();
-        Question question2 = questionRepository.findByIdAndDeletedFalse(2L).get();
-        assertThat(question1).isNotNull();
-        assertThat(question2).isNotNull();
-        assertThat(question1.getTitle()).isEqualTo(Q1.getTitle());
-        assertThat(question2.getTitle()).isEqualTo(Q2.getTitle());
-        assertThat(question1.getContents()).isEqualTo(Q1.getContents());
-        assertThat(question2.getContents()).isEqualTo(Q2.getContents());
-        assertThat(question1.getWriterId()).isEqualTo(Q1.getWriterId());
-        assertThat(question2.getWriterId()).isEqualTo(Q2.getWriterId());
+    void 값_검증() {
+        assertAll(
+                () -> assertThat(Q1.getTitle()).isEqualTo("title1"),
+                () -> assertThat(Q2.getTitle()).isEqualTo("title2"),
+                () -> assertThat(Q1.getContents()).isEqualTo("contents1"),
+                () -> assertThat(Q2.getContents()).isEqualTo("contents2"),
+                () -> assertThat(Q1.getWriterId()).isEqualTo(UserTest.JAVAJIGI.getId()),
+                () -> assertThat(Q2.getWriterId()).isEqualTo(UserTest.SANJIGI.getId())
+        );
     }
 }
