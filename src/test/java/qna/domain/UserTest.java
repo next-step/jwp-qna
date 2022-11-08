a2403f2a415f95f04fa6c482f1cc297f2a66937d
choiyongseok90 <badaelephant>
2022-11-08 19:48:29 +0900

    refactor: 사용자 테스트코드 리팩토링 1. 프라이빗 파라미터로 변경 2. 삭제이력 관련 로직 추가

--- src/test/java/qna/domain/UserTest.java
+++ src/test/java/qna/domain/UserTest.java
@@ -6,61 +6,64 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import qna.UnAuthorizedException;
+import qna.exception.CannotUpdateException;
+import qna.exception.UnAuthorizedException;
 
 public class UserTest {
 
-    public static final User JAVAJIGI = new User("javajigi", "password", "name", "javajigi@slipp.net");
-    public static final User SANJIGI = new User("sanjigi", "password", "name", "sanjigi@slipp.net");
+    private User JAVAJIGI;
+    private User JAVAJIGI_NEW_ID;
+    private User JAVAJIGI_NEW_DATA;
+    private User SANJIGI;
+    private User SANJIGI_NEW_PASSWORD;
+
+    private Question q1;
+
+    @BeforeEach
+    void setup() {
+        JAVAJIGI = new User(1L, "javajigi", "password", "name", "javajigi@slipp.net");
+        JAVAJIGI_NEW_ID = new User(1L, "javajigi_new", "password", "name", "javajigi@slipp.net");
+        JAVAJIGI_NEW_DATA = new User(1L, "javajigi", "password", "new", "new@slipp.net");
+        SANJIGI = new User(2L, "sanjigi", "password", "name", "sanjigi@slipp.net");
+        SANJIGI_NEW_PASSWORD = new User(2L, "sanjigi", "password_new", "name", "sanjigi@slipp.net");
+        q1 = new Question(1L, "title", "content").writeBy(JAVAJIGI);
+    }
+
 
     @DisplayName("유저 정보 변경시 유저의 아이디와 로그인 유저의 아이디와 같지 않으면 UnAuthorizedException 발생")
     @Test
     void update_wrongUserId_exception() {
 
-        User user = new User("user", "password", "name", "name@email.com");
-        User loginUser = new User("userFail", "password", "name", "name@email.com");
-        User target = new User("user", "password", "nameNew", "nameNew@email.com");
-
-        assertThatThrownBy(() -> user.update(loginUser, target)).isInstanceOf(UnAuthorizedException.class);
+        assertThatThrownBy(() -> JAVAJIGI.update(JAVAJIGI_NEW_ID, JAVAJIGI))
+                .isInstanceOf(UnAuthorizedException.class);
     }
 
     @DisplayName("유저 정보 변경시 유저의 비밀번호와 타겟 유저의 비밀번호와 같지 않으면 UnAuthorizedException 발생")
     @Test
     void update_wrongPassword_exception() {
-        User user = new User("user", "password", "name", "name@email.com");
-        User loginUser = new User("user", "password", "name", "name@email.com");
-        User target = new User("user", "passwordFail", "nameNew", "nameNew@email.com");
-
-        assertThatThrownBy(() -> user.update(loginUser, target)).isInstanceOf(UnAuthorizedException.class);
+        assertThatThrownBy(() -> SANJIGI.update(SANJIGI, SANJIGI_NEW_PASSWORD))
+                .isInstanceOf(UnAuthorizedException.class);
 
     }
 
     @DisplayName("로그인 유저의 아이디와 타겟 유저의 비밀번호가 유저 아이디 비밀번호와 동일하면 이름과 이메일 변경이 가능하다")
     @Test
     void update_test() {
-        // given
-        User user = new User("user", "password", "name", "name@email.com");
-        User loginUser = new User("user", "password", "name", "name@email.com");
-        User target = new User("user", "password", "nameNew", "nameNew@email.com");
-        // when
-        user.update(loginUser, target);
-        // then
+
+        JAVAJIGI.update(JAVAJIGI, JAVAJIGI_NEW_DATA);
         assertAll(
-                () -> assertEquals(user.getName(), target.getName()),
-                () -> assertEquals(user.getEmail(), target.getEmail())
+                () -> assertEquals(JAVAJIGI.getName(), JAVAJIGI_NEW_DATA.getName()),
+                () -> assertEquals(JAVAJIGI.getEmail(), JAVAJIGI_NEW_DATA.getEmail())
         );
     }
 
     @DisplayName("유저의 이름과 이메일이 같은지 확인한다")
     @Test
     void equalsNameAndEmail_test() {
-
-        User user = new User("user", "password", "name", "name@email.com");
-        User target = new User("target", "targetPassword", "name", "name@email.com");
-
-        assertTrue(user.equalsNameAndEmail(target));
+        assertTrue(SANJIGI.equalsNameAndEmail(SANJIGI_NEW_PASSWORD));
     }
 
     @DisplayName("게스트 유저를 확인할 수 있다")
@@ -68,8 +71,25 @@ public class UserTest {
     void isGuestUser_test() {
         User guest = User.GUEST_USER;
         assertAll(
-                () -> assertFalse(UserTest.JAVAJIGI.isGuestUser()),
+                () -> assertFalse(JAVAJIGI.isGuestUser()),
                 () -> assertTrue(guest.isGuestUser())
         );
+    }
+
+    @DisplayName("유저의 삭제이력을 추가할 수 있다.")
+    @Test
+    void addDeleteHistory_test() {
+        DeleteHistory d1 = DeleteHistory.of(q1);
+        assertFalse(JAVAJIGI.getDeleteHistories().contains(d1));
+
+        assertEquals(d1.getDeletedBy(), JAVAJIGI);
+    }
+
+    @DisplayName("유저의 삭제이력을 추가할때 작성자가 다르면 CannotUpdateException에러를 발생시킨다.")
+    @Test
+    void addDeleteHistory_exception() {
+        DeleteHistory d1 = DeleteHistory.of(q1);
+        assertEquals(d1.getDeletedBy(), JAVAJIGI);
+        assertThatThrownBy(() -> SANJIGI.addDeleteHistory(d1)).isInstanceOf(CannotUpdateException.class);
     }
 }
