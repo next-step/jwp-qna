319f2c23dd022db65fa16e2f369c71aa6b7c80d1
Heonseop <shinsub6120@gmail.com>
2022-11-08 23:24:45 +0900

    fix : 쓸데없는 주석 삭제

--- src/main/java/qna/domain/BaseTimeEntity.java
+++ src/main/java/qna/domain/BaseTimeEntity.java
@@ -8,16 +8,14 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-//DB에 저장하거나 업데이트 하는 경우 자동으로 데이터베이스에 반영해주는 ANNOTATION
 @EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 public class BaseTimeEntity {
-    //Entity 생성 시간 자동 저장
+
     @CreatedDate
     @Column(updatable = false, nullable = false)
     private LocalDateTime createdAt;
 
-    //Entity값 수정한 시간 자동 저장
     @LastModifiedDate
     private LocalDateTime updatedAt;
 }
