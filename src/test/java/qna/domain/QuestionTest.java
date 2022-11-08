775e765a88495129b882f0e68ddc5034a3280f39
chunhodong <yhms4432@naver.com>
2022-11-08 10:37:31 +0900

    test: QnaServiceTest에서 질문삭제테스트

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import qna.CannotDeleteException;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class QuestionTest {
@@ -14,9 +15,21 @@ public class QuestionTest {
     @DisplayName("질문작성자와 삭제요청자가 다르면 예외발생")
     public void test_throw_exception() {
         Question question = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
+
         assertThatThrownBy(() -> question.delete(UserTest.SANJIGI))
                 .isInstanceOf(CannotDeleteException.class)
                 .hasMessageContaining("질문을 삭제할 권한이 없습니다.");
+    }
+
+    @Test
+    @DisplayName("질문삭제하면 삭제필드에서 true를 반환")
+    public void test_returns_true_when_question_deleted() throws CannotDeleteException {
+        Question question = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
+
+        question.delete(UserTest.JAVAJIGI);
+
+        assertThat(question.isDeleted()).isTrue();
+
     }
 
 }
