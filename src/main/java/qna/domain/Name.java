52f0f414747f970b252f5dbe10b0c9d1e4a7e2f9
namhyun.sung <namhyun.sung@yanolja.com>
2022-11-09 02:25:18 +0900

    refactor(Name): Embeddable 애노테이션 추가

--- src/main/java/qna/domain/Name.java
+++ src/main/java/qna/domain/Name.java
@@ -1,12 +1,17 @@
 package qna.domain;
 
 import javax.persistence.Column;
+import javax.persistence.Embeddable;
 
+@Embeddable
 public class Name {
 
     @Column(name = "name", length = 20, nullable = false)
     private String name;
-    
+
+    public Name() {
+    }
+
     public Name(String name) {
         this.name = name;
     }
