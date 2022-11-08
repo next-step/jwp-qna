92406cf71176bef8786dfe2d9ed7752aaa36a507
HJ Kim <hjkim880527@gmail.com>
2022-11-08 15:32:15 +0900

    refactor: 의미없는 코드 삭제

--- src/main/java/qna/domain/answer/Answer.java
+++ src/main/java/qna/domain/answer/Answer.java
@@ -68,14 +68,9 @@ public class Answer extends BaseEntity {
     }
 
     public DeleteHistory delete(User loginUser) throws CannotDeleteException {
-        if (!writer.equals(loginUser)) {
-            throw new CannotDeleteException("질문을 삭제할 권한이 없습니다.");
-        }
-
         if (!isOwner(loginUser)) {
             throw new CannotDeleteException("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
         }
-
         this.deleted = true;
         return DeleteHistory.createAnswerDeleteHistory(this);
     }
