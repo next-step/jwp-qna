aec4db0f93b1d93152aec389ea1c8789fd43f967
Bryan | JJ <with.beth@gmail.com>
2022-11-09 17:04:59 +0900

    refactor : rename BaseAuditingEntity to BaseTimeEntity

--- src/main/java/qna/domain/BaseTimeEntity.java
+++ src/main/java/qna/domain/BaseTimeEntity.java
@@ -10,7 +10,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-public abstract class BaseAuditingEntity {
+public abstract class BaseTimeEntity {
 
     @CreatedDate
     @Column(nullable = false, updatable = false)
