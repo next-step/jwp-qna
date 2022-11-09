e3e932d5a57a299c31e15bede350acdadf83d15b
mansik <90mansin@gmail.com>
2022-11-09 23:21:37 +0900

    refactor: 공통 날짜 Entity 멤버 변수에 기본 값 셋팅 하도록 변경

--- src/main/java/qna/domain/BaseTime.java
+++ src/main/java/qna/domain/BaseTime.java
@@ -9,16 +9,18 @@ import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
+import static java.time.LocalDateTime.now;
+
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public class BaseTime {
 
     @CreatedDate
     @Column(name= "created_at", nullable = false, updatable = false)
-    private LocalDateTime createdAt;
+    private LocalDateTime createdAt = now();
 
     @LastModifiedDate
     @Column(name= "updated_at")
-    private LocalDateTime updateAt;
+    private LocalDateTime updateAt = now();
 
 }
