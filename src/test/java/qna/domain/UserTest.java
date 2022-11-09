e694bf747b573ac48979371d53683f06ebf6804b
mansik <90mansin@gmail.com>
2022-11-09 23:46:10 +0900

    test: User 저장,찾기 테스트 구현

--- src/test/java/qna/domain/UserTest.java
+++ src/test/java/qna/domain/UserTest.java
@@ -1,6 +1,40 @@
 package qna.domain;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+@DataJpaTest
 public class UserTest {
     public static final User JAVAJIGI = new User(1L, "javajigi", "password", "name", "javajigi@slipp.net");
     public static final User SANJIGI = new User(2L, "sanjigi", "password", "name", "sanjigi@slipp.net");
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void save() {
+        User user = userRepository.save(JAVAJIGI);
+
+        assertAll(
+                () -> assertThat(user).isNotNull(),
+                () -> assertThat(user.getId()).isNotNull()
+        );
+    }
+
+    @Test
+    void saveAndFind() {
+        String expectValue = "javajigi";
+        userRepository.save(JAVAJIGI);
+
+        User user = userRepository.findById(1L).get();
+
+        assertAll(
+                () -> assertThat(user.getId()).isNotNull(),
+                () -> assertThat(user.getUserId()).isEqualTo(expectValue)
+        );
+    }
 }
