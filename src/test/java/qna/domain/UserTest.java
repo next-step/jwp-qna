f845c9eb6c8b3ff8a7de90d8572d195d1ea4017b
chosundeveloper <67059660+chosundeveloper@users.noreply.github.com>
2022-11-10 21:32:37 +0900

    test(UserTest.java) : 유저 생성

--- src/test/java/qna/domain/UserTest.java
+++ src/test/java/qna/domain/UserTest.java
@@ -1,6 +1,18 @@
 package qna.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatNoException;
+
+@DisplayName("유저")
 public class UserTest {
     public static final User JAVAJIGI = new User(1L, "javajigi", "password", "name", "javajigi@slipp.net");
     public static final User SANJIGI = new User(2L, "sanjigi", "password", "name", "sanjigi@slipp.net");
+
+    @DisplayName("유저 생성")
+    @Test
+    void constructor() {
+        assertThatNoException().isThrownBy(() -> new User(1L, "javajigi", "password", "name", "javajigi@slipp.net"));
+    }
 }
