33f70e41068c5a7715408841ec83b47199f2a7f7
develo-pyo <jungpyo9@gmail.com>
2022-11-11 03:16:04 +0900

    refactor: 레파지토리 패키지 경로 생성 및 이동

--- src/main/java/qna/service/DeleteHistoryService.java
+++ src/main/java/qna/service/DeleteHistoryService.java
@@ -4,13 +4,13 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import qna.domain.DeleteHistory;
-import qna.domain.DeleteHistoryRepository;
+import qna.repository.DeleteHistoryRepository;
 
 import java.util.List;
 
 @Service
 public class DeleteHistoryService {
-    private DeleteHistoryRepository deleteHistoryRepository;
+    private final DeleteHistoryRepository deleteHistoryRepository;
 
     public DeleteHistoryService(DeleteHistoryRepository deleteHistoryRepository) {
         this.deleteHistoryRepository = deleteHistoryRepository;
