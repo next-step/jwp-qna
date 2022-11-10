c94563f99d43c286af606279322658900853c1eb
develo-pyo <jungpyo9@gmail.com>
2022-11-11 03:19:48 +0900

    test: 도메인 테스트 수정 및 구현 도메인 테스트 내에 레파지토리 테스트 제거. 도메인 테스트만 할수있도록 수정

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -2,94 +2,38 @@ package qna.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.context.annotation.Import;
-import qna.config.JpaAuditingConfiguration;
-
-import javax.persistence.EntityManager;
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Optional;
+import qna.NotFoundException;
+import qna.UnAuthorizedException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static qna.domain.DomainTestFactory.*;
 
 @DataJpaTest
-@Import(value = {JpaAuditingConfiguration.class})
 public class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
 
-    @Autowired
-    AnswerRepository answerRepository;
-
-    @Autowired
-    QuestionRepository questionRepository;
-
-    @Autowired
-    EntityManager entityManager;
-
     @Test
-    @DisplayName("저장 테스트 : Audit 데이터 Not Null 테스트")
-    public void save() {
-        Answer answer = answerRepository.save(A1);
-        assertThat(answer.getCreatedAt()).isNotNull();
-        assertThat(answer.getUpdatedAt()).isNotNull();
+    @DisplayName("isOwner 테스트 : 답변 작성자가 유저와 같은지 테스트")
+    public void isOwnerTest() {
+        User answerUser = createUser("DEVELOPYO");
+        Question question = createQuestion().writeBy(createUser("TESTER"));
+        Answer answer = createAnswer(answerUser, question);
+
+        assertThat(answer.isOwner(answerUser)).isTrue();
     }
 
     @Test
-    @DisplayName("조회 객체 같은지 테스트")
-    public void find() {
-        Answer answer = answerRepository.save(A1);
-        Answer actual = answerRepository.findById(answer.getId()).get();
-        assertThat(answer).isEqualTo(actual);
+    @DisplayName("answer 생성자 테스트 : 작성자 없을 경우 예외 처리 테스트")
+    public void createAnswerTest() {
+        assertThatThrownBy(() -> new Answer(null, createQuestion(), "contents")).isInstanceOf(UnAuthorizedException.class);
     }
 
     @Test
-    @DisplayName("삭제되지 않은 데이터만 조회되는지 테스트")
-    public void findByQuestionIdAndDeletedFalseTest() {
-        answerRepository.save(A1);
-        Answer answer2 = answerRepository.save(A2);
-        answer2.setDeleted(true);
-        List<Answer> answers = answerRepository.findByQuestionIdAndDeletedFalse(QuestionTest.Q1.getId());
-        assertThat(answers).hasSize(1);
-        assertThat(answers.stream().noneMatch(Answer::isDeleted)).isTrue();
-    }
-
-    @Test
-    @DisplayName("삭제되지 않은 데이터만 조회되는지 테스트")
-    public void findByIdAndDeletedFalseTest() {
-        Answer answer = answerRepository.save(A1);
-        answer.setDeleted(true);
-        Optional<Answer> actual = answerRepository.findByIdAndDeletedFalse(answer.getId());
-        assertThat(actual).isEmpty();
-    }
-
-    @Test
-    @DisplayName("auditing 호출시점 테스트 : @PreUpdate 에서 호출되므로 persist 시점(@PrePersist)이 아닌 update 시점(@PreUpdate)에 저장되는지 확인")
-    public void auditingLastModifiedDateTest() {
-        Answer answer = answerRepository.saveAndFlush(A1);
-        LocalDateTime initialUpdatedAt = answer.getUpdatedAt();
-        answer.setContents("test");
-        LocalDateTime persistUpdateAt = answer.getUpdatedAt();
-        assertThat(initialUpdatedAt).isEqualTo(persistUpdateAt);
-        answerRepository.flush();
-        LocalDateTime updatedUpdateAt = answer.getUpdatedAt();
-        assertThat(updatedUpdateAt).isNotEqualTo(persistUpdateAt);
-    }
-
-    @Test
-    @DisplayName("auditing 호출시점 테스트 : commit 시점에 저장되는지 확인")
-    public void auditingCreatedDateTest() {
-        Answer answer = answerRepository.saveAndFlush(A1);
-        try {
-            Thread.sleep(1000);
-        } catch (Exception e) {
-        }
-        Question question = questionRepository.save(QuestionTest.Q1);
-        LocalDateTime answerCreatedAt = answer.getCreatedAt();
-        LocalDateTime questionCreatedAt = question.getCreatedAt();
-        entityManager.flush();
-        assertThat(answerCreatedAt).isNotEqualTo(questionCreatedAt);
+    @DisplayName("answer 생성자 테스트 : 질문글 없을 경우 예외 처리 테스트")
+    public void createAnswerTest2() {
+        assertThatThrownBy(() -> new Answer(createUser("DEVELOPYO"), null, "contents")).isInstanceOf(NotFoundException.class);
     }
 }
--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -2,46 +2,33 @@ package qna.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.context.annotation.Import;
-import qna.config.JpaAuditingConfiguration;
-
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static qna.domain.DomainTestFactory.*;
 
 @DataJpaTest
-@Import(value = {JpaAuditingConfiguration.class})
 public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
 
-    @Autowired
-    QuestionRepository questionRepository;
-
     @Test
-    @DisplayName("not null 저장 테스트")
-    public void saveTest() {
-        Question question = new Question(null, "content").writeBy(UserTest.JAVAJIGI);
-        assertThatThrownBy(() -> questionRepository.save(question)).isInstanceOf(Exception.class);
+    @DisplayName("작성자가 일치하는지 테스트")
+    public void isOwnerTest() {
+        User user = createUser("DEVELOPYO");
+        Question question = createQuestion().writeBy(user);
+        assertThat(question.isOwner(user)).isTrue();
     }
 
     @Test
-    @DisplayName("삭제된 데이터만 가져오는지 테스트")
-    public void findByDeletedFalseTest() {
-        Question question = questionRepository.save(Q1);
-        question.setDeleted(true);
-        questionRepository.save(Q2);
-        List<Question> byDeletedFalse = questionRepository.findByDeletedFalse();
-        assertThat(byDeletedFalse).hasSize(1);
+    @DisplayName("질문글에 추가한 답변의 질문글 값이 질문글과 동일 객체인지 테스트")
+    public void addAnswerTest() {
+        User user = createUser("DEVELOPYO");
+        Question question = createQuestion();
+        Answer answer = createAnswer(user, question);
+        question.addAnswer(answer);
+
+        assertThat(answer.getQuestion()).isEqualTo(question);
     }
 
-    @Test
-    public void findByIdAndDeletedFalseTest() {
-        Question question = questionRepository.save(Q1);
-        question.setDeleted(true);
-        assertThat(questionRepository.findByIdAndDeletedFalse(question.getId())).isEmpty();
-    }
 }
