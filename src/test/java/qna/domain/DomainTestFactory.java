2da5af8cbb807fd51e429562bfec9c5c88ca141c
develo-pyo <jungpyo9@gmail.com>
2022-11-11 03:17:09 +0900

    feat: 도메인 테스트 픽스처 구현

---
+++ src/test/java/qna/domain/DomainTestFactory.java
@@ -0,0 +1,22 @@
+package qna.domain;
+
+import java.time.LocalDateTime;
+
+public class DomainTestFactory {
+
+    public static Answer createAnswer(User user, Question question) {
+        return new Answer(user, question, "contents");
+    }
+
+    public static User createUser(String id) {
+        return new User(id, "password", "name", "test@test.com");
+    }
+
+    public static Question createQuestion() {
+        return new Question("title", "contents");
+    }
+
+    public static DeleteHistory createDeleteHistory(ContentType contentType, Long contentId, User user) {
+        return new DeleteHistory(contentType, contentId, user, LocalDateTime.now());
+    }
+}
