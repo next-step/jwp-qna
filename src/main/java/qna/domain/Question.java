ede87a169752d09a9fb5072c82f6dd8ed771a6ab
seogineer <dgseo8981@gmail.com>
2022-11-10 11:56:17 +0900

    refactor: Questiona-Answer orphanRemoval 추가

--- src/main/java/qna/domain/Question.java
+++ src/main/java/qna/domain/Question.java
@@ -2,8 +2,10 @@ package qna.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -29,7 +31,12 @@ public class Question extends BaseTimeEntity {
     @OneToOne
     private User writer;
 
-    @OneToMany(mappedBy = "question")
+    @OneToMany(
+        mappedBy = "question",
+        fetch = FetchType.LAZY,
+        cascade = CascadeType.PERSIST,
+        orphanRemoval = true
+    )
     private List<Answer> answers = new ArrayList<>();
 
     @Column(nullable = false)
