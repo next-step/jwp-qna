e3a11105b4c797d3cf3607fb177bc3861258c8e9
seogineer <dgseo8981@gmail.com>
2022-11-10 11:26:26 +0900

    refactor: 관계 매핑 수정
    - User writer -> Long writerId
    - Question @ManyToOne -> @ManyToOne(fetch = FetchType.LAZY)

--- src/main/java/qna/domain/Answer.java
+++ src/main/java/qna/domain/Answer.java
@@ -3,15 +3,17 @@ package qna.domain;
 import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
-import qna.NotFoundException;
-import qna.UnAuthorizedException;
+import qna.common.base.BaseTimeEntity;
+import qna.common.exception.NotFoundException;
+import qna.common.exception.UnAuthorizedException;
 
 @Entity
 @Table
@@ -20,10 +22,10 @@ public class Answer extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @OneToOne
-    private User writer;
+    private Long writerId;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "question_id")
     private Question question;
 
     @Lob
@@ -35,14 +37,14 @@ public class Answer extends BaseTimeEntity {
     protected Answer() {
     }
 
-    public Answer(User writer, Question question, String contents) {
-        this(null, writer, question, contents);
+    public Answer(Long writerId, Question question, String contents) {
+        this(null, writerId, question, contents);
     }
 
-    public Answer(Long id, User writer, Question question, String contents) {
+    public Answer(Long id, Long writerId, Question question, String contents) {
         this.id = id;
 
-        if (Objects.isNull(writer)) {
+        if (Objects.isNull(writerId)) {
             throw new UnAuthorizedException();
         }
 
@@ -50,13 +52,13 @@ public class Answer extends BaseTimeEntity {
             throw new NotFoundException();
         }
 
-        this.writer = writer;
+        this.writerId = writerId;
         this.question = question;
         this.contents = contents;
     }
 
     public boolean isOwner(User writer) {
-        return this.writer.getId().equals(writer.getId());
+        return this.writerId.equals(writer.getId());
     }
 
     public void toQuestion(Question question) {
@@ -68,7 +70,7 @@ public class Answer extends BaseTimeEntity {
     }
 
     public Long getWriterId() {
-        return writer.getId();
+        return writerId;
     }
 
     public Long getQuestionId() {
@@ -91,7 +93,7 @@ public class Answer extends BaseTimeEntity {
     public String toString() {
         return "Answer{" +
                 "id=" + id +
-                ", writer=" + writer +
+                ", writerId=" + writerId +
                 ", question=" + question +
                 ", contents='" + contents + '\'' +
                 ", deleted=" + deleted +
--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -8,8 +8,8 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 @DataJpaTest
 public class AnswerTest {
-    public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
-    public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+    public static final Answer A1 = new Answer(1L, QuestionTest.Q1, "Answers Contents1");
+    public static final Answer A2 = new Answer(2L, QuestionTest.Q1, "Answers Contents2");
 
     @Test
     void 값_검증() {
