4ba1134f5de2efe3b80e3254e7d4e0ac765a7bfb
SanghoonSon <mue0329@naver.com>
2022-11-12 01:53:47 +0900

    feat(Answer): 답변 entity 구성

--- src/main/java/qna/domain/Answer.java
+++ src/main/java/qna/domain/Answer.java
@@ -2,92 +2,104 @@ package qna.domain;
 
 import qna.exception.NotFoundException;
 import qna.exception.UnAuthorizedException;
+import qna.message.AnswerMessage;
 
+import javax.persistence.*;
 import java.util.Objects;
 
-public class Answer {
+@Entity
+@Table(name = "answer")
+public class Answer extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Long writerId;
-    private Long questionId;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(foreignKey = @ForeignKey(name = "fk_answer_user"))
+    private User writer;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(foreignKey = @ForeignKey(name = "fk_answer_question"))
+    private Question question;
+
+    @Lob
     private String contents;
+
+    @Column(columnDefinition = "boolean default false")
     private boolean deleted = false;
 
-    public Answer(User writer, Question question, String contents) {
+    protected Answer() {
+
+    }
+
+    public Answer (User writer, Question question, String contents) {
         this(null, writer, question, contents);
     }
 
-    public Answer(Long id, User writer, Question question, String contents) {
+    public Answer (Long id, User writer, Question question, String contents) {
+        validateWriter(writer);
+        validateQuestion(question);
+
         this.id = id;
-
-        if (Objects.isNull(writer)) {
-            throw new UnAuthorizedException();
-        }
-
-        if (Objects.isNull(question)) {
-            throw new NotFoundException();
-        }
-
-        this.writerId = writer.getId();
-        this.questionId = question.getId();
+        this.writer = writer;
+        toQuestion(question);
         this.contents = contents;
     }
 
-    public boolean isOwner(User writer) {
-        return this.writerId.equals(writer.getId());
+    private void validateQuestion(Question question) {
+        if (Objects.isNull(question)) {
+            throw new NotFoundException(AnswerMessage.ERROR_QUESTION_SHOULD_BE_NOT_NULL.message());
+        }
     }
 
-    public void toQuestion(Question question) {
-        this.questionId = question.getId();
+    private void validateWriter(User writer) {
+        if (Objects.isNull(writer)) {
+            throw new UnAuthorizedException(AnswerMessage.ERROR_WRITER_SHOULD_BE_NOT_NULL.message());
+        }
     }
 
     public Long getId() {
-        return id;
+        return this.id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public User writer() {
+        return this.writer;
     }
 
-    public Long getWriterId() {
-        return writerId;
+    public boolean isOwner(User writer) {
+        return this.writer.equals(writer);
     }
 
-    public void setWriterId(Long writerId) {
-        this.writerId = writerId;
-    }
-
-    public Long getQuestionId() {
-        return questionId;
-    }
-
-    public void setQuestionId(Long questionId) {
-        this.questionId = questionId;
-    }
-
-    public String getContents() {
-        return contents;
-    }
-
-    public void setContents(String contents) {
-        this.contents = contents;
+    public void delete() {
+        this.deleted = true;
     }
 
     public boolean isDeleted() {
-        return deleted;
+        return this.deleted;
     }
 
-    public void setDeleted(boolean deleted) {
-        this.deleted = deleted;
+    public void toQuestion(Question question) {
+        this.question = question;
     }
 
     @Override
-    public String toString() {
-        return "Answer{" +
-                "id=" + id +
-                ", writerId=" + writerId +
-                ", questionId=" + questionId +
-                ", contents='" + contents + '\'' +
-                ", deleted=" + deleted +
-                '}';
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Answer answer = (Answer) o;
+
+        if (!Objects.equals(id, answer.id)) return false;
+        if (!Objects.equals(writer, answer.writer)) return false;
+        return Objects.equals(question, answer.question);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (writer != null ? writer.hashCode() : 0);
+        result = 31 * result + (question != null ? question.hashCode() : 0);
+        return result;
     }
 }
--- src/main/java/qna/repository/AnswerRepository.java
+++ src/main/java/qna/repository/AnswerRepository.java
@@ -2,6 +2,7 @@ package qna.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import qna.domain.Answer;
+import qna.domain.User;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,4 +11,6 @@ public interface AnswerRepository extends JpaRepository<Answer, Long> {
     List<Answer> findByQuestionIdAndDeletedFalse(Long questionId);
 
     Optional<Answer> findByIdAndDeletedFalse(Long id);
+
+    List<Answer> findByWriterAndDeletedFalse(User writer);
 }
