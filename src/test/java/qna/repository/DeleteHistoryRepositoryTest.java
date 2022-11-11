2259a383a7405817603f85dde0fa6bb12a218527
deokmoon <deok2moon@gmail.com>
2022-11-12 01:59:53 +0900

    test: 삭제이력 조회 테스트코드 작성

--- src/test/java/qna/repository/DeleteHistoryRepositoryTest.java
+++ src/test/java/qna/repository/DeleteHistoryRepositoryTest.java
@@ -1,10 +1,21 @@
 package qna.repository;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import qna.domain.Answer;
+import qna.domain.ContentType;
 import qna.domain.DeleteHistory;
+import qna.domain.Question;
+import qna.domain.TestAnswerFactory;
+import qna.domain.TestDeleteHistoryFactory;
+import qna.domain.TestQuestionFactory;
+import qna.domain.TestUserFactory;
+import qna.domain.User;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -21,7 +32,30 @@ class DeleteHistoryRepositoryTest {
 
     @Test
     void deleteHistory_save_test() {
-//        DeleteHistory deleteHistory = deleteHistoryRepository.save(D1);
-//        assertThat(deleteHistory.getId()).isNotNull();
+        // given
+        User writer = TestUserFactory.create("javajigi");
+        Question question = TestQuestionFactory.create(writer);
+        Answer answer = TestAnswerFactory.create(writer, question);
+        DeleteHistory deleteHistory = TestDeleteHistoryFactory.create(ContentType.ANSWER, answer.getId(), answer.getUser());
+        // when
+        DeleteHistory saveDeleteHistory = deleteHistoryRepository.save(deleteHistory);
+        // then
+        assertThat(saveDeleteHistory.getId()).isNotNull();
+    }
+
+    @Test
+    void retreive_deleteHistory_test() {
+        // given
+        User writer = TestUserFactory.create("javajigi");
+        Question question = TestQuestionFactory.create(writer);
+        Answer answer = TestAnswerFactory.create(writer, question);
+        DeleteHistory deleteHistory1 = TestDeleteHistoryFactory.create(ContentType.ANSWER, answer.getId(), answer.getUser());
+        DeleteHistory deleteHistory2 = TestDeleteHistoryFactory.create(ContentType.ANSWER, answer.getId(), answer.getUser());
+        // when
+        deleteHistoryRepository.save(deleteHistory1);
+        deleteHistoryRepository.save(deleteHistory2);
+        List<DeleteHistory> deleteHistories = deleteHistoryRepository.findAll();
+        // then
+        assertThat(deleteHistories).hasSize(2);
     }
 }
