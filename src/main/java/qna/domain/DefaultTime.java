8afcd6c05efda4bd4be131d40db759faa634689f
yomni <yearsheep@gmail.com>
2022-11-11 15:55:47 +0900

    refactor: DefaultTime 사용을 @EnableJpaAuditing 으로 변경

--- src/main/java/qna/domain/DefaultTime.java
+++ src/main/java/qna/domain/DefaultTime.java
@@ -1,20 +1,24 @@
 package qna.domain;
 
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
+import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
 import static java.time.LocalDateTime.*;
 
+@EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 public abstract class DefaultTime {
-
     @Column(nullable = false, updatable = false)
-    private final LocalDateTime createdAt = now();
-    private LocalDateTime updatedAt = now();
+    @CreatedDate
+    private LocalDateTime createdAt;
 
-    public void update() {
-        this.updatedAt = now();
-    }
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
 }
