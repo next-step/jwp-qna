1db4a0dca10ac5cdbd7ed24a7c36dd108e44c9c0
victor-jo <joasjn45@gmail.com>
2022-11-12 21:22:38 +0900

    refactor(deleteHistoryTest): 테스트 방법 리팩터링
    - static 으로 선언된 필드를 사용하지 않고 자체 테스트

--- src/test/java/qna/domain/DeleteHistoryTest.java
+++ src/test/java/qna/domain/DeleteHistoryTest.java
@@ -1,24 +1,18 @@
 package qna.domain;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import qna.config.TruncateConfig;
 
 import java.time.LocalDateTime;
-import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @EnableJpaAuditing
-class DeleteHistoryTest extends TruncateConfig {
-
-    public static final DeleteHistory DH1 = new DeleteHistory(ContentType.ANSWER, 1L, UserTest.JAVAJIGI, LocalDateTime.now());
-    public static final DeleteHistory DH2 = new DeleteHistory(ContentType.QUESTION, 2L, UserTest.JAVAJIGI, LocalDateTime.now());
+class DeleteHistoryTest {
 
     @Autowired
     DeleteHistoryRepository deleteHistories;
@@ -26,28 +20,24 @@ class DeleteHistoryTest extends TruncateConfig {
     @Autowired
     UserRepository users;
 
+    DeleteHistory history;
+    User user;
+
     @BeforeEach
     void setUp() {
-        users.save(UserTest.JAVAJIGI);
-        users.save(UserTest.SANJIGI);
+        user = users.save(new User("userId", "password", "name", "email"));
+        history = deleteHistories.save(new DeleteHistory(ContentType.QUESTION, 1L, user, LocalDateTime.now()));
     }
 
-    @ParameterizedTest(name = "save_테스트")
-    @MethodSource("deleteHistoryTestFixture")
-    void save_테스트(DeleteHistory deleteHistory) {
-        DeleteHistory saved = deleteHistories.save(deleteHistory);
-        assertThat(saved.getCreateDate()).isNotNull();
+    @Test
+    void save_테스트() {
+        assertThat(history.getId()).isNotNull();
+        assertThat(history.getCreateDate()).isNotNull();
     }
 
-    @ParameterizedTest(name = "save_후_findById_테스트")
-    @MethodSource("deleteHistoryTestFixture")
-    void save_후_findById_테스트(DeleteHistory deleteHistory) {
-        DeleteHistory history1 = deleteHistories.save(deleteHistory);
-        DeleteHistory history2 = deleteHistories.findById(history1.getId()).get();
-        assertThat(history1).isEqualTo(history2);
-    }
-
-    static Stream<DeleteHistory> deleteHistoryTestFixture() {
-        return Stream.of(DH1, DH2);
+    @Test
+    void save_후_findById_테스트() {
+        DeleteHistory findHistory = deleteHistories.findById(history.getId()).get();
+        assertThat(history).isEqualTo(findHistory);
     }
 }
