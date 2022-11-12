2c3e8f415c31d66f2ee869abd3a05ec6637e5fb2
Bryan | JJ <with.beth@gmail.com>
2022-11-12 19:18:47 +0900

    feat(User): UserEntity수정

--- src/main/java/qna/domain/User.java
+++ src/main/java/qna/domain/User.java
@@ -98,6 +98,23 @@ public class User extends BaseTimeEntity {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return Objects.equals(id, user.id) && userId.equals(user.userId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, userId);
+    }
+
+    @Override
     public String toString() {
         return "User{" +
             "id=" + id +
