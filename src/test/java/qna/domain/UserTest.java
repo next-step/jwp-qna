e91336cb643d300ae97eb99b6ca00a7af0888c44
victor-jo <joasjn45@gmail.com>
2022-11-12 21:46:35 +0900

    refactor(userTest): 테스트 방법 리팩터링
    - static 으로 선언된 필드를 사용하지 않고 자체 테스트

--- src/test/java/qna/domain/UserTest.java
+++ src/test/java/qna/domain/UserTest.java
@@ -1,6 +1,6 @@
 package qna.domain;
 
-import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -9,12 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import qna.config.TruncateConfig;
 
 import java.util.Arrays;
+import java.util.Optional;
 import java.util.function.Function;
-import java.util.function.Supplier;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -22,7 +20,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DataJpaTest
 @EnableJpaAuditing
-public class UserTest extends TruncateConfig {
+public class UserTest {
     public static final User JAVAJIGI = new User(1L, "javajigi", "password", "name", "javajigi@slipp.net");
     public static final User SANJIGI = new User(2L, "sanjigi", "password", "name", "sanjigi@slipp.net");
 
@@ -30,28 +28,31 @@ public class UserTest extends TruncateConfig {
     UserRepository users;
 
 
+    User user;
+
+    @BeforeEach
+    void setUp() {
+        user = users.save(new User("userId", "password", "name", "email"));
+    }
+
     @Test
     void 중복되는_유저아이디_save() {
-        users.save(JAVAJIGI);
-        User duplicationUser = new User(2L, "javajigi", "password", "name", "hello@its.me");
+        User duplicationUser = new User(2L, "userId", "password", "name", "hello@its.me");
         assertThatThrownBy(() -> users.save(duplicationUser))
             .isInstanceOf(RuntimeException.class);
     }
 
-    @ParameterizedTest(name = "save_테스트")
-    @MethodSource("userTestFixture")
-    void save_테스트(User user) {
-        User saved = users.save(user);
-        assertThat(saved).isEqualTo(user);
-        assertThat(saved.getCreatedAt()).isNotNull();
+    @Test
+    void save_테스트() {
+        assertThat(user.getId()).isNotNull();
+        assertThat(user.getCreatedAt()).isNotNull();
     }
 
-    @ParameterizedTest(name = "save_후_findById_테스트")
-    @MethodSource("userTestFixture")
-    void save_후_findById_테스트(User user) {
-        User user1 = users.save(user);
-        User user2 = users.findById(user1.getId()).get();
-        assertThat(user1).isEqualTo(user2);
+    @Test
+    void save_후_findById_테스트() {
+        Optional<User> maybe = users.findById(user.getId());
+        assertThat(maybe.isPresent()).isTrue();
+        assertThat(maybe.get()).isEqualTo(user);
     }
 
     @ParameterizedTest(name = "{0}")
@@ -87,9 +88,5 @@ public class UserTest extends TruncateConfig {
                 "이메일은 50자를 넘지 않는다", new User("userId", "password", "name", repeatString.apply(EMAIL_LIMIT))
             )
         );
-    }
-
-    static Stream<User> userTestFixture() {
-        return Stream.of(JAVAJIGI, SANJIGI);
     }
 }
