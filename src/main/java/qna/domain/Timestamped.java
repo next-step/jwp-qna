8524d628bfd4be4b3570ab840935d7e4d2d97269
오준오 <jack.ohjoonoh@gmail.com>
2022-11-12 11:43:16 +0900

    feat(domain) : User 도메인 구현

---
+++ src/main/java/qna/domain/Timestamped.java
@@ -0,0 +1,20 @@
+package qna.domain;
+
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class Timestamped {
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createAt;
+    @LastModifiedDate
+    @Column(updatable = false)
+    private LocalDateTime modifiedAt;
+}
