9ebb2b9760ed0f1e0df5aeb3aabe24dc62277bc9
chosundeveloper <67059660+chosundeveloper@users.noreply.github.com>
2022-11-13 19:50:13 +0900

    test(DeleteHistoryiesTest.java) : 삭제 내역 리스트를 추가한다.

--- src/test/java/qna/domain/DeleteHistoriesTest.java
+++ src/test/java/qna/domain/DeleteHistoriesTest.java
@@ -2,6 +2,9 @@ package qna.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
+import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -21,5 +24,23 @@ class DeleteHistoriesTest {
         DeleteHistories deleteHistories = new DeleteHistories();
         deleteHistories.add(DeleteHistory.ofQuestion(question.getId(), javajigi));
         assertThat(deleteHistories.size()).isEqualTo(1);
+    }
+
+    @DisplayName("삭제 내역을 리스트를 추가한다.")
+    @Test
+    void addAll() throws CannotDeleteException {
+
+        User javajigi = UserTest.JAVAJIGI_ID;
+
+        Question question = QuestionTest.QUESTION_1_ID;
+        question.addAnswer(AnswerTest.ANSWER_1_ID);
+        question.addAnswer(AnswerTest.ANSWER_2_ID);
+
+        Answer answer1 = AnswerTest.ANSWER_1_ID;
+        Answer answer2 = AnswerTest.ANSWER_2_ID;
+
+        DeleteHistories deleteHistories = new DeleteHistories();
+        deleteHistories.addAll(Arrays.asList(DeleteHistory.ofAnswer(answer1.getId(), javajigi), DeleteHistory.ofAnswer(answer2.getId(), javajigi)));
+        assertThat(deleteHistories.size()).isEqualTo(2);
     }
 }
