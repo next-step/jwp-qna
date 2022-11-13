2b5f57c83c84a7fc647c73b6df39c1f81f12b650
chosundeveloper <67059660+chosundeveloper@users.noreply.github.com>
2022-11-13 12:04:14 +0900

    test(Questiontest.java) : 질문자와 답변자가 다른 경우 답을 삭제할 수 없다.

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -2,8 +2,10 @@ package qna.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
 
 import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayName("질문")
 public class QuestionTest {
@@ -14,5 +16,13 @@ public class QuestionTest {
     @Test
     void constructor() {
         assertThatNoException().isThrownBy(() -> new Question(1L, "title1", "contents1").writeBy(UserTest.JAVAJIGI));
+    }
+
+    @DisplayName("질문자와 답변자가 다른 경우 답을 삭제할 수 없다.")
+    @Test
+    void delete_fail_exist_answer() {
+        QuestionTest.QUESTION_2.addAnswer(AnswerTest.ANSWER_1);
+        assertThatThrownBy(QuestionTest.QUESTION_2::delete)
+                .isInstanceOf(CannotDeleteException.class);
     }
 }
