e901bcf4f78c315decdbbaea9d809bdc3de1f618
chosundeveloper <67059660+chosundeveloper@users.noreply.github.com>
2022-11-13 19:21:53 +0900

    refactor(DeleteHistoryTest.java) : 미사용 상수 삭제

--- src/test/java/qna/domain/DeleteHistoryTest.java
+++ src/test/java/qna/domain/DeleteHistoryTest.java
@@ -5,13 +5,10 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThatNoException;
 import static qna.domain.AnswerTest.ANSWER_1;
-import static qna.domain.QuestionTest.QUESTION_1;
 import static qna.domain.UserTest.JAVAJIGI;
 
 @DisplayName("삭제 내역")
 public class DeleteHistoryTest {
-    public static final DeleteHistory DELETE_HISTORY_ANSWER = DeleteHistory.ofQuestion(ANSWER_1.getId(), JAVAJIGI);
-    public static final DeleteHistory DELETE_HISTORY_QUESTION = DeleteHistory.ofAnswer(QUESTION_1.getId(), QUESTION_1.getWriter());
 
     @DisplayName("질문 삭제 내역 생성")
     @Test
