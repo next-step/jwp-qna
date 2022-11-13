eae94e1998faf0515963363e6edda91eaf4787d0
민형준 <aksgudwns@gmail.com>
2022-11-14 01:54:41 +0900

    fix - 잘못된 정답 오류메시지 수정

--- src/main/java/qna/domain/ErrorMessage.java
+++ src/main/java/qna/domain/ErrorMessage.java
@@ -2,7 +2,7 @@ package qna.domain;
 
 public enum ErrorMessage {
     CANNOT_DELETE_QUESTION_EXCEPTION("질문을 삭제할 권한이 없습니다."),
-    CANNOT_DELETE_ANSWER_EXCEPTION("질문을 삭제할 권한이 없습니다.");
+    CANNOT_DELETE_ANSWER_EXCEPTION("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
 
     private String message;
 
