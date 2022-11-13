5c3d52b39ac3ccb047a5198ed1a5274d9d5583e1
chosundeveloper <67059660+chosundeveloper@users.noreply.github.com>
2022-11-13 20:03:08 +0900

    test(AnswerTest.java) : 답변을 삭제한다.

--- src/test/java/qna/domain/AnswersTest.java
+++ src/test/java/qna/domain/AnswersTest.java
@@ -4,8 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import qna.CannotDeleteException;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 @DisplayName("답변 일급 콜렉션")
@@ -14,9 +13,11 @@ class AnswersTest {
     @DisplayName("답변을 추가한다.")
     @Test
     void add() {
+
         Answer answer = AnswerTest.ANSWER_1_ID;
         Answers answers = new Answers();
         answers.add(answer);
+
         assertAll(
                 () -> assertThat(answers.getAnswers()).hasSize(1),
                 () -> assertThat(answers.getAnswers()).containsExactly(answer)
@@ -26,10 +27,23 @@ class AnswersTest {
     @DisplayName("질문자와 답변자가 다른 경우 답을 삭제할 수 없다.")
     @Test
     void delete_fail() {
+
         Answer answer = AnswerTest.ANSWER_1_ID;
         Answers answers = new Answers();
         answers.add(answer);
+
         assertThatThrownBy(() -> answers.delete(UserTest.SANJIGI))
                 .isInstanceOf(CannotDeleteException.class);
+    }
+
+    @DisplayName("답변을 삭제한다.")
+    @Test
+    void delete_success() {
+
+        Answer answer = AnswerTest.ANSWER_1_ID;
+        Answers answers = new Answers();
+        answers.add(answer);
+
+        assertThatNoException().isThrownBy(() -> answers.delete(answer.getWriter()));
     }
 }
