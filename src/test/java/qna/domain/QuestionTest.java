596bb3d91e9cdc1677b66c829d5556d380addc3e
지영진 <wldudwls12@gmail.com>
2022-11-14 20:42:14 +0900

    test: given에 테스트 대상을 위한 설정

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,15 +1,12 @@
 package qna.domain;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.jdbc.Sql;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
-@Sql("/truncate.sql")
 @DataJpaTest
 public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
@@ -21,23 +18,19 @@ public class QuestionTest {
     @Autowired
     private UserRepository userRepository;
 
-    @BeforeEach
-    void setUp() {
-        userRepository.save(UserTest.JAVAJIGI);
-        userRepository.save(UserTest.SANJIGI);
-    }
-
     @Test
     void 저장_및_조회() {
-        Question question1 = questionRepository.save(Q1);
-        Question question2 = questionRepository.save(Q2);
+        User user1 = userRepository.save(new User("userId", "password", "name", "email"));
+        User user2 = userRepository.save(new User("userId2", "password", "name", "email"));
+        Question question1 = questionRepository.save(new Question("title", "contents").writeBy(user1));
+        Question question2 = questionRepository.save(new Question("title", "contents").writeBy(user2));
 
         Question retrievedQuestion1 = questionRepository.findById(question1.getId()).get();
         Question retrievedQuestion2 = questionRepository.findById(question2.getId()).get();
 
         assertAll(
-                () -> assertThat(retrievedQuestion1.getId()).isEqualTo(Q1.getId()),
-                () -> assertThat(retrievedQuestion2.getId()).isEqualTo(Q2.getId())
+                () -> assertThat(retrievedQuestion1.getId()).isEqualTo(question1.getId()),
+                () -> assertThat(retrievedQuestion2.getId()).isEqualTo(question2.getId())
         );
     }
 }
--- src/test/java/qna/domain/UserTest.java
+++ src/test/java/qna/domain/UserTest.java
@@ -3,11 +3,9 @@ package qna.domain;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.jdbc.Sql;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@Sql("/truncate.sql")
 @DataJpaTest
 public class UserTest {
     public static final User JAVAJIGI = new User(1L, "javajigi", "password", "name", "javajigi@slipp.net");
