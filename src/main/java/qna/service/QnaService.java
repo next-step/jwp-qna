6f3adad9804e1a12e18176810d808b6c7b12b8ea
deokmoon <deok2moon@gmail.com>
2022-11-15 00:15:41 +0900

    refactor(domain): user 참조객체 필드명 변경에 따른 호출 메서드명 수정  - user -> writer로 변수명 직관적으로 수정

--- src/main/java/qna/service/QnaService.java
+++ src/main/java/qna/service/QnaService.java
@@ -53,7 +53,7 @@ public class QnaService {
         deleteHistories.add(new DeleteHistory(ContentType.QUESTION, questionId, question.getUser(), LocalDateTime.now()));
         for (Answer answer : answers) {
             answer.setDeleted(true);
-            deleteHistories.add(new DeleteHistory(ContentType.ANSWER, answer.getId(), answer.getUser(), LocalDateTime.now()));
+            deleteHistories.add(new DeleteHistory(ContentType.ANSWER, answer.getId(), answer.getWriter(), LocalDateTime.now()));
         }
         deleteHistoryService.saveAll(deleteHistories);
     }
