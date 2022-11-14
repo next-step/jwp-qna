686f9b0954b62fd6ec5530434bbf8251a6853f6e
Heonseop <shinsub6120@gmail.com>
2022-11-15 01:02:00 +0900

    fix : 사용하지 않는 함수 삭제

--- src/test/java/qna/repository/AnswerRepositoryTest.java
+++ src/test/java/qna/repository/AnswerRepositoryTest.java
@@ -3,7 +3,6 @@ package qna.repository;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
-import java.util.List;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -36,13 +35,11 @@ public class AnswerRepositoryTest {
     private TestEntityManager manager;
 
     private User user1;
-    private User user2;
     private Question question;
 
     @BeforeEach
     void before() {
         user1 = userRepository.save(UserTest.JAVAJIGI);
-        user2 = userRepository.save(UserTest.SANJIGI);
         question = questionRepository.save(new Question("title1", "contents1").writeBy(user1));
     }
 
@@ -64,16 +61,6 @@ public class AnswerRepositoryTest {
                 () -> assertThat(answer.getWriter()).isEqualTo(inputAnswer.getWriter()),
                 () -> assertThat(answer.getQuestion()).isEqualTo(inputAnswer.getQuestion())
         );
-    }
-
-    @DisplayName("findByQuestionIdAndDeletedFalse_question_id_기준으로_조회")
-    @Test
-    void findByQuestionIdAndDeletedFalse() {
-        Answer answer1 = answerRepository.save(new Answer(user1, question, "Answers Contents1"));
-        Answer answer2 = answerRepository.save(new Answer(user2, question, "Answers Contents2"));
-        List<Answer> expectedResults = answerRepository.findByQuestionIdAndDeletedFalse(answer1.getQuestion().getId());
-
-        assertThat(expectedResults).contains(answer1);
     }
 
     @DisplayName("findByIdAndDeletedFalse_deleted_false인_data_조회")
