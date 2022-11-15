e9386eaaf1385426fee307ff216e6769bd5da915
soojungkk <sj921101@naver.com>
2022-11-16 01:25:59 +0900

    test : DataJpaTest 테스트 코드 추가

--- src/test/java/qna/domain/AnswerRepositoryTest.java
+++ src/test/java/qna/domain/AnswerRepositoryTest.java
@@ -1,2 +1,27 @@
-package qna.domain;public class AnswerRepositoryTest {
+package qna.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static qna.domain.AnswerTest.A2;
+
+import java.util.Optional;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+public class AnswerRepositoryTest {
+    @Autowired
+    private AnswerRepository answerRepository;
+
+    @Test
+    @DisplayName("answer_id로 검색 테스트")
+    void find_by_answer_id() {
+        // given
+        answerRepository.save(A2);
+        // when
+        Optional<Answer> answer = answerRepository.findById(A2.getId());
+        // then
+        assertThat(answer).contains(A2);
+    }
 }
