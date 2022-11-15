57fe511b61622f50346ed07538df674e20265f19
Bryan | JJ <with.beth@gmail.com>
2022-11-15 16:25:50 +0900

    feat(Question): add delete including answers feature

--- src/main/java/qna/domain/Question.java
+++ src/main/java/qna/domain/Question.java
@@ -1,7 +1,12 @@
 package qna.domain;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Objects;
 import javax.persistence.Column;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
@@ -10,6 +15,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
+import qna.CannotDeleteException;
 
 @Entity
 public class Question extends BaseTimeEntity {
@@ -27,6 +33,9 @@ public class Question extends BaseTimeEntity {
     @JoinColumn(foreignKey = @ForeignKey(name = "fk_question_writer"), name = "writer_id")
     private User writer;
 
+    @Embedded
+    private Answers answers = new Answers();
+
     @Column(nullable = false)
     private boolean deleted = false;
 
@@ -43,6 +52,33 @@ public class Question extends BaseTimeEntity {
         this.contents = contents;
     }
 
+    public List<DeleteHistory> delete(User loginUser) throws CannotDeleteException {
+        final List<DeleteHistory> result = new ArrayList<>();
+        result.add(deleteQuestion(loginUser));
+        result.addAll(deleteAnswers(loginUser));
+        return Collections.unmodifiableList(result);
+    }
+
+    private DeleteHistory deleteQuestion(User loginUser) throws CannotDeleteException {
+        validateOwner(loginUser);
+        deleted = true;
+        return new DeleteHistory(ContentType.QUESTION, getId(), getWriter(), LocalDateTime.now());
+    }
+
+    private void validateOwner(User loginUser) throws CannotDeleteException {
+        if (!isOwner(loginUser)) {
+            throw new CannotDeleteException("질문을 삭제할 권한이 없습니다.");
+        }
+    }
+
+    private List<DeleteHistory> deleteAnswers(User loginUser) throws CannotDeleteException {
+        try {
+            return answers.delete(loginUser);
+        } catch (CannotDeleteException cannotDeleteException) {
+            throw new CannotDeleteException("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
+        }
+    }
+
     public Question writeBy(User writer) {
         this.writer = writer;
         return this;
@@ -54,6 +90,7 @@ public class Question extends BaseTimeEntity {
 
     public void addAnswer(Answer answer) {
         answer.toQuestion(this);
+        answers.add(answer);
     }
 
     public Long getId() {
@@ -74,10 +111,6 @@ public class Question extends BaseTimeEntity {
 
     public boolean isDeleted() {
         return deleted;
-    }
-
-    public void setDeleted(boolean deleted) {
-        this.deleted = deleted;
     }
 
     @Override
---
+++ src/test/java/qna/domain/QuestionTest.java
@@ -0,0 +1,68 @@
+package qna.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
+public class QuestionTest {
+
+    private User userA;
+    private User userB;
+
+    private Question questionFromA;
+
+    @BeforeEach
+    void setUp() {
+        userA = new User(1L, "javajigi", "password", "name", "javajigi@slipp.net");
+        userB = new User(2L, "sanjigi", "password", "name", "sanjigi@slipp.net");
+
+        questionFromA = new Question(100L, "JPA", "JPA Content").writeBy(userA);
+    }
+
+    @DisplayName("로그인 사용자와 질문한 사람이 다른 경우, 질문 삭제 불가능 해야 한다")
+    @Test
+    void delete_differentUser() {
+        assertThatExceptionOfType(CannotDeleteException.class)
+            .isThrownBy(() -> questionFromA.delete(userB))
+            .withMessageContaining("질문을 삭제할 권한이 없습니다");
+    }
+
+    @DisplayName("로그인 사용자와 질문한 사람이 같은 경우, 질문 삭제 가능 해야 한다")
+    @Test
+    void delete_sameUser() throws CannotDeleteException {
+        assertThat(questionFromA.isDeleted()).isFalse();
+
+        final List<DeleteHistory> deleteHistories = questionFromA.delete(userA);
+        assertThat(questionFromA.isDeleted()).isTrue();
+        assertThat(deleteHistories).containsExactly(
+            new DeleteHistory(ContentType.QUESTION, 100L, userA, LocalDateTime.now()));
+    }
+
+    @DisplayName("다른 사람이 쓴 답변이 존재하는 경우, 질문 삭제 불가능 해야 한다")
+    @Test
+    void delete_containsOtherUserAnswers() {
+        questionFromA.addAnswer(new Answer(1L, userB, questionFromA,"It's me again, but.."));
+
+        assertThatExceptionOfType(CannotDeleteException.class)
+            .isThrownBy(() -> questionFromA.delete(userA))
+            .withMessageContaining("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다");
+    }
+
+    @DisplayName("본인 외에 다른 사람이 쓴 답변이 존재하지 않는 경우, 질문과 답변 모두 삭제 가능 해야 한다")
+    @Test
+    void delete_onlyContainsOwnAnswers() throws CannotDeleteException {
+        questionFromA.addAnswer(new Answer(1L, userA, questionFromA,"It's me again, but.."));
+
+        final List<DeleteHistory> deleteHistories = questionFromA.delete(userA);
+        assertThat(questionFromA.isDeleted()).isTrue();
+        assertThat(deleteHistories).containsExactly(
+            new DeleteHistory(ContentType.QUESTION, 100L, userA, LocalDateTime.now()),
+            new DeleteHistory(ContentType.ANSWER, 1L, userA, LocalDateTime.now()));
+    }
+}
