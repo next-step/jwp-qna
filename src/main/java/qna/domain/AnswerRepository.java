6585166257ddb154221959dbba037b9854e3f3c4
dominiqn <dmut7691@gmail.com>
2022-11-15 22:35:19 +0900

    chore: 아직 JPA 애노테이션이 붙지 않은 엔티티 관련 스프링 빈 코드 주석처리

--- src/main/java/qna/domain/AnswerRepository.java
+++ src/main/java/qna/domain/AnswerRepository.java
@@ -1,11 +1,11 @@
-package qna.domain;
-
-import java.util.List;
-import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AnswerRepository extends JpaRepository<Answer, Long> {
-    List<Answer> findByQuestionIdAndDeletedFalse(Long questionId);
-
-    Optional<Answer> findByIdAndDeletedFalse(Long id);
-}
+//package qna.domain;
+//
+//import java.util.List;
+//import java.util.Optional;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface AnswerRepository extends JpaRepository<Answer, Long> {
+//    List<Answer> findByQuestionIdAndDeletedFalse(Long questionId);
+//
+//    Optional<Answer> findByIdAndDeletedFalse(Long id);
+//}
--- src/main/java/qna/domain/QuestionRepository.java
+++ src/main/java/qna/domain/QuestionRepository.java
@@ -1,11 +1,11 @@
-package qna.domain;
-
-import java.util.List;
-import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuestionRepository extends JpaRepository<Question, Long> {
-    List<Question> findByDeletedFalse();
-
-    Optional<Question> findByIdAndDeletedFalse(Long id);
-}
+//package qna.domain;
+//
+//import java.util.List;
+//import java.util.Optional;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface QuestionRepository extends JpaRepository<Question, Long> {
+//    List<Question> findByDeletedFalse();
+//
+//    Optional<Question> findByIdAndDeletedFalse(Long id);
+//}
