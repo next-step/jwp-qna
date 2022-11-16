a16a42f1e471bd4f2fb0d8604567a3e235034a0e
gunkim <gunkim.dev@gmail.com>
2022-11-17 00:20:58 +0900

    test: 답변 테스트 케이스 추가

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import qna.CannotDeleteException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
@@ -18,5 +19,15 @@ class AnswerTest {
         DeleteHistory expected = DeleteHistory.ofAnswer(answer.getId(), UserTest.JAVAJIGI);
 
         assertThat(deleteHistory).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("작성자 외에 사용자가 답변을 삭제할 경우 예외가 발생한다")
+    void delete_thrownIllegalArgument() {
+        Answer answer = new Answer(1L, UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
+
+        assertThatThrownBy(() -> answer.delete(UserTest.SANJIGI))
+                .isInstanceOf(CannotDeleteException.class)
+                .hasMessage("댓글 작성자만 댓글을 삭제할 수 있습니다.");
     }
 }
