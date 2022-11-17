46e1a528dd432afe5ab32aec89a289b9b8b17427
오준오 <jack.ohjoonoh@gmail.com>
2022-11-17 21:46:07 +0900

    test(question) : 테스트 코드 작성

--- src/test/java/qna/service/QnaServiceTest.java
+++ src/test/java/qna/service/QnaServiceTest.java
@@ -52,8 +52,6 @@ class QnaServiceTest {
     public void delete_성공() throws Exception {
         when(questionRepository.findById(question.getId())).thenReturn(
             Optional.of(question));
-        when(answerRepository.findByQuestion_IdAndDeletedFalse(question.getId())).thenReturn(
-            Arrays.asList(answer));
 
         assertThat(question.isDeleted()).isFalse();
         qnaService.deleteQuestion(UserTest.JAVAJIGI, question.getId());
@@ -75,8 +73,6 @@ class QnaServiceTest {
     public void delete_성공_질문자_답변자_같음() throws Exception {
         when(questionRepository.findById(question.getId())).thenReturn(
             Optional.of(question));
-        when(answerRepository.findByQuestion_IdAndDeletedFalse(question.getId())).thenReturn(
-            Arrays.asList(answer));
 
         qnaService.deleteQuestion(UserTest.JAVAJIGI, question.getId());
 
@@ -92,8 +88,6 @@ class QnaServiceTest {
 
         when(questionRepository.findById(question.getId())).thenReturn(
             Optional.of(question));
-        when(answerRepository.findByQuestion_IdAndDeletedFalse(question.getId())).thenReturn(
-            Arrays.asList(answer, answer2));
 
         assertThatThrownBy(() -> qnaService.deleteQuestion(UserTest.JAVAJIGI, question.getId()))
             .isInstanceOf(CannotDeleteException.class);
