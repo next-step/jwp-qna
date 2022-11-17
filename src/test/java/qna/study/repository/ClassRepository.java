402c9a22064d692803e1e85a999f2277b06e53af
hahoho87 <bbaing4564@gmail.com>
2022-11-18 00:02:25 +0900

    test: 학습 테스트 추가(2)
    - N+1 문제 관련 학습 테스트 추가

--- src/test/java/qna/study/repository/ClassRepository.java
+++ src/test/java/qna/study/repository/ClassRepository.java
@@ -1,8 +1,18 @@
 package qna.study.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import qna.study.domain.eager.Class;
 
 public interface ClassRepository extends JpaRepository<Class, Long> {
+
+	@EntityGraph(attributePaths = "students")
+	List<Class> findAll();
+
+	@Query("select c from Class as c join fetch c.students")
+	List<Class> findAllWithJpqlFetchJoin();
 }
