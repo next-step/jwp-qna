a267299c3bed775b322c8b75640d3db94040c338
tlaqk229 <tlaqk229@gmail.com>
2022-11-19 15:59:33 +0900

    feat(exercise): 라이브강의 hands-on-coding 실습코드

---
+++ src/main/java/subway/domain/Line.java
@@ -0,0 +1,55 @@
+package subway.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "line")
+public class Line {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "line", fetch = FetchType.LAZY)
+    private List<Station> stations = new ArrayList<>();
+
+    public Line() {
+    }
+
+    public Line(final String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Station> getStations() {
+        return stations;
+    }
+
+    public void setStations(List<Station> stations) {
+        this.stations = stations;
+    }
+
+    public void addStation(Station station) {
+        this.stations.add(station);
+        station.setLine(this);
+    }
+}
