ca6730092cd716189952cf69fa87d8aa7b8305c8
SanghoonSon <mue0329@naver.com>
2022-11-20 00:52:14 +0900

    test(DeleteHistory): TestFixture 변경에 의한 테스트 코드 수정

--- src/test/java/qna/domain/DeleteHistoriesTest.java
+++ src/test/java/qna/domain/DeleteHistoriesTest.java
@@ -1,39 +1,54 @@
 package qna.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import qna.fixtures.AnswerTestFixture;
-import qna.fixtures.QuestionTestFixture;
+import qna.fixtures.UserTestFixture;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class DeleteHistoriesTest {
+
+    private Question question;
+    private Answer answer;
+    private Answer answer2;
+
+    @BeforeEach
+    void setUp() {
+        question = new Question(1L, UserTestFixture.손상훈, "title", "contents");
+        answer = new Answer(1L, UserTestFixture.손상훈, question, "contents");
+        answer2 = new Answer(2L, UserTestFixture.손상훈, question, "contents");
+    }
+
 
     @Test
     @DisplayName("삭제 이력을 추가한다")
     void add_delete_history_test() {
-        Answer answer = AnswerTestFixture.createAnswerWithId(1L);
+        // given
         DeleteHistories deleteHistories = new DeleteHistories();
+
+        // when
         deleteHistories.add(DeleteHistory.ofAnswer(answer));
+
+        // then
         assertThat(deleteHistories.size()).isEqualTo(1);
     }
 
     @Test
     @DisplayName("다른 삭제 이력을 모두 추가한다")
     void add_all_delete_history_test() {
+        // given
         DeleteHistories questionDeleteHistories = new DeleteHistories();
-        Question question = QuestionTestFixture.createQuestionWithId(1L);
         questionDeleteHistories.add(DeleteHistory.ofQuestion(question));
 
         DeleteHistories answerDeleteHistories = new DeleteHistories();
-        Answer answer = AnswerTestFixture.createAnswerWithId(1L);
         answerDeleteHistories.add(DeleteHistory.ofAnswer(answer));
-        Answer answer2 = AnswerTestFixture.createAnswerWithId(2L);
         answerDeleteHistories.add(DeleteHistory.ofAnswer(answer2));
 
+        // when
         questionDeleteHistories.addAll(answerDeleteHistories);
 
+        // then
         assertThat(questionDeleteHistories.size()).isEqualTo(3);
     }
 }
--- src/test/java/qna/domain/DeleteHistoryTest.java
+++ src/test/java/qna/domain/DeleteHistoryTest.java
@@ -1,27 +1,40 @@
 package qna.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import qna.fixtures.AnswerTestFixture;
 import qna.fixtures.QuestionTestFixture;
+import qna.fixtures.UserTestFixture;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class DeleteHistoryTest {
 
+    private Question question;
+
+    @BeforeEach
+    void setUp() {
+        question = new Question(1L, UserTestFixture.손상훈, "title", "contents");
+    }
+
     @Test
     @DisplayName("주어진 질문의 삭제 이력을 생성한다")
     void create_delete_history_with_question_test() {
-        Question question = QuestionTestFixture.createQuestionWithId(1L);
+        // when
         DeleteHistory deleteHistory = DeleteHistory.ofQuestion(question);
+        // then
         assertThat(deleteHistory).isNotNull();
     }
 
     @Test
     @DisplayName("주어진 답변의 삭제 이력을 생성한다")
     void create_delete_history_with_answer_test() {
-        Answer answer = AnswerTestFixture.createAnswerWithId(1L);
+        // given
+        Answer answer = new Answer(1L, UserTestFixture.손상훈, question, "contents");
+        // when
         DeleteHistory deleteHistory = DeleteHistory.ofAnswer(answer);
+        // then
         assertThat(deleteHistory).isNotNull();
     }
 }
