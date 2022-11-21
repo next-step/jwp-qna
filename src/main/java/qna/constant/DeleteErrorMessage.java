a3d12c90e1d81e7562b307a635659d82fe744d0c
mansik <90mansin@gmail.com>
2022-11-22 01:21:09 +0900

    refactor: 에러 메시지 클래스 구현

---
+++ src/main/java/qna/constant/DeleteErrorMessage.java
@@ -0,0 +1,14 @@
+package qna.constant;
+
+public class DeleteErrorMessage {
+
+    public static final String NOT_HAVE_PERMISSION_DELETE_QUESTION = "질문을 삭제할 권한이 없습니다.";
+
+    public static final String NOT_EMPTY_ANSWER_DELETE_QUESTION = "답변이 있어 삭제할 수 없습니다.";
+
+    public static final String NOT_HAVE_PERMISSION_DELETE_ANSWER = "다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.";
+
+
+
+
+}
