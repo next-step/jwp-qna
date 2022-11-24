d94aa10dd975689d1dd49a6f4126701dc85b45d7
tlaqk229 <tlaqk229@gmail.com>
2022-11-24 18:03:21 +0900

    test(Question): 질문 삭제 권한 확인 테스트 추가

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,24 @@
 package qna.domain;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
 public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+    @DisplayName("질문 삭제 권한 없는 경우 예외처리")
+    @Test
+    void 질문삭제_권한_예외처리() {
+        assertAll(
+                () -> Q1.checkQuestionDeleteAuth(UserTest.JAVAJIGI),
+                () -> assertThatThrownBy(() -> Q1.checkQuestionDeleteAuth(UserTest.SANJIGI))
+                        .isInstanceOf(CannotDeleteException.class)
+        );
+    }
+
 }
