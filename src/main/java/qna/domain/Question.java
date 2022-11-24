e8e58bcc8091216d30ba8c84c9ee01f6e9ea9c40
tlaqk229 <tlaqk229@gmail.com>
2022-11-24 18:23:23 +0900

    feat(Question): 질문에 대한 답변 삭제 기능 추가

--- src/main/java/qna/domain/Question.java
+++ src/main/java/qna/domain/Question.java
@@ -68,8 +68,30 @@ public class Question extends BaseEntity {
     }
 
     public List<DeleteHistory> deleteAnswersBeforeDeleteQuestion() throws CannotDeleteException {
-        //
-        return null;
+        List<DeleteHistory> deleteHistories = new ArrayList<>();
+        checkAnswersDeletable();
+        for (Answer answer : answers) {
+            deleteAnswerIfDeletedFalse(deleteHistories, answer);
+        }
+        return deleteHistories;
+    }
+
+    private void checkAnswersDeletable() throws CannotDeleteException {
+        for (Answer answer : answers) {
+            checkAnswerWriterMatch(answer);
+        }
+    }
+
+    private void checkAnswerWriterMatch(Answer answer) throws CannotDeleteException {
+        if (!answer.isOwner(writer)) {
+            throw new CannotDeleteException("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
+        }
+    }
+
+    private void deleteAnswerIfDeletedFalse(List<DeleteHistory> deleteHistories, Answer answer) {
+        if (!answer.isDeleted()) {
+            deleteHistories.add(answer.deleteAnswer());
+        }
     }
 
     public Long getId() {
