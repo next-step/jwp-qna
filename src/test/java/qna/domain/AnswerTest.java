9c2f1dc989ac2480fa5fc9dcb2b7a280b50f8af0
msares <msares123@gamil.com>
2022-11-28 21:23:58 +0900

    test(AnswerTest,QuestionTest): 삭제 테스트 케이스 추가

--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,6 +1,28 @@
 package qna.domain;
 
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class AnswerTest {
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @Test
+    void 다른유저_답변삭제시_에러() {
+        User anotherUser = UserTest.JAVAJIGI;
+        assertThatThrownBy(() -> A2.delete(anotherUser))
+                .isInstanceOf(CannotDeleteException.class)
+                .hasMessage("답변을 삭제할 권한이 없습니다.");
+    }
+
+    @Test
+    void 답변_삭제시_이력을_반환한다() throws CannotDeleteException {
+        User user = UserTest.JAVAJIGI;
+        DeleteHistory history = A1.delete(user);
+        assertThat(A1.isDeleted()).isTrue();
+        assertThat(history.getContentId()).isEqualTo(A1.getId());
+    }
 }
--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,6 +1,30 @@
 package qna.domain;
 
+import org.junit.jupiter.api.Test;
+import qna.CannotDeleteException;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class QuestionTest {
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
+
+    @Test
+    void 다른유저_질문_삭제시_에러() {
+        Question question = Q2.writeBy(UserTest.JAVAJIGI);
+        assertThatThrownBy(() -> question.deleteWithAnswers(UserTest.JAVAJIGI))
+                .isInstanceOf(CannotDeleteException.class);
+    }
+
+    @Test
+    void 질문_삭제시_이력을_반환한다() throws CannotDeleteException {
+        User user = UserTest.JAVAJIGI;
+        Question question = Q1.writeBy(user);
+        List<DeleteHistory> history = question.deleteWithAnswers(user);
+        assertThat(question.isDeleted()).isTrue();
+        assertThat(history.get(0).getId()).isEqualTo(question.getId());
+    }
 }
